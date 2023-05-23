05ee3b93f59a7134729fd166d4102995838e4a90
sosow0212 <sosow0212@naver.com>
2023-05-23 18:04:49 +0900

    feat: CORS 정책

---
+++ src/main/java/cart/config/WebConfig.java
@@ -0,0 +1,26 @@
+package cart.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("^https?:\\/\\/https://main--chimerical-malabi-ffde60.netlify.app/$"); // e.g. /**, http://domain1.com
+
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setAllowCredentials(true);
+        config.setMaxAge(3600L);
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
