7d830a0246841d9275facea3d0f1eb64e1b06b97
LJW25 <dbjwlee@gmail.com>
2023-05-23 17:15:11 +0900

    feat: 예외 핸들링 추가

--- src/main/java/cart/ui/ControllerExceptionHandler.java
+++ src/main/java/cart/ui/ControllerExceptionHandler.java
@@ -1,23 +1,62 @@
 package cart.ui;
 
+import cart.dto.ExceptionResponse;
 import cart.exception.AuthenticationException;
 import cart.exception.CartItemException;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.stream.Collectors;
 
 @ControllerAdvice
-public class ControllerExceptionHandler {
+public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(AuthenticationException.class)
-    public ResponseEntity<Void> handlerAuthenticationException(AuthenticationException e) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    public ResponseEntity<ExceptionResponse> handlerAuthenticationException(AuthenticationException ex) {
+        logger.info(ex);
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body(new ExceptionResponse("이메일이나 비밀번호를 확인해 주세요."));
     }
 
     @ExceptionHandler(CartItemException.IllegalMember.class)
-    public ResponseEntity<Void> handleException(CartItemException.IllegalMember e) {
-        return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+    public ResponseEntity<ExceptionResponse> handleException(CartItemException.IllegalMember ex) {
+        logger.info(ex);
+
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(new ExceptionResponse("잘못된 사용자입니다."));
     }
 
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatus status,
+                                                                  WebRequest request) {
+        logger.info(ex);
+
+        String fieldErrorMessage = ex.getBindingResult().getFieldErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.joining(System.lineSeparator()));
+
+        return ResponseEntity.badRequest().body(new ExceptionResponse(fieldErrorMessage));
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(
+            Exception ex,
+            Object body,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request) {
+        logger.warn(ex);
+
+        return super.handleExceptionInternal(ex, body, headers, status, request);
+    }
 }
