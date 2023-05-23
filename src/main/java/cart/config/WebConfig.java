ba6636095a92718004a3c919eb815ebec83b9106
java-saeng <gusdn85554@naver.com>
2023-05-23 13:52:26 +0900

    feat : CORS global 설정

---
+++ src/main/java/cart/config/WebConfig.java
@@ -0,0 +1,19 @@
+package cart.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+        .allowedOriginPatterns("*")
+        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+        .allowedHeaders("*")
+        .exposedHeaders("Location")
+        .allowCredentials(true);
+  }
+}
