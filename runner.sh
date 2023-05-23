884418e20e806f786d0a7f55110752633b06c986
0chil <0@chll.it>
2023-05-23 17:39:53 +0900

    chore: 배포 스크립트에 git pull 추가

--- runner.sh
+++ runner.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
 
+echo "\n🐣 Github에서 프로젝트를 Pull 합니다.\n"
+
+git pull
+
 PROCESS_ID=$(lsof -t -i:8080)
 
 if [ -n "$PROCESS_ID" ]; then
-        echo 8080 포트 끄기
+        echo "\n🐣 구동중인 애플리케이션을 종료했습니다. (pid : $PROCESS_ID)\n"
         kill  $PROCESS_ID
 fi
 
-echo `./gradlew clean bootJar`
-echo `nohup java -jar ./build/libs/jwp-shopping-order.jar > spring.log &`
+echo "\n🐣 SpringBoot 프로젝트 빌드를 시작합니다.\n"
+
+./gradlew clean bootJar
+
+echo "\n🐣 SpringBoot 애플리케이션을 실행합니다.\n"
+
+sudo nohup java -jar ./build/libs/jwp-shopping-order.jar > spring.log &
+
 exit
