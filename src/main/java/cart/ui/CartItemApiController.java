1e9659e7ad9e64f9c2665eee2f1d24bf7f5f3897
yoondgu <doyou2ing@naver.com>
2023-05-23 14:06:22 +0900

    feat: Rest API 모든 Origin 허용

--- src/main/java/cart/ui/CartItemApiController.java
+++ src/main/java/cart/ui/CartItemApiController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.net.URI;
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/cart-items")
 public class CartItemApiController {
