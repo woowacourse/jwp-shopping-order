1893a9dbf3c84319a73896f546edc7df2d299630
Woojin Lee <dhkep03@gmail.com>
2023-05-23 16:16:55 +0900

    chore: Production 환경에 다른 Profile 적용

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Step 1: Check if port 8080 is running and kill the process if it is
-if lsof -i :8080; then
+if lsof -i :80; then
     echo "Port 8080 is already in use. Killing the process..."
-    lsof -i :8080 | awk 'NR!=1 {print $2}' | xargs kill -9
+    lsof -i :80 | awk 'NR!=1 {print $2}' | xargs kill -9
 fi
 
 # Step 2: Check if jwp-shopping-order directory exists, and clone it if it doesn't
@@ -27,4 +27,4 @@ echo "Building the project..."
 
 # Step 6: Run the application in the background using 'java -jar' and 'nohup'
 echo "Starting the application..."
-nohup java -jar build/libs/jwp-shopping-order.jar &
+sudo nohup java -jar build/libs/jwp-shopping-order.jar --spring.profiles.active=prod &
