6277b6e7bc4270b58e121f06d7a37ebe36ec7243
nuyh <hjaehyun25@gmail.com>
2023-05-23 15:06:24 +0900

    build: github action 추가 및 요청 URL 로깅 추가

--- src/main/java/cart/WebMvcConfig.java
+++ src/main/java/cart/WebMvcConfig.java
@@ -1,9 +1,11 @@
 package cart;
 
 import cart.dao.MemberDao;
+import cart.ui.LoggingRequestUrlInterceptor;
 import cart.ui.MemberArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -11,9 +13,16 @@ import java.util.List;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
     private final MemberDao memberDao;
+    private final LoggingRequestUrlInterceptor loggingRequestUrlInterceptor;
 
-    public WebMvcConfig(MemberDao memberDao) {
+    public WebMvcConfig(MemberDao memberDao, final LoggingRequestUrlInterceptor loggingRequestUrlInterceptor) {
         this.memberDao = memberDao;
+        this.loggingRequestUrlInterceptor = loggingRequestUrlInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(final InterceptorRegistry registry) {
+        registry.addInterceptor(loggingRequestUrlInterceptor);
     }
 
     @Override
---
+++ src/main/java/cart/ui/LoggingRequestUrlInterceptor.java
@@ -0,0 +1,22 @@
+package cart.ui;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public final class LoggingRequestUrlInterceptor implements HandlerInterceptor {
+    private final Logger logger = LoggerFactory.getLogger(LoggingRequestUrlInterceptor.class);
+
+    @Override
+    public boolean preHandle(final HttpServletRequest request, final HttpServletResponse response, final Object handler) throws Exception {
+        final String url = request.getRequestURL().toString();
+        logger.info("Http Request: {}", url);
+
+        return true;
+    }
+}
