34f9543005e15b4998c4ca835d4d6964a16e116f
This2sho <zx00018@naver.com>
2023-05-23 16:45:53 +0900

    build: cors 설정 추가

--- src/main/java/cart/JwpCartApplication.java
+++ src/main/java/cart/JwpCartApplication.java
@@ -2,6 +2,9 @@ package cart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class JwpCartApplication {
@@ -10,4 +13,13 @@ public class JwpCartApplication {
         SpringApplication.run(JwpCartApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
 }
