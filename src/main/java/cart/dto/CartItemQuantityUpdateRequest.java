32d18bd8f304246e6fc0bc31009aa267b0f1c38f
LJW25 <dbjwlee@gmail.com>
2023-05-23 17:14:39 +0900

    feat: 요청 데이터 검증 추가

--- src/main/java/cart/dto/CartItemQuantityUpdateRequest.java
+++ src/main/java/cart/dto/CartItemQuantityUpdateRequest.java
@@ -1,9 +1,13 @@
 package cart.dto;
 
+import javax.validation.constraints.Positive;
+
 public class CartItemQuantityUpdateRequest {
+
+    @Positive(message = "수량은 양수여야 합니다.")
     private int quantity;
 
-    public CartItemQuantityUpdateRequest() {
+    private CartItemQuantityUpdateRequest() {
     }
 
     public CartItemQuantityUpdateRequest(int quantity) {
--- src/main/java/cart/dto/ProductRequest.java
+++ src/main/java/cart/dto/ProductRequest.java
@@ -1,11 +1,21 @@
 package cart.dto;
 
+import org.hibernate.validator.constraints.URL;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+
 public class ProductRequest {
+
+    @NotBlank(message = "이름은 공백일 수 없습니다.")
     private String name;
+    @Positive(message = "가격은 양수여야 합니다.")
     private int price;
+    @NotBlank(message = "이미지 주소는 공백일 수 없습니다.")
+    @URL(message = "이미지 주소는 URL 형식이어야 합니다.")
     private String imageUrl;
 
-    public ProductRequest() {
+    private ProductRequest() {
     }
 
     public ProductRequest(String name, int price, String imageUrl) {
