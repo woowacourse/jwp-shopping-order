691d8bd30890e0dc45b128b3b68a10459497eb81
Hyeonjae-K <kwonhyeonjae01@gmail.com>
2023-05-23 16:15:52 +0900

    feat: cors 등록

--- src/main/java/cart/WebMvcConfig.java
+++ src/main/java/cart/WebMvcConfig.java
@@ -3,7 +3,6 @@ package cart;
 import cart.dao.MemberDao;
 import cart.ui.MemberArgumentResolver;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -14,26 +13,18 @@ import java.util.List;
 public class WebMvcConfig implements WebMvcConfigurer {
     private final MemberDao memberDao;
 
-    public WebMvcConfig(final MemberDao memberDao) {
+    public WebMvcConfig(MemberDao memberDao) {
         this.memberDao = memberDao;
     }
 
     @Override
-    public void addArgumentResolvers(final List<HandlerMethodArgumentResolver> resolvers) {
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new MemberArgumentResolver(memberDao));
     }
 
     @Override
-    public void addCorsMappings(final CorsRegistry registry) {
+    public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://woowa.store")
-                .allowedMethods(
-                        HttpMethod.GET.name(),
-                        HttpMethod.HEAD.name(),
-                        HttpMethod.POST.name(),
-                        HttpMethod.PUT.name(),
-                        HttpMethod.DELETE.name(),
-                        HttpMethod.PATCH.name()
-                );
+                .allowedOrigins("http://3.37.130.147:8080");
     }
 }
