bce44c03547f91f6ed2b2f2d5a6d8a9ffd15d29a
junpakPark <junpak.park@gmail.com>
2023-05-23 13:47:09 +0900

    feat : CORS 글로벌 설정 완료

--- src/main/java/cart/ui/CartItemApiController.java
+++ src/main/java/cart/ui/CartItemApiController.java
@@ -1,50 +1,60 @@
 package cart.ui;
 
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import cart.application.CartItemService;
-import cart.domain.CartItem;
 import cart.domain.Member;
 import cart.dto.CartItemQuantityUpdateRequest;
 import cart.dto.CartItemRequest;
 import cart.dto.CartItemResponse;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
-import java.net.URI;
-import java.util.List;
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/cart-items")
 public class CartItemApiController {
 
-    private final CartItemService cartItemService;
+	private final CartItemService cartItemService;
 
-    public CartItemApiController(CartItemService cartItemService) {
-        this.cartItemService = cartItemService;
-    }
+	public CartItemApiController(CartItemService cartItemService) {
+		this.cartItemService = cartItemService;
+	}
 
-    @GetMapping
-    public ResponseEntity<List<CartItemResponse>> showCartItems(Member member) {
-        return ResponseEntity.ok(cartItemService.findByMember(member));
-    }
+	@GetMapping
+	public ResponseEntity<List<CartItemResponse>> showCartItems(Member member) {
+		return ResponseEntity.ok(cartItemService.findByMember(member));
+	}
 
-    @PostMapping
-    public ResponseEntity<Void> addCartItems(Member member, @RequestBody CartItemRequest cartItemRequest) {
-        Long cartItemId = cartItemService.add(member, cartItemRequest);
+	@PostMapping
+	public ResponseEntity<Void> addCartItems(Member member, @RequestBody CartItemRequest cartItemRequest) {
+		Long cartItemId = cartItemService.add(member, cartItemRequest);
 
-        return ResponseEntity.created(URI.create("/cart-items/" + cartItemId)).build();
-    }
+		return ResponseEntity.created(URI.create("/cart-items/" + cartItemId)).build();
+	}
 
-    @PatchMapping("/{id}")
-    public ResponseEntity<Void> updateCartItemQuantity(Member member, @PathVariable Long id, @RequestBody CartItemQuantityUpdateRequest request) {
-        cartItemService.updateQuantity(member, id, request);
+	@PatchMapping("/{id}")
+	public ResponseEntity<Void> updateCartItemQuantity(Member member, @PathVariable Long id,
+		@RequestBody CartItemQuantityUpdateRequest request) {
+		cartItemService.updateQuantity(member, id, request);
 
-        return ResponseEntity.ok().build();
-    }
+		return ResponseEntity.ok().build();
+	}
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> removeCartItems(Member member, @PathVariable Long id) {
-        cartItemService.remove(member, id);
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> removeCartItems(Member member, @PathVariable Long id) {
+		cartItemService.remove(member, id);
 
-        return ResponseEntity.noContent().build();
-    }
+		return ResponseEntity.noContent().build();
+	}
 }
--- src/main/java/cart/ui/ProductApiController.java
+++ src/main/java/cart/ui/ProductApiController.java
@@ -1,51 +1,61 @@
 package cart.ui;
 
-import cart.application.ProductService;
-import cart.dto.ProductRequest;
-import cart.dto.ProductResponse;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.net.URI;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import cart.application.ProductService;
+import cart.dto.ProductRequest;
+import cart.dto.ProductResponse;
+
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/products")
 public class ProductApiController {
 
-    private final ProductService productService;
+	private final ProductService productService;
 
-    public ProductApiController(ProductService productService) {
-        this.productService = productService;
-    }
+	public ProductApiController(ProductService productService) {
+		this.productService = productService;
+	}
 
-    @GetMapping
-    public ResponseEntity<List<ProductResponse>> getAllProducts() {
-        return ResponseEntity.ok(productService.getAllProducts());
-    }
+	@GetMapping
+	public ResponseEntity<List<ProductResponse>> getAllProducts() {
+		return ResponseEntity.ok(productService.getAllProducts());
+	}
 
-    @GetMapping("/{id}")
-    public ResponseEntity<ProductResponse> getProductById(@PathVariable Long id) {
-        return ResponseEntity.ok(productService.getProductById(id));
-    }
+	@GetMapping("/{id}")
+	public ResponseEntity<ProductResponse> getProductById(@PathVariable Long id) {
+		return ResponseEntity.ok(productService.getProductById(id));
+	}
 
-    @PostMapping
-    public ResponseEntity<Void> createProduct(@RequestBody ProductRequest productRequest) {
-        Long id = productService.createProduct(productRequest);
-        return ResponseEntity.created(URI.create("/products/" + id)).build();
-    }
+	@PostMapping
+	public ResponseEntity<Void> createProduct(@RequestBody ProductRequest productRequest) {
+		Long id = productService.createProduct(productRequest);
+		return ResponseEntity.created(URI.create("/products/" + id)).build();
+	}
 
-    @PutMapping("/{id}")
-    public ResponseEntity<Void> updateProduct(@PathVariable Long id, @RequestBody ProductRequest productRequest) {
-        productService.updateProduct(id, productRequest);
-        return ResponseEntity.status(HttpStatus.OK).build();
-    }
+	@PutMapping("/{id}")
+	public ResponseEntity<Void> updateProduct(@PathVariable Long id, @RequestBody ProductRequest productRequest) {
+		productService.updateProduct(id, productRequest);
+		return ResponseEntity.status(HttpStatus.OK).build();
+	}
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
-        productService.deleteProduct(id);
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-    }
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+		productService.deleteProduct(id);
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+	}
 
 }
