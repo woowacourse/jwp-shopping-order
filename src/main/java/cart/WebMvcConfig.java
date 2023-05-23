810241d07fef8f49a34d5dccfa0c948055e8171d
Rosie <dev.kayoung@gmail.com>
2023-05-23 18:21:58 +0900

    fix: 모든 메서드 허용

--- src/main/java/cart/WebMvcConfig.java
+++ src/main/java/cart/WebMvcConfig.java
@@ -24,6 +24,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedMethods("*");
     }
 }
