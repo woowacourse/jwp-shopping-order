13b69b14877ac433e9f10276472aa2238725b612
sosow0212 <sosow0212@naver.com>
2023-05-24 16:44:02 +0900

    fix: CORS 해결

--- src/main/java/cart/config/web/WebMvcConfig.java
+++ src/main/java/cart/config/web/WebMvcConfig.java
@@ -5,11 +5,13 @@ import cart.dao.MemberDao;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
+@EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
 
     private final MemberDao memberDao;
