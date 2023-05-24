df1ee3c0d29e1bc5e5a401491cf22f02d517c7e8
somsom13 <somin1113@naver.com>
2023-05-24 15:42:01 +0900

    fix: CORS 메서드 설정

--- src/main/java/cart/WebMvcConfig.java
+++ src/main/java/cart/WebMvcConfig.java
@@ -25,6 +25,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("*");
+            .allowedOrigins("*")
+            .allowedMethods("*");
     }
 }
