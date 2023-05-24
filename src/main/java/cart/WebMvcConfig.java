1890f41b1d1b8edc04331f4124c8e82f35754fa8
swonny <wonny921@gmail.com>
2023-05-24 16:22:09 +0900

    refactor: CORS 설정 변경

--- src/main/java/cart/WebMvcConfig.java
+++ src/main/java/cart/WebMvcConfig.java
@@ -26,6 +26,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE");
+                .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE")
+                .allowCredentials(true);
     }
 }
