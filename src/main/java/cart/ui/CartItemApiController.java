15eea8e6e5879de409ce7fe5f60dac8029afeced
yoondgu <doyou2ing@naver.com>
2023-05-24 16:26:48 +0900

    refactor: CORS 설정 Config 전역 설정으로 변경

--- src/main/java/cart/ui/CartItemApiController.java
+++ src/main/java/cart/ui/CartItemApiController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 import java.net.URI;
 import java.util.List;
 
-@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/cart-items")
 public class CartItemApiController {
---
+++ src/main/java/cart/ui/WebConfig.java
@@ -0,0 +1,20 @@
+package cart.ui;
+
+import java.util.List;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private static final List<String> MAPPING_URLS = List.of("/products/**", "/cart-items/**");
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        for (String mappingUrl : MAPPING_URLS) {
+            registry.addMapping(mappingUrl)
+                    .allowedOriginPatterns("*");
+        }
+    }
+}
