37a003864c21ad54eb9302a46d0ef94e7540be7f
sosow0212 <sosow0212@naver.com>
2023-05-24 20:15:53 +0900

    fix: CORS 해결

--- src/main/java/cart/config/web/WebMvcConfig.java
+++ src/main/java/cart/config/web/WebMvcConfig.java
@@ -27,7 +27,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOriginPatterns("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedMethods("GET", "POST", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .exposedHeaders("Location"); // 노출할 헤더 설정
