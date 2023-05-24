326d20aebb6d2dcc65359dd40d15f2f63f7fae2c
Woojin Lee <dhkep03@gmail.com>
2023-05-24 14:39:13 +0900

    fix: 모든 메서드에 대해 CORS 적용

--- src/main/java/cart/WebMvcConfig.java
+++ src/main/java/cart/WebMvcConfig.java
@@ -24,6 +24,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+                .allowedMethods("*")
                 .allowedOrigins("*");
     }
 }
