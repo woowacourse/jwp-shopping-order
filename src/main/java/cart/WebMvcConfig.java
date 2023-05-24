dea58a29b746258ffdc8f8960f19531760e3feac
Rosie <dev.kayoung@gmail.com>
2023-05-24 15:53:38 +0900

    feat: expoesed header 설정

--- src/main/java/cart/WebMvcConfig.java
+++ src/main/java/cart/WebMvcConfig.java
@@ -3,6 +3,7 @@ package cart;
 import cart.dao.MemberDao;
 import cart.ui.MemberArgumentResolver;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -26,6 +27,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
                 .allowedMethods("*")
+                .exposedHeaders(HttpHeaders.LOCATION)
                 .allowedHeaders("*");
     }
 }
