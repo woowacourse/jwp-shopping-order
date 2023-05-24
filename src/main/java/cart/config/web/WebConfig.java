54b3f926bd2aef208bd9203ebd3417ceff9ea0ce
sosow0212 <sosow0212@naver.com>
2023-05-24 14:23:22 +0900

    refactor: CORS 정책 수정

---
+++ src/main/java/cart/config/web/WebConfig.java
@@ -0,0 +1,17 @@
+package cart.config.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+                .maxAge(3000);
+    }
+}
