8ccc6955fdded2eb612295edf3971a58887452fb
junpakPark <junpak.park@gmail.com>
2023-05-24 21:44:24 +0900

    refactor : allowedOriginPatterns 수정

--- src/main/java/cart/WebMvcConfig.java
+++ src/main/java/cart/WebMvcConfig.java
@@ -26,7 +26,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("*")
+			.allowedOriginPatterns("*")
 			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTION")
 			.allowedHeaders("*")
 			.exposedHeaders("Location")
