2ee385ac4d143dd9f9f3e788765c0cbbe6bab188
woosung1223 <dntjd991223@naver.com>
2023-05-24 16:09:17 +0900

    feat: CORS 환경 설정

--- src/main/java/cart/WebMvcConfig.java
+++ src/main/java/cart/WebMvcConfig.java
@@ -4,6 +4,7 @@ import cart.dao.MemberDao;
 import cart.ui.MemberArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -14,6 +15,14 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     public WebMvcConfig(MemberDao memberDao) {
         this.memberDao = memberDao;
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOriginPatterns("http://192.168.*.*:3000")
+                .allowCredentials(true);
     }
 
     @Override
