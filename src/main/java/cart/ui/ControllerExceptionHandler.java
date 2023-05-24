bd0b2e846db7d2cea6be4e4da9f1cfa1389a32e9
kdkdhoho <hkim4410@naver.com>
2023-05-24 15:02:10 +0900

    feat(ControllerExceptionHandler): Exception 에러 처리 기능 구현

--- src/main/java/cart/ui/ControllerExceptionHandler.java
+++ src/main/java/cart/ui/ControllerExceptionHandler.java
@@ -2,7 +2,6 @@ package cart.ui;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,10 +12,11 @@ public class ControllerExceptionHandler {
     private final Logger log = LoggerFactory.getLogger(getClass());
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleException(final Exception e) {
+    public ResponseEntity<Void> handleException(final Exception e) {
+        log.error("------------------------This is Error------------------");
         log.error(e.getMessage(), e);
         log.error("------------------------This is Error------------------");
 
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        return ResponseEntity.badRequest().build();
     }
 }
