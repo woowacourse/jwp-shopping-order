91d3d23403ebcc4aa46c8aa91b3ecf814619ab02
jaeyeon kim <jakind@naver.com>
2023-05-24 17:49:06 +0900

    feat: cross origin 적용

--- src/main/java/cart/ui/CartItemApiController.java
+++ src/main/java/cart/ui/CartItemApiController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/cart-items")
+@CrossOrigin("*")
 public class CartItemApiController {
 
     private final CartItemService cartItemService;
--- src/main/java/cart/ui/UserController.java
+++ src/main/java/cart/ui/UserController.java
@@ -14,4 +14,5 @@ public class UserController {
         return ResponseEntity.ok()
                 .build();
     }
+
 }
