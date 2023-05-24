586bb8b0a2c4406f98779f880ffda538571dba46
jjongwa <troas96@naver.com>
2023-05-24 14:45:45 +0900

    feat: 와일드카드로 CORS 우선 설정

--- src/main/java/cart/ui/CartItemApiController.java
+++ src/main/java/cart/ui/CartItemApiController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/cart-items")
+@CrossOrigin(origins = "*")
 public class CartItemApiController {
 
     private final CartItemService cartItemService;
