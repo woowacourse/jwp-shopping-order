f147a1a2ffa8fd857a751d538e3add4c573258a3
xxeol2 <jhj01177@naver.com>
2023-05-24 15:24:24 +0900

    feat: cors 와일드카드 적용

--- src/main/java/cart/configuration/CorsConfiguration.java
+++ src/main/java/cart/configuration/CorsConfiguration.java
@@ -8,15 +8,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
 
-    @Value("${ip.address}")
-    private String ipAddress;
-
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080")
-                .allowedOrigins("http://127.0.0.1:8080")
-                .allowedOrigins(ipAddress)
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("*")
                 .maxAge(3000);
     }
 }
