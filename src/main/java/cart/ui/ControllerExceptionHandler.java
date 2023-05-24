b8ac67c034e04a8dfe9b6e9b08b768d95fa7656d
Choi-JJunho <junho5336@gmail.com>
2023-05-24 21:26:21 +0900

    feat: 예외 로깅 추가

--- src/main/java/cart/ui/ControllerExceptionHandler.java
+++ src/main/java/cart/ui/ControllerExceptionHandler.java
@@ -2,6 +2,8 @@ package cart.ui;
 
 import cart.exception.AuthenticationException;
 import cart.exception.CartItemException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,14 +12,23 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
-    @ExceptionHandler(AuthenticationException.class)
-    public ResponseEntity<Void> handlerAuthenticationException(AuthenticationException e) {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass().getSimpleName());
+
+    @ExceptionHandler
+    public ResponseEntity<Void> handleException(Exception e) {
+        logger.error(e.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<Void> handleException(AuthenticationException e) {
+        logger.warn(e.getMessage());
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 
-    @ExceptionHandler(CartItemException.IllegalMember.class)
+    @ExceptionHandler
     public ResponseEntity<Void> handleException(CartItemException.IllegalMember e) {
+        logger.warn(e.getMessage());
         return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
     }
-
 }
