c87023c4e9bbf9355906a854dae4f0f4f3dfb88a
yoondgu <doyou2ing@naver.com>
2023-05-25 11:02:46 +0900

    fix: CORS 설정 OPTIONS 메서드명 수정

--- src/main/java/cart/ui/WebConfig.java
+++ src/main/java/cart/ui/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
         for (String mappingUrl : MAPPING_URLS) {
             registry.addMapping(mappingUrl)
                     .allowedOriginPatterns("*")
-                    .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTION")
+                    .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                     .allowedHeaders("*")
                     .exposedHeaders("Location")
                     .allowCredentials(true);
