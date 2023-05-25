a6512703b7a0fdae4f3d8e05c4d20c9bd5b34cd7
jaeyeon kim <jakind@naver.com>
2023-05-25 10:39:44 +0900

    refactor : import 수정

--- src/main/java/cart/ui/CartItemApiController.java
+++ src/main/java/cart/ui/CartItemApiController.java
@@ -1,20 +1,23 @@
 package cart.ui;
 
 import cart.application.CartItemService;
-import cart.domain.CartItem;
 import cart.domain.Member;
 import cart.dto.CartItemQuantityUpdateRequest;
 import cart.dto.CartItemRequest;
 import cart.dto.CartItemResponse;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("/cart-items")
-@CrossOrigin(origins = "http://localhost:8080")
 public class CartItemApiController {
 
     private final CartItemService cartItemService;
