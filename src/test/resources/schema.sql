7c9963bfb3b55953c9e6031fe3bc1d15eb309ad7
Rosie <dev.kayoung@gmail.com>
2023-05-25 17:05:30 +0900

    feat: 테스트 데이터베이스 분리

---
+++ src/test/resources/schema.sql
@@ -0,0 +1,25 @@
+drop table cart_item;
+drop table member;
+drop table product;
+
+CREATE TABLE IF NOT EXISTS product (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    price INT NOT NULL,
+    image_url VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS member (
+     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+     email VARCHAR(255) NOT NULL UNIQUE,
+     password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS cart_item (
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    member_id BIGINT NOT NULL,
+    product_id BIGINT NOT NULL,
+    quantity INT NOT NULL,
+    FOREIGN KEY (member_id) REFERENCES member(id),
+    FOREIGN KEY (product_id) REFERENCES product(id)
+);
