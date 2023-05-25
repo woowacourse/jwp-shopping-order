93706cf3ab71e3df4d1b17bfc73a6d2c10cfc01b
HubCreator <chocomilk8604@gmail.com>
2023-05-26 08:28:46 +0900

    feat: 시간대 설정

--- src/main/java/cart/JwpCartApplication.java
+++ src/main/java/cart/JwpCartApplication.java
@@ -3,11 +3,18 @@ package cart;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class JwpCartApplication {
+
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(JwpCartApplication.class, args);
     }
-
 }
