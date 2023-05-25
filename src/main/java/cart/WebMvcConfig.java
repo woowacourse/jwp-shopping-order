9de755faae2357ca61884e130ed16bb62728add9
jiwon <ljwon77@daum.net>
2023-05-25 17:31:35 +0900

    feat: CORS 설정 추가

--- src/main/java/cart/WebMvcConfig.java
+++ src/main/java/cart/WebMvcConfig.java
@@ -4,12 +4,14 @@ import cart.dao.MemberDao;
 import cart.ui.MemberArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
     private final MemberDao memberDao;
 
     public WebMvcConfig(MemberDao memberDao) {
@@ -19,5 +21,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new MemberArgumentResolver(memberDao));
+    }
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("https://monumental-kleicha-ad648a.netlify.app/", "http://localhost:9000",
+                "http://localhost:3000", "http://localhost:8080")
+            .allowedMethods("*");
     }
 }
