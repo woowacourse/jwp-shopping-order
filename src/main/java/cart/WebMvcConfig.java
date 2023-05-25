fdef6c357cf12e9288483fc1e6a44056e32a7b21
kwonyj1022 <kwonyj1022@naver.com>
2023-05-25 11:08:26 +0900

    chore: CORS 설정

--- src/main/java/cart/WebMvcConfig.java
+++ src/main/java/cart/WebMvcConfig.java
@@ -4,6 +4,7 @@ import cart.dao.MemberDao;
 import cart.ui.MemberArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -19,5 +20,16 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new MemberArgumentResolver(memberDao));
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .exposedHeaders("Location")
+                .allowedHeaders("*")
+                .allowedOriginPatterns("*")
+                .allowedOrigins("*")
+                .allowCredentials(false)
+                .allowedMethods("GET","POST","PUT","DELETE","PATCH","OPTIONS");
     }
 }
