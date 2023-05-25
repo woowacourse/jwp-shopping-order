19d129bd9d91ddb68592bed9998eca90cdb024f5
junpakPark <junpak.park@gmail.com>
2023-05-25 11:52:03 +0900

    refactor : 목데이터 치킨 샐러드 피자 제거

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,9 +10,6 @@ INSERT INTO product (name, price, image_url) VALUES ('PET보틀-스몰(500ml)', 
 INSERT INTO product (name, price, image_url) VALUES ('PET보틀-동글(400ml)', 44500, 'https://cdn-mart.baemin.com/sellergoods/main/6e1e0dc3-4a10-4729-910a-ff3c837836fe.jpg?h=300&w=300');
 INSERT INTO product (name, price, image_url) VALUES ('PET보틀-딸기라떼(500ml)', 18000, 'https://cdn-mart.baemin.com/sellergoods/main/03e63566-5d56-4dc0-9357-2caaeaeebf7e.jpg?h=300&w=300');
 INSERT INTO product (name, price, image_url) VALUES ('PET보틀-힙 플라스크(500ml)', 21500, 'https://cdn-mart.baemin.com/sellergoods/main/d07bec18-ce84-41c2-8903-61cbd10712b6.jpg?h=300&w=300');
-INSERT INTO product (name, price, image_url) VALUES ('치킨', 10000, 'https://images.unsplash.com/photo-1626082927389-6cd097cdc6ec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80');
-INSERT INTO product (name, price, image_url) VALUES ('샐러드', 20000, 'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80');
-INSERT INTO product (name, price, image_url) VALUES ('피자', 13000, 'https://images.unsplash.com/photo-1595854341625-f33ee10dbf94?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80');
 
 INSERT INTO member (email, password) VALUES ('a@a.com', '1234');
 INSERT INTO member (email, password) VALUES ('b@b.com', '1234');
