0a2c0b67eec5c4afbce1179c8fb59963482d9de3
dooboocookie <bkh3030@gmail.com>
2023-05-25 20:14:56 +0900

    chore: 테스트

--- src/main/java/cart/configuration/CorsConfiguration.java
+++ src/main/java/cart/configuration/CorsConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
+
     @Value("${test}")
     private String test;
 
