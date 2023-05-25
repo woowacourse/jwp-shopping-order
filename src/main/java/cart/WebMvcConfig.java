bb15f4a326d9441a7ae30a18b2b379c6fd551fad
junpakPark <junpak.park@gmail.com>
2023-05-25 10:47:34 +0900

    refactor : allowedMethods 수정

--- src/main/java/cart/WebMvcConfig.java
+++ src/main/java/cart/WebMvcConfig.java
@@ -27,7 +27,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
 			.allowedOriginPatterns("*")
-			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTION")
+			.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTION")
 			.allowedHeaders("*")
 			.exposedHeaders("Location")
 			.allowCredentials(true);
