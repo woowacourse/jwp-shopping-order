02456a09a9036feebf8a3829719b923b7a2e08f9
HubCreator <chocomilk8604@gmail.com>
2023-05-26 08:41:29 +0900

    refactor: 시간대 설정 롤백

--- src/main/java/cart/JwpCartApplication.java
+++ src/main/java/cart/JwpCartApplication.java
@@ -9,11 +9,6 @@ import java.util.TimeZone;
 @SpringBootApplication
 public class JwpCartApplication {
 
-    @PostConstruct
-    void started() {
-        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(JwpCartApplication.class, args);
     }
