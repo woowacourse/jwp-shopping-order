e7eed74b8aafe6abf7d95a0a5bc2c78fb09cbec8
gitchan <eunkeeeee@gmail.com>
2023-05-25 16:18:51 +0900

    feat(application.properties): 제리네 데이터베이스 연동

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,16 +6,16 @@ CREATE TABLE product (
 );
 
 CREATE TABLE member (
-     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-     email VARCHAR(255) NOT NULL UNIQUE,
-     password VARCHAR(255) NOT NULL
+                        id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                        email VARCHAR(255) NOT NULL UNIQUE,
+                        password VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE cart_item (
-    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    member_id BIGINT NOT NULL,
-    product_id BIGINT NOT NULL,
-    quantity INT NOT NULL,
-    FOREIGN KEY (member_id) REFERENCES member(id),
-    FOREIGN KEY (product_id) REFERENCES product(id)
+                           id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                           member_id BIGINT NOT NULL,
+                           product_id BIGINT NOT NULL,
+                           quantity INT NOT NULL,
+                           FOREIGN KEY (member_id) REFERENCES member(id),
+                           FOREIGN KEY (product_id) REFERENCES product(id)
 );
