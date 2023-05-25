ab13b8d7eb196fa3727e7b4def69bf501efcec7a
0chil <0@chll.it>
2023-05-25 12:11:04 +0900

    refactor: DirtiesContext 제거
    - truncate.sql로 대체

--- src/test/java/cart/integration/IntegrationTest.java
+++ src/test/java/cart/integration/IntegrationTest.java
@@ -1,12 +1,13 @@
 package cart.integration;
 
-import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.jdbc.Sql;
 
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+import io.restassured.RestAssured;
+
+@Sql({"classpath:truncate.sql", "classpath:data.sql"})
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class IntegrationTest {
     @LocalServerPort
