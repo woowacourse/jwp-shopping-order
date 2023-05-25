7c2ea5c36409452af9f399eac23bb6e2284ebb9f
somsom13 <somin1113@naver.com>
2023-05-25 11:37:28 +0900

    chore: 배포 스크립트 추가

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+BRANCH_NAME=${1}
+
+# git pull -> 특정 브랜치에서 가져오기
+git pull origin ${BRANCH_NAME}
+
+# 실행시킬 코드가 있는 브랜치로 체크아웃
+git checkout ${BRANCH_NAME}
+
+# 빌드
+./gradlew bootJar
+
+# 이동
+cd ./build/libs
+
+# 실행 중인 Spring Boot 애플리케이션 확인
+PID=$(pgrep -f 'java -jar')
+
+# -z: 문자열이 비여 있지 않은 경우에 참을 반환
+if [ -z "$PID" ]
+then
+  # 애플리케이션 시작 명령어
+  nohup java -jar jwp-shopping-order.jar
+else
+  kill $PID
+  # 애플리케이션 시작 명령어
+  nohup java -jar jwp-shopping-order.jar
+fi
