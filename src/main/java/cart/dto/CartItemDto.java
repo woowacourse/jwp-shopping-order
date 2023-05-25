f8a2ce4cf483cff140819ef1ff04e08413bd1885
greeng00se <thegreengoose7@gmail.com>
2023-05-26 08:54:07 +0900

    refactor: API 명세와 맞지 않았던 부분 수정
    Co-authored-by: aak2075 <lks971003@gmail.com>
    Co-authored-by: wugawuga <dnr3655@naver.com>

--- src/main/java/cart/dto/CartItemDto.java
+++ src/main/java/cart/dto/CartItemDto.java
@@ -11,12 +11,12 @@ public class CartItemDto {
 
     @Schema(description = "장바구니 상품 수량", example = "1")
     private Integer quantity;
-    private ProductDto productDto;
+    private ProductDto product;
 
-    public CartItemDto(final Long id, final Integer quantity, final ProductDto productDto) {
+    public CartItemDto(final Long id, final Integer quantity, final ProductDto product) {
         this.id = id;
         this.quantity = quantity;
-        this.productDto = productDto;
+        this.product = product;
     }
 
     public static CartItemDto from(final CartItem cartItem) {
@@ -32,7 +32,7 @@ public class CartItemDto {
         return quantity;
     }
 
-    public ProductDto getProductDto() {
-        return productDto;
+    public ProductDto getProduct() {
+        return product;
     }
 }
