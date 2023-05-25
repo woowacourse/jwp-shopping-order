6d347b667823726d5159b4cc442c8eec0bde7978
kdkdhoho <hkim4410@naver.com>
2023-05-25 16:03:37 +0900

    feat(ControllerExceptionHandler): 에러 처리

--- src/main/java/cart/ui/ControllerExceptionHandler.java
+++ src/main/java/cart/ui/ControllerExceptionHandler.java
@@ -3,9 +3,15 @@ package cart.ui;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @ExceptionHandler
+    public void handle(final Exception e) {
+        log.error(e.getMessage());
+    }
 }
