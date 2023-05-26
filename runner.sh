9f6f94616298ccc062070b2458a0e97da1deed4e
0chil <0@chll.it>
2023-05-27 02:24:24 +0900

    chore: 배포 스크립트 수정

--- runner.sh
+++ runner.sh
@@ -1,22 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "\n🐣 Github에서 프로젝트를 Pull 합니다.\n"
 
 git pull
-PORT=443
-PROCESS_ID=$(sudo lsof -t -i:$PORT)
+
+JAR_NAME="jwp-shopping-order.jar"
+PROCESS_ID=$(pgrep -f "$JAR_NAME")
 
 if [ -n "$PROCESS_ID" ]; then
-        echo "\n🐣 구동중인 애플리케이션을 종료했습니다. (pid : $PROCESS_ID)\n"
-        sudo kill  $PROCESS_ID
+  sudo kill $PROCESS_ID
+  echo "\n🐣 구동중인 애플리케이션을 종료했습니다. (pid : $PROCESS_ID)\n"
 fi
 
 echo "\n🐣 SpringBoot 프로젝트 빌드를 시작합니다.\n"
 
-./gradlew bootJar -x test
+./gradlew bootJar
 
 echo "\n🐣 SpringBoot 애플리케이션을 실행합니다.\n"
 
-sudo nohup java -jar ./build/libs/jwp-shopping-order.jar > spring.log &
+nohup java -jar ./build/libs/$JAR_NAME --server.port=8086 >spring.log &
 
 exit
