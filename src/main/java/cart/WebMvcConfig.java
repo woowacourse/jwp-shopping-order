5f9dc81c8869c5baf825de02c2720bf638070f1b
iamjooon2 <iamjooon2@gmail.com>
2023-05-26 22:03:31 +0900

    build: 개행 추가

--- src/main/java/cart/WebMvcConfig.java
+++ src/main/java/cart/WebMvcConfig.java
@@ -2,15 +2,16 @@ package cart;
 
 import cart.dao.MemberDao;
 import cart.ui.MemberArgumentResolver;
+import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.List;
-
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
     private final MemberDao memberDao;
+
 
     public WebMvcConfig(MemberDao memberDao) {
         this.memberDao = memberDao;
