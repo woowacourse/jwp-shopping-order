9f5998b69812cdcc47d7fd41a81c3d85c3ae02ba
iamjooon2 <iamjooon2@gmail.com>
2023-05-26 15:50:57 +0900

    build : schema 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,8 +2,9 @@ INSERT INTO product (name, price, image_url) VALUES ('치킨', 10000, 'https://i
 INSERT INTO product (name, price, image_url) VALUES ('샐러드', 20000, 'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80');
 INSERT INTO product (name, price, image_url) VALUES ('피자', 13000, 'https://images.unsplash.com/photo-1595854341625-f33ee10dbf94?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80');
 
-INSERT INTO member (email, password) VALUES ('a@a.com', '1234');
-INSERT INTO member (email, password) VALUES ('b@b.com', '1234');
+INSERT INTO member (email, password, grade) VALUES ('a@a.com', '1234', 'bronze');
+INSERT INTO member (email, password, grade) VALUES ('b@b.com', '1234', 'silver');
+INSERT INTO member (email, password, grade) VALUES ('c@c.com', '1234', 'gold');
 
 INSERT INTO cart_item (member_id, product_id, quantity) VALUES (1, 1, 2);
 INSERT INTO cart_item (member_id, product_id, quantity) VALUES (1, 2, 4);
