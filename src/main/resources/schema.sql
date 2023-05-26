f53eb2d7842da622f6b725a3fda1ef68a0f8cdd5
greeng00se <thegreengoose7@gmail.com>
2023-05-27 02:17:07 +0900

    chore: db schema 내용 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,31 +1,92 @@
-CREATE TABLE IF NOT EXISTS `PRODUCT`
+CREATE TABLE `PRODUCT`
 (
     `id`         long PRIMARY KEY NOT NULL AUTO_INCREMENT,
     `name`       varchar(255)     NOT NULL,
     `image_url`  varchar(1024)    NOT NULL,
     `price`      long             NOT NULL,
-    `created_at` timestamp        NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `updated_at` timestamp        NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    `created_at` timestamp        NOT NULL DEFAULT (CURRENT_TIMESTAMP),
+    `updated_at` timestamp        NOT NULL DEFAULT (CURRENT_TIMESTAMP) ON UPDATE (CURRENT_TIMESTAMP)
 );
 
-CREATE TABLE IF NOT EXISTS `MEMBER`
+CREATE TABLE `MEMBER`
 (
-    `id`         long PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    `email`      varchar(255)     NOT NULL UNIQUE,
-    `password`   varchar(255)     NOT NULL,
-    `created_at` timestamp        NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `updated_at` timestamp        NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    `id`         long PRIMARY KEY    NOT NULL AUTO_INCREMENT,
+    `email`      varchar(255) UNIQUE NOT NULL,
+    `password`   varchar(255)        NOT NULL,
+    `created_at` timestamp           NOT NULL DEFAULT (CURRENT_TIMESTAMP),
+    `updated_at` timestamp           NOT NULL DEFAULT (CURRENT_TIMESTAMP) ON UPDATE (CURRENT_TIMESTAMP)
 );
 
-CREATE TABLE IF NOT EXISTS `CART_ITEM`
+CREATE TABLE `CART_ITEM`
 (
     `id`         long PRIMARY KEY NOT NULL AUTO_INCREMENT,
     `member_id`  long             NOT NULL,
     `product_id` long             NOT NULL,
     `quantity`   long             NOT NULL,
-    `created_at` timestamp        NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `updated_at` timestamp        NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    CONSTRAINT CART_UNIQUE UNIQUE (`member_id`, `product_id`),
-    FOREIGN KEY (`member_id`) REFERENCES `MEMBER` (`id`),
-    FOREIGN KEY (`product_id`) REFERENCES `PRODUCT` (`id`)
+    `created_at` timestamp        NOT NULL DEFAULT (CURRENT_TIMESTAMP),
+    `updated_at` timestamp        NOT NULL DEFAULT (CURRENT_TIMESTAMP) ON UPDATE (CURRENT_TIMESTAMP)
 );
+
+CREATE TABLE `ORDER`
+(
+    `id`           long PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `delivery_fee` long             NOT NULL,
+    `coupon_id`    long,
+    `member_id`    long             NOT NULL,
+    `created_at`   timestamp        NOT NULL DEFAULT (CURRENT_TIMESTAMP),
+    `updated_at`   timestamp        NOT NULL DEFAULT (CURRENT_TIMESTAMP) ON UPDATE (CURRENT_TIMESTAMP)
+);
+
+CREATE TABLE `ORDER_ITEM`
+(
+    `id`         long PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `name`       varchar(255)     NOT NULL,
+    `image_url`  varchar(1024)    NOT NULL,
+    `price`      long             NOT NULL,
+    `quantity`   long             NOT NULL,
+    `order_id`   long             NOT NULL,
+    `created_at` timestamp        NOT NULL DEFAULT (CURRENT_TIMESTAMP),
+    `updated_at` timestamp        NOT NULL DEFAULT (CURRENT_TIMESTAMP) ON UPDATE (CURRENT_TIMESTAMP)
+);
+
+CREATE TABLE `COUPON`
+(
+    `id`                  long PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `name`                varchar(255)     NOT NULL,
+    `policyType`          varchar(255)     NOT NULL,
+    `discountPrice`       long             NOT NULL,
+    `discountPercent`     long             NOT NULL,
+    `discountDeliveryFee` boolean          NOT NULL,
+    `conditionType`       varchar(255)     NOT NULL,
+    `minimumPrice`        long             NOT NULL,
+    `created_at`          timestamp        NOT NULL DEFAULT (CURRENT_TIMESTAMP),
+    `updated_at`          timestamp        NOT NULL DEFAULT (CURRENT_TIMESTAMP) ON UPDATE (CURRENT_TIMESTAMP)
+);
+
+CREATE TABLE `MEMBER_COUPON`
+(
+    `id`         long PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `coupon_id`  long             NOT NULL,
+    `member_id`  long             NOT NULL,
+    `used`       boolean          NOT NULL,
+    `created_at` timestamp        NOT NULL DEFAULT (CURRENT_TIMESTAMP),
+    `updated_at` timestamp        NOT NULL DEFAULT (CURRENT_TIMESTAMP) ON UPDATE (CURRENT_TIMESTAMP)
+);
+
+ALTER TABLE `CART_ITEM`
+    ADD FOREIGN KEY (`member_id`) REFERENCES `MEMBER` (`id`);
+
+ALTER TABLE `CART_ITEM`
+    ADD FOREIGN KEY (`product_id`) REFERENCES `PRODUCT` (`id`);
+
+ALTER TABLE `ORDER`
+    ADD FOREIGN KEY (`member_id`) REFERENCES `MEMBER` (`id`);
+
+ALTER TABLE `ORDER_ITEM`
+    ADD FOREIGN KEY (`order_id`) REFERENCES `ORDER` (`id`);
+
+ALTER TABLE `MEMBER_COUPON`
+    ADD FOREIGN KEY (`coupon_id`) REFERENCES `MEMBER` (`id`);
+
+ALTER TABLE `MEMBER_COUPON`
+    ADD FOREIGN KEY (`member_id`) REFERENCES `MEMBER` (`id`);
