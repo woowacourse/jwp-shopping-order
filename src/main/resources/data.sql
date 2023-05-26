6913f509ab099648044c9e5ac5861cc06a90901e
70825 <hello70825@gmail.com>
2023-05-26 18:59:29 +0900

    feat: 2단계 데이터베이스 스키마 변경
    Co-authored-by: sh111-coder <ohk9134@naver.com>

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,28 @@
-INSERT INTO product (name, price, image_url) VALUES ('치킨', 10000, 'https://images.unsplash.com/photo-1626082927389-6cd097cdc6ec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80');
-INSERT INTO product (name, price, image_url) VALUES ('샐러드', 20000, 'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80');
-INSERT INTO product (name, price, image_url) VALUES ('피자', 13000, 'https://images.unsplash.com/photo-1595854341625-f33ee10dbf94?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80');
+INSERT INTO product (name, price, image_url)
+VALUES ('치킨', 10000, 'https://images.unsplash.com/photo-1626082927389-6cd097cdc6ec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80'),
+       ('샐러드', 20000, 'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80'),
+       ('피자', 13000, 'https://images.unsplash.com/photo-1595854341625-f33ee10dbf94?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80'),
+       ('팬케이크', 8000, 'https://images.unsplash.com/photo-1544726982-b414d58fabaa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8JUVEJThDJUFDJUVDJUJDJTgwJUVDJTlEJUI0JUVEJTgxJUFDfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60');
 
-INSERT INTO member (email, password) VALUES ('a@a.com', '1234');
-INSERT INTO member (email, password) VALUES ('b@b.com', '1234');
+INSERT INTO member (email, password, cash)
+VALUES ('dooly@dooly.com', '1234', 1200000),
+       ('ber@ber.com', '1234', 850000),
+       ('bixx@bixx.com', '1234', 1500000);
 
-INSERT INTO cart_item (member_id, product_id, quantity) VALUES (1, 1, 2);
-INSERT INTO cart_item (member_id, product_id, quantity) VALUES (1, 2, 4);
+INSERT INTO cart_item (member_id, product_id, quantity)
+VALUES (1, 1, 2),
+       (1, 2, 4),
+       (2, 1, 5),
+       (2, 3, 5),
+       (3, 2, 5),
+       (3, 3, 10);
 
-INSERT INTO cart_item (member_id, product_id, quantity) VALUES (2, 3, 5);
+INSERT INTO cart_order (member_id, total_price)
+VALUES (1, 20000),
+       (2, 50000),
+       (3, 100000);
+
+INSERT INTO order_item (cart_order_id, name, price, image_url, quantity)
+VALUES (1, '치킨', 10000, 'https://images.unsplash.com/photo-1626082927389-6cd097cdc6ec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80', 2),
+       (2, '치킨', 10000, 'https://images.unsplash.com/photo-1626082927389-6cd097cdc6ec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80', 5),
+       (3, '샐러드', 20000, 'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80', 5);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,21 +1,45 @@
-CREATE TABLE product (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    price INT NOT NULL,
+CREATE TABLE IF NOT EXISTS member
+(
+    id       BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email    VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    cash     BIGINT       NOT NULL DEFAULT 5000
+    );
+
+CREATE TABLE IF NOT EXISTS product
+(
+    id        BIGINT PRIMARY KEY AUTO_INCREMENT,
+    name      VARCHAR(255) NOT NULL,
+    price     INT          NOT NULL,
     image_url VARCHAR(255) NOT NULL
-);
+    );
 
-CREATE TABLE member (
-     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-     email VARCHAR(255) NOT NULL UNIQUE,
-     password VARCHAR(255) NOT NULL
-);
+CREATE TABLE IF NOT EXISTS cart_item
+(
+    id         BIGINT  NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    member_id  BIGINT  NOT NULL,
+    product_id BIGINT  NOT NULL,
+    quantity   INT     NOT NULL,
+    FOREIGN KEY (member_id) REFERENCES member (id) ON DELETE CASCADE,
+    FOREIGN KEY (product_id) REFERENCES product (id) ON DELETE CASCADE
+    );
 
-CREATE TABLE cart_item (
-    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    member_id BIGINT NOT NULL,
-    product_id BIGINT NOT NULL,
-    quantity INT NOT NULL,
-    FOREIGN KEY (member_id) REFERENCES member(id),
-    FOREIGN KEY (product_id) REFERENCES product(id)
-);
+CREATE TABLE IF NOT EXISTS cart_order
+(
+    id              BIGINT    NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    member_id       BIGINT    NOT NULL,
+    total_price     BIGINT    NOT NULL,
+    created_at      TIMESTAMP NOT NULL DEFAULT now(),
+    FOREIGN KEY (member_id) REFERENCES member (id) ON DELETE CASCADE
+    );
+
+CREATE TABLE IF NOT EXISTS order_item
+(
+    id              BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    cart_order_id   BIGINT       NOT NULL,
+    name            VARCHAR(255) NOT NULL,
+    price           INT          NOT NULL,
+    image_url       VARCHAR(255) NOT NULL,
+    quantity        INT          NOT NULL,
+    FOREIGN KEY (cart_order_id) REFERENCES cart_order (id) ON DELETE CASCADE
+    );
