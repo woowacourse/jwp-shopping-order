9d2addbed74d7efef3d64fd828ba19022b011cf5
greeng00se <thegreengoose7@gmail.com>
2023-05-26 09:33:05 +0900

    feat: Swagger 서버 설정 추가
    Co-authored-by: aak2075 <lks971003@gmail.com>
    Co-authored-by: wugawuga <dnr3655@naver.com>

--- src/main/java/cart/config/OpenApiConfiguration.java
+++ src/main/java/cart/config/OpenApiConfiguration.java
@@ -1,5 +1,7 @@
 package cart.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -8,6 +10,11 @@ import io.swagger.v3.oas.models.security.SecurityScheme.Type;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@OpenAPIDefinition(servers = {
+        @Server(url = "https://h3rb.shop", description = "허브"),
+        @Server(url = "https://m4co.shop", description = "마코"),
+        @Server(url = "https://wuga.shop", description = "우가")
+})
 @Configuration
 public class OpenApiConfiguration {
 
