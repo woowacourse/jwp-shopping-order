c6d60e124c5af4e6f657da82a22b82d15262ddf9
woo-chang <clllickme@naver.com>
2023-05-28 00:24:59 +0900

    refactor: 인터셉터 경로 추가

--- src/main/java/cart/config/WebMvcConfig.java
+++ src/main/java/cart/config/WebMvcConfig.java
@@ -23,7 +23,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new BasicAuthInterceptor(new BasicAuthenticationExtractor(), memberService));
+        registry.addInterceptor(new BasicAuthInterceptor(new BasicAuthenticationExtractor(), memberService))
+                .addPathPatterns("/cart-items/**");
     }
 
     @Override
