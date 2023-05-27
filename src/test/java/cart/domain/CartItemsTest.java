f2addcd000b2e9865ee6822d7fbeb83015f2e929
yujamint <woawoa0513@gmail.com>
2023-05-28 01:50:03 +0900

    test: CartItem 일급 컬렉션 기능 테스트 작성

---
+++ src/test/java/cart/domain/CartItemsTest.java
@@ -0,0 +1,30 @@
+package cart.domain;
+
+import static cart.fixtures.CartItemFixtures.바닐라_크림_콜드브루_ID_4_3개_17400원;
+import static cart.fixtures.CartItemFixtures.유자_민트_티_ID_1_5개_29500원;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import cart.domain.order.Price;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@SuppressWarnings("NonAsciiCharacters")
+class CartItemsTest {
+
+    @DisplayName("장바구니 상품들의 합산 금액을 계산한다")
+    @Test
+    void sumOfPrice() {
+        // given
+        final Member member = new Member(1L, "test@email.com", "password");
+        final List<CartItem> items = List.of(유자_민트_티_ID_1_5개_29500원(member), 바닐라_크림_콜드브루_ID_4_3개_17400원(member));
+        final CartItems cartItems = new CartItems(items);
+
+        // when
+        Price sum = cartItems.sumOfPrice();
+
+        // then
+        assertThat(sum.getValue()).isEqualTo(46_900);
+    }
+
+}
