81b9c5b7413c9ddd9b9ff1aad405bf4d1c71dea7
woo-chang <clllickme@naver.com>
2023-05-27 14:38:02 +0900

    feat: 예외 형식 통일

--- src/main/java/cart/ui/ControllerExceptionHandler.java
+++ src/main/java/cart/ui/ControllerExceptionHandler.java
@@ -6,27 +6,35 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@ControllerAdvice
-public class ControllerExceptionHandler {
+@RestControllerAdvice
+public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
+
+    private static final String ERROR_LOG_FORMAT = "<Exception Class>: %s, <Exception Message>: %s";
 
     private final Logger log = LoggerFactory.getLogger(getClass());
 
-    @ExceptionHandler(AuthenticationException.class)
-    public ResponseEntity<Void> handlerAuthenticationException(AuthenticationException e) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-    }
-
-    @ExceptionHandler(CartItemException.IllegalMember.class)
-    public ResponseEntity<Void> handleException(CartItemException.IllegalMember e) {
-        return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
-    }
-
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<Void> exception(Exception e) {
-        log.warn("Unexpected Exception = ", e);
-        return ResponseEntity.internalServerError().build();
+    public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        log.error(String.format(ERROR_LOG_FORMAT, e.getClass(), e.getMessage()));
+        return ResponseEntity.internalServerError()
+                .body(new ErrorResponse("죄송합니다. 서버에서 오류가 발생했습니다. 잠시 후에 다시 시도해주세요."));
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ErrorResponse> handlerAuthenticationException(AuthenticationException e) {
+        log.warn(String.format(ERROR_LOG_FORMAT, e.getClass(), e.getMessage()));
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body(new ErrorResponse(e.getMessage()));
+    }
+
+    @ExceptionHandler(CartItemException.class)
+    public ResponseEntity<ErrorResponse> handleCartItemException(CartItemException e) {
+        log.warn(String.format(ERROR_LOG_FORMAT, e.getClass(), e.getMessage()));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse(e.getMessage()));
     }
 }
---
+++ src/main/java/cart/ui/ErrorResponse.java
@@ -0,0 +1,14 @@
+package cart.ui;
+
+public class ErrorResponse {
+
+    private final String message;
+
+    public ErrorResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
