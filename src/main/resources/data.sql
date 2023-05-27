cb2b8e097e165b200cacd81e71d7d67ae45207cf
dev_kong <ahdjd5@gmail.com>
2023-05-27 13:39:03 +0900

    fix: init data 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,13 @@
+SET REFERENTIAL_INTEGRITY FALSE;
+truncate table `order` restart identity ;
+truncate table cart_item restart identity ;
+truncate table product restart identity ;
+truncate table member restart identity ;
+truncate table grade restart identity ;
+
 INSERT INTO grade (grade) VALUES (1);
 INSERT INTO grade (grade) VALUES (2);
 INSERT INTO grade (grade) VALUES (3);
-
 
 INSERT INTO product (name, price, image_url) VALUES ('치킨', 10000, 'https://images.unsplash.com/photo-1626082927389-6cd097cdc6ec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80');
 INSERT INTO product (name, price, image_url) VALUES ('샐러드', 20000, 'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80');
@@ -14,3 +20,5 @@ INSERT INTO member (email, password, grade_id) VALUES ('c@c.com', '1234', 3);
 INSERT INTO cart_item (member_id, product_id, quantity) VALUES (1, 1, 2);
 INSERT INTO cart_item (member_id, product_id, quantity) VALUES (1, 2, 4);
 INSERT INTO cart_item (member_id, product_id, quantity) VALUES (2, 3, 5);
+
+SET REFERENTIAL_INTEGRITY TRUE;
