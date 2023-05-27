e43727c433ef8655f1f7ffca44365d6e26cdbe94
Hyeonjae-K <kwonhyeonjae01@gmail.com>
2023-05-27 15:25:25 +0900

    feat: 테스트 데이터베이스 환경 추가

---
+++ src/test/resources/init.sql
@@ -0,0 +1,82 @@
+DROP TABLE IF EXISTS order_product;
+DROP TABLE IF EXISTS order_history;
+DROP TABLE IF EXISTS cart_item;
+DROP TABLE IF EXISTS member;
+DROP TABLE IF EXISTS product;
+
+CREATE TABLE product
+(
+    id         BIGINT PRIMARY KEY AUTO_INCREMENT,
+    name       VARCHAR(255)                       NOT NULL,
+    price      INT                                NOT NULL,
+    image_url  VARCHAR(255)                       NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+CREATE TABLE member
+(
+    id         BIGINT PRIMARY KEY AUTO_INCREMENT,
+    email      VARCHAR(255) UNIQUE                NOT NULL,
+    password   VARCHAR(255)                       NOT NULL,
+    point      INT      DEFAULT 0                 NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+CREATE TABLE cart_item
+(
+    id         BIGINT PRIMARY KEY AUTO_INCREMENT,
+    member_id  BIGINT                             NOT NULL,
+    product_id BIGINT                             NOT NULL,
+    quantity   INT                                NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+
+    FOREIGN KEY (member_id) REFERENCES member (id),
+    FOREIGN KEY (product_id) REFERENCES product (id)
+);
+
+CREATE TABLE order_history
+(
+    id           BIGINT PRIMARY KEY AUTO_INCREMENT,
+    member_id    BIGINT                             NOT NULL,
+    total_amount INT                                NOT NULL,
+    used_point   INT                                NOT NULL,
+    saved_point  INT                                NOT NULL,
+    created_at   DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+
+    FOREIGN KEY (member_id) REFERENCES member (id)
+);
+
+CREATE TABLE order_product
+(
+    id         BIGINT PRIMARY KEY AUTO_INCREMENT,
+    order_id   BIGINT                             NOT NULL,
+    product_id BIGINT                             NOT NULL,
+    quantity   INT                                NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+
+    FOREIGN KEY (order_id) REFERENCES order_history (id),
+    FOREIGN KEY (product_id) REFERENCES product (id)
+);
+
+INSERT INTO product (name, price, image_url)
+VALUES ('치킨', 10000,
+        'https://images.unsplash.com/photo-1626082927389-6cd097cdc6ec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80');
+INSERT INTO product (name, price, image_url)
+VALUES ('샐러드', 20000,
+        'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80');
+INSERT INTO product (name, price, image_url)
+VALUES ('피자', 13000,
+        'https://images.unsplash.com/photo-1595854341625-f33ee10dbf94?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80');
+
+INSERT INTO member (email, password)
+VALUES ('a@a.com', '1234');
+INSERT INTO member (email, password)
+VALUES ('b@b.com', '1234');
+
+INSERT INTO cart_item (member_id, product_id, quantity)
+VALUES (1, 1, 2);
+INSERT INTO cart_item (member_id, product_id, quantity)
+VALUES (1, 2, 4);
+
+INSERT INTO cart_item (member_id, product_id, quantity)
+VALUES (2, 3, 5);
