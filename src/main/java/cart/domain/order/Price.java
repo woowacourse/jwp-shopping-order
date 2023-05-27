e7dc3e530d5e0565b184b99849359e54f624284b
yujamint <woawoa0513@gmail.com>
2023-05-27 18:42:47 +0900

    test: 금액 값 객체 생성

---
+++ src/main/java/cart/domain/order/Price.java
@@ -0,0 +1,41 @@
+package cart.domain.order;
+
+import java.util.Objects;
+
+public class Price {
+    private static final int MINIMUM_VALUE = 0;
+
+    private final int value;
+
+    public Price(final int value) {
+        validateLessThanMinimumValue(value);
+        this.value = value;
+    }
+
+    private void validateLessThanMinimumValue(final int value) {
+        if (value < MINIMUM_VALUE) {
+            throw new IllegalArgumentException("가격은 음수일 수 없습니다");
+        }
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final Price price = (Price) o;
+        return value == price.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
