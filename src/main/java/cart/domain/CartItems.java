bb29e6cc581172e8dc997d486ea0a01deaf97367
yujamint <woawoa0513@gmail.com>
2023-05-28 03:14:23 +0900

    feat: 장바구니 내 주인 불일치 유효성 검사 기능 구현

--- src/main/java/cart/domain/CartItems.java
+++ src/main/java/cart/domain/CartItems.java
@@ -9,12 +9,23 @@ public class CartItems {
 
     public CartItems(final List<CartItem> cartItems) {
         validateItemsLength(cartItems);
+        validateMember(cartItems);
         this.cartItems = cartItems;
     }
 
     private void validateItemsLength(final List<CartItem> cartItems) {
         if (cartItems.isEmpty()) {
             throw new IllegalArgumentException("장바구니 상품이 존재하지 않습니다.");
+        }
+    }
+
+    private void validateMember(final List<CartItem> cartItems) {
+        final long memberCount = cartItems.stream()
+                .map(CartItem::getMember)
+                .distinct()
+                .count();
+        if (memberCount != 1) {
+            throw new IllegalArgumentException("다른 Member의 장바구니 상품이 포함되어 있습니다");
         }
     }
 
@@ -27,5 +38,9 @@ public class CartItems {
 
     public List<CartItem> getCartItems() {
         return cartItems;
+    }
+
+    public Member getMember() {
+        return cartItems.get(0).getMember();
     }
 }
