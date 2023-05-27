ac2b8f0674bc06ca352eb25f7e44042b14e62f90
yujamint <woawoa0513@gmail.com>
2023-05-28 03:13:53 +0900

    test: 장바구니 내 주인 불일치 유효성 검사 기능 테스트 작성

--- src/test/java/cart/domain/CartItemsTest.java
+++ src/test/java/cart/domain/CartItemsTest.java
@@ -2,6 +2,7 @@ package cart.domain;
 
 import static cart.fixtures.CartItemFixtures.바닐라_크림_콜드브루_ID_4_3개_17400원;
 import static cart.fixtures.CartItemFixtures.유자_민트_티_ID_1_5개_29500원;
+import static cart.fixtures.CartItemFixtures.자몽_허니_블랙티_ID_2_7개_39900원;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -13,6 +14,30 @@ import org.junit.jupiter.api.Test;
 
 @SuppressWarnings("NonAsciiCharacters")
 class CartItemsTest {
+
+    @DisplayName("다른 Member의 장바구니 상품이 포함되어 있으면 예외가 발생한다")
+    @Test
+    void createCartItems_containsOtherMember_throws() {
+        // given
+        final Member member = new Member(1L, "test@email.com", "password");
+        final Member otherMember = new Member(2L, "other@email.com", "password");
+        final List<CartItem> cartItems = List.of(유자_민트_티_ID_1_5개_29500원(member), 자몽_허니_블랙티_ID_2_7개_39900원(otherMember));
+
+        // when, then
+        assertThatThrownBy(() -> new CartItems(cartItems))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("다른 Member의 장바구니 상품이 포함되어 있습니다");
+
+    }
+
+    @DisplayName("장바구니 상품이 존재하지 않으면 예외가 발생한다")
+    @Test
+    void createCartItems_ItemNotExist_throws() {
+        // when, then
+        assertThatThrownBy(() -> new CartItems(Collections.emptyList()))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("장바구니 상품이 존재하지 않습니다.");
+    }
 
     @DisplayName("장바구니 상품들의 합산 금액을 계산한다")
     @Test
@@ -27,15 +52,6 @@ class CartItemsTest {
 
         // then
         assertThat(sum.getValue()).isEqualTo(46_900);
-    }
-
-    @DisplayName("장바구니 상품이 존재하지 않으면 예외가 발생한다")
-    @Test
-    void createCartItems_ItemNotExist_throws() {
-        // when, then
-        assertThatThrownBy(() -> new CartItems(Collections.emptyList()))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("장바구니 상품이 존재하지 않습니다.");
     }
 
 }
