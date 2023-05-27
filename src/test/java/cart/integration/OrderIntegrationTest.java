2747837019e775c3fd599d4b436f8d8a1487b9a2
dev_kong <ahdjd5@gmail.com>
2023-05-27 11:34:42 +0900

    test: 주문조회(단건) 인수테스트 작성

--- src/test/java/cart/integration/OrderIntegrationTest.java
+++ src/test/java/cart/integration/OrderIntegrationTest.java
@@ -36,18 +36,7 @@ public class OrderIntegrationTest extends IntegrationTest {
     @DisplayName("장바구니에 담긴 상품을 주문한다.")
     void order() {
         //given
-        final OrderRequest orderRequest = new OrderRequest(List.of(1L, 2L));
-
-        //when
-        final ExtractableResponse<Response> response = given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .auth().preemptive().basic(member.getEmail(), member.getPassword())
-                .body(orderRequest)
-                .when()
-                .post("/orders")
-                .then()
-                .log().all()
-                .extract();
+        final ExtractableResponse<Response> response = createOrder(List.of(1L, 2L));
 
         //then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
@@ -57,18 +46,7 @@ public class OrderIntegrationTest extends IntegrationTest {
     @DisplayName("존재하지 않는 cartItemId가 포함되어 있는 경우 400을 반환한다.")
     void orderFailByNonExistedCartItem() {
         //given
-        final OrderRequest orderRequest = new OrderRequest(List.of(1L, 2L, 10L));
-
-        //when
-        final ExtractableResponse<Response> response = given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .auth().preemptive().basic(member.getEmail(), member.getPassword())
-                .body(orderRequest)
-                .when()
-                .post("/orders")
-                .then()
-                .log().all()
-                .extract();
+        final ExtractableResponse<Response> response = createOrder(List.of(1L, 2L, 10L));
 
         //then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
@@ -78,18 +56,7 @@ public class OrderIntegrationTest extends IntegrationTest {
     @DisplayName("다른 멤버의 장바구니 아이템을 구매하려는 경우 403을 반환한다.")
     void orderFailByMemberDoesNotHaveCartItem() {
         //given
-        final OrderRequest orderRequest = new OrderRequest(List.of(1L, 2L, 3L));
-
-        //when
-        final ExtractableResponse<Response> response = given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .auth().preemptive().basic(member.getEmail(), member.getPassword())
-                .body(orderRequest)
-                .when()
-                .post("/orders")
-                .then()
-                .log().all()
-                .extract();
+        final ExtractableResponse<Response> response = createOrder(List.of(1L, 2L, 3L));
 
         //then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.FORBIDDEN.value());
@@ -99,10 +66,37 @@ public class OrderIntegrationTest extends IntegrationTest {
     @DisplayName("존재하지 않는 cartItemId를 통해 구매를 하는 경우 400을 응답한다.")
     void orderFailByUnExistedCartItem() {
         //given
-        final OrderRequest orderRequest = new OrderRequest(List.of(1L, 2L, 10L));
+        final ExtractableResponse<Response> response = createOrder(List.of(1L, 2L, 10L));
+
+        //then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    @Test
+    @DisplayName("orderId를 통해 주문내역을 조회한다.")
+    void findOrderById() {
+        //given
+        final ExtractableResponse<Response> order = createOrder(List.of(1L, 2L));
+        final long orderId = Long.parseLong(order.header("Location").split("/")[2]);
 
         //when
         final ExtractableResponse<Response> response = given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .auth().preemptive().basic(member.getEmail(), member.getPassword())
+                .when()
+                .get("/orders/" + orderId)
+                .then()
+                .log().all()
+                .extract();
+
+        //then
+        assertThat(response.jsonPath().getLong("id")).isEqualTo(orderId);
+    }
+
+    private ExtractableResponse<Response> createOrder(List<Long> cartItemIds) {
+        final OrderRequest orderRequest = new OrderRequest(cartItemIds);
+
+        return given().log().all()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .auth().preemptive().basic(member.getEmail(), member.getPassword())
                 .body(orderRequest)
@@ -111,8 +105,5 @@ public class OrderIntegrationTest extends IntegrationTest {
                 .then()
                 .log().all()
                 .extract();
-
-        //then
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 }
