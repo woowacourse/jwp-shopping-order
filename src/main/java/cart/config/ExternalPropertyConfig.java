b246d57b827e4ac6ec5f0ae265dd8d7736eaa2e3
woo-chang <clllickme@naver.com>
2023-05-29 02:18:49 +0900

    refactor: 로컬과 prod 환경 분리

--- src/main/java/cart/config/ExternalPropertyConfig.java
+++ src/main/java/cart/config/ExternalPropertyConfig.java
@@ -1,13 +1,15 @@
 package cart.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @PropertySource(
-        value = {"file:/Users/choeuchang/Desktop/wooteco/course/level2/application-prod.yml"},
+        value = {"file:/home/ubuntu/application-prod.yml"},
         factory = YamlPropertySourceFactory.class
 )
+@Profile("prod")
 public class ExternalPropertyConfig {
 
 }
