295d67cbe6896540036e430f074c530772a6042f
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-05-29 00:06:15 +0900

    feat: 사용자 주문 목록 조회 기능 추가

--- src/main/java/cart/dao/OrderDao.java
+++ src/main/java/cart/dao/OrderDao.java
@@ -2,9 +2,12 @@ package cart.dao;
 
 import cart.domain.Member;
 import cart.domain.Order;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
@@ -27,26 +30,45 @@ public class OrderDao {
                 "SELECT orders.id, orders.member_id, orders.created_at, orders.total_price, orders.final_price, member.id, member.email, member.password "
                         + "FROM orders "
                         + "INNER JOIN member ON orders.member_id = member.id";
-        return jdbcTemplate.query(findAllQuery, (rs, rowNum) -> {
-            final long orderId = rs.getLong("id");
-            final LocalDateTime createdAt = rs.getTimestamp("created_at").toLocalDateTime();
-            final int totalPrice = rs.getInt("total_price");
-            final int finalPrice = rs.getInt("final_price");
-            final Long memberId = rs.getLong("member.id");
-            final String email = rs.getString("member.email");
-            final String password = rs.getString("member.password");
-            final Member member = new Member(memberId, email, password);
-            return new Order(orderId, createdAt, member, totalPrice, finalPrice);
-        });
+        return jdbcTemplate.query(findAllQuery, orderRowMapper());
     }
 
     public Long saveOrder(final Order order) {
         final MapSqlParameterSource parameters = new MapSqlParameterSource()
                 .addValue("member_id", order.getMember().getId())
+                .addValue("created_at", LocalDateTime.now())
                 .addValue("total_price", order.getTotalPrice())
-                .addValue("final_price", order.getFinalPrice())
-                .addValue("created_at", LocalDateTime.now());
+                .addValue("final_price", order.getFinalPrice());
 
         return insertAction.executeAndReturnKey(parameters).longValue();
+    }
+
+    public List<Order> findOrderByMember(final Long memberId) {
+        final String findAllQuery =
+                "SELECT orders.id, orders.member_id, orders.created_at, orders.total_price, orders.final_price, member.id, member.email, member.password "
+                        + "FROM orders "
+                        + "INNER JOIN member ON orders.member_id = member.id "
+                        + "WHERE orders.member_id = ?";
+
+        return jdbcTemplate.query(findAllQuery, orderRowMapper(), memberId);
+    }
+
+    private RowMapper<Order> orderRowMapper() {
+        return (rs, rowNum) -> {
+            final Member member = memberMapper(rs);
+
+            final long orderId = rs.getLong("id");
+            final LocalDateTime createdAt = rs.getTimestamp("created_at").toLocalDateTime();
+            final int totalPrice = rs.getInt("total_price");
+            final int finalPrice = rs.getInt("final_price");
+            return new Order(orderId, createdAt, member, totalPrice, finalPrice);
+        };
+    }
+
+    private Member memberMapper(final ResultSet rs) throws SQLException {
+        final Long memberId = rs.getLong("member.id");
+        final String email = rs.getString("member.email");
+        final String password = rs.getString("member.password");
+        return new Member(memberId, email, password);
     }
 }
---
+++ src/test/java/cart/dao/OrderDaoTest.java
@@ -0,0 +1,69 @@
+package cart.dao;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import cart.domain.Member;
+import cart.domain.Order;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@SuppressWarnings("NonAsciiCharacters")
+@JdbcTest
+class OrderDaoTest {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    private MemberDao memberDao;
+
+    private OrderDao orderDao;
+
+    @BeforeEach
+    void setUp() {
+        memberDao = new MemberDao(jdbcTemplate);
+
+        orderDao = new OrderDao(jdbcTemplate);
+    }
+
+    @Test
+    void 주문_생성_테스트() {
+        final Member memberFixture = getSavedMemberFixture("testEmail");
+        final Order order = new Order(memberFixture, 50_000, 45_000);
+
+        orderDao.saveOrder(order);
+
+        final List<Order> allOrders = orderDao.findAll();
+        assertThat(allOrders).hasSize(1);
+        assertThat(allOrders.get(0).getMember().getEmail()).isEqualTo("testEmail");
+    }
+
+    @Test
+    void 사용자_주문_목록_조회_테스트() {
+        //given
+        final Member memberFixtureA = getSavedMemberFixture("testEmailA");
+        final Order orderA = new Order(memberFixtureA, 70_000, 65_000);
+        final Member memberFixtureB = getSavedMemberFixture("testEmailB");
+        final Order orderB = new Order(memberFixtureB, 30_000, 28_000);
+        orderDao.saveOrder(orderA);
+        orderDao.saveOrder(orderB);
+
+        //when
+        final List<Order> orderByMemberA = orderDao.findOrderByMember(memberFixtureA.getId());
+
+        //then
+        assertThat(orderByMemberA).hasSize(1);
+        final Order memberAOrder = orderByMemberA.get(0);
+        assertThat(memberAOrder.getMember().getEmail()).isEqualTo("testEmailA");
+        assertThat(memberAOrder.getTotalPrice()).isEqualTo(70_000);
+    }
+
+    private Member getSavedMemberFixture(final String email) {
+        final Member member = new Member(email, "password");
+        final Long saveMemberId = memberDao.saveMember(member);
+
+        return new Member(saveMemberId, member.getEmail(), member.getPassword());
+    }
+}
