8b5f05dab4853cc4146a11fb414132406dda6882
sosow0212 <sosow0212@naver.com>
2023-05-28 22:10:12 +0900

    fix: DTO 이름이 반대로 되어있던 것 해결

--- src/main/java/cart/dto/payment/PaymentRequest.java
+++ src/main/java/cart/dto/payment/PaymentRequest.java
@@ -1,24 +1,24 @@
 package cart.dto.payment;
 
 import cart.dto.coupon.CouponIdRequest;
-import cart.dto.product.ProductIdResponse;
+import cart.dto.product.ProductIdRequest;
 
 import java.util.List;
 
 public class PaymentRequest {
 
-    private List<ProductIdResponse> products;
+    private List<ProductIdRequest> products;
     private List<CouponIdRequest> coupons;
 
     public PaymentRequest() {
     }
 
-    public PaymentRequest(final List<ProductIdResponse> products, final List<CouponIdRequest> coupons) {
+    public PaymentRequest(final List<ProductIdRequest> products, final List<CouponIdRequest> coupons) {
         this.products = products;
         this.coupons = coupons;
     }
 
-    public List<ProductIdResponse> getProducts() {
+    public List<ProductIdRequest> getProducts() {
         return products;
     }
 
---
+++ src/main/java/cart/dto/product/ProductIdRequest.java
@@ -0,0 +1,24 @@
+package cart.dto.product;
+
+public class ProductIdRequest {
+
+    private Long id;
+    private int quantity;
+
+    public ProductIdRequest() {
+
+    }
+
+    public ProductIdRequest(final Long id, final int quantity) {
+        this.id = id;
+        this.quantity = quantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+}
