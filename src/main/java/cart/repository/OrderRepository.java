b784f5f322af4663da901fcaed59ffb27d945952
hong-sile <gurwns9325@naver.com>
2023-05-28 18:18:48 +0900

    feat: order를 id기준으로 조회하는 기능 추가

--- src/main/java/cart/repository/OrderRepository.java
+++ src/main/java/cart/repository/OrderRepository.java
@@ -1,13 +1,23 @@
 package cart.repository;
 
+import static java.util.stream.Collectors.toMap;
+
+import cart.dao.MemberDao;
 import cart.dao.OrderDao;
 import cart.dao.OrderProductDao;
+import cart.dao.ProductDao;
 import cart.dao.dto.OrderDto;
 import cart.dao.dto.OrderProductDto;
 import cart.domain.Order;
 import cart.domain.OrderProduct;
+import cart.domain.Product;
+import cart.domain.Quantity;
+import cart.exception.OrderNotFoundException;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -15,18 +25,22 @@ public class OrderRepository {
 
     private final OrderDao orderDao;
     private final OrderProductDao orderProductDao;
+    private final ProductDao productDao;
+    private final MemberDao memberDao;
 
-    public OrderRepository(final OrderDao orderDao, final OrderProductDao orderProductDao) {
+    public OrderRepository(final OrderDao orderDao, final OrderProductDao orderProductDao, final ProductDao productDao,
+                           final MemberDao memberDao) {
         this.orderDao = orderDao;
         this.orderProductDao = orderProductDao;
+        this.productDao = productDao;
+        this.memberDao = memberDao;
     }
 
-
     public Order save(final Order order) {
-        OrderDto orderDto = OrderDto.from(order);
-        Long orderId = orderDao.insert(orderDto);
+        final OrderDto orderDto = OrderDto.from(order);
+        final Long orderId = orderDao.insert(orderDto);
 
-        List<OrderProduct> orderProductsAfterSave = saveOrderProducts(orderId, order.getOrderProducts());
+        final List<OrderProduct> orderProductsAfterSave = saveOrderProducts(orderId, order.getOrderProducts());
 
         return new Order(orderId, order.getTimeStamp(), order.getMember(), orderProductsAfterSave);
     }
@@ -40,5 +54,24 @@ public class OrderRepository {
                     orderProduct.getQuantity()));
         }
         return orderProductsAfterSave;
+    }
+
+    public Order findById(final Long id) {
+        final OrderDto orderDto = orderDao.findById(id).orElseThrow(OrderNotFoundException::new);
+        final List<OrderProduct> orderProducts = findOrderProductsByOrderId(id);
+        return new Order(orderDto.getId(), orderDto.getTimeStamp()
+                , memberDao.getMemberById(orderDto.getMemberId()), orderProducts);
+    }
+
+    private List<OrderProduct> findOrderProductsByOrderId(final Long id) {
+        final List<OrderProductDto> orderProductDtos = orderProductDao.findByOrderId(id);
+        final Map<Long, Product> products = orderProductDtos.stream()
+                .map(OrderProductDto::getProductId)
+                .map(productDao::getProductById)
+                .collect(toMap(Product::getId, Function.identity()));
+        return orderProductDtos.stream()
+                .map(dto -> new OrderProduct(dto.getId(), products.get(dto.getProductId()),
+                        new Quantity(dto.getQuantity())))
+                .collect(Collectors.toUnmodifiableList());
     }
 }
--- src/test/java/cart/repository/OrderRepositoryTest.java
+++ src/test/java/cart/repository/OrderRepositoryTest.java
@@ -1,5 +1,8 @@
 package cart.repository;
 
+import static cart.TestDataFixture.MEMBER_1;
+import static cart.TestDataFixture.PRODUCT_1;
+import static cart.TestDataFixture.PRODUCT_2;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import cart.dao.MemberDao;
@@ -7,64 +10,55 @@ import cart.dao.OrderDao;
 import cart.dao.OrderProductDao;
 import cart.dao.ProductDao;
 import cart.domain.CartItem;
-import cart.domain.Member;
 import cart.domain.Order;
 import cart.domain.OrderProduct;
-import cart.domain.Product;
 import java.util.List;
-import javax.sql.DataSource;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.context.annotation.Import;
 
 @JdbcTest
+@Import({OrderRepository.class, OrderDao.class, OrderProductDao.class, ProductDao.class, MemberDao.class})
 class OrderRepositoryTest {
 
-    private static final Member MEMBER = new Member(1L, "a@a", "1234");
-    private static final int PRICE = 1_000_000_000;
-    private static final Product HONG_HONG = new Product(1L, "홍홍", PRICE, "hognhong.com");
-    private static final Product HONG_SILE = new Product(2L, "홍실", PRICE, "hongsil.com");
-
     @Autowired
-    private DataSource dataSource;
     private OrderRepository orderRepository;
-    private JdbcTemplate jdbcTemplate;
-    private MemberDao memberDao;
-    private ProductDao productDao;
-
-    @BeforeEach
-    void beforeEach() {
-        jdbcTemplate = new JdbcTemplate(dataSource);
-        orderRepository = new OrderRepository(new OrderDao(dataSource), new OrderProductDao(dataSource));
-        memberDao = new MemberDao(jdbcTemplate);
-        productDao = new ProductDao(jdbcTemplate);
-    }
 
     @Test
-    @DisplayName("Order 를 저장한다.")
+    @DisplayName("Order를 저장하는 기능 테스트")
     void saveOrder() {
-        initData();
-        CartItem hongHongCart = new CartItem(MEMBER, HONG_HONG);
-        CartItem hongSileCart = new CartItem(MEMBER, HONG_SILE);
-        Order order = Order.of(MEMBER, List.of(hongHongCart, hongSileCart));
+        final CartItem cartItem1 = new CartItem(MEMBER_1, PRODUCT_1);
+        final CartItem cartItem2 = new CartItem(MEMBER_1, PRODUCT_2);
+        final Order order = Order.of(MEMBER_1, List.of(cartItem1, cartItem2));
 
-        Order orderAfterSave = orderRepository.save(order);
+        final Order orderAfterSave = orderRepository.save(order);
 
         assertThat(orderAfterSave)
                 .extracting(Order::getTimeStamp, Order::getMember)
-                .contains(order.getTimeStamp(), MEMBER);
+                .contains(order.getTimeStamp(), MEMBER_1);
         assertThat(orderAfterSave.getOrderProducts())
                 .extracting(OrderProduct::getProduct)
-                .contains(hongHongCart.getProduct(), hongSileCart.getProduct());
+                .contains(cartItem1.getProduct(), cartItem2.getProduct());
     }
 
-    private void initData() {
-        memberDao.addMember(MEMBER);
-        productDao.createProduct(HONG_HONG);
-        productDao.createProduct(HONG_SILE);
-    }
+    @Test
+    @DisplayName("Order를 조회하는 기능 테스트")
+    void findById() {
+        final CartItem cartItem1 = new CartItem(MEMBER_1, PRODUCT_1);
+        final CartItem cartItem2 = new CartItem(MEMBER_1, PRODUCT_2);
+        final Order order = Order.of(MEMBER_1, List.of(cartItem1, cartItem2));
 
+        final Long orderId = orderRepository.save(order).getId();
+
+        final Order orderAfterSave = orderRepository.findById(orderId);
+
+        assertThat(orderAfterSave)
+                .extracting(Order::getTimeStamp, Order::getMember)
+                .contains(order.getTimeStamp(), MEMBER_1);
+        assertThat(orderAfterSave.getOrderProducts())
+                .extracting(OrderProduct::getProduct)
+                .contains(cartItem1.getProduct(), cartItem2.getProduct());
+    }
 }
