0487b5c7626707180d1d57337b685e04e8cc919b
yujamint <woawoa0513@gmail.com>
2023-05-29 04:18:11 +0900

    test(OrderItemDaoTest): 주문 상품 저장/조회 기능 테스트 작성

---
+++ src/test/java/cart/repository/dao/OrderItemDaoTest.java
@@ -0,0 +1,81 @@
+package cart.repository.dao;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import cart.domain.CartItem;
+import cart.domain.CartItems;
+import cart.domain.Member;
+import cart.domain.order.DiscountPolicy;
+import cart.domain.order.FixedDiscountPolicy;
+import cart.domain.order.Order;
+import cart.domain.order.OrderItems;
+import cart.entity.OrderEntity;
+import cart.entity.OrderItemEntity;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@JdbcTest
+class OrderItemDaoTest {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    private OrderItemDao orderItemDao;
+    private OrderDao orderDao;
+    private CartItemDao cartItemDao;
+
+    private MemberDao memberDao;
+
+    @BeforeEach
+    void setUp() {
+        this.orderItemDao = new OrderItemDao(jdbcTemplate);
+        this.orderDao = new OrderDao(jdbcTemplate);
+        this.memberDao = new MemberDao(jdbcTemplate);
+        this.cartItemDao = new CartItemDao(jdbcTemplate);
+    }
+
+    @Test
+    void saveAllOrderItems() {
+        // given
+        final Member findMember = memberDao.getMemberById(1L);
+        final List<CartItem> findCartItems = cartItemDao.findByMemberId(findMember.getId());
+        final CartItems cartItems = new CartItems(findCartItems, findMember);
+
+        final OrderItems orderItems = OrderItems.from(cartItems);
+        DiscountPolicy discountPolicy = FixedDiscountPolicy.from(orderItems.sumOfPrice());
+        final Order order = new Order(findMember, orderItems, discountPolicy);
+
+        final Long orderId = orderDao.createOrder(OrderEntity.from(order));
+        final List<OrderItemEntity> orderItemEntities = OrderItemEntity.from(orderId, orderItems.getOrderItems());
+
+        // when
+        assertDoesNotThrow(() -> orderItemDao.saveAll(orderItemEntities));
+    }
+
+    @Test
+    void getOrderItemsByOrderId() {
+        // given
+        final Member findMember = memberDao.getMemberById(1L);
+        final List<CartItem> findCartItems = cartItemDao.findByMemberId(findMember.getId());
+        final CartItems cartItems = new CartItems(findCartItems, findMember);
+
+        final OrderItems orderItems = OrderItems.from(cartItems);
+        DiscountPolicy discountPolicy = FixedDiscountPolicy.from(orderItems.sumOfPrice());
+        final Order order = new Order(findMember, orderItems, discountPolicy);
+        final Long orderId = orderDao.createOrder(OrderEntity.from(order));
+
+        final List<OrderItemEntity> orderItemEntities = OrderItemEntity.from(orderId, orderItems.getOrderItems());
+        orderItemDao.saveAll(orderItemEntities);
+
+        // when
+        List<OrderItemEntity> findOrderItems = orderItemDao.getOrderItemsByOrderId(orderId);
+
+        // then
+        assertThat(findOrderItems).hasSize(orderItems.getOrderItems().size());
+    }
+}
