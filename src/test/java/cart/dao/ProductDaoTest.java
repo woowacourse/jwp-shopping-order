2bef63cad1a2125ec23de620da610771c715048c
yujamint <woawoa0513@gmail.com>
2023-05-29 01:43:35 +0900

    test: 상품 Dao 테스트 작성

---
+++ src/test/java/cart/dao/ProductDaoTest.java
@@ -0,0 +1,64 @@
+package cart.dao;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import cart.domain.Product;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@JdbcTest
+class ProductDaoTest {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    private ProductDao productDao;
+
+    @BeforeEach
+    void setUp() {
+        this.productDao = new ProductDao(jdbcTemplate);
+    }
+
+    @Test
+    void createProduct() {
+        // given
+        final Product product = new Product("product", 1_000, "www.image.png");
+
+        // when
+        final Long saveId = productDao.createProduct(product);
+
+        // then
+        assertThat(saveId).isNotNull();
+    }
+
+    @Test
+    void getProductById() {
+        // given
+        final Product product = new Product("product", 1_000, "www.image.png");
+        final Long saveId = productDao.createProduct(product);
+
+        // when
+        final Product findProduct = productDao.getProductById(saveId);
+
+        // then
+        assertThat(findProduct.getName()).isEqualTo(product.getName());
+        assertThat(findProduct.isDeleted()).isFalse();
+    }
+
+    @Test
+    void deleteProduct() {
+        // given
+        final Product product = new Product("product", 1_000, "www.image.png");
+        final Long saveId = productDao.createProduct(product);
+        productDao.deleteProduct(saveId);
+
+        // when
+        final Product findProduct = productDao.getProductById(saveId);
+
+        // then
+        assertThat(findProduct.isDeleted()).isTrue();
+    }
+}
