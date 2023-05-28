85c6ef2524bd3014b00712d6aff231b5197f65d9
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2023-05-28 19:49:46 +0900

    test: ci/cd

--- src/main/java/cart/TestController.java
+++ src/main/java/cart/TestController.java
@@ -1,12 +1,12 @@
 package cart;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class TestController {
 
-    @GetMapping
+    @GetMapping("/hello")
     public String hello() {
         return "hello";
     }
