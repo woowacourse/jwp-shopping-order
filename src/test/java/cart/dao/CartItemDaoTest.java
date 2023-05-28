0302def5b6a641911abe8dca45211a018511149f
yujamint <woawoa0513@gmail.com>
2023-05-28 17:17:38 +0900

    test: 입력받은 ID들을 기준으로 장바구니 상품을 조회하는 기능 테스트 작성

---
+++ src/test/java/cart/dao/CartItemDaoTest.java
@@ -0,0 +1,53 @@
+package cart.dao;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import cart.domain.CartItem;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@JdbcTest
+class CartItemDaoTest {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    private CartItemDao cartItemDao;
+
+    @BeforeEach
+    void setUp() {
+        cartItemDao = new CartItemDao(jdbcTemplate);
+    }
+
+    @DisplayName("여러 ID를 입력받아 해당하는 row를 모두 조회한다")
+    @Test
+    void findByIds() {
+        // given
+        final List<Long> cartItemIds = List.of(1L, 2L, 5L);
+
+        // when
+        final List<CartItem> cartItems = cartItemDao.findByIds(cartItemIds);
+
+        // then
+        assertThat(cartItems).map(CartItem::getId)
+                .isEqualTo(cartItemIds);
+    }
+
+    @DisplayName("존재하지 않는 id가 포함되어 있으면 해당 id의 장바구니 상품은 조회되지 않는다")
+    @Test
+    void findByIds_containsNonExistId_notFound() {
+        // given
+        final List<Long> cartItemIds = List.of(1L, 2L, Long.MAX_VALUE);
+
+        // when
+        final List<CartItem> cartItems = cartItemDao.findByIds(cartItemIds);
+
+        // then
+        assertThat(cartItems).hasSize(cartItemIds.size() - 1);
+    }
+}
