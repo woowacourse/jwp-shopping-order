cbb52f3362c2ce6dce583e16b18735bb4bae158f
HyunSeong Jeong <chocomilk8604@gmail.com>
2023-05-28 10:28:10 +0900

    feat: 사용자 포인트 요청 구현 및 테스트 케이스 작성

---
+++ src/test/java/cart/integration/MemberIntegrationTest.java
@@ -0,0 +1,40 @@
+package cart.integration;
+
+import cart.dao.MemberDao;
+import cart.domain.Member;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+
+import static org.hamcrest.Matchers.is;
+
+public class MemberIntegrationTest extends IntegrationTest {
+
+    @Autowired
+    private MemberDao memberDao;
+
+    @BeforeEach
+    void setUp() {
+        super.setUp();
+    }
+
+    @DisplayName("유저가 가지고 있는 포인트를 확인한다.")
+    @Test
+    void getPoint() {
+        // given
+        final Member member = memberDao.getMemberById(1L);
+
+        // when, then
+        RestAssured
+                .given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .auth().preemptive().basic(member.getEmail(), member.getPassword())
+                .when()
+                .get("members/point")
+                .then().log().all()
+                .body("point", is(10000));
+    }
+}
