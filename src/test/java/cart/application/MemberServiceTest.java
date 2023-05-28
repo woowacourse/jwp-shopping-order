86670efab3a47bb85f9436d4d84aecde88eee20d
woo-chang <clllickme@naver.com>
2023-05-28 20:40:22 +0900

    feat: 서비스 테스트 어노테이션 구현

--- src/test/java/cart/application/MemberServiceTest.java
+++ src/test/java/cart/application/MemberServiceTest.java
@@ -2,42 +2,46 @@ package cart.application;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.mockito.BDDMockito.anyString;
-import static org.mockito.BDDMockito.given;
 
+import cart.dao.MemberDao;
 import cart.domain.Member;
-import cart.repository.MemberRepository;
+import cart.repository.mapper.MemberMapper;
+import cart.test.ServiceTest;
 import cart.ui.controller.dto.response.MemberResponse;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 
-@ExtendWith(MockitoExtension.class)
+@ServiceTest
 class MemberServiceTest {
 
-    @InjectMocks
+    @Autowired
     private MemberService memberService;
 
-    @Mock
-    private MemberRepository memberRepository;
+    @Autowired
+    private MemberDao memberDao;
 
     @Test
     @DisplayName("getAllMembers 메서드는 모든 멤버를 응답한다.")
     void getAllMembers() {
-        Member memberA = new Member(1L, "a@a.com", "password1", 0);
-        Member memberB = new Member(2L, "b@b.com", "password2", 0);
-        given(memberRepository.getAllMembers()).willReturn(List.of(memberA, memberB));
+        Member memberA = new Member("a@a.com", "password1", 0);
+        Member memberB = new Member("b@b.com", "password2", 0);
+        Long memberIdA = memberDao.addMember(MemberMapper.toEntity(memberA));
+        Long memberIdB = memberDao.addMember(MemberMapper.toEntity(memberB));
 
         List<MemberResponse> response = memberService.getAllMembers();
 
         assertAll(
                 () -> assertThat(response).hasSize(2),
-                () -> assertThat(response.get(0)).usingRecursiveComparison().isEqualTo(MemberResponse.from(memberA)),
-                () -> assertThat(response.get(1)).usingRecursiveComparison().isEqualTo(MemberResponse.from(memberB))
+                () -> assertThat(response.get(0).getId()).isEqualTo(memberIdA),
+                () -> assertThat(response.get(0)).usingRecursiveComparison()
+                        .ignoringFields("id")
+                        .isEqualTo(MemberResponse.from(memberA)),
+                () -> assertThat(response.get(1).getId()).isEqualTo(memberIdB),
+                () -> assertThat(response.get(1)).usingRecursiveComparison()
+                        .ignoringFields("id")
+                        .isEqualTo(MemberResponse.from(memberB))
         );
     }
 
@@ -45,7 +49,7 @@ class MemberServiceTest {
     @DisplayName("getMemberByEmailAndPassword 메서드는 이메일, 비밀번호를 통해 멤버를 응답한다.")
     void GetMemberByEmailAndPassword() {
         Member member = new Member(1L, "a@a.com", "password1", 0);
-        given(memberRepository.getMemberByEmailAndPassword(anyString(), anyString())).willReturn(member);
+        memberDao.addMember(MemberMapper.toEntity(member));
 
         MemberResponse response = memberService.getMemberByEmailAndPassword("a@a.com", "password1");
 
