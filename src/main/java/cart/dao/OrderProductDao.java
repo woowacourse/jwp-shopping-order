957a13b740c690e6a602ea49db7af636ae5a4f66
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-05-29 01:14:21 +0900

    feat: 주문 상품 조회 기능 추가

---
+++ src/main/java/cart/dao/OrderProductDao.java
@@ -0,0 +1,108 @@
+package cart.dao;
+
+import cart.domain.Member;
+import cart.domain.Order;
+import cart.domain.OrderProduct;
+import cart.domain.Product;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class OrderProductDao {
+
+    private final JdbcTemplate jdbcTemplate;
+    private final SimpleJdbcInsert insertAction;
+
+    public OrderProductDao(final JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.insertAction = new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName("order_product")
+                .usingGeneratedKeyColumns("id");
+    }
+
+    public List<OrderProduct> findAll() {
+        final String findAllQuery =
+                "SELECT order_product.id, order_product.quantity, "
+                        + "orders.id, orders.created_at, orders.total_price, orders.final_price, "
+                        + "product.id, product.name, product.price, product.image_url, "
+                        + "member.id, member.email, member.password "
+                        + "FROM order_product "
+                        + "INNER JOIN orders ON order_product.order_id = orders.id "
+                        + "INNER JOIN product ON order_product.product_id = product.id "
+                        + "INNER JOIN member ON orders.member_id = member.id";
+
+        return jdbcTemplate.query(findAllQuery, orderProductRowMapper());
+    }
+
+    public void saveOrderProducts(final List<OrderProduct> orderProducts) {
+        final String saveCarQuery = "INSERT INTO order_product(order_id, product_id, quantity) VALUES (?, ?, ?)";
+
+        final List<Object[]> insertValues = new ArrayList<>();
+        for (final OrderProduct orderProduct : orderProducts) {
+            insertValues.add(new Object[]{
+                    orderProduct.getOrder().getId(),
+                    orderProduct.getProduct().getId(),
+                    orderProduct.getQuantity()
+            });
+        }
+        jdbcTemplate.batchUpdate(saveCarQuery, insertValues);
+    }
+
+    public List<OrderProduct> findByOrderId(final Long orderId) {
+        final String findAllQuery =
+                "SELECT order_product.id, order_product.quantity, "
+                        + "orders.id, orders.created_at, orders.total_price, orders.final_price, "
+                        + "product.id, product.name, product.price, product.image_url, "
+                        + "member.id, member.email, member.password "
+                        + "FROM order_product "
+                        + "INNER JOIN orders ON order_product.order_id = orders.id "
+                        + "INNER JOIN product ON order_product.product_id = product.id "
+                        + "INNER JOIN member ON orders.member_id = member.id "
+                        + "WHERE order_product.order_id = ?";
+
+        return jdbcTemplate.query(findAllQuery, orderProductRowMapper(), orderId);
+    }
+
+    private RowMapper<OrderProduct> orderProductRowMapper() {
+        return (rs, rowNum) -> {
+            final Member member = memberMapper(rs);
+            final Order order = orderMapper(rs, member);
+            final Product product = productMapper(rs);
+
+            final long orderProductId = rs.getLong("id");
+            final int quantity = rs.getInt("quantity");
+            return new OrderProduct(orderProductId, order, product, quantity);
+        };
+    }
+
+    private Member memberMapper(final ResultSet rs) throws SQLException {
+        final Long memberId = rs.getLong("member.id");
+        final String email = rs.getString("email");
+        final String password = rs.getString("password");
+        return new Member(memberId, email, password);
+    }
+
+    private Order orderMapper(final ResultSet rs, final Member member) throws SQLException {
+        final long orderId = rs.getLong("orders.id");
+        final LocalDateTime createdAt = rs.getTimestamp("created_at").toLocalDateTime();
+        final int totalPrice = rs.getInt("total_price");
+        final int finalPrice = rs.getInt("final_price");
+
+        return new Order(orderId, createdAt, member, totalPrice, finalPrice);
+    }
+
+    private Product productMapper(final ResultSet rs) throws SQLException {
+        final Long productId = rs.getLong("product.id");
+        final String name = rs.getString("name");
+        final int price = rs.getInt("price");
+        final String imageUrl = rs.getString("image_url");
+        return new Product(productId, name, price, imageUrl);
+    }
+}
---
+++ src/test/java/cart/dao/OrderProductDaoTest.java
@@ -0,0 +1,100 @@
+package cart.dao;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import cart.domain.Member;
+import cart.domain.Order;
+import cart.domain.OrderProduct;
+import cart.domain.Product;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@SuppressWarnings("NonAsciiCharacters")
+@JdbcTest
+class OrderProductDaoTest {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    private MemberDao memberDao;
+    private ProductDao productDao;
+    private OrderDao orderDao;
+
+    private OrderProductDao orderProductDao;
+
+    @BeforeEach
+    void setUp() {
+        memberDao = new MemberDao(jdbcTemplate);
+        productDao = new ProductDao(jdbcTemplate);
+        orderDao = new OrderDao(jdbcTemplate);
+
+        orderProductDao = new OrderProductDao(jdbcTemplate);
+    }
+
+    @Test
+    void 주문_상품_전체_저장_테스트() {
+        //given
+        final Member memberFixture = getSavedMemberFixture("testMember");
+        final Product testProductA = getSavedProductFixture("testProductA");
+        final Product testProductB = getSavedProductFixture("testProductB");
+        final Order orderFixture = getSavedOrderFixture(memberFixture);
+        final List<OrderProduct> orderProducts = List.of(
+                new OrderProduct(orderFixture, testProductA, 3),
+                new OrderProduct(orderFixture, testProductB, 1)
+        );
+
+        //when
+        orderProductDao.saveOrderProducts(orderProducts);
+
+        //then
+        final List<OrderProduct> all = orderProductDao.findAll();
+        assertThat(all).hasSize(2);
+        assertThat(all.get(0).getProduct().getName()).isEqualTo("testProductA");
+        assertThat(all.get(0).getOrder().getMember().getEmail()).isEqualTo("testMember");
+    }
+
+    @Test
+    void 주문으로_상품_조회_테스트() {
+        //given
+        final Member memberFixture = getSavedMemberFixture("testMember");
+        final Product testProductA = getSavedProductFixture("testProductA");
+        final Order orderFixtureA = getSavedOrderFixture(memberFixture);
+        final Order orderFixtureB = getSavedOrderFixture(memberFixture);
+        final List<OrderProduct> orderProducts = List.of(
+                new OrderProduct(orderFixtureA, testProductA, 3),
+                new OrderProduct(orderFixtureB, testProductA, 4)
+        );
+        orderProductDao.saveOrderProducts(orderProducts);
+
+        //when
+        final List<OrderProduct> byOrderId = orderProductDao.findByOrderId(orderFixtureA.getId());
+
+        //then
+        assertThat(byOrderId).hasSize(1);
+        assertThat(byOrderId.get(0).getOrder().getId()).isEqualTo(orderFixtureA.getId());
+    }
+
+    private Member getSavedMemberFixture(final String email) {
+        final Member member = new Member(email, "password");
+        final Long saveMemberId = memberDao.saveMember(member);
+
+        return new Member(saveMemberId, member.getEmail(), member.getPassword());
+    }
+
+    private Product getSavedProductFixture(final String productName) {
+        final Product product = new Product(productName, 3_000, "testImage");
+        final Long savedProductId = productDao.saveProduct(product);
+
+        return new Product(savedProductId, product.getName(), product.getPrice(), product.getImageUrl());
+    }
+
+    private Order getSavedOrderFixture(final Member member) {
+        final Order order = new Order(member, 3_000, 3_000);
+        final Long saveOrderId = orderDao.saveOrder(order);
+
+        return new Order(saveOrderId, null, member, order.getTotalPrice(), order.getFinalPrice());
+    }
+}
