be1285b1e0b188f2fb24a3498b8cccd658624d4c
sosow0212 <sosow0212@naver.com>
2023-05-28 21:28:17 +0900

    feat: 결제 테이블, 구매 내역 테이블 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -11,7 +11,7 @@ CREATE TABLE product
     name      VARCHAR(255) NOT NULL,
     price     INT          NOT NULL,
     image_url VARCHAR(255) NOT NULL,
-    isOnSale  BOOLEAN NOT NULL,
+    isOnSale  BOOLEAN      NOT NULL,
     policy_id BIGINT       NOT NULL,
     FOREIGN KEY (policy_id) REFERENCES policy (id)
 );
@@ -48,4 +48,31 @@ CREATE TABLE cart_item
     quantity   INT    NOT NULL,
     FOREIGN KEY (cart_id) REFERENCES cart (id),
     FOREIGN KEY (product_id) REFERENCES product (id) ON DELETE CASCADE
+);
+
+CREATE TABLE order_table
+(
+    id           BIGINT    NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    member_id    BIGINT    NOT NULL,
+    created_at   TIMESTAMP NOT NULL,
+    delivery_fee INT       NOT NULL,
+    FOREIGN KEY (member_id) REFERENCES member (id)
+);
+
+CREATE TABLE order_item_history
+(
+    id             BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    product_id     BIGINT       NOT NULL,
+    product_name   VARCHAR(255) NOT NULL,
+    price          INT          NOT NULL,
+    order_table_id BIGINT       NOT NULL,
+    FOREIGN KEY (order_table_id) REFERENCES order_table (id)
+);
+
+CREATE TABLE coupon_history
+(
+    id             BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name           VARCHAR(255) NOT NULL,
+    order_table_id BIGINT       NOT NULL,
+    FOREIGN KEY (order_table_id) REFERENCES order_table (id)
 );
