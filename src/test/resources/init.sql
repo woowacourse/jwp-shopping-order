a77b213a0836edc0e98f76538a29971d1e7442ba
Hyeonjae-K <kwonhyeonjae01@gmail.com>
2023-05-28 20:00:11 +0900

    fix: default 값이 있는 컬럼에 대해 not null 옵션 제거

--- src/test/resources/init.sql
+++ src/test/resources/init.sql
@@ -7,28 +7,28 @@ DROP TABLE IF EXISTS product;
 CREATE TABLE product
 (
     id         BIGINT PRIMARY KEY AUTO_INCREMENT,
-    name       VARCHAR(255)                        NOT NULL,
-    price      INT                                 NOT NULL,
-    image_url  VARCHAR(255)                        NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+    name       VARCHAR(255) NOT NULL,
+    price      INT          NOT NULL,
+    image_url  VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE member
 (
     id         BIGINT PRIMARY KEY AUTO_INCREMENT,
-    email      VARCHAR(255) UNIQUE                 NOT NULL,
-    password   VARCHAR(255)                        NOT NULL,
-    point      INT       DEFAULT 0                 NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+    email      VARCHAR(255) UNIQUE NOT NULL,
+    password   VARCHAR(255)        NOT NULL,
+    point      INT       DEFAULT 0,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE cart_item
 (
     id         BIGINT PRIMARY KEY AUTO_INCREMENT,
-    member_id  BIGINT                              NOT NULL,
-    product_id BIGINT                              NOT NULL,
-    quantity   INT                                 NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    member_id  BIGINT NOT NULL,
+    product_id BIGINT NOT NULL,
+    quantity   INT    NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
     FOREIGN KEY (member_id) REFERENCES member (id) ON DELETE CASCADE,
     FOREIGN KEY (product_id) REFERENCES product (id) ON DELETE CASCADE
@@ -37,11 +37,11 @@ CREATE TABLE cart_item
 CREATE TABLE order_history
 (
     id           BIGINT PRIMARY KEY AUTO_INCREMENT,
-    member_id    BIGINT                              NOT NULL,
-    total_amount INT                                 NOT NULL,
-    used_point   INT                                 NOT NULL,
-    saved_point  INT                                 NOT NULL,
-    created_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    member_id    BIGINT NOT NULL,
+    total_amount INT    NOT NULL,
+    used_point   INT    NOT NULL,
+    saved_point  INT    NOT NULL,
+    created_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
     FOREIGN KEY (member_id) REFERENCES member (id) ON DELETE CASCADE
 );
@@ -49,10 +49,10 @@ CREATE TABLE order_history
 CREATE TABLE order_product
 (
     id         BIGINT PRIMARY KEY AUTO_INCREMENT,
-    order_id   BIGINT                              NOT NULL,
-    product_id BIGINT                              NOT NULL,
-    quantity   INT                                 NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    order_id   BIGINT NOT NULL,
+    product_id BIGINT NOT NULL,
+    quantity   INT    NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
     FOREIGN KEY (order_id) REFERENCES order_history (id) ON DELETE CASCADE,
     FOREIGN KEY (product_id) REFERENCES product (id) ON DELETE CASCADE
