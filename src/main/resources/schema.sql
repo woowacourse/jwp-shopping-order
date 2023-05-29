6a5bc6a04d509a54d9779291e432dde5c7a06c67
ReO <yt930310@gmail.com>
2023-05-29 22:19:26 +0900

    feat: 스키마 세팅

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,9 +6,9 @@ CREATE TABLE product (
 );
 
 CREATE TABLE member (
-     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-     email VARCHAR(255) NOT NULL UNIQUE,
-     password VARCHAR(255) NOT NULL
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE cart_item (
@@ -17,5 +17,45 @@ CREATE TABLE cart_item (
     product_id BIGINT NOT NULL,
     quantity INT NOT NULL,
     FOREIGN KEY (member_id) REFERENCES member(id),
+    FOREIGN KEY (product_id) REFERENCES product(id)
+);
+
+CREATE TABLE coupon (
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    min_price INT NOT NULL,
+    max_price INT NOT NULL,
+    type VARCHAR(255) NOT NULL,
+    discount_amount INT,
+    discount_percentage DOUBLE
+);
+
+CREATE TABLE member_coupon (
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    member_id BIGINT NOT NULL,
+    coupon_id BIGINT NOT NULL,
+    expired_at DATETIME NOT NULL,
+    is_used BOOL NOT NULL,
+    FOREIGN KEY (member_id) REFERENCES member(id),
+    FOREIGN KEY (coupon_id) REFERENCES coupon(id)
+);
+
+CREATE TABLE `order` (
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    member_id BIGINT NOT NULL,
+    coupon_id BIGINT NOT NULL,
+    shipping_fee INT NOT NULL,
+    total_price INT NOT NULL,
+    create_at DATETIME DEFAULT current_timestamp,
+    FOREIGN KEY (member_id) REFERENCES member(id),
+    FOREIGN KEY (coupon_id) REFERENCES coupon(id)
+);
+
+CREATE TABLE order_item (
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    order_id BIGINT NOT NULL,
+    product_id BIGINT NOT NULL,
+    quantity INT NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES `order`(id),
     FOREIGN KEY (product_id) REFERENCES product(id)
 );
