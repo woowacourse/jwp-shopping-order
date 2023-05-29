4b8ba369922ef71a720bd1da2ed6343b51dd8983
cpot5620 <=cpot5620@naver.com>
2023-05-29 18:05:24 +0900

    feat: Price 객체 구현

---
+++ src/main/java/cart/domain/product/Price.java
@@ -0,0 +1,25 @@
+package cart.domain.product;
+
+import cart.exception.GlobalException;
+
+public class Price {
+    private static final int MIN_PRICE = 1;
+    private static final int MAX_PRICE = 10_000_000;
+
+    private final int price;
+
+    public Price(int price) {
+        validate(price);
+        this.price = price;
+    }
+
+    private void validate(int price) {
+        if (price < MIN_PRICE || price > MAX_PRICE) {
+            throw new GlobalException("상품 가격은 1원 이상, 10,000,000원 이하여야 합니다.");
+        }
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
---
+++ src/test/java/cart/domain/product/PriceTest.java
@@ -0,0 +1,24 @@
+package cart.domain.product;
+
+import cart.exception.GlobalException;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+class PriceTest {
+
+    @ParameterizedTest(name = "상품 가격은 1원 이상, 10,000,000원 이하여야 합니다.")
+    @ValueSource(ints = {-1, 0, 10_000_001})
+    void createPriceTest1(int price) {
+        assertThatThrownBy(() -> new Price(price))
+                .isInstanceOf(GlobalException.class);
+    }
+
+    @ParameterizedTest(name = "상품 가격은 1원 이상, 10,000,000원 이하여야 합니다.")
+    @ValueSource(ints = {1, 5_000_000, 10_000_000})
+    void createPriceTest2(int price) {
+        assertDoesNotThrow(() -> new Price(price));
+    }
+}
