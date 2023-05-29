caf382996db62080c7efc3bf33439fc18f42a994
cpot5620 <=cpot5620@naver.com>
2023-05-29 17:15:57 +0900

    feat: 데이터베이스 테이블 생성

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,21 +1,91 @@
-CREATE TABLE product (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    price INT NOT NULL,
-    image_url VARCHAR(255) NOT NULL
+USE `shopping-order`;
+# 멤버
+CREATE TABLE IF NOT EXISTS member
+(
+    id       INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name     VARCHAR(10)  NOT NULL UNIQUE, # 4~10
+    password VARCHAR(64)  NOT NULL,        # 4~10 (넣을 때는 인코딩된 값을 넣는 걸루), SHA256으로 암호화
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE member (
-     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-     email VARCHAR(255) NOT NULL UNIQUE,
-     password VARCHAR(255) NOT NULL
+# 상품
+CREATE TABLE IF NOT EXISTS product
+(
+    id        INT UNSIGNED AUTO_INCREMENT,
+    name      VARCHAR(20)  NOT NULL, # 1~20
+    price     INT          NOT NULL, # 1 ~ 10,000,000 (할인 시 금액 내림)
+    image_url VARCHAR(512) NOT NULL,
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE cart_item (
-    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    member_id BIGINT NOT NULL,
-    product_id BIGINT NOT NULL,
-    quantity INT NOT NULL,
-    FOREIGN KEY (member_id) REFERENCES member(id),
-    FOREIGN KEY (product_id) REFERENCES product(id)
+# 장바구니 아이템
+CREATE TABLE IF NOT EXISTS cart_item
+(
+    id         INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    member_id  INT UNSIGNED NOT NULL,
+    product_id INT UNSIGNED NOT NULL,
+    quantity   INT          NOT NULL, # 1 ~ 1000개
+    PRIMARY KEY (id),
+    FOREIGN KEY (member_id) REFERENCES member (id),
+    FOREIGN KEY (product_id) REFERENCES product (id)
+);
+
+# 쿠폰
+CREATE TABLE IF NOT EXISTS coupon
+(
+    id            INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name          VARCHAR(50)  NOT NULL, # 1 ~ 50자
+    discount_rate INT          NOT NULL, # 5 ~ 90%
+    `period`      INT          NOT NULL, # 1 ~ 365일
+    expired_date  DATETIME     NOT NULL, # 최대 만료 기간 (LocalDateTime.MAX)
+    UNIQUE (name, discount_rate),
+    PRIMARY KEY (id)
+);
+
+# 멤버 쿠폰
+CREATE TABLE IF NOT EXISTS member_coupon
+(
+    id           INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    member_id    INT UNSIGNED NOT NULL,
+    coupon_id    INT UNSIGNED NOT NULL,
+    issued_date  DATETIME     NOT NULL, # LocalDateTime.NOW()
+    expired_date DATETIME     NOT NULL, # issued_date + period (coupon), 쿠폰의 expired_date보다 작거나 같은 값
+    PRIMARY KEY (id),
+    FOREIGN KEY (member_id) REFERENCES member (id),
+    FOREIGN KEY (coupon_id) REFERENCES coupon (id)
+);
+
+# 주문 - 주문할 때 총 상품 개수는 최대 1000개
+CREATE TABLE IF NOT EXISTS `order`
+(
+    id                  INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    member_id           INT UNSIGNED NOT NULL,
+    total_product_price INT UNSIGNED NOT NULL,
+    delivery_price      INT UNSIGNED NOT NULL,
+    order_date          DATETIME     NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (member_id) REFERENCES member (id)
+);
+
+# 주문 상품
+CREATE TABLE IF NOT EXISTS order_product
+(
+    id                    INT UNSIGNED    NOT NULL AUTO_INCREMENT,
+    order_id              INT UNSIGNED    NOT NULL,
+    product_id            INT UNSIGNED    NOT NULL,
+    ordered_product_price BIGINT UNSIGNED NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (order_id) REFERENCES `order` (id),
+    FOREIGN KEY (product_id) REFERENCES product (id)
+);
+
+# 주문 쿠폰
+CREATE TABLE IF NOT EXISTS order_coupon
+(
+    id        INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    order_id  INT UNSIGNED NOT NULL,
+    coupon_id INT UNSIGNED NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (order_id) REFERENCES `order` (id),
+    FOREIGN KEY (coupon_id) REFERENCES coupon (id)
 );
