e6cedc75d3c46f6fc0cf42428da7013410add347
splitube <voicesplit@daum.net>
2023-05-29 20:01:28 +0900

    feat: 배송비 정책 생성

---
+++ src/main/java/cart/domain/delivery/BasicDeliveryPolicy.java
@@ -0,0 +1,14 @@
+package cart.domain.delivery;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BasicDeliveryPolicy implements DeliveryPolicy {
+
+    private static final long BASIC_FEE = 3000L;
+
+    @Override
+    public Long getDeliveryFee(final Long productPrice) {
+        return BASIC_FEE;
+    }
+}
