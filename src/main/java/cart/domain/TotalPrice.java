460739b9cdafeb8a87252b682b84f0c338d1a50b
Gyuseong Lee <lks971003@gmail.com>
2023-05-30 01:11:48 +0900

    feat: 금액 검증 로직 추가

--- src/main/java/cart/domain/TotalPrice.java
+++ src/main/java/cart/domain/TotalPrice.java
@@ -1,14 +1,25 @@
 package cart.domain;
 
+import cart.exception.InvalidPriceException;
+
 public class TotalPrice {
 
     private final long orderPrice;
     private final long deliveryFee;
 
     public TotalPrice(final long orderPrice, final long deliveryFee) {
+        validate(orderPrice);
+        validate(deliveryFee);
         this.orderPrice = orderPrice;
         this.deliveryFee = deliveryFee;
     }
+
+    public void validate(final long price) {
+        if (price <= 0) {
+            throw new InvalidPriceException("가격은 0이하가 될 수 없습니다");
+        }
+    }
+
     public TotalPrice discountAmount(final long amount) {
         return new TotalPrice(orderPrice - amount, deliveryFee);
     }
---
+++ src/main/java/cart/exception/InvalidPriceException.java
@@ -0,0 +1,7 @@
+package cart.exception;
+
+public class InvalidPriceException extends CartException {
+    public InvalidPriceException(final String message) {
+        super(message);
+    }
+}
