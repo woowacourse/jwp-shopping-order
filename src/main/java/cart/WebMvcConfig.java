47ce722dbccb4a7c499358a5a9b96ad157913559
jeomxon <jeomxon@gmail.com>
2023-05-29 16:26:43 +0900

    feat: CORS 설정 추가

--- src/main/java/cart/WebMvcConfig.java
+++ src/main/java/cart/WebMvcConfig.java
@@ -3,7 +3,10 @@ package cart;
 import cart.dao.MemberDao;
 import cart.ui.MemberArgumentResolver;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -12,12 +15,28 @@ import java.util.List;
 public class WebMvcConfig implements WebMvcConfigurer {
     private final MemberDao memberDao;
 
-    public WebMvcConfig(MemberDao memberDao) {
+    public WebMvcConfig(final MemberDao memberDao) {
         this.memberDao = memberDao;
     }
 
     @Override
-    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+    public void addArgumentResolvers(final List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new MemberArgumentResolver(memberDao));
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedHeaders("*")
+                .allowedOrigins("*")
+                .allowedHeaders(
+                        HttpMethod.POST.name(),
+                        HttpMethod.GET.name(),
+                        HttpMethod.PATCH.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name()
+                )
+                .exposedHeaders(HttpHeaders.LOCATION)
+                .maxAge(3000);
     }
 }
