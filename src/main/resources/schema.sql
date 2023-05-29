434acf52fef0a864ded06cedec8693cf118e14ee
Kong-hana01 <gksqlsl11@khu.ac.kr>
2023-05-29 18:29:09 +0900

    docs: 데이터베이스 sql 쿼리문 작성

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -19,3 +19,33 @@ CREATE TABLE cart_item (
     FOREIGN KEY (member_id) REFERENCES member(id),
     FOREIGN KEY (product_id) REFERENCES product(id)
 );
+
+CREATE TABLE orders (
+   id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   member_id BIGINT NOT NULL,
+   used_point INT NOT NULL,
+   FOREIGN KEY (member_id) REFERENCES member(id)
+);
+
+CREATE TABLE point (
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    member_id BIGINT NOT NULL,
+    orders_id BIGINT NOT NULL,
+    current_point INT NOT NULL,
+    comment VARCHAR(255) NOT NULL,
+    create_at DATE NOT NULL DEFAULT (CURRENT_TIMESTAMP),
+    expired_at DATE NOT NULL,
+    status TINYINT NOT NULL,
+    FOREIGN KEY (member_id) REFERENCES member(id),
+    FOREIGN KEY (orders_id) REFERENCES orders(id)
+);
+
+CREATE TABLE orders_item (
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    orders_id BIGINT NOT NULL,
+    product_id BIGINT NOT NULL,
+    quantity INT NOT NULL,
+    total_price INT NOT NULL,
+    FOREIGN KEY (orders_id) REFERENCES orders(id),
+    FOREIGN KEY (product_id) REFERENCES product(id)
+);
