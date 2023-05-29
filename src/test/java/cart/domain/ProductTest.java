5a98a70204fe10885151509a0730ff13fe105c56
DongUk <68818952+Kim0914@users.noreply.github.com>
2023-05-29 16:30:17 +0900

    test: 상품 생성 테스트 추가
    Co-authored-by: seokjin8678 <seokjin8678@gmail.com>

---
+++ src/test/java/cart/domain/ProductTest.java
@@ -0,0 +1,25 @@
+package cart.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Test;
+
+@DisplayNameGeneration(ReplaceUnderscores.class)
+@SuppressWarnings("NonAsciiCharacters")
+class ProductTest {
+
+    @Test
+    void 상품이_정상적으로_생성된다() {
+        Product product = new Product(1L, "젤리", 1000, "http://image.com/image.png");
+
+        assertAll(
+                () -> assertThat(product.getId()).isEqualTo(1L),
+                () -> assertThat(product.getName()).isEqualTo("젤리"),
+                () -> assertThat(product.getPrice().getAmount()).isEqualTo(1000),
+                () -> assertThat(product.getImageUrl()).isEqualTo("http://image.com/image.png")
+        );
+    }
+}
