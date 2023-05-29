0f580f02bf713118106b1a6b7a7a63dce29cf635
Be-student <presentsong@naver.com>
2023-05-30 00:04:09 +0900

    refactor: 인터페이스에 클래스 추가

--- src/main/java/coupon/domain/discountcondition/DiscountCondition.java
+++ src/main/java/coupon/domain/discountcondition/DiscountCondition.java
@@ -4,5 +4,8 @@ import coupon.domain.Order;
 
 public interface DiscountCondition {
 
+    DiscountCondition NoneDiscountCondition = order -> false;
+    DiscountCondition AlwaysDiscountCondition = order -> true;
+
     boolean isSatisfiedBy(Order order);
 }
--- src/test/java/coupon/domain/discountcondition/DiscountConditionTest.java
+++ src/test/java/coupon/domain/discountcondition/DiscountConditionTest.java
@@ -1,18 +1,31 @@
 package coupon.domain.discountcondition;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 import org.junit.jupiter.api.Test;
 
 @DisplayNameGeneration(ReplaceUnderscores.class)
-class AlwaysDiscountConditionTest {
+class DiscountConditionTest {
+
+    @Test
+    void 언제나_할인하지_않는_할인조건() {
+        // given
+        DiscountCondition discountCondition = DiscountCondition.NoneDiscountCondition;
+
+        // when
+        boolean result = discountCondition.isSatisfiedBy(null);
+
+        // then
+        assertFalse(result);
+    }
 
     @Test
     void 언제나_할인_조건을_만족한다() {
         // given
-        DiscountCondition discountCondition = new AlwaysDiscountCondition();
+        DiscountCondition discountCondition = DiscountCondition.AlwaysDiscountCondition;
 
         // when
         boolean result = discountCondition.isSatisfiedBy(null);
