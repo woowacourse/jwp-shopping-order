00a4ae00a5b0efbdde7aae8616921e631be0ca7d
cpot5620 <=cpot5620@naver.com>
2023-05-29 18:05:08 +0900

    feat: ProductName 객체 구현

--- src/test/java/cart/domain/member/MemberNameTest.java
+++ src/test/java/cart/domain/member/MemberNameTest.java
@@ -8,6 +8,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class MemberNameTest {
+
     @ParameterizedTest(name = "사용자의 이름은 4글자 이상, 10글자 이하여야 한다.")
     @ValueSource(strings = {"1", "123", "", "이것은 열한글자입니다"})
     void createMemberNameTest1(String name) {
--- src/test/java/cart/domain/member/PasswordTest.java
+++ src/test/java/cart/domain/member/PasswordTest.java
@@ -8,16 +8,17 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class PasswordTest {
+
     @ParameterizedTest(name = "비밀번호는 4글자 이상, 10글자 이하여야 한다.")
     @ValueSource(strings = {"1", "123", "", "이것은 열한글자입니다"})
-    void createMemberNameTest1(String password) {
+    void createMemberPasswordTest1(String password) {
         assertThatThrownBy(() -> new Password(password))
                 .isInstanceOf(GlobalException.class);
     }
 
     @ParameterizedTest(name = "비밀번호는 4글자 이상, 10글자 이하여야 한다.")
     @ValueSource(strings = {"네글자다", "이것은 열글자입니다"})
-    void createMemberNameTest2(String password) {
+    void createMemberPasswordTest2(String password) {
         assertDoesNotThrow(() -> new Password(password));
     }
 }
