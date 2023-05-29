70698df1df254511f31c9aaf5958990fcdd93537
Be-student <presentsong@naver.com>
2023-05-29 23:55:11 +0900

    feat: 쿠폰 이름 구현

---
+++ src/main/java/coupon/domain/CouponName.java
@@ -0,0 +1,41 @@
+package coupon.domain;
+
+import java.util.Objects;
+
+public class CouponName {
+
+    private static final int MAX_NAME_LENGTH = 255;
+    private final String value;
+
+    public CouponName(String value) {
+        validateValue(value);
+        this.value = value;
+    }
+
+    private void validateValue(String value) {
+        if (value.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("쿠폰 이름은 " + MAX_NAME_LENGTH + "자를 초과할 수 없습니다.");
+        }
+    }
+
+    public String getName() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        CouponName that = (CouponName) o;
+        return Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
---
+++ src/test/java/coupon/domain/CouponNameTest.java
@@ -0,0 +1,44 @@
+package coupon.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Test;
+
+@DisplayNameGeneration(ReplaceUnderscores.class)
+class CouponNameTest {
+
+    @Test
+    void 쿠폰_이름이_정상적으로_생성된다() {
+        CouponName couponName = new CouponName("쿠폰 이름");
+
+        assertThat(couponName.getName()).isEqualTo("쿠폰 이름");
+    }
+
+    @Test
+    void 이름이_255자를_초과하면_예외가_발생한다() {
+        String input = "쿠".repeat(256);
+
+        assertThatThrownBy(() -> new CouponName(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("쿠폰 이름은 255자를 초과할 수 없습니다.");
+    }
+
+    @Test
+    void 이름이_같으면_같은_쿠폰_이름이다() {
+        CouponName couponName1 = new CouponName("쿠폰 이름");
+        CouponName couponName2 = new CouponName("쿠폰 이름");
+
+        assertThat(couponName1).isEqualTo(couponName2);
+    }
+
+    @Test
+    void 이름이_다르면_다른_쿠폰_이름이다() {
+        CouponName couponName1 = new CouponName("쿠폰 이름1");
+        CouponName couponName2 = new CouponName("쿠폰 이름2");
+
+        assertThat(couponName1).isNotEqualTo(couponName2);
+    }
+}
