c46d5c2345ee749b25181f075f7969e83bfa614b
sh111-coder <ohk9134@naver.com>
2023-05-29 15:12:10 +0900

    refactor: 2단계 schema, data 리팩토링

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS member
     email    VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     cash     BIGINT       NOT NULL DEFAULT 5000
-);
+    );
 
 CREATE TABLE IF NOT EXISTS product
 (
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS product
     name      VARCHAR(255) NOT NULL,
     price     INT          NOT NULL,
     image_url VARCHAR(255) NOT NULL
-);
+    );
 
 CREATE TABLE IF NOT EXISTS cart_item
 (
@@ -20,23 +20,26 @@ CREATE TABLE IF NOT EXISTS cart_item
     member_id  BIGINT  NOT NULL,
     product_id BIGINT  NOT NULL,
     quantity   INT     NOT NULL,
-    is_ordered BOOLEAN NOT NULL DEFAULT FALSE,
     FOREIGN KEY (member_id) REFERENCES member (id) ON DELETE CASCADE,
-    FOREIGN KEY (product_id) REFERENCES product (id)
-);
+    FOREIGN KEY (product_id) REFERENCES product (id) ON DELETE CASCADE
+    );
 
 CREATE TABLE IF NOT EXISTS cart_order
 (
-    id          BIGINT    NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    total_price BIGINT    NOT NULL,
-    created_at  TIMESTAMP NOT NULL DEFAULT now()
-);
+    id              BIGINT    NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    member_id       BIGINT    NOT NULL,
+    total_price     BIGINT    NOT NULL,
+    created_at      TIMESTAMP NOT NULL DEFAULT now(),
+    FOREIGN KEY (member_id) REFERENCES member (id) ON DELETE CASCADE
+    );
 
 CREATE TABLE IF NOT EXISTS order_item
 (
-    id            BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    cart_item_id  BIGINT NOT NULL,
-    cart_order_id BIGINT NOT NULL,
-    FOREIGN KEY (cart_item_id) REFERENCES cart_item (id),
+    id              BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    cart_order_id   BIGINT       NOT NULL,
+    name            VARCHAR(255) NOT NULL,
+    price           INT          NOT NULL,
+    image_url       VARCHAR(255) NOT NULL,
+    quantity        INT          NOT NULL,
     FOREIGN KEY (cart_order_id) REFERENCES cart_order (id) ON DELETE CASCADE
-);
+    );
