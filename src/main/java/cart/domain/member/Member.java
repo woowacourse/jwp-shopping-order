476fef0298389ea2d37618a66320444e2a4af3ec
cpot5620 <=cpot5620@naver.com>
2023-05-29 17:36:52 +0900

    feat: Password 객체 구현

---
+++ src/main/java/cart/domain/member/Member.java
@@ -0,0 +1,44 @@
+package cart.domain.member;
+
+import cart.domain.CartItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Member {
+    private Long id;
+    private MemberName name;
+    private Password password;
+    private List<CartItem> cartItems;
+
+    public Member(Long id, String name, String password, List<CartItem> cartItems) {
+        this.id = id;
+        this.name = new MemberName(name);
+        this.password = new Password(password);
+        this.cartItems = cartItems;
+    }
+
+    public Member(Long id, String name, String password) {
+        this(id, name, password, new ArrayList<>());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name.getName();
+    }
+
+    public String getPassword() {
+        return password.getPassword();
+    }
+
+    public List<CartItem> getCartItems() {
+        return cartItems;
+    }
+
+    public boolean checkPassword(String password) {
+        return this.password.getPassword().equals(password);
+    }
+}
--- src/test/java/cart/domain/member/PasswordTest.java
+++ src/test/java/cart/domain/member/PasswordTest.java
@@ -1,4 +1,4 @@
-package cart.domain;
+package cart.domain.member;
 
 import cart.exception.GlobalException;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -7,18 +7,17 @@ import org.junit.jupiter.params.provider.ValueSource;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
-class MemberNameTest {
-    @ParameterizedTest(name = "사용자의 이름은 4글자 이상, 10글자 이하여야 한다.")
+class PasswordTest {
+    @ParameterizedTest(name = "비밀번호는 4글자 이상, 10글자 이하여야 한다.")
     @ValueSource(strings = {"1", "123", "", "이것은 열한글자입니다"})
-    void createMemberNameTest1(String name) {
-        assertThatThrownBy(() -> new MemberName(name))
+    void createMemberNameTest1(String password) {
+        assertThatThrownBy(() -> new Password(password))
                 .isInstanceOf(GlobalException.class);
     }
 
-    @ParameterizedTest(name = "사용자의 이름은 4글자 이상, 10글자 이하여야 한다.")
+    @ParameterizedTest(name = "비밀번호는 4글자 이상, 10글자 이하여야 한다.")
     @ValueSource(strings = {"네글자다", "이것은 열글자입니다"})
-    void createMemberNameTest2(String name) {
-        assertDoesNotThrow(() -> new MemberName(name));
+    void createMemberNameTest2(String password) {
+        assertDoesNotThrow(() -> new Password(password));
     }
-
 }
