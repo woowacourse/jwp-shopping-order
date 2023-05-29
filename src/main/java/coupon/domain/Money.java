a63dcd152ce3491c71a407222adb229f2b41dcf7
Be-student <presentsong@naver.com>
2023-05-29 23:30:12 +0900

    feat: 돈 객체 구현

---
+++ src/main/java/coupon/domain/Money.java
@@ -0,0 +1,45 @@
+package coupon.domain;
+
+import java.util.Objects;
+
+public final class Money {
+
+    private static final int MIN_VALUE = 0;
+    private final long value;
+
+    public Money(long value) {
+        validateValue(value);
+        this.value = value;
+    }
+
+    private void validateValue(long value) {
+        if (value < MIN_VALUE) {
+            throw new IllegalArgumentException("금액은 " + MIN_VALUE + "원 이상이어야 합니다.");
+        }
+    }
+
+    public long getValue() {
+        return value;
+    }
+
+    public Money getMoneyByPercentage(int percentage) {
+        return new Money(value * percentage / 100);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Money money = (Money) o;
+        return value == money.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
---
+++ src/test/java/coupon/domain/MoneyTest.java
@@ -0,0 +1,76 @@
+package coupon.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+@DisplayNameGeneration(ReplaceUnderscores.class)
+class MoneyTest {
+
+    @ParameterizedTest
+    @ValueSource(longs = {0, 1, 100, 1000, 10000})
+    void 금액이_0원_이상이면_생성에_성공한다(long value) {
+        // given
+        // when
+        Money money = new Money(value);
+
+        // then
+        assertSoftly(softly -> {
+            softly.assertThat(money).isNotNull();
+            softly.assertThat(money.getValue()).isEqualTo(value);
+        });
+    }
+
+    @ParameterizedTest
+    @ValueSource(longs = {-1, -100, -1000, -10000})
+    void 금액이_0원_미만이면_생성에_실패한다(long value) {
+        // given
+        // when
+        // then
+        assertThatThrownBy(() -> new Money(value))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("금액은 0원 이상이어야 합니다.");
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"0, 0", "1, 100", "10, 1000", "100, 10000"})
+    void 금액의_비율에_해당하는_금액을_반환한다(int input, int expected) {
+        // given
+        Money money = new Money(10000);
+
+        // when
+        Money result = money.getMoneyByPercentage(input);
+
+        // then
+        assertThat(result.getValue()).isEqualTo(expected);
+    }
+
+    @Test
+    void 같은_금액이면_같은_객체다() {
+        // given
+        Money money1 = new Money(10000);
+        Money money2 = new Money(10000);
+
+        // when
+        // then
+        assertThat(money1).isEqualTo(money2);
+    }
+
+    @Test
+    void 다른_금액이면_다른_객체다() {
+        // given
+        Money money1 = new Money(10000);
+        Money money2 = new Money(20000);
+
+        // when
+        // then
+        assertThat(money1).isNotEqualTo(money2);
+    }
+}
