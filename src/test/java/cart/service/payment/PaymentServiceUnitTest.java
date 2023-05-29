116cfa0f80bd72ebd643ec8b7fec0b67b1015b15
sosow0212 <sosow0212@naver.com>
2023-05-29 23:29:50 +0900

    test: PaymentController 통합테스트 작성

---
+++ src/test/java/cart/service/payment/PaymentServiceUnitTest.java
@@ -0,0 +1,115 @@
+package cart.service.payment;
+
+import cart.domain.cart.Cart;
+import cart.domain.coupon.Coupon;
+import cart.domain.coupon.Coupons;
+import cart.domain.discount.PolicyDiscount;
+import cart.domain.member.Member;
+import cart.dto.coupon.CouponIdRequest;
+import cart.dto.coupon.CouponsApplyRequest;
+import cart.dto.order.OrderResponse;
+import cart.dto.payment.PaymentRequest;
+import cart.dto.payment.PaymentResponse;
+import cart.dto.payment.PaymentUsingCouponsResponse;
+import cart.dto.product.ProductIdRequest;
+import cart.repository.cart.CartRepository;
+import cart.repository.coupon.CouponRepository;
+import cart.repository.order.OrderRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static cart.fixture.CartFixture.createCart;
+import static cart.fixture.CouponFixture.createCoupons;
+import static cart.fixture.MemberFixture.createMember;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class PaymentServiceUnitTest {
+
+    @InjectMocks
+    private PaymentService paymentService;
+
+    @Mock
+    private CartRepository cartRepository;
+
+    @Mock
+    private CouponRepository couponRepository;
+
+    @Mock
+    private OrderRepository orderRepository;
+
+    @DisplayName("구매 페이지를 조회한다.")
+    @Test
+    void find_payment_page() {
+        // given
+        Member member = createMember();
+        Coupons coupons = createCoupons();
+        member.initCoupons(coupons);
+        Cart cart = createCart();
+
+        given(cartRepository.findCartByMemberId(member.getId())).willReturn(cart);
+
+        // when
+        PaymentResponse result = paymentService.findPaymentPage(member);
+
+        // then
+        assertAll(
+                () -> assertThat(result.getProducts().size()).isEqualTo(2),
+                () -> assertThat(result.getCoupons().size()).isEqualTo(2)
+        );
+    }
+
+    @DisplayName("쿠폰을 적용하면, 적용 후 가격이 나온다.")
+    @Test
+    void apply_coupon_and_returns_price() {
+        // given
+        Member member = createMember();
+        Coupons coupons = createCoupons();
+        Coupons reqCoupons = new Coupons(List.of(new Coupon(1L, "쿠폰", new PolicyDiscount(1000))));
+        member.initCoupons(coupons);
+        CouponsApplyRequest request = new CouponsApplyRequest(List.of(new CouponIdRequest(1L)));
+        Cart cart = createCart();
+
+        given(cartRepository.findCartByMemberId(member.getId())).willReturn(cart);
+        given(couponRepository.findAllByCouponIds(List.of(1L))).willReturn(reqCoupons);
+
+        // when
+        PaymentUsingCouponsResponse result = paymentService.applyCoupons(member, request);
+
+        // then
+        assertAll(
+                () -> assertThat(result.getProducts().size()).isEqualTo(2),
+                () -> assertThat(result.getDeliveryPrice().getDiscountPrice()).isEqualTo(0)
+        );
+    }
+
+    @DisplayName("결제를 한다.")
+    @Test
+    void pay() {
+        // given
+        Member member = createMember();
+        PaymentRequest req = new PaymentRequest(List.of(new ProductIdRequest(1L, 1)), List.of(new CouponIdRequest(1L)));
+        Cart cart = createCart();
+        member.initCoupons(createCoupons());
+        Coupons coupons = new Coupons(List.of(new Coupon(1L, "쿠폰", new PolicyDiscount(1000))));
+
+        given(cartRepository.findCartByMemberId(member.getId())).willReturn(cart);
+        given(couponRepository.findAllByCouponIds(List.of(1L))).willReturn(coupons);
+
+        // when
+        long id = paymentService.pay(member, req);
+
+        // then
+        verify(orderRepository).save(any(), any(OrderResponse.class));
+    }
+}
---
+++ src/test/resources/setData.sql
@@ -0,0 +1,41 @@
+SET FOREIGN_KEY_CHECKS = 0;
+TRUNCATE TABLE order_item_history;
+TRUNCATE TABLE coupon_history;
+TRUNCATE TABLE order_table;
+TRUNCATE TABLE cart_item;
+TRUNCATE TABLE cart;
+TRUNCATE TABLE coupon;
+TRUNCATE TABLE product;
+TRUNCATE TABLE member;
+TRUNCATE TABLE policy;
+SET FOREIGN_KEY_CHECKS = 1;
+
+INSERT INTO policy (id, isPercentage, amount) VALUES (1, true, 0);
+INSERT INTO policy (id, isPercentage, amount) VALUES (2, true, 0);
+INSERT INTO policy (id, isPercentage, amount) VALUES (3, true, 0);
+INSERT INTO policy (id, isPercentage, amount) VALUES (4, true, 0);
+
+INSERT INTO policy (id, isPercentage, amount) VALUES (5, true, 10);
+INSERT INTO policy (id, isPercentage, amount) VALUES (6, false, 2000);
+INSERT INTO policy (id, isPercentage, amount) VALUES (7, false, 3000);
+
+INSERT INTO product (id, name, price, image_url, isOnSale, policy_id) VALUES (1, '치킨', 10000, 'https://images.unsplash.com/photo-1626082927389-6cd097cdc6ec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80', false, 1);
+INSERT INTO product (id, name, price, image_url , isOnSale, policy_id) VALUES (2, '샐러드', 20000, 'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80', false, 2);
+INSERT INTO product (id, name, price, image_url , isOnSale, policy_id) VALUES (3, '피자', 20000, 'https://images.unsplash.com/photo-1595854341625-f33ee10dbf94?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80', false, 3);
+INSERT INTO product (id, name, price, image_url , isOnSale, policy_id) VALUES (4, '보쌈', 20000, 'https://images.unsplash.com/photo-1595854341625-f33ee10dbf94?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80', false, 4);
+
+
+INSERT INTO member (id, email, password) VALUES (1, 'a@a.com', '1234');
+INSERT INTO member (id, email, password) VALUES (2, 'b@b.com', '1234');
+
+INSERT INTO coupon (id, name, policy_id, member_id) VALUES (1, '전체 10% 할인 쿠폰', 5, 1);
+INSERT INTO coupon (id, name, policy_id, member_id) VALUES (2, '전체 2000원 할인 쿠폰', 6, 1);
+INSERT INTO coupon (id, name, policy_id, member_id) VALUES (3, 'DELIVERY_FREE', 7, 1);
+INSERT INTO coupon (id, name, policy_id, member_id) VALUES (4, 'DELIVERY_FREE', 7, 2);
+
+INSERT INTO cart (id, member_id) values ('1', '1');
+INSERT INTO cart (id, member_id) values ('2', '2');
+
+INSERT INTO cart_item (id, cart_id, product_id, quantity) VALUES (1, 1, 1, 10);
+INSERT INTO cart_item (id, cart_id, product_id, quantity) VALUES (2, 1, 2, 10);
+INSERT INTO cart_item (id, cart_id, product_id, quantity) VALUES (3, 1, 3, 10);
