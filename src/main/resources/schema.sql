4ae70299b99f19ad0dc73d49c8659292bf2ee3bb
Combi153 <jcm04@naver.com>
2023-05-29 11:14:32 +0900

    feat: DB Table 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,21 +1,40 @@
-CREATE TABLE product (
+CREATE TABLE product if not exists (
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     price INT NOT NULL,
     image_url VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE member (
+CREATE TABLE member if not exists (
      id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
      email VARCHAR(255) NOT NULL UNIQUE,
-     password VARCHAR(255) NOT NULL
+     password VARCHAR(255) NOT NULL,
+     point BIGINT UNSIGNED NOT NULL
 );
 
-CREATE TABLE cart_item (
+CREATE TABLE cart_item if not exists (
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     member_id BIGINT NOT NULL,
     product_id BIGINT NOT NULL,
     quantity INT NOT NULL,
     FOREIGN KEY (member_id) REFERENCES member(id),
+    FOREIGN KEY (product_id) REFERENCES product(id)
+);
+
+CREATE TABLE order if not exists (
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    member_id BIGINT UNSIGNED NOT NULL,
+    payment BIGINT UNSIGNED NOT NULL,
+    discount_point BIGINT UNSIGNED NOT NULL,
+    order_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (member_id) REFERENCES member(id)
+);
+
+CREATE TABLE order_detail if not exists (
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    order_id BIGINT UNSIGNED NOT NULL,
+    product_id BIGINT UNSIGNED NOT NULL,
+    quantity BIGINT UNSIGNED NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES order(id),
     FOREIGN KEY (product_id) REFERENCES product(id)
 );
