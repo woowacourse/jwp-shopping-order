999c9b1ba3cc2186080adea8865cf646a74f1571
java-saeng <gusdn85554@naver.com>
2023-05-29 15:22:33 +0900

    feat : 데이터베이스 스키마 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,21 +1,39 @@
-CREATE TABLE product (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    price INT NOT NULL,
+CREATE TABLE product
+(
+    id        BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name      VARCHAR(255) NOT NULL,
+    price     BIGINT       NOT NULL,
     image_url VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE member (
-     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-     email VARCHAR(255) NOT NULL UNIQUE,
-     password VARCHAR(255) NOT NULL
+CREATE TABLE member
+(
+    id       BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email    VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE cart_item (
-    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    member_id BIGINT NOT NULL,
+CREATE TABLE cart_item
+(
+    id         BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    member_id  BIGINT NOT NULL,
     product_id BIGINT NOT NULL,
-    quantity INT NOT NULL,
-    FOREIGN KEY (member_id) REFERENCES member(id),
-    FOREIGN KEY (product_id) REFERENCES product(id)
+    quantity   INT    NOT NULL,
+);
+
+CREATE TABLE orders
+(
+    id           BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    member_id    BIGINT NOT NULL,
+    delivery_fee BIGINT NOT NULL
+);
+
+CREATE TABLE order_item
+(
+    id        BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    order_id  BIGINT       NOT NULL,
+    name      varchar(255) NOT NULL,
+    price     BIGINT       NOT NULL,
+    image_url varchar(255) NOT NULL,
+    quantity  INT          NOT NULL
 );
