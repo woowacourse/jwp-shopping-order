898282fed2919172903583b4ade7916e7bac11b8
Be-student <presentsong@naver.com>
2023-05-30 00:41:32 +0900

    feat: 쿠폰에 id 추가

--- src/main/java/coupon/domain/Coupon.java
+++ src/main/java/coupon/domain/Coupon.java
@@ -5,14 +5,20 @@ import coupon.domain.discountpolicy.DiscountPolicy;
 
 public class Coupon {
 
+    private final Long couponId;
     private final CouponName name;
     private final DiscountCondition discountCondition;
     private final DiscountPolicy discountPolicy;
 
-    public Coupon(String name, DiscountCondition discountCondition, DiscountPolicy discountPolicy) {
+    public Coupon(Long couponId, String name, DiscountCondition discountCondition, DiscountPolicy discountPolicy) {
+        this.couponId = couponId;
         this.name = new CouponName(name);
         this.discountCondition = discountCondition;
         this.discountPolicy = discountPolicy;
+    }
+
+    public Coupon(String name, DiscountCondition discountCondition, DiscountPolicy discountPolicy) {
+        this(null, name, discountCondition, discountPolicy);
     }
 
     public Money calculateDiscountAmount(Order order) {
@@ -24,5 +30,9 @@ public class Coupon {
 
     public CouponName getName() {
         return name;
+    }
+
+    public Long getCouponId() {
+        return couponId;
     }
 }
