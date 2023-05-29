29e13c6cf028f81081128d3fd5c1329229afc0f4
Cyma-s <ssol0319@naver.com>
2023-05-29 15:21:22 +0900

    refactor: Request validation 추가

--- src/main/java/cart/dto/CartItemQuantityUpdateRequest.java
+++ src/main/java/cart/dto/CartItemQuantityUpdateRequest.java
@@ -1,9 +1,12 @@
 package cart.dto;
 
+import javax.validation.constraints.Positive;
+
 public class CartItemQuantityUpdateRequest {
+    @Positive(message = "상품 수량은 양수여야 합니다.")
     private int quantity;
 
-    public CartItemQuantityUpdateRequest() {
+    private CartItemQuantityUpdateRequest() {
     }
 
     public CartItemQuantityUpdateRequest(int quantity) {
