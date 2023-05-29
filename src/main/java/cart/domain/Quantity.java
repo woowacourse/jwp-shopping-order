ce033547b0a1ba392e8210d7e655b5d95a8360d8
DongUk <68818952+Kim0914@users.noreply.github.com>
2023-05-29 16:13:16 +0900

    feat: 수량 검증 기능 추가
    Co-authored-by: seokjin8678 <seokjin8678@gmail.com>

--- src/main/java/cart/domain/Quantity.java
+++ src/main/java/cart/domain/Quantity.java
@@ -3,14 +3,21 @@ package cart.domain;
 import java.util.Objects;
 
 public class Quantity {
-    private final int value;
+    private final int amount;
 
-    public Quantity(int value) {
-        this.value = value;
+    public Quantity(int amount) {
+        validate(amount);
+        this.amount = amount;
     }
 
-    public int getValue() {
-        return value;
+    private void validate(int amount) {
+        if (amount < 1) {
+            throw new IllegalArgumentException("수량은 최소 1개 이상 부터 가능합니다.");
+        }
+    }
+
+    public int getAmount() {
+        return amount;
     }
 
     @Override
@@ -22,11 +29,11 @@ public class Quantity {
             return false;
         }
         Quantity quantity = (Quantity) o;
-        return getValue() == quantity.getValue();
+        return getAmount() == quantity.getAmount();
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getValue());
+        return Objects.hash(getAmount());
     }
 }
---
+++ src/test/java/cart/domain/QuantityTest.java
@@ -0,0 +1,32 @@
+package cart.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+@DisplayNameGeneration(ReplaceUnderscores.class)
+@SuppressWarnings("NonAsciiCharacters")
+class QuantityTest {
+
+    @Test
+    void 수량이_정상적으로_생성된다() {
+        int amount = 100;
+
+        Quantity quantity = new Quantity(amount);
+
+        assertThat(quantity.getAmount()).isEqualTo(amount);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0})
+    void 수량에_1보다_작은_수가_할당되면_예외가_발생한다(int amount) {
+        assertThatThrownBy(() -> new Quantity(amount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("수량은 최소 1개 이상 부터 가능합니다.");
+    }
+}
