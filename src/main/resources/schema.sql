a34313d3f1823c938e8579015d69dcc79a5c7a8a
seokhwan-an <asok369258@naver.com>
2023-05-30 14:57:29 +0900

    chore: db 테이블 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,21 +1,50 @@
-CREATE TABLE product (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    price INT NOT NULL,
-    image_url VARCHAR(255) NOT NULL
+CREATE TABLE IF NOT EXISTS product
+(
+    id            BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name          VARCHAR(255) NOT NULL,
+    price         INT          NOT NULL,
+    image_url     VARCHAR(255) NOT NULL,
+    is_discounted TINYINT      NOT NULL,
+    discount_rate INT(3)       NOT NULL
 );
 
-CREATE TABLE member (
-     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-     email VARCHAR(255) NOT NULL UNIQUE,
-     password VARCHAR(255) NOT NULL
+CREATE TABLE IF NOT EXISTS member
+(
+    id                    BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email                 VARCHAR(255) NOT NULL UNIQUE,
+    password              VARCHAR(255) NOT NULL,
+    rank                  VARCHAR(255) NOT NULL,
+    total_purchase_amount BIGINT       NOT NULL
 );
 
-CREATE TABLE cart_item (
-    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    member_id BIGINT NOT NULL,
+CREATE TABLE IF NOT EXISTS cart_item
+(
+    id         BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    quantity   INT    NOT NULL,
+    member_id  BIGINT NOT NULL,
     product_id BIGINT NOT NULL,
-    quantity INT NOT NULL,
-    FOREIGN KEY (member_id) REFERENCES member(id),
-    FOREIGN KEY (product_id) REFERENCES product(id)
+    FOREIGN KEY (member_id) REFERENCES member (id),
+    FOREIGN KEY (product_id) REFERENCES product (id)
 );
+
+CREATE TABLE IF NOT EXISTS orders
+(
+    id                     BIGINT   NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    total_price            BIGINT   NOT NULL,
+    discounted_total_price BIGINT   NOT NULL,
+    delivery_fee           INT      NOT NULL,
+    ordered_at             DATETIME NOT NULL,
+    member_id              BIGINT   NOT NULL,
+    FOREIGN KEY (member_id) REFERENCES member (id)
+);
+
+CREATE TABLE IF NOT EXISTS ordered_item
+(
+    id               BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    product_name     VARCHAR(255) NOT NULL,
+    product_price    INT          NOT NULL,
+    product_image    VARCHAR(255) NOT NULL,
+    product_quantity INT          NOT NULL,
+    order_id         BIGINT       NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES orders (id)
+)
