6053b0ef8803fa6c6af7afe6028c6ffc1cfa83cc
aiaiaiai1 <cvc100326@naver.com>
2023-05-30 13:52:27 +0900

    feat : Member가 가진 coupon 목록 확인 기능 추가

--- src/main/java/cart/dto/CouponResponse.java
+++ src/main/java/cart/dto/CouponResponse.java
@@ -5,14 +5,16 @@ import cart.domain.Coupon;
 public class CouponResponse {
     private final Long id;
     private final String name;
+    private final int discountPercent;
 
-    public CouponResponse(Long id, String name) {
+    public CouponResponse(Long id, String name, int discountPercent) {
         this.id = id;
         this.name = name;
+        this.discountPercent = discountPercent;
     }
 
     public static CouponResponse of(Coupon coupon) {
-        return new CouponResponse(coupon.getId(), coupon.getName());
+        return new CouponResponse(coupon.getId(), coupon.getName(), coupon.getDiscountPercent().getValue());
     }
 
     public Long getId() {
@@ -21,5 +23,9 @@ public class CouponResponse {
 
     public String getName() {
         return name;
+    }
+
+    public int getDiscountPercent() {
+        return discountPercent;
     }
 }
---
+++ src/main/java/cart/service/CouponService.java
@@ -0,0 +1,27 @@
+package cart.service;
+
+import cart.dao.CouponDao;
+import cart.domain.Coupon;
+import cart.domain.Member;
+import cart.dto.CouponResponse;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CouponService {
+
+    private final CouponDao couponDao;
+
+    public CouponService(CouponDao couponDao) {
+        this.couponDao = couponDao;
+    }
+
+    public List<CouponResponse> findByMember(Member member) {
+        List<Coupon> coupons = couponDao.findByMemberId(member.getId());
+        return coupons.stream()
+                .map(CouponResponse::of)
+                .collect(Collectors.toList());
+    }
+}
