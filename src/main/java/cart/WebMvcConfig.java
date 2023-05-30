6ff7ea02fe629a9d92842af1c00d697a4279a734
JunH <tjdtls690@gmail.com>
2023-05-30 10:51:50 +0900

    :sparkles: feat : 인메모리 DB 설정 및 CORS 설정

--- src/main/java/cart/WebMvcConfig.java
+++ src/main/java/cart/WebMvcConfig.java
@@ -4,6 +4,7 @@ import cart.dao.MemberDao;
 import cart.ui.MemberArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -11,11 +12,23 @@ import java.util.List;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
     private final MemberDao memberDao;
-
+    
     public WebMvcConfig(MemberDao memberDao) {
         this.memberDao = memberDao;
     }
-
+    
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins(
+                        "https://kyw0716.github.io",
+                        "https://gyeongza.github.io"
+                )
+                .exposedHeaders("Location")
+                .allowCredentials(true);
+    }
+    
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new MemberArgumentResolver(memberDao));
