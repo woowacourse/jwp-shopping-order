740c6dfe9dbfac8a68fd4c8795eb450b0c8d600a
hum02 <ehdgma02@gmail.com>
2023-05-30 12:01:21 +0900

    data.sql test시에 적용

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,17 +1,17 @@
-CREATE TABLE product if not exists (
+CREATE TABLE if not exists product (
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     price INT NOT NULL,
     image_url VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE member if not exists (
+CREATE TABLE if not exists member (
      id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
      email VARCHAR(255) NOT NULL UNIQUE,
      password VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE cart_item if not exists (
+CREATE TABLE if not exists cart_item(
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     member_id BIGINT NOT NULL,
     product_id BIGINT NOT NULL,
