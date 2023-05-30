6fad44dd026f5130d084b3a987fe944b9b1a4326
70825 <hello70825@gmail.com>
2023-05-30 21:51:30 +0900

    feat: 로그 기능 추가

--- src/main/java/cart/ui/ControllerExceptionHandler.java
+++ src/main/java/cart/ui/ControllerExceptionHandler.java
@@ -3,6 +3,8 @@ package cart.ui;
 import cart.dto.ErrorResponse;
 import cart.exception.AuthenticationException;
 import cart.exception.CartItemException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,18 +17,34 @@ import java.util.stream.Collectors;
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
+    private final static String LOG_FORMAT = "Class : {}, Message : {}";
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @ExceptionHandler(AuthenticationException.class)
     public ResponseEntity<Void> handlerAuthenticationException(final AuthenticationException e) {
+        logger.error(LOG_FORMAT,
+                e.getClass().getSimpleName(),
+                e.getMessage());
+
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 
     @ExceptionHandler(CartItemException.IllegalMember.class)
     public ResponseEntity<Void> handleException(final CartItemException.IllegalMember e) {
+        logger.error(LOG_FORMAT,
+                e.getClass().getSimpleName(),
+                e.getMessage());
+
         return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
+        logger.error(LOG_FORMAT,
+                e.getClass().getSimpleName(),
+                e.getMessage());
+
         final String errorMessage = e.getBindingResult().getFieldErrors().stream()
                 .map(DefaultMessageSourceResolvable::getDefaultMessage)
                 .collect(Collectors.joining(System.lineSeparator()));
