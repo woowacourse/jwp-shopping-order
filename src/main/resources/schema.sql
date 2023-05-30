5764968863e1d3aed7b22780158403f6c8d5e429
2014012606 <2014012606@hanyang.ac.kr>
2023-05-30 17:48:32 +0900

    chore: db 테이블 초기화

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,25 +1,45 @@
-CREATE TABLE if not exists product (
+CREATE TABLE if not exists product
+(
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     price INT NOT NULL,
     image_url VARCHAR(255) NOT NULL,
-    point_ratio BIGINT NOT NULL,
+    point_ratio DOUBLE NOT NULL,
     point_available BOOLEAN
 );
 
-CREATE TABLE if not exists member (
-     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-     email VARCHAR(255) NOT NULL UNIQUE,
-     password VARCHAR(255) NOT NULL,
-     point INT NOT NULL,
-     CONSTRAINT only_positive_point check (point >= 0)
+CREATE TABLE if not exists member
+(
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    point BIGINT NOT NULL
 );
 
-CREATE TABLE if not exists cart_item (
+CREATE TABLE if not exists cart_item
+(
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     member_id BIGINT NOT NULL,
     product_id BIGINT NOT NULL,
-    quantity INT NOT NULL,
-    FOREIGN KEY (member_id) REFERENCES member(id),
-    FOREIGN KEY (product_id) REFERENCES product(id)
+    quantity INT NOT NULL
+);
+
+CREATE TABLE if not exists orders
+(
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    member_id BIGINT NOT NULL,
+    original_price BIGINT NOT NULL,
+    used_point BIGINT NOT NULL,
+    point_to_add BIGINT NOT NULL
+);
+
+CREATE TABLE if not exists order_info
+(
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    order_id BIGINT NOT NULL,
+    product_id BIGINT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    price BIGINT NOT NULL,
+    image_url VARCHAR(255) NOT NULL,
+    quantity BIGINT NOT NULL
 );
