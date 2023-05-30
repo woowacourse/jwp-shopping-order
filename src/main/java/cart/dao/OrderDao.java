6d8d9d2a010535a2772c7282f61a5a018a2ccd1e
kiarakim <kiarakim413@gmail.com>
2023-05-30 16:57:12 +0900

    refactor: 컨벤션 준수

--- src/main/java/cart/dao/OrderDao.java
+++ src/main/java/cart/dao/OrderDao.java
@@ -3,6 +3,7 @@ package cart.dao;
 import cart.domain.CartItem;
 import cart.domain.Member;
 import cart.entity.OrderEntity;
+
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
@@ -16,55 +17,55 @@ import java.util.Map;
 @Component
 public class OrderDao {
 
-    private static final RowMapper<OrderEntity> orderRowMapper = (rs, rowNum) ->
-            new OrderEntity(rs.getLong("id"),
-                    rs.getLong("member_id"),
-                    rs.getInt("used_points"),
-                    rs.getInt("saving_rate"));
+	private static final RowMapper<OrderEntity> orderRowMapper = (rs, rowNum) ->
+		new OrderEntity(rs.getLong("id"),
+			rs.getLong("member_id"),
+			rs.getInt("used_points"),
+			rs.getInt("saving_rate"));
 
-    private final JdbcTemplate jdbcTemplate;
-    private final SimpleJdbcInsert orderInsert;
-    private final SimpleJdbcInsert orderItemInsert;
+	private final JdbcTemplate jdbcTemplate;
+	private final SimpleJdbcInsert orderInsert;
+	private final SimpleJdbcInsert orderItemInsert;
 
+	public OrderDao(final JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+		this.orderInsert = new SimpleJdbcInsert(jdbcTemplate)
+			.withTableName("orders")
+			.usingGeneratedKeyColumns("id");
+		this.orderItemInsert = new SimpleJdbcInsert(jdbcTemplate)
+			.withTableName("order_item")
+			.usingGeneratedKeyColumns("id");
+	}
 
-    public OrderDao(final JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-        this.orderInsert = new SimpleJdbcInsert(jdbcTemplate)
-                .withTableName("orders")
-                .usingGeneratedKeyColumns("id");
-        this.orderItemInsert = new SimpleJdbcInsert(jdbcTemplate)
-                .withTableName("order_item")
-                .usingGeneratedKeyColumns("id");
-    }
+	public Long createOrder(final int usedPoints, final List<CartItem> cartItems, final int savingRate,
+		final Member member) {
 
+		final long id = orderInsert.executeAndReturnKey(
+			Map.of("member_id", member.getId(), "used_points", usedPoints, "saving_rate", savingRate)).longValue();
 
-    public Long createOrder(final int usedPoints, final List<CartItem> cartItems, final int savingRate, final Member member) {
+		List<Map<String, Object>> batchValues = new ArrayList<>();
+		for (final CartItem cartItem : cartItems) {
+			Map<String, Object> batchMap = new HashMap<>();
+			batchMap.put("order_id", id);
+			batchMap.put("product_id", cartItem.getProduct().getId());
+			batchMap.put("product_name", cartItem.getProduct().getName());
+			batchMap.put("product_price", cartItem.getProduct().getPrice());
+			batchMap.put("product_quantity", cartItem.getQuantity());
+			batchMap.put("product_image_url", cartItem.getProduct().getImageUrl());
+			batchValues.add(batchMap);
+		}
+		orderItemInsert.executeBatch(batchValues.toArray(new Map[0]));
 
-        final long id = orderInsert.executeAndReturnKey(Map.of("member_id", member.getId(), "used_points", usedPoints, "saving_rate", savingRate)).longValue();
+		return id;
+	}
 
-        List<Map<String, Object>> batchValues = new ArrayList<>();
-        for (final CartItem cartItem : cartItems) {
-            Map<String, Object> batchMap = new HashMap<>();
-            batchMap.put("order_id", id);
-            batchMap.put("product_id", cartItem.getProduct().getId());
-            batchMap.put("product_name", cartItem.getProduct().getName());
-            batchMap.put("product_price", cartItem.getProduct().getPrice());
-            batchMap.put("product_quantity", cartItem.getQuantity());
-            batchMap.put("product_image_url", cartItem.getProduct().getImageUrl());
-            batchValues.add(batchMap);
-        }
-        orderItemInsert.executeBatch(batchValues.toArray(new Map[0]));
+	public OrderEntity findById(final Long id, final Long memberId) {
+		String sql = "SELECT * FROM orders WHERE id = ? AND member_id = ?";
+		return jdbcTemplate.queryForObject(sql, orderRowMapper, id, memberId);
+	}
 
-        return id;
-    }
-
-    public OrderEntity findById(final Long id, final Long memberId) {
-        String sql = "SELECT * FROM orders WHERE id = ? AND member_id = ?";
-        return jdbcTemplate.queryForObject(sql, orderRowMapper, id, memberId);
-    }
-
-    public List<OrderEntity> findAll(final Long memberId) {
-        String sql = "SELECT * FROM orders WHERE member_id = ?";
-        return jdbcTemplate.query(sql, orderRowMapper, memberId);
-    }
+	public List<OrderEntity> findAll(final Long memberId) {
+		String sql = "SELECT * FROM orders WHERE member_id = ?";
+		return jdbcTemplate.query(sql, orderRowMapper, memberId);
+	}
 }
--- src/main/java/cart/dao/OrderItemDao.java
+++ src/main/java/cart/dao/OrderItemDao.java
@@ -1,6 +1,7 @@
 package cart.dao;
 
 import cart.entity.OrderItemEntity;
+
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
@@ -9,25 +10,25 @@ import java.util.List;
 
 @Component
 public class OrderItemDao {
-    private static final RowMapper<OrderItemEntity> orderItemRowMapper = ((rs, rowNum) ->
-            new OrderItemEntity(
-                    rs.getLong("id"),
-                    rs.getLong("order_id"),
-                    rs.getLong("product_id"),
-                    rs.getString("product_name"),
-                    rs.getInt("product_price"),
-                    rs.getInt("product_quantity"),
-                    rs.getString("product_image_url")
-            ));
+	private static final RowMapper<OrderItemEntity> orderItemRowMapper = ((rs, rowNum) ->
+		new OrderItemEntity(
+			rs.getLong("id"),
+			rs.getLong("order_id"),
+			rs.getLong("product_id"),
+			rs.getString("product_name"),
+			rs.getInt("product_price"),
+			rs.getInt("product_quantity"),
+			rs.getString("product_image_url")
+		));
 
-    private final JdbcTemplate jdbcTemplate;
+	private final JdbcTemplate jdbcTemplate;
 
-    public OrderItemDao(final JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
+	public OrderItemDao(final JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
 
-    public List<OrderItemEntity> findByOrderId(final Long orderId) {
-        final String sql = "SELECT * FROM order_item WHERE order_id = ?";
-        return jdbcTemplate.query(sql, orderItemRowMapper, orderId);
-    }
+	public List<OrderItemEntity> findByOrderId(final Long orderId) {
+		final String sql = "SELECT * FROM order_item WHERE order_id = ?";
+		return jdbcTemplate.query(sql, orderItemRowMapper, orderId);
+	}
 }
