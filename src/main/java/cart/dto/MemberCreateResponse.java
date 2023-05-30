1a88a6689b5c4783cb0ffd699095b0058087ddb3
gitchan <eunkeeeee@gmail.com>
2023-05-30 14:35:37 +0900

    fix(MemberCreateResponse): 클래스 생성

---
+++ src/main/java/cart/dto/MemberCreateResponse.java
@@ -0,0 +1,32 @@
+package cart.dto;
+
+import cart.domain.Member;
+
+public class MemberCreateResponse {
+
+    private final Long id;
+    private final String email;
+    private final String password;
+
+    public MemberCreateResponse(final Long id, final String email, final String password) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }
+
+    public static MemberCreateResponse from(final Member member) {
+        return new MemberCreateResponse(member.getId(), member.getEmail(), member.getPassword());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
