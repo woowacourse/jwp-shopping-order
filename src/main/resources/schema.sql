1dd6c840fa3bc645144642d9f32f3b823a3375a7
hum02 <ehdgma02@gmail.com>
2023-05-30 11:56:12 +0900

    db 생성 if not exists로 수정, data.sql 삭제

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,17 +1,17 @@
-CREATE TABLE product (
+CREATE TABLE product if not exists (
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     price INT NOT NULL,
     image_url VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE member (
+CREATE TABLE member if not exists (
      id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
      email VARCHAR(255) NOT NULL UNIQUE,
      password VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE cart_item (
+CREATE TABLE cart_item if not exists (
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     member_id BIGINT NOT NULL,
     product_id BIGINT NOT NULL,
