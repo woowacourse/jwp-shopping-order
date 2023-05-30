5f5aaf9322d88182749e40eae0b82394dff5554d
yoondgu <doyou2ing@naver.com>
2023-05-30 15:53:50 +0900

    refactor: 커스텀 예외에 대한 Exception Handler 메서드 추가

--- src/main/java/cart/infrastructure/ControllerExceptionHandler.java
+++ src/main/java/cart/infrastructure/ControllerExceptionHandler.java
@@ -1,7 +1,9 @@
 package cart.infrastructure;
 
+import cart.dto.ErrorResponse;
 import cart.exception.AuthenticationException;
 import cart.exception.CartItemException;
+import cart.exception.MoneyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,13 +14,32 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class ControllerExceptionHandler {
 
     @ExceptionHandler(AuthenticationException.class)
-    public ResponseEntity<Void> handlerAuthenticationException(final AuthenticationException e) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    public ResponseEntity<ErrorResponse> handlerAuthenticationException(final AuthenticationException exception) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ErrorResponse(exception.getMessage()));
     }
 
     @ExceptionHandler(CartItemException.IllegalMember.class)
-    public ResponseEntity<Void> handleException(final CartItemException.IllegalMember e) {
-        return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+    public ResponseEntity<ErrorResponse> handleCartItemException(final CartItemException.IllegalMember exception) {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(new ErrorResponse(exception.getMessage()));
     }
 
+    @ExceptionHandler(CartItemException.IllegalId.class)
+    public ResponseEntity<ErrorResponse> handleCartItemException(final CartItemException.IllegalId exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(exception.getMessage()));
+    }
+
+    @ExceptionHandler(CartItemException.IllegalQuantity.class)
+    public ResponseEntity<ErrorResponse> handleCartItemException(final CartItemException.IllegalQuantity exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(exception.getMessage()));
+    }
+
+    @ExceptionHandler(MoneyException.IllegalValue.class)
+    public ResponseEntity<ErrorResponse> handleMoneyException(final MoneyException.IllegalValue exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(exception.getMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(final Exception exception) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse(exception.getMessage()));
+    }
 }
