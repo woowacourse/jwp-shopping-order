ef12b94aaa3176320902a7f83a29b33a48e0bac4
gitchan <eunkeeeee@gmail.com>
2023-05-30 14:51:27 +0900

    feat(schema.sql): 테이블 구조 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,33 +1,24 @@
 CREATE TABLE product
 (
-    id         BIGINT PRIMARY KEY AUTO_INCREMENT,
-    name       VARCHAR(255) NOT NULL,
-    price      INT          NOT NULL,
-    image_url  VARCHAR(255) NOT NULL
-
---     created_at timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP,
---     updated_at timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    id        BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name      VARCHAR(255) NOT NULL,
+    price     INT          NOT NULL,
+    image_url VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE member
 (
-    id         BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    email      VARCHAR(255) NOT NULL UNIQUE,
-    password   VARCHAR(255) NOT NULL
-
---     created_at timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP,
---     updated_at timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    id       BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email    VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE cart_item
 (
-    id         BIGINT    NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    member_id  BIGINT    NOT NULL,
-    product_id BIGINT    NOT NULL,
-    quantity   INT       NOT NULL,
-
---     created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
---     updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    id         BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    member_id  BIGINT NOT NULL,
+    product_id BIGINT NOT NULL,
+    quantity   INT    NOT NULL,
 
     FOREIGN KEY (member_id) REFERENCES member (id),
     FOREIGN KEY (product_id) REFERENCES product (id)
@@ -35,21 +26,25 @@ CREATE TABLE cart_item
 
 CREATE TABLE order_history
 (
-    id         BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    member_id  BIGINT NOT NULL,
---     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    id          BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    member_id   BIGINT NOT NULL,
+    used_point  INT    NOT NULL,
+    order_price INT    NOT NULL,
 
     FOREIGN KEY (member_id) REFERENCES member (id)
 );
 
 CREATE TABLE order_product
 (
-    id               BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    order_history_id BIGINT NOT NULL,
-    cart_item_id     BIGINT NOT NULL,
+    id               BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    order_history_id BIGINT       NOT NULL,
+    product_id       BIGINT       NOT NULL,
+    name             VARCHAR(255) NOT NULL,
+    price            INT          NOT NULL,
+    image_url        VARCHAR(255) NOT NULL,
+    quantity         INT          NOT NULL,
 
-    FOREIGN KEY (order_history_id) REFERENCES order_history (id),
-    FOREIGN KEY (cart_item_id) REFERENCES cart_item (id)
+    FOREIGN KEY (order_history_id) REFERENCES order_history (id)
 );
 
 CREATE TABLE point
