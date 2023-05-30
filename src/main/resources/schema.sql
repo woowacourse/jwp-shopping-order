c00f5f761a53c22aa2fa783a8c8846526abed488
hanueleee <ezzeezz@naver.com>
2023-05-30 12:09:27 +0900

    feat(db): 주문 관련 테이블들 추가 (orders, order_item, payment)

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -30,13 +30,13 @@ CREATE TABLE cart_item
     member_id  BIGINT    NOT NULL,
     product_id BIGINT    NOT NULL,
     quantity   INT       NOT NULL,
-    FOREIGN KEY (member_id) REFERENCES member (id),
-    FOREIGN KEY (product_id) REFERENCES product (id),
     created_at TIMESTAMP NOT NULL
         DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL
         DEFAULT CURRENT_TIMESTAMP
-        ON UPDATE CURRENT_TIMESTAMP
+        ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (member_id) REFERENCES member (id),
+    FOREIGN KEY (product_id) REFERENCES product (id)
 );
 
 CREATE TABLE point
@@ -48,5 +48,44 @@ CREATE TABLE point
         DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL
         DEFAULT CURRENT_TIMESTAMP
-        ON UPDATE CURRENT_TIMESTAMP
-)
+        ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (member_id) REFERENCES member (id)
+);
+
+CREATE TABLE orders
+(
+    id         BIGINT    NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    member_id  BIGINT    NOT NULL,
+    created_at TIMESTAMP NOT NULL
+        DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL
+        DEFAULT CURRENT_TIMESTAMP
+        ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (member_id) REFERENCES member (id)
+);
+
+CREATE TABLE order_item
+(
+    id         BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    orders_id   BIGINT NOT NULL,
+    product_id BIGINT NOT NULL,
+    quantity   INT       NOT NULL,
+    created_at TIMESTAMP NOT NULL
+        DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL
+        DEFAULT CURRENT_TIMESTAMP
+        ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (orders_id) REFERENCES orders (id),
+    FOREIGN KEY (product_id) REFERENCES product (id)
+);
+
+CREATE TABLE payment
+(
+    id         BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    orders_id   BIGINT NOT NULL,
+    total_product_price BIGINT NOT NULL,
+    total_delivery_price BIGINT NOT NULL,
+    use_point BIGINT NOT NULL DEFAULT 0,
+    total_payment BIGINT NOT NULL,
+    FOREIGN KEY (orders_id) REFERENCES orders (id)
+);
