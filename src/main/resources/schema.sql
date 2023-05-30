b0c3d8c13f8c3036236c36554a26319297275093
eunbii0213 <eunbii0213@naver.com>
2023-05-30 14:54:26 +0900

    docs: 기능 목록 작성

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,14 +1,18 @@
 CREATE TABLE product (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     price INT NOT NULL,
-    image_url VARCHAR(255) NOT NULL
+    image VARCHAR(255) NOT NULL,
+    isDiscounted TINYINT NOT NULL,
+    discount_rate INT(3)
 );
 
 CREATE TABLE member (
      id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
      email VARCHAR(255) NOT NULL UNIQUE,
-     password VARCHAR(255) NOT NULL
+     password VARCHAR(255) NOT NULL,
+     rank VARCHAR(255),
+     total_price BIGINT NOT NULL
 );
 
 CREATE TABLE cart_item (
@@ -18,4 +22,24 @@ CREATE TABLE cart_item (
     quantity INT NOT NULL,
     FOREIGN KEY (member_id) REFERENCES member(id),
     FOREIGN KEY (product_id) REFERENCES product(id)
+);
+
+CREATE TABLE orders (
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    member_id BIGINT NOT NULL,
+    total_price BIGINT NOT NULL,
+    ordered_at DATETIME NOT NULL default current_timestamp,
+    delivery_fee INT NOT NULL,
+    discounted_total_price INT NOT NULL,
+    FOREIGN KEY (member_id) REFERENCES member(id)
+);
+
+CREATE TABLE ordered_item (
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    orders_id BIGINT NOT NULL,
+    product_name VARCHAR(255) NOT NULL,
+    product_price INT NOT NULL,
+    product_image VARCHAR(255) NOT NULL,
+    product_quantity INT NOT NULL,
+    FOREIGN KEY (orders_id) REFERENCES orders(id)
 );
