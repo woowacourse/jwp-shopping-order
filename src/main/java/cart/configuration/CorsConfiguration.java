0fa82cd7edfa7d92e3275fd02f9bf760a1f44d4c
xxeol2 <jhj01177@naver.com>
2023-05-30 16:17:51 +0900

    feat: cors 설정

---
+++ src/main/java/cart/configuration/CorsConfiguration.java
@@ -0,0 +1,18 @@
+package cart.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .maxAge(3000);
+    }
+}
