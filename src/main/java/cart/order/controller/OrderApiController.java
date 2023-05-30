c28e28a7efdbc4309ed0a64dade9751a42683880
java-saeng <gusdn85554@naver.com>
2023-05-30 13:57:17 +0900

    feat : 주문 목록 조회 API 구현

--- src/main/java/cart/order/controller/OrderApiController.java
+++ src/main/java/cart/order/controller/OrderApiController.java
@@ -2,9 +2,13 @@ package cart.order.controller;
 
 import cart.member.domain.Member;
 import cart.order.application.OrderCommandService;
+import cart.order.application.OrderQueryService;
+import cart.order.application.dto.OrderResponse;
 import cart.order.application.dto.RegisterOrderRequest;
 import java.net.URI;
+import java.util.List;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderApiController {
 
   private final OrderCommandService orderCommandService;
+  private final OrderQueryService orderQueryService;
 
-  public OrderApiController(final OrderCommandService orderCommandService) {
+  public OrderApiController(
+      final OrderCommandService orderCommandService,
+      final OrderQueryService orderQueryService
+  ) {
     this.orderCommandService = orderCommandService;
+    this.orderQueryService = orderQueryService;
   }
 
   @PostMapping("/orders")
@@ -26,5 +35,10 @@ public class OrderApiController {
     Long savedId = orderCommandService.registerOrder(member, registerOrderRequest);
 
     return ResponseEntity.created(URI.create("/orders/" + savedId)).build();
+  }
+
+  @GetMapping("/orders")
+  public ResponseEntity<List<OrderResponse>> showOrders(final Member member) {
+    return ResponseEntity.ok(orderQueryService.searchOrders(member));
   }
 }
--- src/test/java/cart/order/controller/OrderApiControllerTest.java
+++ src/test/java/cart/order/controller/OrderApiControllerTest.java
@@ -1,11 +1,14 @@
 package cart.order.controller;
 
 import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import cart.helper.IntegrationTestHelper;
 import cart.member.dao.MemberDao;
 import cart.member.domain.Member;
+import cart.order.application.dto.OrderResponse;
 import cart.order.application.dto.RegisterOrderRequest;
 import java.math.BigDecimal;
 import java.util.List;
@@ -47,5 +50,31 @@ class OrderApiControllerTest extends IntegrationTestHelper {
 
     //then
     assertNotNull(location);
+  }
+
+  @Test
+  @DisplayName("showOrders() : 사용자가 주문한 주문 목록들을 성공적으로 조회한다면 200 OK 를 반환한다.")
+  void test_showOrders() throws Exception {
+    //given
+    final Member member = memberDao.getMemberById(1L);
+
+    //when
+    List<OrderResponse> orderRespons =
+        given().log().all()
+            .auth().preemptive().basic(member.getEmail(), member.getPassword())
+            .when()
+            .get("/orders")
+            .then()
+            .log().all()
+            .statusCode(HttpStatus.OK.value())
+            .extract()
+            .jsonPath()
+            .getList(".", OrderResponse.class);
+
+    //then
+    assertAll(
+        () -> assertEquals(1, orderRespons.size()),
+        () -> assertEquals(3, orderRespons.get(0).getProducts().size())
+    );
   }
 }
