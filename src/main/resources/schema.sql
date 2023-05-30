b3d73962f7836163953632fce64ce09f47deb033
hectick <ch.yeondev@gmail.com>
2023-05-30 12:13:27 +0900

    feat: 테이블 구조 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,17 +1,18 @@
-CREATE TABLE product (
+create table if not exists product
+(
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     price INT NOT NULL,
     image_url VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE member (
-     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-     email VARCHAR(255) NOT NULL UNIQUE,
-     password VARCHAR(255) NOT NULL
+create table if not exists member (
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE cart_item (
+create table if not exists cart_item (
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     member_id BIGINT NOT NULL,
     product_id BIGINT NOT NULL,
@@ -20,28 +21,30 @@ CREATE TABLE cart_item (
     FOREIGN KEY (product_id) REFERENCES product(id)
 );
 
-CREATE TABLE orders (
+create table if not exists orders (
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	member_id BIGINT NOT NULL,
-	total_price INT NOT NULL,
-	FOREIGN KEY (member_id) REFERENCES member(id)
+    member_id BIGINT NOT NULL,
+    total_price INT NOT NULL,
+    FOREIGN KEY (member_id) REFERENCES member(id)
 );
 
-CREATE TABLE order_item (
+create table if not exists order_item (
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    order_id BIGINT NOT NULL,
-    product_id BIGINT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    price INT NOT NULL,
+    image_url VARCHAR(255) NOT NULL,
     quantity INT NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES orders(id),
-    FOREIGN KEY (product_id) REFERENCES product(id)
+    product_id BIGINT NOT NULL,
+    orders_id BIGINT NOT NULL,
+    FOREIGN KEY (orders_id) REFERENCES orders(id)
 );
 
-CREATE TABLE shipping_fee (
+create table if not exists shipping_fee (
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	fee int NOT NULL
+    fee int NOT NULL
 );
 
-CREATE TABLE shipping_discount_policy (
+create table if not exists shipping_discount_policy (
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	threshold BIGINT NOT NULL
+    threshold BIGINT NOT NULL
 );
