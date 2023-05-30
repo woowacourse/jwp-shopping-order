2a5dd30b69d0f37b8e8908bab1ec58fcaf9f04e6
dooboocookie <bkh3030@gmail.com>
2023-05-30 15:57:26 +0900

    feat: Member nickname 추가

--- src/main/java/cart/dao/MemberCouponDao.java
+++ src/main/java/cart/dao/MemberCouponDao.java
@@ -5,10 +5,12 @@ import cart.dao.entity.MemberCouponEntity;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class MemberCouponDao {
     private final JdbcTemplate jdbcTemplate;
 
