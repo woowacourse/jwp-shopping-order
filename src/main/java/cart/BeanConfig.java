b69e32447cc6fbb723a05533086f8b607260970a
jaehee329 <jjhs9803@gmail.com>
2023-05-30 20:50:54 +0900

    feat: 포인트 계산 로직 구현

--- src/main/java/cart/BeanConfig.java
+++ src/main/java/cart/BeanConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 
 import cart.domain.Order;
 import cart.domain.Paginator;
+import cart.domain.PointCalculator;
 
 @Configuration
 public class BeanConfig {
@@ -12,5 +13,10 @@ public class BeanConfig {
     @Bean
     public Paginator<Order> orderPaginator() {
         return new Paginator<>(10, Order::getOrderAt);
+    }
+
+    @Bean
+    public PointCalculator pointCalculator() {
+        return new PointCalculator();
     }
 }
