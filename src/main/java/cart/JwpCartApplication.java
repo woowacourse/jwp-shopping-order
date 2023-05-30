40fe5bafe1df49b731c98b47379048336fcbf020
shin-mallang <huipulci1@naver.com>
2023-05-30 18:13:03 +0900

    !!

--- src/main/java/cart/JwpCartApplication.java
+++ src/main/java/cart/JwpCartApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JwpCartApplication {
 
     public static void main(String[] args) {
-        System.out.println("!!!");
+        System.out.println("!!f!");
         SpringApplication.run(JwpCartApplication.class, args);
     }
 
