b0360f36751c46c8498f9ecd12bd41b29a80db0e
splitube <voicesplit@daum.net>
2023-05-30 18:34:41 +0900

    refactor: https 백서버 allowedOrigin 추가

--- src/main/java/cart/WebMvcConfig.java
+++ src/main/java/cart/WebMvcConfig.java
@@ -2,15 +2,15 @@ package cart;
 
 import cart.dao.MemberDao;
 import cart.ui.MemberArgumentResolver;
+import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.List;
-
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
     private final MemberDao memberDao;
 
     public WebMvcConfig(MemberDao memberDao) {
@@ -20,8 +20,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000", "http://218.39.176.142:3000", "https://react-shopping-cart-woowa.netlify.app/")
-            .allowedMethods("GET","POST","PATCH","PUT","DELETE","OPTION")
+            .allowedOrigins(
+                "http://localhost:3000",
+                "http://218.39.176.142:3000",
+                "https://react-shopping-cart-woowa.netlify.app/",
+                "https://woowasplit.shop/"
+            )
+            .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE", "OPTION")
             .allowedHeaders("*")
             .allowCredentials(true);
     }
