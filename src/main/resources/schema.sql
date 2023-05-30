90e0ccd249be6ada02ae8f70fa908a9056e9251f
jeomxon <jeomxon@gmail.com>
2023-05-30 15:18:06 +0900

    chore: 테이블 더미 데이터 삭제 및 테이블 설계 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,28 +1,50 @@
-drop table if exists cart_item;
-drop table if exists product;
-drop table if exists member;
-
 CREATE TABLE if not exists product
 (
-    id        BIGINT PRIMARY KEY AUTO_INCREMENT,
-    name      VARCHAR(255) NOT NULL,
-    price     INT          NOT NULL,
-    image_url VARCHAR(255) NOT NULL
+    id            BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name          VARCHAR(255) NOT NULL,
+    price         INT          NOT NULL,
+    image_url     VARCHAR(255) NOT NULL,
+    is_discounted tinyint      not null,
+    discount_rate int(3)       not null
 );
 
 CREATE TABLE if not exists member
 (
-    id       BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    email    VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
+    id                    BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email                 VARCHAR(255) NOT NULL UNIQUE,
+    password              VARCHAR(255) NOT NULL,
+    rank                  varchar(255) not null,
+    total_purchase_amount bigint       not null
 );
 
 CREATE TABLE if not exists cart_item
 (
     id         BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    quantity   INT    NOT NULL,
     member_id  BIGINT NOT NULL,
     product_id BIGINT NOT NULL,
-    quantity   INT    NOT NULL,
     FOREIGN KEY (member_id) REFERENCES member (id),
     FOREIGN KEY (product_id) REFERENCES product (id)
+);
+
+create table if not exists orders
+(
+    id                     bigint   not null auto_increment primary key,
+    total_price            bigint   not null,
+    discounted_total_price bigint   not null,
+    delivery_fee           int      not null,
+    ordered_at             datetime not null,
+    member_id              bigint   not null,
+    foreign key (member_id) references member (id)
+);
+
+create table if not exists ordered_item
+(
+    id               bigint       not null auto_increment primary key,
+    product_name     varchar(255) not null,
+    product_price    int          not null,
+    product_image    varchar(255) not null,
+    product_quantity int          not null,
+    order_id         bigint       not null,
+    foreign key (order_id) references orders (id)
 );
