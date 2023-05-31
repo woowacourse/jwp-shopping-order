f59db61002fcbe296b4fcd1d30fb1fa319f96803
be-student <presentsong@naver.com>
2023-05-31 10:57:01 +0900

    feat: orderCoupon 구현

---
+++ src/main/java/com/woowahan/techcourse/order/domain/OrderCoupon.java
@@ -0,0 +1,14 @@
+package com.woowahan.techcourse.order.domain;
+
+public class OrderCoupon {
+
+    private final Long id;
+
+    public OrderCoupon(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
