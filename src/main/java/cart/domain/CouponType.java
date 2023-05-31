dfa7736e4caeff003bbdb643034037215e0e8d48
hong-sile <gurwns9325@naver.com>
2023-05-31 19:50:13 +0900

    feat: 쿠폰타입 이름으로 enum 객체를 조회하는 기능 추가

--- src/main/java/cart/domain/CouponType.java
+++ src/main/java/cart/domain/CouponType.java
@@ -1,6 +1,24 @@
 package cart.domain;
 
+import cart.exception.CouponTypeNotFoundException;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public enum CouponType {
+
     RATE_DISCOUNT,
-    CONSTANT_DISCOUNT
+    CONSTANT_DISCOUNT;
+
+    private static final Map<String, CouponType> COUPON_TYPE_MAP
+            = Arrays.stream(CouponType.values())
+            .collect(Collectors.toMap(CouponType::toString, Function.identity()));
+
+    public static CouponType from(final String name) {
+        if (COUPON_TYPE_MAP.containsKey(name)) {
+            throw new CouponTypeNotFoundException();
+        }
+        return COUPON_TYPE_MAP.get(name);
+    }
 }
