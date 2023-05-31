139e200c9bdff090acb3e78b8152cee185e38139
jeonghoon <shb03323@naver.com>
2023-05-31 15:11:37 +0900

    feat: 할인 가격 계산 기능 구현

--- src/main/java/cart/domain/DiscountPriceCalculator.java
+++ src/main/java/cart/domain/DiscountPriceCalculator.java
@@ -2,12 +2,19 @@ package cart.domain;
 
 public class DiscountPriceCalculator {
 
+    private static final Price NO_DISCOUNT_AMOUNT = new Price(0);
     private static final Price FIRST_STEP_DISCOUNT_BOUNDARY_PRICE = new Price(30000);
     private static final Price FIRST_STEP_DISCOUNT_AMOUNT = new Price(2000);
     private static final Price SECOND_STEP_DISCOUNT_BOUNDARY_PRICE = new Price(50000);
     private static final Price SECOND_STEP_DISCOUNT_AMOUNT = new Price(5000);
 
     public Price calculate(final Price price) {
-        return null;
+        if (price.isGreaterThanOrEqualTo(SECOND_STEP_DISCOUNT_BOUNDARY_PRICE)) {
+            return SECOND_STEP_DISCOUNT_AMOUNT;
+        }
+        if (price.isGreaterThanOrEqualTo(FIRST_STEP_DISCOUNT_BOUNDARY_PRICE)) {
+            return FIRST_STEP_DISCOUNT_AMOUNT;
+        }
+        return NO_DISCOUNT_AMOUNT;
     }
 }
