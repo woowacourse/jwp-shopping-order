bf606288e4043a4dffdef8de8a24fa0d53554a98
BGuga <tmdwns9913@gmail.com>
2023-05-31 22:33:34 +0900

    feat: Coupon 테이블 정의 및 CouponDao 생성

---
+++ src/main/java/cart/dao/CouponDao.java
@@ -0,0 +1,37 @@
+package cart.dao;
+
+import cart.domain.Coupon;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Repository
+public class CouponDao {
+
+    private final JdbcTemplate jdbcTemplate;
+    private final RowMapper<Coupon> couponRowMapper = (rs, rowNum) -> new Coupon(
+            rs.getInt("id"),
+            rs.getInt("discount"),
+            rs.getString("name")
+    );
+
+    public CouponDao(final JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Coupon> findCouponById(final long memberId) {
+        final String findUserCouponSql = "select coupon_id from user_coupon where member_id = ?";
+        final List<Long> userCouponIds = jdbcTemplate.queryForList(findUserCouponSql, Long.class, memberId);
+        return allCoupon().stream()
+                .filter(coupon -> userCouponIds.contains(coupon.getId()))
+                .collect(Collectors.toList());
+    }
+
+    private List<Coupon> allCoupon() {
+        final String allCouponSql = "select * from coupon";
+        return jdbcTemplate.query(allCouponSql, couponRowMapper);
+    }
+}
---
+++ src/test/java/cart/dao/CouponDaoTest.java
@@ -0,0 +1,66 @@
+package cart.dao;
+
+import cart.domain.Coupon;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JdbcTest
+class CouponDaoTest {
+    @Autowired
+    DataSource dataSource;
+
+    CouponDao couponDao;
+
+    @BeforeEach
+    void setting() {
+        couponDao = new CouponDao(new JdbcTemplate(dataSource));
+    }
+
+    /**
+     * coupon (id, name, discount)
+     * user_coupon (id, member_id, coupon_id )
+     */
+    @DisplayName("유저의 모든 쿠폰을 가져온다.")
+    @Test
+    void findCouponById() throws SQLException {
+        //given
+
+        //targetCoupon
+        dataSource.getConnection()
+                .prepareStatement("insert into coupon values (1, 1000, '1000')").executeUpdate();
+        dataSource.getConnection()
+                .prepareStatement("insert into coupon values (2, 2000, '2000')").executeUpdate();
+
+        //otherCoupon
+        dataSource.getConnection()
+                .prepareStatement("insert into coupon values (3, 3000, '3000')").executeUpdate();
+
+        //targetUser
+        dataSource.getConnection()
+                .prepareStatement("insert into user_coupon values (1, 1, 1)").executeUpdate();
+        dataSource.getConnection()
+                .prepareStatement("insert into user_coupon values (2, 1, 2)").executeUpdate();
+        //otherUser
+        dataSource.getConnection()
+                .prepareStatement("insert into user_coupon values (3, 2, 3)").executeUpdate();
+
+        //when
+        final List<Coupon> couponById = couponDao.findCouponById(1L);
+
+        //then
+        assertThat(couponById).containsExactly(
+                new Coupon(1L, 1000, "1000"),
+                new Coupon(2L, 2000, "2000")
+        );
+    }
+}
