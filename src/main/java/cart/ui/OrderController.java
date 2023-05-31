34d53cd7f89cafb9bbb4390396e6d64c94a59cea
woosung1223 <dntjd991223@naver.com>
2023-05-31 14:01:21 +0900

    feat: 주문 관련 API 생성

---
+++ src/main/java/cart/ui/OrderController.java
@@ -0,0 +1,45 @@
+package cart.ui;
+
+import cart.application.OrderService;
+import cart.domain.Member;
+import cart.dto.OrderRequest;
+import cart.dto.OrderResponse;
+import cart.dto.SpecificOrderResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/orders")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<OrderResponse>> getAllOrders(Member member) {
+        List<OrderResponse> responses = orderService.getAllOrders(member);
+        return ResponseEntity.ok(responses);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SpecificOrderResponse> getSpecificOrder(Member member) {
+        SpecificOrderResponse response = orderService.getSpecificOrder(member);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> issueOrder(Member member, @RequestBody OrderRequest request) {
+        orderService.issue(member, request);
+        // TODO: CREATED URI 반환
+        return ResponseEntity.ok().build();
+    }
+}
