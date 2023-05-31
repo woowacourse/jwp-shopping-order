62b7931e67a257ad19b15457c42efc22269c4c12
wugawuga <dnr3655@naver.com>
2023-05-31 13:55:41 +0900

    feat: request valid 적용

--- src/main/java/cart/dto/CouponSaveRequest.java
+++ src/main/java/cart/dto/CouponSaveRequest.java
@@ -1,20 +1,26 @@
 package cart.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PositiveOrZero;
 
 @Schema(description = "쿠폰 저장 Request")
 public class CouponSaveRequest {
 
     @Schema(description = "쿠폰명", example = "10000원 이상 10% 할인 쿠폰")
+    @NotBlank(message = "쿠폰명은 공백일 수 없습니다.")
     private final String name;
 
     @Schema(description = "쿠폰타입", example = "RATE")
+    @NotBlank(message = "쿠폰타입은 공백일 수 없습니다.")
     private final String discountPolicyType;
 
-    @Schema(description = "할인가격", example = "10")
+    @Schema(description = "할인정도(가격, %)", example = "10")
+    @PositiveOrZero(message = "할인정도는 최소 0 이상입니다.")
     private final Long discountValue;
 
     @Schema(description = "쿠폰적용조건", example = "10000")
+    @PositiveOrZero(message = "쿠폰적용조건은 최소 0원 이상입니다.")
     private final Long minimumPrice;
 
     public CouponSaveRequest(final String name, final String discountPolicyType, final Long discountValue,
--- src/main/java/cart/dto/OrderSaveRequest.java
+++ src/main/java/cart/dto/OrderSaveRequest.java
@@ -2,11 +2,17 @@ package cart.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Schema(description = "주문 추가 Request")
 public class OrderSaveRequest {
 
     @Schema(description = "주문할 상품들", example = "[1, 2]")
+    @Valid
+    @NotNull
+    @Size(min = 1, message = "주문할 상품 개수는 최소 1개 이상입니다.")
     private final List<Long> orderItems;
 
     @Schema(description = "사용자 쿠폰 Id", example = "1")
