48b083434e4b53d74c268c6fdc8433345120ed32
odo27 <odo27@naver.com>
2023-05-31 16:06:19 +0900

    feat: OrdersEntity 구현

---
+++ src/main/java/cart/entity/OrdersEntity.java
@@ -0,0 +1,57 @@
+package cart.entity;
+
+import java.sql.Timestamp;
+
+public class OrdersEntity {
+
+    private final Long id;
+    private final Long memberId;
+    private final Long pointId;
+    private final int earnedPoint;
+    private final int usedPoint;
+    private final Timestamp createdAt;
+
+    public OrdersEntity(final Long id, final OrdersEntity other) {
+        this(id, other.memberId, other.pointId, other.earnedPoint, other.usedPoint, other.createdAt);
+    }
+
+    public OrdersEntity(
+            final Long id,
+            final Long memberId,
+            final Long pointId,
+            final int earnedPoint,
+            final int usedPoint,
+            final Timestamp createdAt
+    ) {
+        this.id = id;
+        this.memberId = memberId;
+        this.pointId = pointId;
+        this.earnedPoint = earnedPoint;
+        this.usedPoint = usedPoint;
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public Long getPointId() {
+        return pointId;
+    }
+
+    public int getEarnedPoint() {
+        return earnedPoint;
+    }
+
+    public int getUsedPoint() {
+        return usedPoint;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+}
---
+++ src/test/java/cart/entity/OrdersEntityTest.java
@@ -0,0 +1,44 @@
+package cart.entity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import java.sql.Timestamp;
+
+import org.junit.jupiter.api.Test;
+
+class OrdersEntityTest {
+
+    @Test
+    void construct() {
+        final OrdersEntity result = new OrdersEntity(
+                1L, 2L, 3L, 100, 200,
+                Timestamp.valueOf("2023-05-31 10:00:00")
+        );
+        assertAll(
+                () -> assertThat(result.getId()).isEqualTo(1L),
+                () -> assertThat(result.getMemberId()).isEqualTo(2L),
+                () -> assertThat(result.getPointId()).isEqualTo(3L),
+                () -> assertThat(result.getEarnedPoint()).isEqualTo(100),
+                () -> assertThat(result.getUsedPoint()).isEqualTo(200),
+                () -> assertThat(result.getCreatedAt()).isEqualTo(Timestamp.valueOf("2023-05-31 10:00:00"))
+        );
+    }
+
+    @Test
+    void constructWithOther() {
+        final OrdersEntity ordersEntity = new OrdersEntity(
+                1L, 2L, 3L, 100, 200,
+                Timestamp.valueOf("2023-05-31 10:00:00")
+        );
+        final OrdersEntity result = new OrdersEntity(10L, ordersEntity);
+        assertAll(
+                () -> assertThat(result.getId()).isEqualTo(10L),
+                () -> assertThat(result.getMemberId()).isEqualTo(2L),
+                () -> assertThat(result.getPointId()).isEqualTo(3L),
+                () -> assertThat(result.getEarnedPoint()).isEqualTo(100),
+                () -> assertThat(result.getUsedPoint()).isEqualTo(200),
+                () -> assertThat(result.getCreatedAt()).isEqualTo(Timestamp.valueOf("2023-05-31 10:00:00"))
+        );
+    }
+}
