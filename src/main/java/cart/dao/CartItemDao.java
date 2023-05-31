ad52153a1486722d9f555cc3e530fbd5a79f8a72
kdkdhoho <hkim4410@naver.com>
2023-05-31 09:47:29 +0900

    refactor(CartItemDao): 단순히 테이블에 접근만 하도록 수정 및 테스트 코드 작성

--- src/main/java/cart/dao/CartItemDao.java
+++ src/main/java/cart/dao/CartItemDao.java
@@ -1,101 +1,76 @@
 package cart.dao;
 
-import cart.domain.CartItem;
-import cart.domain.Member;
-import cart.domain.Product;
+import cart.domain.CartItemEntity;
+import cart.exception.CartItemNotFoundException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
-
-import java.sql.PreparedStatement;
-import java.sql.Statement;
 import java.util.List;
-import java.util.Objects;
 
 @Repository
 public class CartItemDao {
-    private final JdbcTemplate jdbcTemplate;
 
-    public CartItemDao(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
+    private static final String TABLE = "cart_item";
+    private static final String ID = "id";
+    private static final String MEMBER_ID = "member_id";
+    private static final String PRODUCT_ID = "product_id";
+    private static final String QUANTITY = "quantity";
+    private static final String ALL_COLUMN = String.join(", ", ID, MEMBER_ID, PRODUCT_ID, QUANTITY);
 
-    public List<CartItem> findByMemberId(Long memberId) {
-        String sql = "SELECT cart_item.id, cart_item.member_id, member.email, product.id, product.name, product.price, product.image_url, cart_item.quantity " +
-                "FROM cart_item " +
-                "INNER JOIN member ON cart_item.member_id = member.id " +
-                "INNER JOIN product ON cart_item.product_id = product.id " +
-                "WHERE cart_item.member_id = ?";
-        return jdbcTemplate.query(sql, new Object[]{memberId}, (rs, rowNum) -> {
-            String email = rs.getString("email");
-            Long productId = rs.getLong("product.id");
-            String name = rs.getString("name");
-            int price = rs.getInt("price");
-            String imageUrl = rs.getString("image_url");
-            Long cartItemId = rs.getLong("cart_item.id");
-            int quantity = rs.getInt("cart_item.quantity");
-            Member member = new Member(memberId, email, null);
-            Product product = new Product(productId, name, price, imageUrl);
-            return new CartItem(cartItemId, quantity, product, member);
-        });
-    }
-
-    public Long save(CartItem cartItem) {
-        KeyHolder keyHolder = new GeneratedKeyHolder();
-
-        jdbcTemplate.update(connection -> {
-            PreparedStatement ps = connection.prepareStatement(
-                    "INSERT INTO cart_item (member_id, product_id, quantity) VALUES (?, ?, ?)",
-                    Statement.RETURN_GENERATED_KEYS
+    private static final RowMapper<CartItemEntity> rowMapper = (resultSet, rowNum) ->
+            new CartItemEntity(
+                    resultSet.getLong(ID),
+                    resultSet.getLong(MEMBER_ID),
+                    resultSet.getLong(PRODUCT_ID),
+                    resultSet.getInt(QUANTITY)
             );
 
-            ps.setLong(1, cartItem.getMember().getId());
-            ps.setLong(2, cartItem.getProduct().getId());
-            ps.setInt(3, cartItem.getQuantity());
+    private final JdbcTemplate jdbcTemplate;
+    private final SimpleJdbcInsert simpleJdbcInsert;
 
-            return ps;
-        }, keyHolder);
-
-        return Objects.requireNonNull(keyHolder.getKey()).longValue();
+    public CartItemDao(final JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.simpleJdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName(TABLE)
+                .usingGeneratedKeyColumns(ID);
     }
 
-    public CartItem findById(Long id) {
-        String sql = "SELECT cart_item.id, cart_item.member_id, member.email, product.id, product.name, product.price, product.image_url, cart_item.quantity " +
-                "FROM cart_item " +
-                "INNER JOIN member ON cart_item.member_id = member.id " +
-                "INNER JOIN product ON cart_item.product_id = product.id " +
-                "WHERE cart_item.id = ?";
-        List<CartItem> cartItems = jdbcTemplate.query(sql, new Object[]{id}, (rs, rowNum) -> {
-            Long memberId = rs.getLong("member_id");
-            String email = rs.getString("email");
-            Long productId = rs.getLong("id");
-            String name = rs.getString("name");
-            int price = rs.getInt("price");
-            String imageUrl = rs.getString("image_url");
-            Long cartItemId = rs.getLong("cart_item.id");
-            int quantity = rs.getInt("cart_item.quantity");
-            Member member = new Member(memberId, email, null);
-            Product product = new Product(productId, name, price, imageUrl);
-            return new CartItem(cartItemId, quantity, product, member);
-        });
-        return cartItems.isEmpty() ? null : cartItems.get(0);
+    public long insert(final CartItemEntity cartItemEntity) {
+        SqlParameterSource params = new BeanPropertySqlParameterSource(cartItemEntity);
+
+        return simpleJdbcInsert.executeAndReturnKey(params).longValue();
     }
 
+    public CartItemEntity findById(final Long id) {
+        String sql = "SELECT " + ALL_COLUMN + " FROM " + TABLE + " WHERE id = ?;";
 
-    public void delete(Long memberId, Long productId) {
-        String sql = "DELETE FROM cart_item WHERE member_id = ? AND product_id = ?";
-        jdbcTemplate.update(sql, memberId, productId);
+        try {
+            return jdbcTemplate.queryForObject(sql, rowMapper, id);
+        } catch (final EmptyResultDataAccessException e) {
+            throw new CartItemNotFoundException();
+        }
     }
 
-    public void deleteById(Long id) {
-        String sql = "DELETE FROM cart_item WHERE id = ?";
+    public List<CartItemEntity> findAll() {
+        String sql = "SELECT " + ALL_COLUMN + " FROM " + TABLE + ";";
+
+        return jdbcTemplate.query(sql, rowMapper);
+    }
+
+    public void update(final CartItemEntity cartItemEntity) {
+        String sql = "UPDATE " + TABLE + " SET member_id = ?, product_id = ?, quantity = ? WHERE id = ?;";
+
+        jdbcTemplate.update(sql, cartItemEntity.getMemberId(), cartItemEntity.getProductId(), cartItemEntity.getQuantity(), cartItemEntity.getId());
+    }
+
+    public void deleteById(final long id) {
+        String sql = "DELETE FROM " + TABLE + " WHERE id = ?;";
+
         jdbcTemplate.update(sql, id);
-    }
-
-    public void updateQuantity(CartItem cartItem) {
-        String sql = "UPDATE cart_item SET quantity = ? WHERE id = ?";
-        jdbcTemplate.update(sql, cartItem.getQuantity(), cartItem.getId());
     }
 }
 
---
+++ src/test/java/cart/dao/CartItemDaoTest.java
@@ -0,0 +1,105 @@
+package cart.dao;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import cart.domain.CartItemEntity;
+import cart.domain.Member;
+import cart.domain.Product;
+import cart.exception.CartItemNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+
+class CartItemDaoTest extends DaoTest {
+
+    private static final Member dummyMember = new Member(1L, "email1", "pw1", 1);
+    private static final Product dummyProduct = new Product(1L, "name1", 1, "imageUrl1", 1);
+    private static final CartItemEntity dummyCartItemEntity = new CartItemEntity(1L, 1L, 1);
+
+    private MemberDao memberDao;
+    private ProductDao productDao;
+    private CartItemDao cartItemDao;
+
+    @BeforeEach
+    void setUp() {
+        this.cartItemDao = new CartItemDao(jdbcTemplate);
+        this.memberDao = new MemberDao(jdbcTemplate);
+        this.productDao = new ProductDao(jdbcTemplate);
+
+        insertDummyMemberAndProduct();
+    }
+
+    private void insertDummyMemberAndProduct() {
+        memberDao.insert(dummyMember);
+        productDao.insert(dummyProduct);
+    }
+
+    @Test
+    void 데이터를_삽입한다() {
+        // when
+        long savedId = cartItemDao.insert(dummyCartItemEntity);
+
+        // then
+        assertThat(savedId).isEqualTo(1);
+    }
+
+    @Test
+    void ID로_데이터를_조회한다() {
+        // given
+        long savedId = cartItemDao.insert(dummyCartItemEntity);
+
+        // when
+        CartItemEntity foundCartItemEntity = cartItemDao.findById(savedId);
+
+        // then
+        assertThat(foundCartItemEntity.getQuantity()).isEqualTo(1);
+    }
+
+    @Test
+    void 모든_데이터를_조회한다() {
+        // given
+        cartItemDao.insert(dummyCartItemEntity);
+
+        // when
+        List<CartItemEntity> result = cartItemDao.findAll();
+
+        // then
+        assertThat(result).hasSize(1);
+    }
+
+    @Test
+    void 존재하지_않는_데이터를_조회하면_예외가_발생한다() {
+        // expect
+        assertThatThrownBy(() -> cartItemDao.findById(213213L))
+                .isInstanceOf(CartItemNotFoundException.class);
+    }
+
+    @Test
+    void 데이터를_수정한다() {
+        // given
+        long savedId = cartItemDao.insert(dummyCartItemEntity);
+
+        CartItemEntity newCartItemEntity = new CartItemEntity(savedId, 1L, 1L, 2);
+
+        // when
+        cartItemDao.update(newCartItemEntity);
+
+        // then
+        CartItemEntity foundCartItemEntity = cartItemDao.findById(savedId);
+        assertThat(foundCartItemEntity.getQuantity()).isEqualTo(2);
+    }
+
+    @Test
+    void ID로_데이터를_삭제한다() {
+        // given
+        long savedId = cartItemDao.insert(dummyCartItemEntity);
+
+        // when
+        cartItemDao.deleteById(savedId);
+
+        // then
+        assertThatThrownBy(() -> cartItemDao.findById(savedId))
+                .isInstanceOf(CartItemNotFoundException.class);
+    }
+}
