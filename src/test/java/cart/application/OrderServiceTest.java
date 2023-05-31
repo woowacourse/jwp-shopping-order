b30102956373fc4f96e0270307f466b881fda533
zillionme <beatus96@naver.com>
2023-05-31 18:09:56 +0900

    test: member의 전체 주문 내역 조회 기능 테스트

--- src/test/java/cart/application/OrderServiceTest.java
+++ src/test/java/cart/application/OrderServiceTest.java
@@ -3,6 +3,9 @@ package cart.application;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import cart.domain.Member;
+import cart.domain.Order;
+import cart.domain.OrderItem;
+import cart.domain.Product;
 import cart.dto.OrderAddRequest;
 import cart.dto.OrderItemRequest;
 import java.time.LocalDateTime;
@@ -36,6 +39,39 @@ class OrderServiceTest {
     
     @Test
     void findOrdersByMemberId() {
+        //given
+        Member member = new Member(1L, "abc@gmail.com", "12345");
+        LocalDateTime createdAt = LocalDateTime.of(23, 05, 30, 13, 11, 30);
+        OrderAddRequest firstRequest = new OrderAddRequest(
+                createdAt,
+                List.of(new OrderItemRequest(1L, 2),
+                        new OrderItemRequest(2L, 5)));
+        OrderAddRequest secondRequest = new OrderAddRequest(
+                createdAt,
+                List.of(new OrderItemRequest(3L, 3)));
+        orderService.addOrder(member, firstRequest);
+        orderService.addOrder(member, secondRequest);
+    
+        //expected
+        Order firstOrder = new Order(1L,
+                member.getId(),
+                List.of(new OrderItem(1L, member.getId(), new Product(1l, "1번 상품", 1000, "1번 상품url"), 2),
+                        new OrderItem(2l, member.getId(), new Product(2l, "2번 상품", 2000, "2번 상품url"), 5)),
+                3000,
+                0,
+                createdAt
+        );
+        Order secondOrder = new Order(2L,
+                member.getId(),
+                List.of(new OrderItem(3L, member.getId(), new Product(3l, "3번 상품", 1000, "3번 상품url"), 3)),
+                3000,
+                0,
+                createdAt
+        );
+        List<Order> expected = List.of(firstOrder, secondOrder);
+        assertThat(orderService.findOrdersByMember(member))
+                .usingRecursiveComparison()
+                .isEqualTo(expected);
     }
     
     
