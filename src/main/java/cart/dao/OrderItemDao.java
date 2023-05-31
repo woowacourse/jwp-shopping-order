e35978176704e69cddacf524c63a533e70aa1389
Kong-hana01 <gksqlsl11@khu.ac.kr>
2023-05-31 11:28:16 +0900

    feat: OrderItem 테이블에 값을 저장하는 기능 구현

--- src/main/java/cart/dao/OrderItemDao.java
+++ src/main/java/cart/dao/OrderItemDao.java
@@ -2,10 +2,12 @@ package cart.dao;
 
 import cart.domain.OrderItem;
 import cart.domain.Product;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
@@ -21,10 +23,28 @@ public class OrderItemDao {
 
     public List<OrderItem> findAll() {
         String sql = "select * from orders_item left join product on orders_item.product_id = product.id";
-        return jdbcTemplate.query(sql, new OrderRowMapper());
+        return jdbcTemplate.query(sql, new OrderItemRowMapper());
     }
 
-    private static class OrderRowMapper implements RowMapper<OrderItem> {
+    public void saveAll(Long orderId, List<OrderItem> orderItems) {
+        jdbcTemplate.batchUpdate("insert into orders_item(orders_id, product_id, quantity, total_price) values(?, ?, ?, ?)",
+                new BatchPreparedStatementSetter() {
+                    @Override
+                    public void setValues(PreparedStatement ps, int i) throws SQLException {
+                        ps.setLong(1, orderId);
+                        ps.setLong(2, orderItems.get(i).getProduct().getId());
+                        ps.setInt(3, orderItems.get(i).getQuantity());
+                        ps.setInt(4, orderItems.get(i).getTotalPrice());
+                    }
+
+                    @Override
+                    public int getBatchSize() {
+                        return orderItems.size();
+                    }
+                });
+    }
+
+    private static class OrderItemRowMapper implements RowMapper<OrderItem> {
 
         @Override
         public OrderItem mapRow(ResultSet rs, int rowNum) throws SQLException {
--- src/test/java/cart/dao/OrderItemDaoTest.java
+++ src/test/java/cart/dao/OrderItemDaoTest.java
@@ -1,21 +1,18 @@
 package cart.dao;
 
 import cart.domain.OrderItem;
-import cart.domain.Product;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.jdbc.Sql;
 
-import javax.sql.DataSource;
 import java.util.List;
 
+import static cart.ProductFixture.product1;
+import static cart.ProductFixture.product2;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -24,13 +21,11 @@ class OrderItemDaoTest {
 
     private JdbcTemplate jdbcTemplate;
     private OrderItemDao orderItemDao;
-    private DataSource dataSource;
 
     @Autowired
-    public OrderItemDaoTest(JdbcTemplate jdbcTemplate, DataSource dataSource) {
+    public OrderItemDaoTest(JdbcTemplate jdbcTemplate) {
         this.orderItemDao = new OrderItemDao(jdbcTemplate);
         this.jdbcTemplate = jdbcTemplate;
-        this.dataSource = dataSource;
     }
 
     @BeforeEach
@@ -46,9 +41,20 @@ class OrderItemDaoTest {
     void findAll() {
         List<OrderItem> orderItems = orderItemDao.findAll();
 
-        Product product1 = new Product(1L, "치킨", 10000, "https://images.unsplash.com/photo-1626082927389-6cd097cdc6ec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80");
-        Product product2 = new Product(2L, "샐러드", 20000, "https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80");
-
         assertThat(orderItems).containsExactlyInAnyOrder(new OrderItem(product1, 3, 30000), new OrderItem(product2, 2, 40000));
+    }
+
+    @DisplayName("주문 정보를 추가할 수 있다.")
+    @Test
+    void saveAll() {
+        OrderItem orderItem1 = new OrderItem(product1, 4, 40000);
+        OrderItem orderItem2 = new OrderItem(product2, 3, 60000);
+
+        List<OrderItem> orderItems = List.of(orderItem1, orderItem2);
+        orderItemDao.saveAll(1L, orderItems);
+
+        List<OrderItem> actualOrderItems = orderItemDao.findAll();
+
+        assertThat(actualOrderItems).containsAnyOf(orderItem1, orderItem2);
     }
 }
