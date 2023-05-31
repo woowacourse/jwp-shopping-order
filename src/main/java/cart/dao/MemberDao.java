8e112d153fdcbc02f1120bb9cdc0c5dc5126c1ef
DongUk <68818952+Kim0914@users.noreply.github.com>
2023-05-31 12:00:06 +0900

    refactor: MemberDao 메서드 명 변경
    Co-authored-by: seokjin8678 <seokjin8678@gmail.com>

--- src/main/java/cart/dao/MemberDao.java
+++ src/main/java/cart/dao/MemberDao.java
@@ -5,18 +5,23 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
-import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class MemberDao {
 
     private final JdbcTemplate jdbcTemplate;
+    private final SimpleJdbcInsert simpleJdbcInsert;
 
     public MemberDao(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+        this.simpleJdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName("member")
+                .usingGeneratedKeyColumns("id");
     }
 
     public Optional<Member> getMemberById(Long id) {
@@ -24,13 +29,7 @@ public class MemberDao {
                 "FROM member " +
                 "JOIN member_point ON member_point.member_id = member.id " +
                 "WHERE member.id = ? ";
-        try {
-            Member member = jdbcTemplate.queryForObject(sql, new MemberRowMapper(), id);
-            return Optional.ofNullable(member);
-        } catch (DataAccessException e) {
-            e.printStackTrace();
-            return Optional.empty();
-        }
+        return jdbcTemplate.query(sql, new MemberRowMapper(), id).stream().findAny();
     }
 
     public Optional<Member> getMemberByEmail(String email) {
@@ -38,18 +37,14 @@ public class MemberDao {
                 "FROM member " +
                 "JOIN member_point ON member_point.member_id = member.id " +
                 "WHERE member.email = ? ";
-        try {
-            Member member = jdbcTemplate.queryForObject(sql, new MemberRowMapper(), email);
-            return Optional.ofNullable(member);
-        } catch (DataAccessException e) {
-            e.printStackTrace();
-            return Optional.empty();
-        }
+        return jdbcTemplate.query(sql, new MemberRowMapper(), email).stream().findAny();
     }
 
-    public void addMember(Member member) {
-        String sql = "INSERT INTO member (email, password) VALUES (?, ?)";
-        jdbcTemplate.update(sql, member.getEmail(), member.getPassword());
+    public Long save(Member member) {
+        MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource();
+        mapSqlParameterSource.addValue("email", member.getEmail());
+        mapSqlParameterSource.addValue("password", member.getPassword());
+        return simpleJdbcInsert.executeAndReturnKey(mapSqlParameterSource).longValue();
     }
 
     public void updateMember(Member member) {
