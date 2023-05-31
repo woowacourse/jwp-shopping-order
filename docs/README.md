8590f9539706859ce9b0a6356550ed062f1c1fed
eunbii0213 <eunbii0213@naver.com>
2023-05-31 17:08:28 +0900

    test: 특정 주문의 상세 정보 확인하는 테스트 작성

---
+++ docs/README.md
@@ -0,0 +1,57 @@
+# 기능 목록
+
+
+## 장바구니에 담은 상품들 주문하기
+- [x] POST 요청, URI = "/orders"인 API 작성
+- [x] OrderCreateRequest 생성
+- [x] service에서 domain 객체 생성 (Cart)
+  - [x] CartItem들의 List를 가지고 있다
+- [x] domain으로 dao에 요청 전송
+- [x] 주문한 상품들의 총액을 계산
+- [x] 배송비 계산
+- [x] 상품 할인 계산
+- [x] dao에서 영속화 작업 수행
+  - [x] 주문한 상품들은 ordered_item에 추가
+  - [x] 주문한 상품들은 장바구니에서 삭제
+
+
+## 특정 멤버의 주문 전체 목록 조회하기
+- [] GET 요청, URI = "/members/{memberId}/orders"인 API 작성
+- [] service에서 domain 객체 생성 (Orders)
+  - [] Order들의 List를 가지고 있다 
+- [] domain으로 dao에 요청 전송
+- [] dao에서 영속화 작업 수행
+  - [] 전체 주문 목록을 조회하는 쿼리문 작성
+- [] OrdersResponse 생성
+
+
+## 특정 멤버의 특정 주문 조회하기
+- [x] GET 요청, URI = "/members/{memberId}/orders/{orderId}"인 API 작성
+- [x] service에서 dao로 요청 전송
+- [x] domain으로 dao에 요청 전송
+- [x] dao에서 영속화 작업 수행
+  - [x] 특정 주문의 정보들을 조회하는 쿼리문 작성
+- [x] OrderResponse 생성
+
+
+## 멤버 등급 조회하기
+- [] GET 요청, URI = "/member"인 API 작성
+- [] service에서 domain 객체 생성(Member)
+  - [] rank와 discountRate를 가지고 있다
+- [] domain에서 dao로 영속화 작업 수행
+  - [] 멤버의 등급과 할인율을 조회하는 쿼리문 작성
+- [] MemberResponse 작성 
+
+## 상품 전체 조회하기
+- [] GET 요청, URI = "/products"인 API 작성
+- [] service에서 domain 객체 생성(Products)
+  - [] Product의 List를 가지고 있다 
+- [] domain으로 dao에 요청 전송
+- [] dao에서 영속화 작업 수행
+  - [] 모든 상품들을 조회하는 쿼리문 작성 
+- [] ProductsResponse 작성
+
+
+## 주디가 했던 고민들
+1. 현재는 Domain과 Entity가 분리 되어있지 않은 상황같습니다. 개인적으로는 Domain과 Entity를 분리하여 DB에 data를 넣고 뺄 때는 entity를 사용하고 그렇지 않을 때는 domain을 사용했었는데, 리뷰어님은 어떻게 domain과 entity의 역할을 구분지으셨나요?
+2. 
---
+++ src/test/java/cart/integration/MemberIntegrationTest.java
@@ -0,0 +1,144 @@
+package cart.integration;
+
+import cart.application.OrderedItemDao;
+import cart.dao.CartItemDao;
+import cart.dao.MemberDao;
+import cart.dao.OrderDao;
+import cart.domain.Member;
+import cart.dto.CartItemRequest;
+import cart.dto.OrderCreateRequest;
+import cart.dto.OrderItemRequest;
+import cart.dto.OrderResponse;
+import cart.dto.ProductRequest;
+import cart.dto.ProductResponse;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MemberIntegrationTest extends IntegrationTest {
+
+    @Autowired
+    private OrderDao orderDao;
+    @Autowired
+    private CartItemDao cartItemDao;
+    @Autowired
+    private OrderedItemDao orderedItemDao;
+    @Autowired
+    private MemberDao memberDao;
+    private Long productId;
+    private Long productId2;
+    private Member member;
+    private Member member2;
+
+    @BeforeEach
+    void setUp() {
+        super.setUp();
+
+        productId = createProduct(new ProductRequest("치킨", 10_000, "http://example.com/chicken.jpg", 0));
+        productId2 = createProduct(new ProductRequest("피자", 10_000, "http://example.com/pizza.jpg", 50));
+
+        member = memberDao.getMemberById(1L); //일반 등급
+        member2 = memberDao.getMemberById(2L); //실버 등급
+    }
+
+    private Long createProduct(ProductRequest productRequest) {
+        ExtractableResponse<Response> response = given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(productRequest)
+                .when()
+                .post("/products")
+                .then()
+                .statusCode(HttpStatus.CREATED.value())
+                .extract();
+
+        return getIdFromCreatedResponse(response);
+    }
+
+    private long getIdFromCreatedResponse(ExtractableResponse<Response> response) {
+        return Long.parseLong(response.header("Location").split("/")[2]);
+    }
+
+    @Test
+    @DisplayName("특정 멤버의 특정 주문을 조회한다")
+    void get_order() {
+        Long cartItemId = createCartItem(member2, new CartItemRequest(productId2));
+        Long cartItemId2 = createCartItem(member2, new CartItemRequest(productId));
+        Long orderId = requestOrder(member2, cartItemId, cartItemId2);
+        ExtractableResponse<Response> response = requestGetOrder(member2, orderId);
+        OrderResponse orderResponse = getOrderResponse(member2, orderId);
+
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(orderResponse.getId()).isNotNull();
+        assertThat(orderResponse.getDiscountedTotalItemPrice()).isEqualTo(14_000);
+        assertThat(orderResponse.getTotalItemPrice()).isEqualTo(20_000);
+        assertThat(orderResponse.getShippingFee()).isEqualTo(3_000);
+        assertThat(orderResponse.getTotalPrice()).isEqualTo(17_000);
+    }
+
+    private Long createCartItem(Member member, CartItemRequest cartItemRequest) {
+        ExtractableResponse<Response> response = given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .auth().preemptive().basic(member.getEmail(), member.getPassword())
+                .body(cartItemRequest)
+                .when()
+                .post("/cart-items")
+                .then()
+                .statusCode(HttpStatus.CREATED.value())
+                .extract();
+
+        return getIdFromCreatedResponse(response);
+    }
+
+    private Long requestOrder(Member member, Long cartItemId, Long cartItemId2) {
+        ExtractableResponse<Response> response = given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .auth().preemptive().basic(member.getEmail(), member.getPassword())
+                .body(new OrderCreateRequest(List.of(
+                        new OrderItemRequest(cartItemId, 1),
+                        new OrderItemRequest(cartItemId2, 1)
+                )))
+                .when()
+                .post("/orders")
+                .then()
+                .statusCode(HttpStatus.CREATED.value())
+                .extract();
+
+        return getIdFromCreatedResponse(response);
+    }
+
+    private ExtractableResponse<Response> requestGetOrder(Member member, Long orderId) {
+        return given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .auth().preemptive().basic(member.getEmail(), member.getPassword())
+                .when()
+                .get("/members/" + member.getId() + "/orders/" + orderId)
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .extract();
+    }
+
+    private OrderResponse getOrderResponse(Member member, Long orderId) {
+
+        return given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .auth().preemptive().basic(member.getEmail(), member.getPassword())
+                .when()
+                .get("/members/" + member.getId() + "/orders/" + orderId)
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .extract()
+                .jsonPath()
+                .getObject(".", OrderResponse.class);
+    }
+}
