0dbceb5f3588283b9846a100326e926b3f91f6d8
kdkdhoho <hkim4410@naver.com>
2023-05-31 11:19:42 +0900

    refactor(ProductResponse): 필드 추가 및 코드 개선

--- src/main/java/cart/dto/ProductResponse.java
+++ src/main/java/cart/dto/ProductResponse.java
@@ -1,37 +1,20 @@
 package cart.dto;
 
 import cart.domain.Product;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@AllArgsConstructor
+@Getter
 public class ProductResponse {
-    private Long id;
-    private String name;
-    private int price;
-    private String imageUrl;
 
-    private ProductResponse(Long id, String name, int price, String imageUrl) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-        this.imageUrl = imageUrl;
-    }
+    private final Long id;
+    private final String name;
+    private final int price;
+    private final String imageUrl;
+    private final int stock;
 
-    public static ProductResponse of(Product product) {
-        return new ProductResponse(product.getId(), product.getName(), product.getPrice(), product.getImageUrl());
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
+    public static ProductResponse from(final Product product) {
+        return new ProductResponse(product.getId(), product.getName(), product.getPrice(), product.getImageUrl(), product.getStock());
     }
 }
