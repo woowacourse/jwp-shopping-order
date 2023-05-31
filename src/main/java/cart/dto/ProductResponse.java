8a92fb5311853f92642fe8c294275baf7ea7d23f
woosung1223 <dntjd991223@naver.com>
2023-05-31 14:03:42 +0900

    feat: 상품 관련 API 수정

--- src/main/java/cart/dto/ProductResponse.java
+++ src/main/java/cart/dto/ProductResponse.java
@@ -8,16 +8,22 @@ public class ProductResponse {
     private final String name;
     private final int price;
     private final String imageUrl;
+    private final double pointRatio;
+    private final boolean pointAvailable;
 
-    private ProductResponse(Long id, String name, int price, String imageUrl) {
+    private ProductResponse(Long id, String name, int price, String imageUrl,
+                            double pointRatio, boolean pointAvailable) {
         this.id = id;
         this.name = name;
         this.price = price;
         this.imageUrl = imageUrl;
+        this.pointRatio = pointRatio;
+        this.pointAvailable = pointAvailable;
     }
 
     public static ProductResponse of(Product product) {
-        return new ProductResponse(product.getId(), product.getName(), product.getPrice(), product.getImageUrl());
+        return new ProductResponse(product.getId(), product.getName(), product.getPrice(),
+                product.getImageUrl(), product.getPointRatio(), product.isPointAvailable());
     }
 
     public Long getId() {
@@ -34,5 +40,13 @@ public class ProductResponse {
 
     public String getImageUrl() {
         return imageUrl;
+    }
+
+    public double getPointRatio() {
+        return pointRatio;
+    }
+
+    public boolean isPointAvailable() {
+        return pointAvailable;
     }
 }
