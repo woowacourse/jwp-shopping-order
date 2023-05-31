c4ba82051deb99baf6d5114ed075ce4e64c510e7
Ethan <kim76744dev@gmail.com>
2023-05-31 16:53:06 +0900

    refactor: 오류 메세지를 stacktrace로 변경

--- src/main/java/cart/controller/ControllerExceptionHandler.java
+++ src/main/java/cart/controller/ControllerExceptionHandler.java
@@ -9,6 +9,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
@@ -16,32 +19,38 @@ public class ControllerExceptionHandler {
 
     @ExceptionHandler(AuthenticationException.class)
     public ResponseEntity<String> handlerAuthenticationException(AuthenticationException e) {
-        log.warn("[ERROR] 권한이 없는 사용자입니다. = {}", e.getMessage());
+        log.warn("[ERROR] 권한이 없는 사용자입니다. = {}", createErrorMessage(e));
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("권한이 없는 사용자입니다.");
     }
 
     @ExceptionHandler(CartItemException.IllegalMember.class)
     public ResponseEntity<String> handleException(CartItemException.IllegalMember e) {
-        log.warn("[ERROR] 권한이 없는 사용자입니다. = {}", e.getMessage());
+        log.warn("[ERROR] 권한이 없는 사용자입니다. = {}", createErrorMessage(e));
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body("권한이 없는 사용자입니다.");
     }
 
     @ExceptionHandler(IllegalStateException.class)
     public ResponseEntity<String> handleIllegalStatementException(final IllegalStateException e) {
-        log.error("[ERROR] 예외가 발생 했습니다. = {}", e.getMessage());
+        log.error("[ERROR] 예외가 발생 했습니다. = {}", createErrorMessage(e));
         return ResponseEntity.internalServerError().body("서버 오류 입니다.");
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleIllegalArgumentException(final IllegalArgumentException e) {
-        log.warn("[ERROR] 예외가 발생 했습니다. = {}", e.getMessage());
+        log.warn("[ERROR] 예외가 발생 했습니다. = {}", createErrorMessage(e));
         return ResponseEntity.badRequest().body("잘못된 값을 입력했습니다.");
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleIllegalArgumentException(final Exception e) {
-        log.error("[ERROR] 예상치 못한 예외가 발생 했습니다. = {}", e.getMessage());
+        log.error("[ERROR] 예상치 못한 예외가 발생 했습니다. = {}", createErrorMessage(e));
         return ResponseEntity.badRequest().body("예상치 못한 예외가 발생했습니다.");
+    }
+
+    private static String createErrorMessage(final Exception e) {
+        StringWriter sw = new StringWriter();
+        e.printStackTrace(new PrintWriter(sw));
+        return sw.toString();
     }
 }
 
