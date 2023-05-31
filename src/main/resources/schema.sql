07b27c33dafc8341bf1b4e9cc7034e06e6e2d3ec
LJW25 <dbjwlee@gmail.com>
2023-05-31 22:40:07 +0900

    chore: 데이터 구조 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,21 +1,50 @@
-CREATE TABLE product (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    price INT NOT NULL,
-    image_url TEXT NOT NULL
+CREATE TABLE product
+(
+    id        BIGINT PRIMARY KEY AUTO_INCREMENT,
+    name      VARCHAR(255) NOT NULL,
+    price     INT          NOT NULL,
+    image_url TEXT         NOT NULL
 );
 
-CREATE TABLE member (
-     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-     email VARCHAR(255) NOT NULL UNIQUE,
-     password VARCHAR(255) NOT NULL
+CREATE TABLE member
+(
+    id       BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email    VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    points   INT          NOT NULL DEFAULT 0
 );
 
-CREATE TABLE cart_item (
-    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    member_id BIGINT NOT NULL,
+CREATE TABLE cart_item
+(
+    id         BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    member_id  BIGINT NOT NULL,
     product_id BIGINT NOT NULL,
-    quantity INT NOT NULL,
-    FOREIGN KEY (member_id) REFERENCES member(id) ON DELETE CASCADE,
-    FOREIGN KEY (product_id) REFERENCES product(id) ON DELETE CASCADE
+    quantity   INT    NOT NULL,
+    FOREIGN KEY (member_id) REFERENCES member (id) ON DELETE CASCADE,
+    FOREIGN KEY (product_id) REFERENCES product (id) ON DELETE CASCADE
 );
+
+CREATE TABLE orders
+(
+    id            BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    member_id     BIGINT NOT NULL,
+    earned_points INT    NOT NULL,
+    used_points   INT    NOT NULL,
+    total_price   INT    NOT NULL,
+    pay_price     INT    NOT NULL,
+    order_date    DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (member_id) REFERENCES member (id) ON DELETE CASCADE
+);
+
+CREATE TABLE order_item
+(
+    id         BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    order_id   BIGINT       NOT NULL,
+    product_id BIGINT       NOT NULL,
+    name       VARCHAR(255) NOT NULL,
+    price      INT          NOT NULL,
+    image_url  TEXT         NOT NULL,
+    quantity   INT          NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES orders (id) ON DELETE CASCADE
+);
+
