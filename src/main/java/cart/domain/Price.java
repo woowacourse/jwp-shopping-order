24756700b82f8dc52cb4b23a8b3b4f44dfe494e7
jeonghoon <shb03323@naver.com>
2023-05-31 14:13:33 +0900

    feat: Price 도메인 객체 생성

---
+++ src/main/java/cart/domain/Price.java
@@ -0,0 +1,14 @@
+package cart.domain;
+
+public final class Price {
+
+    private final int amount;
+
+    public Price(final int amount) {
+        this.amount = amount;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+}
--- src/main/java/cart/domain/Product.java
+++ src/main/java/cart/domain/Product.java
@@ -4,10 +4,10 @@ public class Product {
 
     private final Long id;
     private final String name;
-    private final int price;
+    private final Price price;
     private final String imageUrl;
 
-    public Product(final Long id, final String name, final int price, final String imageUrl) {
+    public Product(final Long id, final String name, final Price price, final String imageUrl) {
         this.id = id;
         this.name = name;
         this.price = price;
@@ -22,7 +22,7 @@ public class Product {
         return name;
     }
 
-    public int getPrice() {
+    public Price getPrice() {
         return price;
     }
 
