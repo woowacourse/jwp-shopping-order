9ff784c0e4d7aa65b87f6b805848b11f15d46419
gitchan <eunkeeeee@gmail.com>
2023-05-31 16:57:08 +0900

    feat(DataInitializer): 더미 데이터 추가

--- src/main/java/cart/initializer/DataInitializer.java
+++ src/main/java/cart/initializer/DataInitializer.java
@@ -26,9 +26,11 @@ public class DataInitializer {
 
     @PostConstruct
     public void init() {
-        final Long 깃짱 = productRepository.createProduct(new Product("깃짱", 13000, "#"));
-        final Long 제리 = productRepository.createProduct(new Product("제리", 13000, "#"));
-        final Long 호이 = productRepository.createProduct(new Product("호이", 13000, "#"));
+        final Long 깃짱 = productRepository.createProduct(new Product("깃짱", 13000, "https://lh3.google.com/u/0/d/1YelRXToHj3qh19EiTZzXHa5D8UVSy95n=w2940-h1546-iv1"));
+        final Long 제리 = productRepository.createProduct(new Product("제리", 13000, "https://lh3.google.com/u/0/d/1YelRXToHj3qh19EiTZzXHa5D8UVSy95n=w2940-h1546-iv1"));
+        final Long 호이 = productRepository.createProduct(new Product("호이", 13000, "https://lh3.google.com/u/0/d/1Pcyd8ngTr0TFBBSnANJsXhCwnnXGCLZz=w1294-h1546-iv1"));
+        final Long 링링 = productRepository.createProduct(new Product("링링", 13000, "https://lh3.google.com/u/0/d/1bMoOTubWF_gKvF4rSxN3woBY6PjjQMl4=w1294-h1546-iv1"));
+        final Long 글로 = productRepository.createProduct(new Product("글로", 13000, "https://lh3.google.com/u/0/d/1KIhTiIEn0NyXVYpY4Lv9XqzAAAyBIkVx=w1294-h1546-iv1"));
 
         final Member savedMember1 = memberRepository.addMember(new Member("a@a.com", "1234"));
         final Member savedMember2 = memberRepository.addMember(new Member("b@b.com", "1234"));
