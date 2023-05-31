14324f25d89a92013a2fd6ed9b02b73bf0d43ca5
This2sho <zx00018@naver.com>
2023-05-31 22:34:22 +0900

    refactor(CarItem): 처음 수량 정할 수 있도록 기본 생성자 추가

--- src/main/java/cart/domain/CartItem.java
+++ src/main/java/cart/domain/CartItem.java
@@ -10,10 +10,12 @@ public class CartItem {
     private final Product product;
     private final Member member;
 
-    public CartItem(Member member, Product product) {
-        this.quantity = 1;
-        this.member = member;
-        this.product = product;
+    public CartItem(Product product, Member member) {
+        this(1, product, member);
+    }
+
+    public CartItem(Integer quantity, Product product, Member member) {
+        this(null, quantity, product, member);
     }
 
     public CartItem(Long id, int quantity, Product product, Member member) {
