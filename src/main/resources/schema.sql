c186b7dec7dedaa87f4ec3bf4fcc70021bd52a90
rawfishthelgh <rg970604@naver.com>
2023-06-01 14:24:56 +0900

    fix : unsigned 해제

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS product
 (
-    id        BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    id        BIGINT PRIMARY KEY AUTO_INCREMENT,
     name      VARCHAR(255) NOT NULL,
     price     INT          NOT NULL,
     image_url VARCHAR(255) NOT NULL
@@ -8,38 +8,38 @@ CREATE TABLE IF NOT EXISTS product
 
 CREATE TABLE IF NOT EXISTS member
 (
-    id       BIGINT       UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id       BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email    VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    point    BIGINT UNSIGNED NOT NULL
+    point    BIGINT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS cart_item
 (
-    id         BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    member_id  BIGINT UNSIGNED NOT NULL,
-    product_id BIGINT UNSIGNED NOT NULL,
-    quantity   INT    UNSIGNED NOT NULL,
+    id         BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    member_id  BIGINT NOT NULL,
+    product_id BIGINT NOT NULL,
+    quantity   INT    NOT NULL,
     FOREIGN KEY (member_id) REFERENCES member (id),
     FOREIGN KEY (product_id) REFERENCES product (id)
 );
 
 CREATE TABLE IF NOT EXISTS orders
 (
-    id             BIGINT   UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    member_id      BIGINT UNSIGNED NOT NULL,
-    payment        BIGINT UNSIGNED NOT NULL,
-    discount_point BIGINT UNSIGNED NOT NULL,
+    id             BIGINT   NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    member_id      BIGINT NOT NULL,
+    payment        BIGINT NOT NULL,
+    discount_point BIGINT NOT NULL,
     order_date     DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (member_id) REFERENCES member (id)
 );
 
 CREATE TABLE IF NOT EXISTS order_detail
 (
-    id         BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    order_id   BIGINT UNSIGNED NOT NULL,
-    product_id BIGINT UNSIGNED NOT NULL,
-    quantity   BIGINT UNSIGNED NOT NULL,
+    id         BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    order_id   BIGINT NOT NULL,
+    product_id BIGINT NOT NULL,
+    quantity   BIGINT NOT NULL,
     FOREIGN KEY (order_id) REFERENCES orders (id),
     FOREIGN KEY (product_id) REFERENCES product (id)
 );
