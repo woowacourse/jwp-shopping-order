dd099206b1f27ffba7e7ad1d4c0f7cf3f258f0bc
BGuga <tmdwns9913@gmail.com>
2023-06-01 20:24:43 +0900

    feat: 주문 api 작성

--- src/main/java/cart/ui/OrderApiController.java
+++ src/main/java/cart/ui/OrderApiController.java
@@ -3,12 +3,18 @@ package cart.ui;
 import cart.application.OrderService;
 import cart.domain.Member;
 import cart.dto.request.OrderRequestDto;
+import cart.dto.response.OrderDetail;
+import cart.dto.response.OrderInfo;
 import cart.dto.response.OrderResponseDto;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/orders")
@@ -21,7 +27,17 @@ public class OrderApiController {
     }
 
     @PostMapping
-    private ResponseEntity<OrderResponseDto> order(final Member member, final @RequestBody OrderRequestDto orderRequestDto) {
+    public ResponseEntity<OrderResponseDto> order(final Member member, final @RequestBody OrderRequestDto orderRequestDto) {
         return ResponseEntity.ok(orderService.order(member, orderRequestDto));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<OrderInfo>> orderList(final Member member) {
+        return ResponseEntity.ok(orderService.findOrderOf(member));
+    }
+
+    @GetMapping("/{orderId}")
+    public ResponseEntity<OrderDetail> orderDetail(final Member member, @PathVariable final Integer orderId) {
+        return ResponseEntity.ok(orderService.findOrderDetail(member, orderId));
     }
 }
