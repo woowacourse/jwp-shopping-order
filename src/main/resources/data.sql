accbb82db680c0eea028ce2a5ff636971721adea
hectick <ch.yeondev@gmail.com>
2023-06-01 15:15:44 +0900

    feat: 더미 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,20 @@
 INSERT INTO product (name, price, image_url) VALUES ('치킨', 10000, 'https://images.unsplash.com/photo-1626082927389-6cd097cdc6ec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80');
 INSERT INTO product (name, price, image_url) VALUES ('샐러드', 20000, 'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80');
 INSERT INTO product (name, price, image_url) VALUES ('피자', 13000, 'https://images.unsplash.com/photo-1595854341625-f33ee10dbf94?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80');
+INSERT INTO product (name, price, image_url) VALUES ('스네이크후르츠맛 쿠키', 360000, 'https://i.namu.wiki/i/y1gSed-Qkl7yfDtP_V6IcKx0r3SwfUqkkMbdW1En_YqgZ0VKtYCQ4jnhYTbVAmLhq8KaGfxirN-FDkJdo6UjUUY4LKWJmnxsfbmsJ_XdiEV5sj5DMDzmpo7hy7_VHTCgsBwQOy0Q6X3t1uTCbdU22Q.webp');
+INSERT INTO product (name, price, image_url) VALUES ('용감한 쿠키', 10000, 'https://i.namu.wiki/i/ihoqFNM0IFbbKUUIDaHRt0zfnKltRedhq38y75gEBUIwSptzv2-SK-4JOmAeMIPYyAiikkG25HQRjzuFTa3Iz9ZTjWkeGhbJfZmVHZRUMvIE-9QXFeFNnkyA3jHKWsgj7-yewoMXFBZeJsRbCddR0Q.webp');
+
+INSERT INTO product (name, price, image_url) VALUES ('딸기맛 쿠키', 15000, 'https://i.namu.wiki/i/EsDK5g6TDdpfRpcFwKPAwW5350jgrPrQIRO7za30gWALykMCgK94f0mmhhWKNt58AzLlni_VSHBrq4QUNsd63QfeW3UtFW8IgX01mrZVSQVrlMnqney4bJLyNPlgkiclpa743EuAAhPQ1kk88UXuZA.webp');
+INSERT INTO product (name, price, image_url) VALUES ('백련 드래곤 쿠키', 500000, 'https://i.namu.wiki/i/0C45lplfn_6xy4szKeLSyo7UwGOwwPm3IppZJ_GNsuxMgNTh4OGEfqCtj6tyKMAukjO6BOCjIBK0QXHVLjPnes9rmf_If_jqjhp-eL6y2jCVb9Xs7kqKZ73dSPM-WEtzD864Z-1H-b0JDWLM2gEThQ.webp');
+INSERT INTO product (name, price, image_url) VALUES ('시간지기 쿠키', 60000000, 'https://i.namu.wiki/i/y-A3MAxdHpVzn0X074tP-Q-kMpwSVhbfOMroUxFRI-YoxIWx5V6pDubm0HmF-s18zXrxZAY1q250I70l3wut--Pxm_oC2afrmthfw72fWZNO9F6SgAk3pi9pwVdlkrIH9rN1Ox-dBaPweAX7GVXRXQ.webp');
+INSERT INTO product (name, price, image_url) VALUES ('크루아상맛 쿠키', 440000, 'https://i.namu.wiki/i/j_GiU0zVm_Yq94nazkL-zIpTBsaOy69i9dvpMUpT1SYtiRJq9mqfggxclBggTwTAcWlr60wl84UVbU1PI_vARA.webp');
+INSERT INTO product (name, price, image_url) VALUES ('리치 드래곤 쿠키', 7000000, 'https://i.namu.wiki/i/vAZ4mbPwSjP9Q6dznSKrU1U4-9awMA0Afda7gJejzsC_zpYMowXviLTJfDuy93FM_jchDKFkfpAMKQnd2BCcA3QuxJQSyvPyOPVnTBNWIFfCr-jZR_e6_bHg8Z4iASSy-Qzbs8j2H3vSz0sro9wyjw.webp');
+
+INSERT INTO product (name, price, image_url) VALUES ('악마맛 쿠키', 400000, 'https://i.namu.wiki/i/6szjK8cVmFVJgngJBaD6a265sWbAF-atGnAas0_bDohvjfi-Rn9iG9xFCDZOFQsTmgS8uEQXL64AlJ-LAq05NIN917VyqD44aFjHUJgB7SG0i830WrteDa6q4YarXZvC9_0y4tJoef5UrIw0kStU-g.webp');
+INSERT INTO product (name, price, image_url) VALUES ('닌자맛 쿠키', 3300, 'https://i.namu.wiki/i/oZi5AaS0sUrZ6LZzQyjtAlDaNHKmEyh1-leupTZnW48cVQF3Oktx4T84eXqP-MWaKYXPtw5eTA-vdjmTfEJ-7S1ttECyg-aC8E5wr1I6kPwT5iHJVv6tql5OH4Z8Wg_U963w0MFEUEHk1WOrqdiwvg.webp');
+INSERT INTO product (name, price, image_url) VALUES ('샤벳상어맛 쿠키', 100000, 'https://i.namu.wiki/i/43db1Wd7-ql5g_2D9a0rPGFb0dPO-jBYhSIE-hfmp63d0EkMVuC2Bm5gilF-9gvYUAdYSzP642Sy9JUFtPSUUkA4P-jKMWXDNkl_2nWd6VO0si908T3_lk0IUw6xsvgnYZ-erzzB-ENspj-HTdWN-w.webp');
+INSERT INTO product (name, price, image_url) VALUES ('허브맛 쿠키', 20000, 'https://i.namu.wiki/i/GHqwGVWw6WU1j0Gp2Rv2f9e-2_9Qb66uG9nOna_bhz4S4TavsoVNuKJuKuFiYz0HKR4hkdfAMjbchptyt63CuaaTKGmtxxh29V9nbdtkL7-DTW18edoNG0Lggt6kaDQwW91J1ROj_nMlYUHFiwzuDA.webp');
+INSERT INTO product (name, price, image_url) VALUES ('천사맛 쿠키', 30000, 'https://i.namu.wiki/i/nyFHUXwj6U06m7zievxa1gP29-Zjt23dOZgH8Ljoo_ATZasP1QgNY3EpUGMTXiYMSsX6g_-mpSa-e-2BsG6CxYuosRlhWI7Yp7ellYv89XdSz91tdCLdBjZJXeDaejUb7xuLckVTw9jL2JtHSUn7wQ.webp');
 
 INSERT INTO member (email, password) VALUES ('a@a.com', '1234');
 INSERT INTO member (email, password) VALUES ('b@b.com', '1234');
