03759740656b594a456f0a52cb4ad839d15ac542
be-student <presentsong@naver.com>
2023-06-01 16:51:23 +0900

    feat: 401, 403 에러 메시지 추가

--- src/main/java/com/woowahan/techcourse/cart/ui/ControllerExceptionHandler.java
+++ src/main/java/com/woowahan/techcourse/cart/ui/ControllerExceptionHandler.java
@@ -2,6 +2,9 @@ package com.woowahan.techcourse.cart.ui;
 
 import com.woowahan.techcourse.cart.exception.AuthenticationException;
 import com.woowahan.techcourse.cart.exception.IllegalMemberException;
+import com.woowahan.techcourse.common.ui.ErrorResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
@@ -13,13 +16,19 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class ControllerExceptionHandler {
 
+    Logger logger = LoggerFactory.getLogger(getClass());
+
     @ExceptionHandler(AuthenticationException.class)
-    public ResponseEntity<Void> handlerAuthenticationException(AuthenticationException e) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    public ResponseEntity<ErrorResponse> handlerAuthenticationException(AuthenticationException e) {
+        logger.warn(e.getMessage());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body(new ErrorResponse("로그인이 필요합니다."));
     }
 
     @ExceptionHandler(IllegalMemberException.class)
-    public ResponseEntity<Void> handleException(IllegalMemberException e) {
-        return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+    public ResponseEntity<ErrorResponse> handleException(IllegalMemberException e) {
+        logger.warn(e.getMessage());
+        return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                .body(new ErrorResponse("권한이 없는 사용자가 접근했습니다"));
     }
 }
