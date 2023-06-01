7769e9ea1b6c1a162e18b53b4a0001d543a65f8a
kokodak <kokodakadokok@gmail.com>
2023-06-01 11:05:57 +0900

    feat: CouponDao in query 기능 추가

--- src/main/java/cart/coupon/infrastructure/dao/CouponDao.java
+++ src/main/java/cart/coupon/infrastructure/dao/CouponDao.java
@@ -2,12 +2,16 @@ package cart.coupon.infrastructure.dao;
 
 import cart.common.annotation.Dao;
 import cart.coupon.infrastructure.entity.CouponEntity;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 
@@ -19,9 +23,11 @@ public class CouponDao {
 
     private final JdbcTemplate jdbcTemplate;
     private final SimpleJdbcInsert simpleJdbcInsert;
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
     public CouponDao(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+        this.namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(jdbcTemplate);
         this.simpleJdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
                 .withTableName("coupon")
                 .usingGeneratedKeyColumns("id");
@@ -43,5 +49,12 @@ public class CouponDao {
     public List<CouponEntity> findAllByMemberId(Long memberId) {
         String sql = "SELECT * FROM coupon WHERE member_id = ?";
         return jdbcTemplate.query(sql, couponRowMapper, memberId);
+    }
+
+    public List<CouponEntity> findAllByIds(List<Long> ids) {
+        String sql = "SELECT * FROM coupon WHERE id IN (:ids)";
+        MapSqlParameterSource source = new MapSqlParameterSource();
+        source.addValue("ids", ids);
+        return namedParameterJdbcTemplate.query(sql, source, couponRowMapper);
     }
 }
