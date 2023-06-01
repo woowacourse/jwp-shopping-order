bb8595f44191628498ae02d41d4d58f9d1b75684
xxeol2 <jhj01177@naver.com>
2023-06-01 11:20:54 +0900

    feat: MemberCoupon 도메인 구현

---
+++ src/main/java/cart/domain/coupon/MemberCoupon.java
@@ -0,0 +1,61 @@
+package cart.domain.coupon;
+
+import cart.domain.cartitem.CartItems;
+import java.time.LocalDateTime;
+
+public class MemberCoupon {
+
+    private final Long id;
+    private final Coupon coupon;
+    private final boolean isUsed;
+    private final LocalDateTime expiredAt;
+    private final LocalDateTime createdAt;
+
+    public MemberCoupon(final Coupon coupon, final LocalDateTime expiredAt) {
+        this(null, coupon, false, expiredAt, null);
+    }
+
+    public MemberCoupon(final Long id, final Coupon coupon, final boolean isUsed, final LocalDateTime expiredAt,
+            final LocalDateTime createdAt) {
+        this.id = id;
+        this.coupon = coupon;
+        this.isUsed = isUsed;
+        this.expiredAt = expiredAt;
+        this.createdAt = createdAt;
+    }
+
+    public boolean isApplicable(final CartItems cartItems) {
+        if (isUsed || !checkValidPeriod()) {
+            return false;
+        }
+        return coupon.isApplicable(cartItems);
+    }
+
+    private boolean checkValidPeriod() {
+        return expiredAt.isAfter(LocalDateTime.now());
+    }
+
+    public int getDiscountPrice(final CartItems cartItems) {
+        return coupon.getDiscountPrice(cartItems);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Coupon getCoupon() {
+        return coupon;
+    }
+
+    public boolean isUsed() {
+        return isUsed;
+    }
+
+    public LocalDateTime getExpiredAt() {
+        return expiredAt;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+}
---
+++ src/test/java/cart/domain/coupon/MemberCouponTest.java
@@ -0,0 +1,81 @@
+package cart.domain.coupon;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+
+import cart.domain.cartitem.CartItems;
+import cart.fixture.CouponFixture.금액_10000원이상_1000원할인;
+import java.time.LocalDateTime;
+import java.util.Collections;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@SuppressWarnings("NonAsciiCharacters")
+class MemberCouponTest {
+
+    @Nested
+    class 적용_가능여부_조회시 {
+
+        @Test
+        void 만료되었으면_거짓() {
+            // given
+            CartItems cartItems = new CartItems(Collections.emptyList());
+            MemberCoupon memberCoupon = new MemberCoupon(1L, 금액_10000원이상_1000원할인.COUPON, false, LocalDateTime.MIN,
+                    LocalDateTime.now());
+
+            // when
+            boolean applicable = memberCoupon.isApplicable(cartItems);
+
+            // then
+            assertThat(applicable).isFalse();
+        }
+
+        @Test
+        void 이미_사용된_쿠폰이면_거짓() {
+            // given
+            CartItems cartItems = new CartItems(Collections.emptyList());
+            MemberCoupon memberCoupon = new MemberCoupon(1L, 금액_10000원이상_1000원할인.COUPON, true, LocalDateTime.MAX,
+                    LocalDateTime.now());
+
+            // when
+            boolean applicable = memberCoupon.isApplicable(cartItems);
+
+            // then
+            assertThat(applicable).isFalse();
+        }
+
+        @Test
+        void 쿠폰_조건과_맞지않으면_거짓() {
+            // given
+            MemberCoupon memberCoupon = new MemberCoupon(1L, 금액_10000원이상_1000원할인.COUPON, false, LocalDateTime.MAX,
+                    LocalDateTime.now());
+            CartItems cartItems = Mockito.mock(CartItems.class);
+            given(cartItems.getTotalPrice()).willReturn(2000);
+
+            // then
+            boolean applicable = memberCoupon.isApplicable(cartItems);
+
+            // then
+            assertThat(applicable).isFalse();
+        }
+
+        @Test
+        void 조건을_만족하면_참() {
+            // given
+            MemberCoupon memberCoupon = new MemberCoupon(1L, 금액_10000원이상_1000원할인.COUPON, false, LocalDateTime.MAX,
+                    LocalDateTime.now());
+            CartItems cartItems = Mockito.mock(CartItems.class);
+            given(cartItems.getTotalPrice()).willReturn(10000);
+
+            // then
+            boolean applicable = memberCoupon.isApplicable(cartItems);
+
+            // then
+            assertThat(applicable).isTrue();
+        }
+    }
+}
