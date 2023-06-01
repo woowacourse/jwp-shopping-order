a8c87949eaad9be0e24fe3f67a49b4ac3356040d
swonny <wonny921@gmail.com>
2023-06-01 14:38:24 +0900

    feat: 데이터베이스 DDL 작성

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,22 +1,41 @@
 CREATE TABLE IF NOT EXISTS product
 (
-    id        BIGINT PRIMARY KEY AUTO_INCREMENT,
-    name      VARCHAR(255) NOT NULL,
-    price     INT          NOT NULL,
-    image_url VARCHAR(255) NOT NULL
+    id                  BIGINT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name                VARCHAR(255)    NOT NULL,
+    price               INT             NOT NULL,
+    image_url           VARCHAR(255)    NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS member
 (
-    id       BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    email    VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
+    id                  BIGINT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email               VARCHAR(255)    NOT NULL UNIQUE,
+    password            VARCHAR(255)    NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS cart_item
 (
-    id         BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    member_id  BIGINT NOT NULL,
-    product_id BIGINT NOT NULL,
-    quantity   INT    NOT NULL
+    id                  BIGINT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    member_id           BIGINT          NOT NULL,
+    product_id          BIGINT          NOT NULL,
+    quantity            INT             NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS orders
+(
+    id                  BIGINT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    member_id           BIGINT          NOT NULL FOREIGN KEY REFERENCES member(id),
+    total_payment       DOUBLE          NOT NULL,
+    used_point          INT             NOT NULL,
+    created_at          TIMESTAMP       NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS order_item
+(
+    id                  BIGINT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    order_id            BIGINT          NOT NULL FOREIGN KEY REFERENCES orders(id),
+    product_name        VARCHAR(255)    NOT NULL,
+    product_price       INT             NOT NULL,
+    product_image_url   VARCHAR(255)    NOT NULL,
+    quantity            INT             NOT NULL
 );
