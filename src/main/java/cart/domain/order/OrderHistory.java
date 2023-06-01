d837c335396e1a5174dd2577ab826f04dfe3e507
HyunSeo Park (Hyena) <goatsfish@gmail.com>
2023-06-01 17:34:58 +0900

    feat: 주문 기록 도메인 추가

---
+++ src/main/java/cart/domain/order/OrderHistory.java
@@ -0,0 +1,75 @@
+package cart.domain.order;
+
+import cart.domain.member.Member;
+import cart.domain.vo.Money;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class OrderHistory {
+
+    private final Long id;
+    private final Member member;
+    private final OrderItems orderItems;
+    private final Money totalPrice;
+    private final Money usePoint;
+    private final LocalDateTime createdAt;
+
+    public OrderHistory(Long id, Member member, OrderItems orderItems, Money totalPrice, Money usePoint, LocalDateTime createdAt) {
+        this.id = id;
+        this.member = member;
+        this.orderItems = orderItems;
+        this.totalPrice = totalPrice;
+        this.usePoint = usePoint;
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public OrderItems getOrderItems() {
+        return orderItems;
+    }
+
+    public Money getTotalPrice() {
+        return totalPrice;
+    }
+
+    public Money getUsePoint() {
+        return usePoint;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderHistory that = (OrderHistory) o;
+        return Objects.equals(member, that.member) && Objects.equals(orderItems, that.orderItems) && Objects.equals(totalPrice, that.totalPrice) && Objects.equals(usePoint, that.usePoint) && Objects.equals(createdAt, that.createdAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(member, orderItems, totalPrice, usePoint, createdAt);
+    }
+
+    @Override
+    public String toString() {
+        return "OrderHistory{" +
+                "id=" + id +
+                ", member=" + member +
+                ", orderItems=" + orderItems +
+                ", totalPrice=" + totalPrice +
+                ", usePoint=" + usePoint +
+                ", createdAt=" + createdAt +
+                '}';
+    }
+}
