e156174c4159c1291c3eb267132c2ec653ad5fcf
hong-sile <gurwns9325@naver.com>
2023-06-01 16:25:07 +0900

    test: 멤버만 데이터베이스에 초기화되있도록 수정

---
+++ src/test/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO member (email, password) VALUES ('a@a.com', '1234');
+INSERT INTO member (email, password) VALUES ('b@b.com', '1234');
+INSERT INTO member (email, password) VALUES ('sangun', '1234');
+INSERT INTO member (email, password) VALUES ('lopi', '1234');
