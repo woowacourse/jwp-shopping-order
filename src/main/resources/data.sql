ba1e47124c5331cd85a710044fd19034a08584e9
Choi-JJunho <junho5336@gmail.com>
2023-06-01 19:34:02 +0900

    chore: 더미데이터 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -56,15 +56,7 @@ INSERT INTO product (name, price, image_url)
 VALUES ('피자', 13000,
         'https://images.unsplash.com/photo-1595854341625-f33ee10dbf94?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80');
 
-INSERT INTO member (email, password)
-VALUES ('a@a.com', '1234');
-INSERT INTO member (email, password)
-VALUES ('b@b.com', '1234');
-
-INSERT INTO cart_item (member_id, product_id, quantity)
-VALUES (1, 1, 2);
-INSERT INTO cart_item (member_id, product_id, quantity)
-VALUES (1, 2, 4);
-
-INSERT INTO cart_item (member_id, product_id, quantity)
-VALUES (2, 3, 5);
+INSERT INTO member (email, password, money, point)
+VALUES ('a@a.com', '1234', 1000000, 50000);
+INSERT INTO member (email, password, money, point)
+VALUES ('b@b.com', '1234', 1000000, 50000);
