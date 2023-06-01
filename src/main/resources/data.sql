22964605c38d6f4a553e186f7076d8de3d151b83
nuyh <hjaehyun25@gmail.com>
2023-06-01 21:43:18 +0900

    build: 회원 테이블에 등급 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,7 +8,9 @@ INSERT INTO product (name, price, image_url)
 VALUES ('피자', 13000,
         'https://images.unsplash.com/photo-1595854341625-f33ee10dbf94?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80');
 
-INSERT INTO member (email, password)
-VALUES ('a@a.com', '1234');
-INSERT INTO member (email, password)
-VALUES ('b@b.com', '1234');
+INSERT INTO member (email, password, grade)
+VALUES ('a@a.com', '1234', 'GOLD');
+INSERT INTO member (email, password, grade)
+VALUES ('b@b.com', '1234', 'SILVER');
+INSERT INTO member (email, password, grade)
+VALUES ('c@c.com', '1234', 'BRONZE');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -10,7 +10,8 @@ CREATE TABLE IF NOT EXISTS member
 (
     id       BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email    VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    grade    VARCHAR(20)
 );
 
 CREATE TABLE IF NOT EXISTS cart_item
