c6ccdf73ceeb8c618b082b95cb24a7be7fd22d05
dev_kong <ahdjd5@gmail.com>
2023-06-01 21:27:11 +0900

    refa: memberRepository 기능 구현

---
+++ src/main/java/cart/repository/MemberRepository.java
@@ -0,0 +1,21 @@
+package cart.repository;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import cart.dao.MemberDao;
+import cart.domain.Member;
+
+@Repository
+public class MemberRepository {
+    private final MemberDao memberDao;
+
+    public MemberRepository(MemberDao memberDao) {
+        this.memberDao = memberDao;
+    }
+
+    public List<Member> findAll() {
+        return memberDao.findAll();
+    }
+}
