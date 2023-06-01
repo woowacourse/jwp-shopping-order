57965419c659eec3bbcc3a5ecadaaedf3a26b1f0
greeng00se <thegreengoose7@gmail.com>
2023-06-01 13:38:56 +0900

    chore: db schema, data 설정 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,33 +1,33 @@
-INSERT INTO PRODUCT (name, image_url, price)
+INSERT INTO product (name, image_url, price)
 VALUES ('피자스쿨 치즈피자', 'https://t1.daumcdn.net/cfile/tistory/2647BE3754B7E8B733', 8900);
-INSERT INTO PRODUCT (name, image_url, price)
+INSERT INTO product (name, image_url, price)
 VALUES ('도미노 치즈피자', 'https://cdn.dominos.co.kr/admin/upload/goods/20200311_TI57KvOH.jpg', 23900);
-INSERT INTO PRODUCT (name, image_url, price)
+INSERT INTO product (name, image_url, price)
 VALUES ('서오릉 치즈피자', 'https://blog.kakaocdn.net/dn/bLtveJ/btqTJT1U7rX/sNzWENAmkZGyG5vOeknzzk/img.jpg', 20000);
-INSERT INTO PRODUCT (name, image_url, price)
+INSERT INTO product (name, image_url, price)
 VALUES ('도미노 페페로니피자', 'https://cdn.dominos.co.kr/admin/upload/goods/20200311_x8StB1t3.jpg', 25900);
 
-INSERT INTO MEMBER (email, password)
+INSERT INTO member (email, password)
 VALUES ('pizza1@pizza.com', 'pizza');
-INSERT INTO MEMBER (email, password)
+INSERT INTO member (email, password)
 VALUES ('pizza2@pizza.com', 'pizza');
 
-INSERT INTO COUPON (name, policy_type, discount_value, minimum_price)
+INSERT INTO coupon (name, policy_type, discount_value, minimum_price)
 VALUES ('30000원 이상 3000원 할인 쿠폰', 'PRICE', 3000, 30000);
-INSERT INTO COUPON (name, policy_type, discount_value, minimum_price)
+INSERT INTO coupon (name, policy_type, discount_value, minimum_price)
 VALUES ('30000원 이상 4000원 할인 쿠폰', 'PRICE', 4000, 30000);
 
-INSERT INTO MEMBER_COUPON (coupon_id, member_id, used)
+INSERT INTO member_coupon (coupon_id, member_id, used)
 values (1, 1, false);
-INSERT INTO MEMBER_COUPON (coupon_id, member_id, used)
+INSERT INTO member_coupon (coupon_id, member_id, used)
 values (2, 1, false);
-INSERT INTO MEMBER_COUPON (coupon_id, member_id, used)
+INSERT INTO member_coupon (coupon_id, member_id, used)
 values (1, 2, false);
-INSERT INTO MEMBER_COUPON (coupon_id, member_id, used)
+INSERT INTO member_coupon (coupon_id, member_id, used)
 values (2, 2, false);
 
-INSERT INTO ORDERS (delivery_fee, member_coupon_id, member_id)
+INSERT INTO orders (delivery_fee, member_coupon_id, member_id)
 values (3000, 1, 1);
 
-INSERT INTO ORDER_ITEM (name, image_url, price, quantity, order_id)
+INSERT INTO order_item (name, image_url, price, quantity, order_id)
 values ('피자스쿨 치즈피자', 'https://t1.daumcdn.net/cfile/tistory/2647BE3754B7E8B733', 8900, 1, 1);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,89 +1,89 @@
-CREATE TABLE IF NOT EXISTS `PRODUCT`
+CREATE TABLE IF NOT EXISTS product
 (
-    `id`         long PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    `name`       varchar(255)     NOT NULL,
-    `image_url`  varchar(1024)    NOT NULL,
-    `price`      long             NOT NULL,
-    `created_at` timestamp        NOT NULL DEFAULT (CURRENT_TIMESTAMP),
-    `updated_at` timestamp        NOT NULL DEFAULT (CURRENT_TIMESTAMP) ON UPDATE (CURRENT_TIMESTAMP)
+    id         BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name       varchar(255)       NOT NULL,
+    image_url  varchar(1024)      NOT NULL,
+    price      long               NOT NULL,
+    created_at timestamp          NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp          NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS `MEMBER`
+CREATE TABLE IF NOT EXISTS member
 (
-    `id`         long PRIMARY KEY    NOT NULL AUTO_INCREMENT,
-    `email`      varchar(255) UNIQUE NOT NULL,
-    `password`   varchar(255)        NOT NULL,
-    `created_at` timestamp           NOT NULL DEFAULT (CURRENT_TIMESTAMP),
-    `updated_at` timestamp           NOT NULL DEFAULT (CURRENT_TIMESTAMP) ON UPDATE (CURRENT_TIMESTAMP)
+    id         BIGINT PRIMARY KEY  NOT NULL AUTO_INCREMENT,
+    email      varchar(255) UNIQUE NOT NULL,
+    password   varchar(255)        NOT NULL,
+    created_at timestamp           NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp           NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS `CART_ITEM`
+CREATE TABLE IF NOT EXISTS cart_item
 (
-    `id`         long PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    `member_id`  long             NOT NULL,
-    `product_id` long             NOT NULL,
-    `quantity`   long             NOT NULL,
-    `created_at` timestamp        NOT NULL DEFAULT (CURRENT_TIMESTAMP),
-    `updated_at` timestamp        NOT NULL DEFAULT (CURRENT_TIMESTAMP) ON UPDATE (CURRENT_TIMESTAMP)
+    id         BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    member_id  BIGINT             NOT NULL,
+    product_id BIGINT             NOT NULL,
+    quantity   long               NOT NULL,
+    created_at timestamp          NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp          NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS `ORDERS`
+CREATE TABLE IF NOT EXISTS orders
 (
-    `id`               long PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    `delivery_fee`     long             NOT NULL,
-    `member_coupon_id` long,
-    `member_id`        long             NOT NULL,
-    `created_at`       timestamp        NOT NULL DEFAULT (CURRENT_TIMESTAMP),
-    `updated_at`       timestamp        NOT NULL DEFAULT (CURRENT_TIMESTAMP) ON UPDATE (CURRENT_TIMESTAMP)
+    id               BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    delivery_fee     long               NOT NULL,
+    member_coupon_id BIGINT,
+    member_id        BIGINT             NOT NULL,
+    created_at       timestamp          NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at       timestamp          NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS `ORDER_ITEM`
+CREATE TABLE IF NOT EXISTS order_item
 (
-    `id`         long PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    `name`       varchar(255)     NOT NULL,
-    `image_url`  varchar(1024)    NOT NULL,
-    `price`      long             NOT NULL,
-    `quantity`   long             NOT NULL,
-    `order_id`   long             NOT NULL,
-    `created_at` timestamp        NOT NULL DEFAULT (CURRENT_TIMESTAMP),
-    `updated_at` timestamp        NOT NULL DEFAULT (CURRENT_TIMESTAMP) ON UPDATE (CURRENT_TIMESTAMP)
+    id         BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name       varchar(255)       NOT NULL,
+    image_url  varchar(1024)      NOT NULL,
+    price      long               NOT NULL,
+    quantity   long               NOT NULL,
+    order_id   BIGINT             NOT NULL,
+    created_at timestamp          NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp          NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS `COUPON`
+CREATE TABLE IF NOT EXISTS coupon
 (
-    `id`             long PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    `name`           varchar(255)     NOT NULL,
-    `policy_type`    varchar(255)     NOT NULL,
-    `discount_value` long             NOT NULL,
-    `minimum_price`  long             NOT NULL,
-    `created_at`     timestamp        NOT NULL DEFAULT (CURRENT_TIMESTAMP),
-    `updated_at`     timestamp        NOT NULL DEFAULT (CURRENT_TIMESTAMP) ON UPDATE (CURRENT_TIMESTAMP)
+    id             BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name           varchar(255)       NOT NULL,
+    policy_type    varchar(255)       NOT NULL,
+    discount_value long               NOT NULL,
+    minimum_price  long               NOT NULL,
+    created_at     timestamp          NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at     timestamp          NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS `MEMBER_COUPON`
+CREATE TABLE IF NOT EXISTS member_coupon
 (
-    `id`         long PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    `coupon_id`  long             NOT NULL,
-    `member_id`  long             NOT NULL,
-    `used`       boolean          NOT NULL,
-    `created_at` timestamp        NOT NULL DEFAULT (CURRENT_TIMESTAMP),
-    `updated_at` timestamp        NOT NULL DEFAULT (CURRENT_TIMESTAMP) ON UPDATE (CURRENT_TIMESTAMP)
+    id         BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    coupon_id  BIGINT             NOT NULL,
+    member_id  BIGINT             NOT NULL,
+    used       boolean            NOT NULL,
+    created_at timestamp          NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp          NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-ALTER TABLE `CART_ITEM`
-    ADD FOREIGN KEY (`member_id`) REFERENCES `MEMBER` (`id`);
+ALTER TABLE cart_item
+    ADD FOREIGN KEY (member_id) REFERENCES member (id);
 
-ALTER TABLE `CART_ITEM`
-    ADD FOREIGN KEY (`product_id`) REFERENCES `PRODUCT` (`id`);
+ALTER TABLE cart_item
+    ADD FOREIGN KEY (product_id) REFERENCES product (id);
 
-ALTER TABLE `ORDERS`
-    ADD FOREIGN KEY (`member_id`) REFERENCES `MEMBER` (`id`);
+ALTER TABLE orders
+    ADD FOREIGN KEY (member_id) REFERENCES member (id);
 
-ALTER TABLE `ORDER_ITEM`
-    ADD FOREIGN KEY (`order_id`) REFERENCES `ORDERS` (`id`);
+ALTER TABLE order_item
+    ADD FOREIGN KEY (order_id) REFERENCES orders (id);
 
-ALTER TABLE `MEMBER_COUPON`
-    ADD FOREIGN KEY (`coupon_id`) REFERENCES `COUPON` (`id`);
+ALTER TABLE member_coupon
+    ADD FOREIGN KEY (coupon_id) REFERENCES coupon (id);
 
-ALTER TABLE `MEMBER_COUPON`
-    ADD FOREIGN KEY (`member_id`) REFERENCES `MEMBER` (`id`);
+ALTER TABLE member_coupon
+    ADD FOREIGN KEY (member_id) REFERENCES member (id);
