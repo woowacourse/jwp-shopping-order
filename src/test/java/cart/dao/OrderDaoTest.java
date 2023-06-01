11cf3fc241d312d0be9486753e79ed5158363752
LJW25 <dbjwlee@gmail.com>
2023-06-01 21:18:54 +0900

    test: DB 주문 목록 페이지 단위 조회 테스트 구현

--- src/test/java/cart/dao/OrderDaoTest.java
+++ src/test/java/cart/dao/OrderDaoTest.java
@@ -9,6 +9,8 @@ import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.jdbc.Sql;
 
+import java.util.List;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
@@ -19,10 +21,10 @@ class OrderDaoTest {
 
     OrderDao orderDao;
 
-
     private OrderDaoTest(@Autowired final JdbcTemplate jdbcTemplate) {
         this.orderDao = new OrderDao(jdbcTemplate);
     }
+
 
     @DisplayName("주문 저장")
     @Test
@@ -45,6 +47,28 @@ class OrderDaoTest {
                 () -> assertThat(orderEntity.getId()).isEqualTo(id),
                 () -> assertThat(orderEntity.getMemberId()).isEqualTo(Fixture.order1.getMemberId()),
                 () -> assertThat(orderEntity.getTotalPrice()).isEqualTo(Fixture.order1.getTotalPrice())
+        );
+    }
+
+    @DisplayName("30개의 목록에서 내역을 인덱스로 조회한다")
+    @Test
+    void findByIndexRange() {
+        // given
+        for (int i = 0; i < 30; i++) {
+            orderDao.insert(Fixture.order1);
+        }
+
+        // when
+        final List<OrderEntity> orderEntitiesZero = orderDao.findByIndexRange(1L, 0);
+        final List<OrderEntity> orderEntitiesTen = orderDao.findByIndexRange(1L, 10);
+        final List<OrderEntity> orderEntitiesTwenty = orderDao.findByIndexRange(1L, 20);
+
+        // then
+        assertAll(
+                () -> assertThat(orderEntitiesZero.size()).isEqualTo(20),
+                () -> assertThat(orderEntitiesTen.size()).isEqualTo(20),
+                () -> assertThat(orderEntitiesTwenty.size()).isEqualTo(10),
+                () -> assertThat(orderEntitiesZero.get(0).getId() - orderEntitiesZero.get(19).getId()).isEqualTo(19L)
         );
     }
 }
