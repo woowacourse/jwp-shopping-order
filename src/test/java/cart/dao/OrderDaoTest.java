9dfd827004ed113a0a25a32128f2687da6032217
LJW25 <dbjwlee@gmail.com>
2023-06-01 19:49:15 +0900

    test: DB 주문 조회 테스트 작성

--- src/test/java/cart/dao/OrderDaoTest.java
+++ src/test/java/cart/dao/OrderDaoTest.java
@@ -1,6 +1,7 @@
 package cart.dao;
 
 import cart.Fixture;
+import cart.entity.OrderEntity;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.jdbc.Sql;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 
 @JdbcTest
@@ -27,5 +29,22 @@ class OrderDaoTest {
     void insert() {
         // when & then
         assertThat(orderDao.insert(Fixture.order1)).isPositive();
+    }
+
+    @DisplayName("ID로 주문 조회")
+    @Test
+    void findById() {
+        // given
+        final Long id = orderDao.insert(Fixture.order1);
+
+        // when
+        final OrderEntity orderEntity = orderDao.findById(id);
+
+        // then
+        assertAll(
+                () -> assertThat(orderEntity.getId()).isEqualTo(id),
+                () -> assertThat(orderEntity.getMemberId()).isEqualTo(Fixture.order1.getMemberId()),
+                () -> assertThat(orderEntity.getTotalPrice()).isEqualTo(Fixture.order1.getTotalPrice())
+        );
     }
 }
--- src/test/java/cart/dao/OrderItemDaoTest.java
+++ src/test/java/cart/dao/OrderItemDaoTest.java
@@ -1,6 +1,7 @@
 package cart.dao;
 
 import cart.Fixture;
+import cart.domain.OrderItem;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +9,10 @@ import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.jdbc.Sql;
 
+import java.util.List;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @JdbcTest
 @Sql("classpath:schema.sql")
@@ -30,5 +34,22 @@ class OrderItemDaoTest {
 
         // when & then
         assertThat(orderItemDao.insert(orderId, Fixture.orderItem1)).isPositive();
+    }
+
+    @DisplayName("주문 ID로 주문 상품 목록 조회")
+    @Test
+    void findByOrderId() {
+        // given
+        final Long orderId = orderDao.insert(Fixture.order1);
+        orderItemDao.insert(orderId, Fixture.orderItem1);
+
+        // when
+        final List<OrderItem> actual = orderItemDao.findByOrderId(orderId);
+
+        // then
+        assertAll(
+                () -> assertThat(actual.size()).isEqualTo(1),
+                () -> assertThat(actual.get(0).getName()).isEqualTo(Fixture.orderItem1.getName())
+        );
     }
 }
