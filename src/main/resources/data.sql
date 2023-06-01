120df0d8a58649fd3810a0f075e668e598645426
HyunSeo Park (Hyena) <goatsfish@gmail.com>
2023-06-02 08:23:49 +0900

    chore: mysql 데이터 삭제 쿼리 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
-TRUNCATE TABLE order_item RESTART IDENTITY;
-TRUNCATE TABLE orders RESTART IDENTITY;
-TRUNCATE TABLE cart_item RESTART IDENTITY;
-TRUNCATE TABLE product RESTART IDENTITY;
-TRUNCATE TABLE member RESTART IDENTITY;
+TRUNCATE TABLE order_item;
+TRUNCATE TABLE orders;
+TRUNCATE TABLE cart_item;
+TRUNCATE TABLE product;
+TRUNCATE TABLE member;
 
 INSERT INTO product (name, price, image_url) VALUES ('치킨', 10000, 'https://images.unsplash.com/photo-1626082927389-6cd097cdc6ec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80');
 INSERT INTO product (name, price, image_url) VALUES ('샐러드', 20000, 'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80');
