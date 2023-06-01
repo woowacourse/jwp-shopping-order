66cb8a4d6ed3b8cba00b5bd1abf21913aceaeb13
Ethan <kim76744dev@gmail.com>
2023-06-01 17:50:59 +0900

    style: 예외 메세지 변경

--- src/main/java/cart/exception/CannotChangeCouponStatusException.java
+++ src/main/java/cart/exception/CannotChangeCouponStatusException.java
@@ -3,6 +3,6 @@ package cart.exception;
 public class CannotChangeCouponStatusException extends IllegalArgumentException {
 
     public CannotChangeCouponStatusException() {
-        super("사용하지 않은 쿠폰을 삭제할 수 없습니다.");
+        super("사용하지 않은 쿠폰을 재발급 할 수 없습니다.");
     }
 }
