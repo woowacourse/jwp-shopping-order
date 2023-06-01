67f91be88fcd315886d261ad619adde993bce948
70825 <hello70825@gmail.com>
2023-06-01 21:42:10 +0900

    feat: 기본 데이터 추가

--- src/main/java/cart/dto/OrderItemDto.java
+++ src/main/java/cart/dto/OrderItemDto.java
@@ -2,26 +2,28 @@ package cart.domain;
 
 import cart.dto.OrderCartItemDto;
 
-public class OrderItem {
+public class OrderItemDto {
 
     private final Long id;
     private final CartOrder cartOrder;
+    private final Long productId;
     private final String name;
     private final int price;
     private final String imageUrl;
     private final int quantity;
 
-    public OrderItem(final Long id, final CartOrder cartOrder, final String name, final int price, final String imageUrl, final int quantity) {
+    public OrderItemDto(final Long id, final CartOrder cartOrder, final Long productId, final String name, final int price, final String imageUrl, final int quantity) {
         this.id = id;
         this.cartOrder = cartOrder;
+        this.productId = productId;
         this.name = name;
         this.price = price;
         this.imageUrl = imageUrl;
         this.quantity = quantity;
     }
 
-    public static OrderItem of(final CartOrder cartOrder, final OrderCartItemDto orderCartItemDto, final int quantity) {
-        return new OrderItem(
+    public static OrderItemDto of(final CartOrder cartOrder, final OrderCartItemDto orderCartItemDto, final int quantity) {
+        return new OrderItemDto(
                 null,
                 cartOrder,
                 orderCartItemDto.getOrderCartItemName(),
@@ -37,6 +39,10 @@ public class OrderItem {
 
     public CartOrder getCartOrder() {
         return cartOrder;
+    }
+
+    public Long getProductId() {
+        return productId;
     }
 
     public String getName() {
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,8 +5,7 @@ VALUES ('치킨', 10000, 'https://images.unsplash.com/photo-1626082927389-6cd097
        ('팬케이크', 8000, 'https://images.unsplash.com/photo-1544726982-b414d58fabaa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8JUVEJThDJUFDJUVDJUJDJTgwJUVDJTlEJUI0JUVEJTgxJUFDfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60');
 
 INSERT INTO member (email, password, cash)
-VALUES ('a@a.com', '1234', 500),
-       ('dooly@dooly.com', '1234', 1200000),
+VALUES ('dooly@dooly.com', '1234', 1200000),
        ('ber@ber.com', '1234', 850000),
        ('bixx@bixx.com', '1234', 1500000);
 
@@ -21,9 +20,18 @@ VALUES (1, 1, 2),
 INSERT INTO cart_order (member_id, total_price)
 VALUES (1, 20000),
        (2, 50000),
-       (3, 100000);
+       (3, 100000),
+       (1, 40000),
+       (2, 40000),
+       (3, 40000);
 
 INSERT INTO order_item (cart_order_id, product_id, name, price, image_url, quantity)
 VALUES (1, 1, '치킨', 10000, 'https://images.unsplash.com/photo-1626082927389-6cd097cdc6ec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80', 2),
        (2, 1, '치킨', 10000, 'https://images.unsplash.com/photo-1626082927389-6cd097cdc6ec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80', 5),
-       (3, 2, '샐러드', 20000, 'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80', 5);
+       (3, 2, '샐러드', 20000, 'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80', 5),
+       (4, 1, '치킨', 10000, 'https://images.unsplash.com/photo-1626082927389-6cd097cdc6ec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80', 2),
+       (4, 2, '샐러드', 20000, 'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80', 1),
+       (5, 1, '치킨', 10000, 'https://images.unsplash.com/photo-1626082927389-6cd097cdc6ec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80', 2),
+       (5, 2, '샐러드', 20000, 'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80', 1),
+       (6, 1, '치킨', 10000, 'https://images.unsplash.com/photo-1626082927389-6cd097cdc6ec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80', 2),
+       (6, 2, '샐러드', 20000, 'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80', 1);
