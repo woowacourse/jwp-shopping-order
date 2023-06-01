eee4cd30fd98b8b1ed902a486f19a0da7d6458da
iamjooon2 <iamjooon2@gmail.com>
2023-06-01 21:36:07 +0900

    feat: @GetMapping 어노테이션 추가

--- src/main/java/cart/ui/api/MemberApiController.java
+++ src/main/java/cart/ui/api/MemberApiController.java
@@ -4,6 +4,7 @@ import cart.application.MemberService;
 import cart.dto.response.MemberResponse;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,7 @@ public class MemberApiController {
         this.memberService = memberService;
     }
 
+    @GetMapping
     public ResponseEntity<List<MemberResponse>> getAllMember(){
         final List<MemberResponse> memberResponses = memberService.getAll();
         return ResponseEntity.ok(memberResponses);
