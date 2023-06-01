d49f25df775282c20c0659f3911d65c792b38e99
mcodnjs <codnjs8989@gmail.com>
2023-06-02 03:35:49 +0900

    feat: Product 검증 로직 추가

--- src/main/java/cart/domain/Product.java
+++ src/main/java/cart/domain/Product.java
@@ -1,22 +1,38 @@
 package cart.domain;
 
 public class Product {
-    private Long id;
-    private String name;
-    private int price;
-    private String imageUrl;
+    private final Long id;
+    private final String name;
+    private final int price;
+    private final String imageUrl;
 
     public Product(String name, int price, String imageUrl) {
+        this(null, name, price, imageUrl);
+    }
+
+    public Product(Long id, String name, int price, String imageUrl) {
+        validate(name, price);
+        this.id = id;
         this.name = name;
         this.price = price;
         this.imageUrl = imageUrl;
     }
 
-    public Product(Long id, String name, int price, String imageUrl) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-        this.imageUrl = imageUrl;
+    private void validate(final String name, final int price) {
+        validateName(name);
+        validatePrice(price);
+    }
+
+    private void validatePrice(final int price) {
+        if (price < 1 || price > 10_000_000) {
+            throw new IllegalArgumentException("상품 가격은 1원 이상 10,000,000원 이하로 입력해주세요.");
+        }
+    }
+
+    private void validateName(final String name) {
+        if (name.length() < 1 || name.length() > 20) {
+            throw new IllegalArgumentException("상품 이름은 1글자 이상 20글자 이하로 입력해주세요.");
+        }
     }
 
     public Long getId() {
