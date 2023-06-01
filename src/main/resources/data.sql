1774e4e81502070e0e94f1f24813bfad1fd24dea
donghae-kim <donghae1999@gmail.com>
2023-06-01 22:29:22 +0900

    feat: CORS 설정 및 DB 설정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,20 @@
-INSERT INTO product (name, price, image_url) VALUES ('치킨', 10000, 'https://images.unsplash.com/photo-1626082927389-6cd097cdc6ec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80');
-INSERT INTO product (name, price, image_url) VALUES ('샐러드', 20000, 'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80');
-INSERT INTO product (name, price, image_url) VALUES ('피자', 13000, 'https://images.unsplash.com/photo-1595854341625-f33ee10dbf94?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80');
+INSERT INTO product (name, price, image_url) VALUES ('바다',10000, 'https://img.lovepik.com/photo/20211119/large/lovepik-the-sea-picture_500143289.jpg');
+INSERT INTO product (name, price, image_url) VALUES ('우아한형제들',90000, 'https://image.kmib.co.kr/online_image/2021/1217/2021121717103643262_1639728637_0016582097.jpg');
+INSERT INTO product (name, price, image_url) VALUES ('네이버',100000, 'https://www.navercorp.com/img/ko/og/logo.png');
+INSERT INTO product (name, price, image_url) VALUES ('카카오',80000, 'https://t1.kakaocdn.net/kakaocorp/corp_thumbnail/Kakao.png');
+INSERT INTO product (name, price, image_url) VALUES ('라인',70000, 'https://vos.line-scdn.net/strapi-cluster-instance-bucket-84/brand_03_2e76928f15.jpeg');
+INSERT INTO product (name, price, image_url) VALUES ('야놀자',60000, 'https://yanolja.in/static/share/share_thumb_1200x630.jpg');
+INSERT INTO product (name, price, image_url) VALUES ('당근마켓',50000, 'https://img.sbs.co.kr/newimg/news/20201027/201484632.jpg');
+INSERT INTO product (name, price, image_url) VALUES ('토스',90000, 'https://cdn.bizwatch.co.kr/news/photo/2022/09/05/b056d82a574f458946063dd8d2740334.jpg');
+INSERT INTO product (name, price, image_url) VALUES ('두나무',40000, 'https://media.licdn.com/dms/image/C510BAQEcBzvlhAv6VQ/company-logo_200_200/0/1576729756780?e=2147483647&v=beta&t=UlFn7z0aP9AYZNrAW9Ktrdv1PRT9oTc3SWLnG_nBZS4');
+INSERT INTO product (name, price, image_url) VALUES ('오늘의집',10000, 'https://s3-ap-northeast-1.amazonaws.com/bucketplace-v2-development/uploads/default_images/open_graph_icon_2.png');
+INSERT INTO product (name, price, image_url) VALUES ('쿠팡',5000, 'https://blog.kakaocdn.net/dn/6ui8u/btqDtzRrCzg/ZM8q87L5frjEl0za6UnIk1/img.jpg');
+INSERT INTO product (name, price, image_url) VALUES ('몰로코',3000, 'https://ditoday.com/wp-content/uploads/2021/02/digital_insight_moloco_logo.jpg');
 
 INSERT INTO member (email, password) VALUES ('a@a.com', '1234');
 INSERT INTO member (email, password) VALUES ('b@b.com', '1234');
 
-INSERT INTO cart_item (member_id, product_id, quantity) VALUES (1, 1, 2);
-INSERT INTO cart_item (member_id, product_id, quantity) VALUES (1, 2, 4);
-
-INSERT INTO cart_item (member_id, product_id, quantity) VALUES (2, 3, 5);
+INSERT INTO coupon (`name`, discount_type,minimum_price, discount_price, discount_rate) VALUES ('5000원 할인 쿠폰', 'reduction',10000,5000,0);
+INSERT INTO coupon (`name`, discount_type,minimum_price, discount_price, discount_rate) VALUES ('10000 할인 쿠폰', 'reduction',10000,10000,0);
+INSERT INTO coupon (`name`, discount_type,minimum_price, discount_price, discount_rate) VALUES ('50% 할인 쿠폰', 'percent',5000,0,0.5);
+INSERT INTO coupon (`name`, discount_type,minimum_price, discount_price, discount_rate) VALUES ('20% 할인 쿠폰', 'percent',5000,0,0.2);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,21 +1,66 @@
-CREATE TABLE product (
+CREATE TABLE if not exists product (
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     price INT NOT NULL,
     image_url VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE member (
-     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-     email VARCHAR(255) NOT NULL UNIQUE,
-     password VARCHAR(255) NOT NULL
+CREATE TABLE if not exists member (
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE cart_item (
+CREATE TABLE if not exists cart_item (
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     member_id BIGINT NOT NULL,
     product_id BIGINT NOT NULL,
     quantity INT NOT NULL,
     FOREIGN KEY (member_id) REFERENCES member(id),
     FOREIGN KEY (product_id) REFERENCES product(id)
+);
+
+CREATE TABLE if not exists coupon(
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(255) NOT NULL,
+    discount_type varchar(255) NOT NULL,
+    minimum_price INT NOT NULL,
+    discount_price INT NOT NULL,
+    discount_rate DOUBLE NOT NULL
+);
+
+CREATE TABLE if not exists member_coupon(
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    coupon_id BIGINT NOT NULL,
+    member_id BIGINT NOT NULL,
+    availability BOOLEAN NOT NULL,
+    FOREIGN KEY (coupon_id) REFERENCES coupon(id),
+    FOREIGN KEY (member_id) REFERENCES member(id)
+);
+
+CREATE TABLE if not exists orders(
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    member_id BIGINT NOT NULL,
+    original_price INT NOT NULL,
+    discount_price INT NOT NULL,
+    confirm_state BOOLEAN NOT NULL,
+    FOREIGN KEY (member_id) REFERENCES member(id)
+);
+
+CREATE TABLE if not exists member_coupon(
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    coupon_id BIGINT NOT NULL,
+    member_id BIGINT NOT NULL,
+    FOREIGN KEY (coupon_id) REFERENCES coupon(id),
+    FOREIGN KEY (member_id) REFERENCES member(id)
+);
+
+CREATE TABLE if not exists order_product(
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name varchar(255) NOT NULL,
+    image_url varchar(255) NOT NULL,
+    price INT NOT NULL,
+    quantity INT NOT NULL,
+    order_id BIGINT NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES orders(id) on delete cascade
 );
