da38303d8c2fd959607bc7b41c725adf4c516b2a
nuyh <hjaehyun25@gmail.com>
2023-06-01 20:39:04 +0900

    feat: 회원 등급별 할인 정책 추가

---
+++ src/main/java/cart/domain/MemberGrade.java
@@ -0,0 +1,7 @@
+package cart.domain;
+
+public enum MemberGrade {
+    GOLD,
+    SILVER,
+    BRONZE;
+}
---
+++ src/main/java/cart/domain/discount/MemberGradeDiscountPolicy.java
@@ -0,0 +1,32 @@
+package cart.domain.discount;
+
+import cart.domain.MemberGrade;
+
+import static cart.domain.MemberGrade.*;
+
+public final class MemberGradeDiscountPolicy implements DiscountPolicy {
+    private static final double GOLD_DISCOUNT_RATE = 0.05;
+    private static final double SILVER_DISCOUNT_RATE = 0.03;
+    private static final double BRONZE_DISCOUNT_RATE = 0.01;
+
+    private final MemberGrade memberGrade;
+
+    public MemberGradeDiscountPolicy(final MemberGrade memberGrade) {
+        this.memberGrade = memberGrade;
+    }
+
+    @Override
+    public int calculateDiscountAmount(final int price) {
+        if (GOLD.equals(memberGrade)) {
+            return (int) (price * GOLD_DISCOUNT_RATE);
+        }
+        if (SILVER.equals(memberGrade)) {
+            return (int) (price * SILVER_DISCOUNT_RATE);
+        }
+        if (BRONZE.equals(memberGrade)) {
+            return (int) (price * BRONZE_DISCOUNT_RATE);
+        }
+
+        return 0;
+    }
+}
