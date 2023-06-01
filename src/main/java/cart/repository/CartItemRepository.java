e776de6b442ad647dbbcb6569db5d21fc1e67a0a
dev_kong <ahdjd5@gmail.com>
2023-06-01 21:07:15 +0900

    refa: CartItemRepository 기능 구현

---
+++ src/main/java/cart/repository/CartItemRepository.java
@@ -0,0 +1,42 @@
+package cart.repository;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import cart.dao.CartItemDao;
+import cart.domain.CartItem;
+import cart.domain.CartItems;
+import cart.domain.Member;
+
+@Repository
+@Transactional(readOnly = true)
+public class CartItemRepository {
+    private final CartItemDao cartItemDao;
+
+    public CartItemRepository(CartItemDao cartItemDao) {
+        this.cartItemDao = cartItemDao;
+    }
+
+    public CartItem findById(Long id) {
+        return cartItemDao.findById(id);
+    }
+
+    public CartItems findByMember(Member member) {
+        final List<CartItem> cartItems = cartItemDao.findByMemberId(member.getId());
+        return CartItems.from(cartItems, member);
+    }
+
+    public Long add(CartItem cartItem) {
+        return cartItemDao.save(cartItem);
+    }
+
+    public void updateQuantity(CartItem cartItem) {
+        cartItemDao.updateQuantity(cartItem);
+    }
+
+    public void remove(Long id) {
+        cartItemDao.deleteById(id);
+    }
+}
