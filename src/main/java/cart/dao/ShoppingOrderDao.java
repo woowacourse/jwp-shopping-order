ca58c0def74ed15d909fb22eccd013d24aec5fa7
yenawee <yaena0319@naver.com>
2023-06-02 15:59:05 +0900

    fix : 외부 서버 schema error 관련 삽입 구문 수정

--- src/main/java/cart/dao/ShoppingOrderDao.java
+++ src/main/java/cart/dao/ShoppingOrderDao.java
@@ -4,13 +4,17 @@ import cart.domain.Order;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.Objects;
 
 @Repository
 public class ShoppingOrderDao {
@@ -39,12 +43,22 @@ public class ShoppingOrderDao {
     }
 
     public Long save(Order order) {
-        Map<String, Object> params = new HashMap<>();
-        params.put("member_id", order.getMember().getId());
-        params.put("ordered_at", order.getOrderedAt());
-        params.put("used_point", order.getUsedPoint().getValue());
+        KeyHolder keyHolder = new GeneratedKeyHolder();
 
-        return insertAction.executeAndReturnKey(params).longValue();
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(
+                    "INSERT INTO shopping_order (member_id, ordered_at, used_point) VALUES (?, ?, ?)",
+                    Statement.RETURN_GENERATED_KEYS
+            );
+
+            ps.setLong(1, order.getMember().getId());
+            ps.setTimestamp(2, Timestamp.valueOf(order.getOrderedAt()));
+            ps.setLong(3, order.getUsedPoint().getValue());
+
+            return ps;
+        }, keyHolder);
+
+        return Objects.requireNonNull(keyHolder.getKey()).longValue();
     }
 
     public List<OrderResponseEntity> findAll(Long id) {
