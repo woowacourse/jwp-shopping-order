e041119983b9b638a218a43b64e24e50944ddb97
ezzanzzan <cjsdmswjd821@naver.com>
2023-06-02 13:38:48 +0900

    feat: 테이블 추가 - shopping_order table - ordered_item table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,25 +1,43 @@
-DROP TABLE cart_item;
-DROP TABLE product;
-DROP TABLE member;
-
-CREATE TABLE IF NOT EXISTS product (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    price INT NOT NULL,
+CREATE TABLE IF NOT EXISTS product
+(
+    id        BIGINT PRIMARY KEY AUTO_INCREMENT,
+    name      VARCHAR(255) NOT NULL,
+    price     INT          NOT NULL,
     image_url VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS member (
-     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-     email VARCHAR(255) NOT NULL UNIQUE,
-     password VARCHAR(255) NOT NULL
+CREATE TABLE IF NOT EXISTS member
+(
+    id       BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email    VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS cart_item (
-    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    member_id BIGINT NOT NULL,
+CREATE TABLE IF NOT EXISTS cart_item
+(
+    id         BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    member_id  BIGINT NOT NULL,
     product_id BIGINT NOT NULL,
-    quantity INT NOT NULL,
-    FOREIGN KEY (member_id) REFERENCES member(id),
-    FOREIGN KEY (product_id) REFERENCES product(id)
+    quantity   INT    NOT NULL,
+    FOREIGN KEY (member_id) REFERENCES member (id),
+    FOREIGN KEY (product_id) REFERENCES product (id)
+);
+
+CREATE TABLE IF NOT EXISTS shopping_order
+(
+    id         BIGINT   NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    member_id  BIGINT   NOT NULL,
+    ordered_at DATETIME NOT NULL,
+    used_point BIGINT   NOT NULL DEFAULT 0,
+    FOREIGN KEY (member_id) REFERENCES member (id)
+);
+
+CREATE TABLE IF NOT EXISTS ordered_item
+(
+    id                BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    product_id        BIGINT NOT NULL,
+    shopping_order_id BIGINT NOT NULL,
+    quantity          INT    NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES product (id),
+    FOREIGN KEY (shopping_order_id) REFERENCES shopping_order (id)
 );
