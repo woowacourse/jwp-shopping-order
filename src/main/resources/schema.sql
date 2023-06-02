27014199b756ab8602e533d67eb8d9a46574783a
nuyh <hjaehyun25@gmail.com>
2023-06-02 21:26:10 +0900

    build: 멤버 테이블 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,13 +1,3 @@
-UPDATE member
-SET grade='GOLD'
-WHERE email = 'a@a.com';
-UPDATE member
-SET grade='SILVER'
-WHERE email = 'b@b.com';
-UPDATE member
-SET grade='BRONZE'
-WHERE email = 'c@c.com';
-
 CREATE TABLE IF NOT EXISTS product
 (
     id        BIGINT PRIMARY KEY AUTO_INCREMENT,
