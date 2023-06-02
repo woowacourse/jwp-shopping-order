b56bef04071b72b0123afcfba463a5dd702a3073
echo <echo.backend@gmail.com>
2023-06-03 02:17:36 +0900

    feat: Delivery, Discount Dao 작성

---
+++ src/main/java/cart/dao/DefaultDiscountPolicyDao.java
@@ -0,0 +1,29 @@
+package cart.dao;
+
+import cart.domain.DefaultDiscountPolicy;
+import cart.domain.DiscountPolicy;
+import cart.domain.Money;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class DefaultDiscountPolicyDao {
+    private final JdbcTemplate jdbcTemplate;
+
+    public DefaultDiscountPolicyDao(final JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<DiscountPolicy> findDefault() {
+        final String sql = "SELECT * FROM default_discount_policy";
+        return this.jdbcTemplate.query(sql, (rs, rowNum) -> {
+            final long id = rs.getLong("id");
+            final String name = rs.getString("name");
+            final int threshold = rs.getInt("threshold");
+            final double discountRate = rs.getDouble("discount_rate");
+            return new DefaultDiscountPolicy(id, name, Money.from(threshold), discountRate);
+        });
+    }
+}
--- src/main/java/cart/repository/DiscountPolicyDbRepository.java
+++ src/main/java/cart/repository/DiscountPolicyDbRepository.java
@@ -1,12 +1,21 @@
 package cart.repository;
 
+import cart.dao.DefaultDiscountPolicyDao;
 import cart.domain.DiscountPolicy;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public class DiscountPolicyDbRepository implements DiscountPolicyRepository {
+    private final DefaultDiscountPolicyDao defaultDiscountPolicyDao;
+
+    public DiscountPolicyDbRepository(final DefaultDiscountPolicyDao defaultDiscountPolicyDao) {
+        this.defaultDiscountPolicyDao = defaultDiscountPolicyDao;
+    }
+
     @Override
     public List<DiscountPolicy> findDefault() {
-        return null;
+        return this.defaultDiscountPolicyDao.findDefault();
     }
 }
