2f7c55f85dd36526b15daf3c8d19b574afff242f
the9kim <the9rla@gmail.com>
2023-06-02 16:05:01 +0900

    chore : 주문 api uri 수정

--- src/main/java/cart/ui/OrderApiController.java
+++ src/main/java/cart/ui/OrderApiController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.net.URI;
 
 @RestController
-@RequestMapping("/cart/order")
+@RequestMapping("/orders")
 public class OrderApiController {
 
     private final OrderService orderService;
