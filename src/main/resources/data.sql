79974cfa64838db2403df357e6d7d0605fc4f46a
HyunSeo Park (Hyena) <goatsfish@gmail.com>
2023-06-02 09:47:06 +0900

    chore: 데이터 스키마 값 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,17 +4,28 @@ TRUNCATE TABLE cart_item;
 TRUNCATE TABLE product;
 TRUNCATE TABLE member;
 
-INSERT INTO product (name, price, image_url) VALUES ('치킨', 10000, 'https://images.unsplash.com/photo-1626082927389-6cd097cdc6ec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80');
-INSERT INTO product (name, price, image_url) VALUES ('샐러드', 20000, 'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80');
-INSERT INTO product (name, price, image_url) VALUES ('피자', 13000, 'https://images.unsplash.com/photo-1595854341625-f33ee10dbf94?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80');
+INSERT INTO product (name, price, image_url)
+VALUES ('치킨', 10000,
+        'https://images.unsplash.com/photo-1626082927389-6cd097cdc6ec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80');
+INSERT INTO product (name, price, image_url)
+VALUES ('샐러드', 20000,
+        'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80');
+INSERT INTO product (name, price, image_url)
+VALUES ('피자', 13000,
+        'https://images.unsplash.com/photo-1595854341625-f33ee10dbf94?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80');
 
-INSERT INTO member (email, password, money, point) VALUES ('a@a.com', '1234', 10000, 10000);
-INSERT INTO member (email, password, money, point) VALUES ('b@b.com', '1234', 10000, 10000);
+INSERT INTO member (email, password, money, point)
+VALUES ('a@a.com', '1234', 100000000, 100000000);
+INSERT INTO member (email, password, money, point)
+VALUES ('b@b.com', '1234', 100000000, 100000000);
 
-INSERT INTO cart_item (member_id, product_id, quantity) VALUES (1, 1, 2);
-INSERT INTO cart_item (member_id, product_id, quantity) VALUES (1, 2, 4);
+INSERT INTO cart_item (member_id, product_id, quantity)
+VALUES (1, 1, 2);
+INSERT INTO cart_item (member_id, product_id, quantity)
+VALUES (1, 2, 4);
 
-INSERT INTO cart_item (member_id, product_id, quantity) VALUES (2, 3, 5);
+INSERT INTO cart_item (member_id, product_id, quantity)
+VALUES (2, 3, 5);
 
 INSERT INTO product (name, price, image_url)
 VALUES ('PET보틀-삼각(330ml)', 65300,
@@ -30,13 +41,3 @@ VALUES ('PET보틀-원형(600ml)', 47500,
 INSERT INTO product(name, price, image_url)
 values ('PET보틀-납작(260ml)', 64200,
         'https://cdn-mart.baemin.com/sellergoods/main/d07bec18-ce84-41c2-8903-61cbd10712b6.jpg?h=300&w=300');
-
-INSERT INTO product (name, price, image_url)
-VALUES ('치킨', 10000,
-        'https://images.unsplash.com/photo-1626082927389-6cd097cdc6ec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80');
-INSERT INTO product (name, price, image_url)
-VALUES ('샐러드', 20000,
-        'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80');
-INSERT INTO product (name, price, image_url)
-VALUES ('피자', 13000,
-        'https://images.unsplash.com/photo-1595854341625-f33ee10dbf94?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80');
