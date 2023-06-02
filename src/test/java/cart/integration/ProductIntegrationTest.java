edceb10cab4e5084a48a98a27f28157e5d0001e2
jeonghoon <shb03323@naver.com>
2023-06-02 16:26:06 +0900

    feat: 상품 여러 개 조회 통합 테스트 작성

--- src/test/java/cart/integration/ProductIntegrationTest.java
+++ src/test/java/cart/integration/ProductIntegrationTest.java
@@ -2,13 +2,20 @@ package cart.integration;
 
 import cart.controller.dto.request.ProductRequest;
 import cart.controller.dto.response.ProductResponse;
+import com.jayway.jsonpath.Configuration;
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DisplayName("상품 통합 테스트")
 public class ProductIntegrationTest extends IntegrationTest {
@@ -68,5 +75,69 @@ public class ProductIntegrationTest extends IntegrationTest {
         assertThat(responseProduct.getId()).isNotNull();
         assertThat(responseProduct.getName()).isEqualTo("피자");
         assertThat(responseProduct.getPrice()).isEqualTo(15_000);
+    }
+
+    @Nested
+    @DisplayName("원하는 상품 목록 조회")
+    class GetMultiple {
+
+        @Test
+        @DisplayName("성공")
+        void success() {
+            // given
+            final String ids = "1,2,3";
+
+            // when
+            final ExtractableResponse<Response> response = given()
+                    .pathParam("ids", ids)
+                    .when().get("/products")
+                    .then()
+                    .extract();
+
+            // then
+            final Configuration conf = Configuration.defaultConfiguration();
+            final DocumentContext documentContext = JsonPath.using(conf).parse(response.asString());
+
+            assertAll(
+                    () -> assertThat(documentContext.read("$.size()", Integer.class)).isEqualTo(3),
+                    () -> assertThat(documentContext.read("$[0].id", Long.class)).isEqualTo(1L),
+                    () -> assertThat(documentContext.read("$[1].id", Long.class)).isEqualTo(2L),
+                    () -> assertThat(documentContext.read("$[2].id", Long.class)).isEqualTo(3L)
+            );
+        }
+
+        @Test
+        @DisplayName("실패 - 잘못된 path parameter 형식")
+        void fail_when_invalid_path_parameter_format() {
+            // given
+            final String ids = "1-2-3";
+
+            // when
+            final ExtractableResponse<Response> response = given()
+                    .pathParam("ids", ids)
+                    .when().get("/products")
+                    .then()
+                    .extract();
+
+            // then
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        }
+
+        @Test
+        @DisplayName("실패 - 존재하지 않는 product id")
+        void fail_when_product_not_found() {
+            // given
+            final String ids = "1,2,3,1111";
+
+            // when
+            final ExtractableResponse<Response> response = given()
+                    .pathParam("ids", ids)
+                    .when().get("/products")
+                    .then()
+                    .extract();
+
+            // then
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.NOT_FOUND.value());
+        }
     }
 }
