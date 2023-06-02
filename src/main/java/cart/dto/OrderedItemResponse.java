bcd6a28fd67bc7c469afd57deefc5b3cbcc75b23
eunbii0213 <eunbii0213@naver.com>
2023-06-02 19:04:04 +0900

    fix: contextLoad 성공하도록 수정

--- src/main/java/cart/dto/OrderedItemResponse.java
+++ src/main/java/cart/dto/OrderedItemResponse.java
@@ -3,6 +3,8 @@ package cart.dto;
 public class OrderedItemResponse {
 
     private int quantity;
+    //TODO: OrderedItemsResponse? OrderedItemResponse로 변경(quantity필드 안에 넣기)
+    //TODO: Cost Controller... 삭제
     private ProductResponse productResponse;
 
     public ProductResponse getProductResponse() {
