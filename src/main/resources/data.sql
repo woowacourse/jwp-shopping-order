ab587ae91241d9c6d7f2bded307936d31a006255
jeomxon <jeomxon@gmail.com>
2023-06-02 22:49:13 +0900

    chore: 데이터 더미 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO product (name, price, image_url)
+VALUES ('치킨', 10000,
+        'https://images.unsplash.com/photo-1626082927389-6cd097cdc6ec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80');
+
+INSERT INTO member (email, password)
+VALUES ('jeomxon@gmail.com', 'abcd1234@');
