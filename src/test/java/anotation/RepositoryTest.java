b4fd53dbe0f2fe7fbb5d634b5bcdbb414769f972
jaeyeon kim <jakind@naver.com>
2023-06-02 14:38:11 +0900

    refactor : RepositoryTest 수정

--- src/test/java/anotation/RepositoryTest.java
+++ src/test/java/anotation/RepositoryTest.java
@@ -4,6 +4,8 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.FilterType;
@@ -14,10 +16,11 @@ import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
-@Sql(value = {"/drop-test.sql", "/schema-test.sql", "/data-test.sql"}, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(value = {"/truncate-test.sql", "/data-test.sql"}, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
 @JdbcTest(includeFilters = {
         @Filter(type = FilterType.ANNOTATION, value = Repository.class)
 })
+@AutoConfigureTestDatabase(replace = Replace.NONE)
 @ActiveProfiles("test")
 public @interface RepositoryTest {
 }
