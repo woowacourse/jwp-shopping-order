52a86d1cd2f97b68fc910583675b44bb00034451
kdkdhoho <hkim4410@naver.com>
2023-06-02 09:39:03 +0900

    feat(OrderEntity): OrderEntity 추가

---
+++ src/main/java/cart/domain/OrderEntity.java
@@ -0,0 +1,43 @@
+package cart.domain;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class OrderEntity {
+
+    private final Long id;
+    private final LocalDateTime dateTime;
+    private final Long memberId;
+    private final Long productId;
+    private final String productName;
+    private final int productPrice;
+    private final String productImageUrl;
+    private final int quantity;
+    private final int totalProductPrice;
+    private final int totalDeliveryFee;
+    private final int usePoint;
+    private final int totalPrice;
+
+    public OrderEntity(final LocalDateTime dateTime, final Long memberId, final Long productId, final String productName,
+                       final int productPrice, final String productImageUrl, final int quantity, final int totalProductPrice,
+                       final int totalDeliveryFee, final int usePoint, final int totalPrice) {
+        this(null, dateTime, memberId, productId, productName, productPrice, productImageUrl, quantity, totalProductPrice, totalDeliveryFee, usePoint, totalPrice);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderEntity orderEntity = (OrderEntity) o;
+        return Objects.equals(id, orderEntity.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/test/java/cart/domain/OrderEntityTest.java
@@ -0,0 +1,20 @@
+package cart.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.Test;
+
+class OrderEntityTest {
+
+    @Test
+    void ID가_같으면_같은_객체이다() {
+        System.out.println(LocalDateTime.now());
+        OrderEntity orderEntity = new OrderEntity(1L, LocalDateTime.now(), 1L, 1L, "name", 1, "image", 1, 1, 1, 1, 1);
+        OrderEntity sameExpect = new OrderEntity(1L, LocalDateTime.now(), 1L, 1L, "name", 1, "image", 1, 1, 1, 1, 1);
+        OrderEntity differentExpect = new OrderEntity(2L, LocalDateTime.now(), 1L, 1L, "name", 1, "image", 1, 1, 1, 1, 1);
+
+        assertThat(orderEntity).isEqualTo(sameExpect);
+        assertThat(orderEntity).isNotEqualTo(differentExpect);
+    }
+}
