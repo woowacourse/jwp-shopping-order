975e46a9f06802cbe98395e5a6ac3ba747b47490
Be-student <presentsong@naver.com>
2023-06-02 22:18:46 +0900

    feat: BasicAuthExtractor 구현

---
+++ src/test/java/com/woowahan/techcourse/member/ui/BasicAuthExtractorTest.java
@@ -0,0 +1,34 @@
+package com.woowahan.techcourse.member.ui;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import com.woowahan.techcourse.cart.exception.AuthenticationException;
+import java.util.List;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.Test;
+
+@SuppressWarnings({"NonAsciiCharacters", "SpellCheckingInspection"})
+@DisplayNameGeneration(ReplaceUnderscores.class)
+class BasicAuthExtractorTest {
+
+    @Test
+    void 헤더가_null일때_예외() {
+        assertThatThrownBy(() -> BasicAuthExtractor.extractDecodedCredentials(null))
+                .isInstanceOf(AuthenticationException.class);
+    }
+
+    @Test
+    void 헤더가_BASIC으로_시작하지_않으면_예외() {
+        assertThatThrownBy(() -> BasicAuthExtractor.extractDecodedCredentials("header"))
+                .isInstanceOf(AuthenticationException.class);
+    }
+
+    @Test
+    void 헤더에서_정상적으로_데이터를_가져올_수_있다() {
+        String[] result = BasicAuthExtractor.extractDecodedCredentials("Basic dXNlcm5hbWU6cGFzc3dvcmQ=");
+
+        assertThat(result).containsAll(List.of("username", "password"));
+    }
+}
