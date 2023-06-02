aedee0a033f48b96a38bddb023bd64ae9eafd811
donghae-kim <donghae1999@gmail.com>
2023-06-03 00:00:44 +0900

    reefactor: Repository 어노테이션 수정

--- src/main/java/cart/domain/repository/OrderRepository.java
+++ src/main/java/cart/domain/repository/OrderRepository.java
@@ -3,7 +3,6 @@ package cart.domain.repository;
 import cart.domain.Member;
 import cart.domain.Order;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
