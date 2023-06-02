dc2bc1c4aef7db6b488c80ea87f0f1e207dcf8b1
hong-sile <gurwns9325@naver.com>
2023-06-02 13:42:54 +0900

    refactor: 값을 저장한 후 테스트 하도록 리팩터링

--- src/test/java/cart/dao/OrderDaoTest.java
+++ src/test/java/cart/dao/OrderDaoTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import cart.dao.dto.OrderDto;
 import java.time.LocalDateTime;
+import java.util.Optional;
 import javax.sql.DataSource;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -43,30 +44,26 @@ class OrderDaoTest {
 
         final Long orderId = orderDao.insert(orderDto);
 
-        final String sql = "SELECT * FROM orders WHERE id = ?";
-        final OrderDto queryResultOrder = jdbcTemplate.queryForObject(sql, orderDtoRowMapper, orderId);
-
-        assertThat(queryResultOrder)
-                .extracting(OrderDto::getId, OrderDto::getMemberId, OrderDto::getMemberCouponId, OrderDto::getTimeStamp)
-                .containsExactly(orderId, orderDto.getMemberId(), orderDto.getMemberCouponId(),
-                        orderDto.getTimeStamp());
+        final Optional<OrderDto> order = orderDao.findById(orderId);
+        assertThat(order.get())
+                .usingRecursiveComparison()
+                .ignoringFields("id")
+                .isEqualTo(orderDto);
     }
 
     @Test
     @DisplayName("orderDto를 조회하는 기능 테스트")
     void findByIdTest() {
         final LocalDateTime dateTime = LocalDateTime.now().withNano(0);
-        final OrderDto orderDto = new OrderDto(1L, 1L, 2L, dateTime);
-        final String sql = "INSERT INTO orders (id, member_id, coupon_id, time_stamp) VALUES (?, ?, ?, ?)";
-        jdbcTemplate.update(sql, orderDto.getId(), orderDto.getMemberId(), orderDto.getMemberCouponId(),
-                orderDto.getTimeStamp());
+        final OrderDto orderDto = new OrderDto(1L, 2L, dateTime);
+        final Long orderId = orderDao.insert(orderDto);
 
-        final OrderDto queryResultOrder = orderDao.findById(orderDto.getId())
-                .orElseThrow(IllegalArgumentException::new);
+        final OrderDto result = orderDao.findById(orderId)
+                .orElseThrow(RuntimeException::new);
 
-        assertThat(queryResultOrder)
-                .extracting(OrderDto::getId, OrderDto::getMemberId, OrderDto::getMemberCouponId, OrderDto::getTimeStamp)
-                .containsExactly(orderDto.getId(), orderDto.getMemberId(), orderDto.getMemberCouponId(),
-                        orderDto.getTimeStamp());
+        assertThat(result)
+                .usingRecursiveComparison()
+                .ignoringFields("id")
+                .isEqualTo(orderDto);
     }
 }
--- src/test/java/cart/repository/OrderRepositoryTest.java
+++ src/test/java/cart/repository/OrderRepositoryTest.java
@@ -6,9 +6,10 @@ import static cart.TestDataFixture.PRODUCT_2;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import cart.RepositoryTest;
+import cart.dao.CartItemDao;
 import cart.domain.order.Order;
-import cart.domain.order.OrderProduct;
 import cart.domain.product.CartItem;
+import cart.domain.product.Product;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,14 +19,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 class OrderRepositoryTest {
 
     @Autowired
+    private ProductRepository productRepository;
+    @Autowired
+    private CartItemDao cartItemDao;
+    @Autowired
     private OrderRepository orderRepository;
 
     @Test
     @DisplayName("Order를 저장하는 기능 테스트")
     void saveOrder() {
-        final CartItem cartItem1 = new CartItem(MEMBER_1, PRODUCT_1);
-        final CartItem cartItem2 = new CartItem(MEMBER_1, PRODUCT_2);
-        final List<CartItem> cartItems = List.of(cartItem1, cartItem2);
+        final Product savedProduct1 = productRepository.insertProduct(PRODUCT_1);
+        final Product savedProduct2 = productRepository.insertProduct(PRODUCT_2);
+        final CartItem savedCartItem1 = cartItemDao.save(new CartItem(MEMBER_1, savedProduct1));
+        final CartItem savedCartItem2 = cartItemDao.save(new CartItem(MEMBER_1, savedProduct2));
+        final List<CartItem> cartItems = List.of(savedCartItem1, savedCartItem2);
         final Order order = Order.of(MEMBER_1, cartItems);
 
         final Order orderAfterSave = orderRepository.save(order, cartItems);
@@ -34,18 +41,19 @@ class OrderRepositoryTest {
                 .extracting(Order::getTimeStamp, Order::getMemberId)
                 .contains(order.getTimeStamp(), MEMBER_1.getId());
         assertThat(orderAfterSave.getOrderProducts())
-                .extracting(OrderProduct::getProduct)
-                .contains(cartItem1.getProduct(), cartItem2.getProduct());
+                .usingRecursiveComparison()
+                .isEqualTo(cartItems);
     }
 
     @Test
     @DisplayName("Order를 조회하는 기능 테스트")
     void findById() {
-        final CartItem cartItem1 = new CartItem(MEMBER_1, PRODUCT_1);
-        final CartItem cartItem2 = new CartItem(MEMBER_1, PRODUCT_2);
-        final List<CartItem> cartItems = List.of(cartItem1, cartItem2);
+        final Product savedProduct1 = productRepository.insertProduct(PRODUCT_1);
+        final Product savedProduct2 = productRepository.insertProduct(PRODUCT_2);
+        final CartItem savedCartItem1 = cartItemDao.save(new CartItem(MEMBER_1, savedProduct1));
+        final CartItem savedCartItem2 = cartItemDao.save(new CartItem(MEMBER_1, savedProduct2));
+        final List<CartItem> cartItems = List.of(savedCartItem1, savedCartItem2);
         final Order order = Order.of(MEMBER_1, cartItems);
-
         final Long orderId = orderRepository.save(order, cartItems).getId();
 
         final Order orderAfterSave = orderRepository.findById(orderId);
@@ -54,7 +62,7 @@ class OrderRepositoryTest {
                 .extracting(Order::getTimeStamp, Order::getMemberId)
                 .contains(order.getTimeStamp(), MEMBER_1.getId());
         assertThat(orderAfterSave.getOrderProducts())
-                .extracting(OrderProduct::getProduct)
-                .contains(cartItem1.getProduct(), cartItem2.getProduct());
+                .usingRecursiveComparison()
+                .isEqualTo(cartItems);
     }
 }
