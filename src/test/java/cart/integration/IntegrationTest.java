849df7a5fe61f84e323c7096096c2ae12e2d94c4
This2sho <zx00018@naver.com>
2023-06-02 18:42:42 +0900

    refactor: 생성한 테스트용 도메인 객체 생성 객체 사용

--- src/test/java/cart/integration/IntegrationTest.java
+++ src/test/java/cart/integration/IntegrationTest.java
@@ -1,14 +1,10 @@
 package cart.integration;
 
-import cart.dto.request.MemberCreateRequest;
 import cart.support.TestSupport;
 import io.restassured.RestAssured;
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
@@ -20,26 +16,5 @@ public class IntegrationTest extends TestSupport {
     @BeforeEach
     void setUp() {
         RestAssured.port = port;
-    }
-
-    ExtractableResponse<Response> requestCreateMember(String email, String password) {
-        MemberCreateRequest request = new MemberCreateRequest(email, password);
-        return RestAssured
-                .given()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(request)
-                .when()
-                .post("/members")
-                .then().extract();
-    }
-
-    ExtractableResponse<Response> requestShowMemberPoint(String email, String password) {
-        return RestAssured
-                .given()
-                .auth().preemptive().basic(email, password)
-                .when()
-                .get("members/points")
-                .then()
-                .extract();
     }
 }
--- src/test/java/cart/integration/MemberIntegrationTest.java
+++ src/test/java/cart/integration/MemberIntegrationTest.java
@@ -3,6 +3,8 @@ package cart.integration;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
+import cart.domain.Member;
+import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
@@ -14,11 +16,15 @@ public class MemberIntegrationTest extends IntegrationTest {
     @DisplayName("초기 멤버를 생성하면 기본 포인트는 5,000원이다.")
     @Test
     void member_initial_point() {
-        String email = "test@email.com";
-        String password = "123";
-        requestCreateMember(email, password);
+        Member member = memberTestSupport.builder().build();
 
-        ExtractableResponse<Response> response = requestShowMemberPoint(email, password);
+        ExtractableResponse<Response> response = RestAssured
+                .given()
+                .auth().preemptive().basic(member.getEmail(), member.getPassword())
+                .when()
+                .get("members/points")
+                .then()
+                .extract();
         int point = response.jsonPath().getInt("point");
 
         assertAll(
