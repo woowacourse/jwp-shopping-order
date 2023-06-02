903e678fa8df83ac6bcce5e91b06f41e6c51e9dd
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-06-02 17:56:26 +0900

    feat: 포인트 적립 정책에 기간 계산 추가

--- src/main/java/cart/domain/point/SavePointPolicy.java
+++ src/main/java/cart/domain/point/SavePointPolicy.java
@@ -1,9 +1,17 @@
 package cart.domain.point;
 
-public class SavePointPolicy implements PointPolicy {
+import java.time.LocalDateTime;
+import java.time.temporal.TemporalAdjusters;
 
-    public int calculate(int payment) {
+public class SavePointPolicy {
+
+    public static int calculate(int payment) {
         PointAccumulationRate pointAccumulationRate = PointAccumulationRate.findBy(payment);
         return (int) Math.floor(payment * pointAccumulationRate.getRate());
+    }
+
+    public static LocalDateTime getExpiredAt(LocalDateTime now) {
+        LocalDateTime plus3Months = now.plusMonths(3);
+        return plus3Months.with(TemporalAdjusters.firstDayOfMonth());
     }
 }
