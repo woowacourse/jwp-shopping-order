9d86eaa5b6dc91f0b7634fa193c62374fa7f1a4c
jeomxon <jeomxon@gmail.com>
2023-06-02 22:53:01 +0900

    chore: 더미 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,7 @@
-INSERT INTO product (name, price, image_url)
+INSERT INTO product (name, price, image_url, is_discounted, discount_rate)
 VALUES ('치킨', 10000,
-        'https://images.unsplash.com/photo-1626082927389-6cd097cdc6ec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80');
+        'https://images.unsplash.com/photo-1626082927389-6cd097cdc6ec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80',
+        true, 20);
 
-INSERT INTO member (email, password)
-VALUES ('jeomxon@gmail.com', 'abcd1234@');
+INSERT INTO member (email, password, rank, total_purchase_amount)
+VALUES ('jeomxon@gmail.com', 'abcd1234@', 'NORMAL', 0);
