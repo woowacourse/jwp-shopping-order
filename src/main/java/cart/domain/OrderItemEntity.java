3bc55c263477469b0d2a8b722c4b83070a75d644
kdkdhoho <hkim4410@naver.com>
2023-06-02 16:41:34 +0900

    feat(OrderItemEntity): OrderItemEntity 구현

---
+++ src/main/java/cart/domain/OrderItemEntity.java
@@ -0,0 +1,34 @@
+package cart.domain;
+
+import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class OrderItemEntity {
+
+    private final Long id;
+    private final Long productId;
+    private final String productName;
+    private final int productPrice;
+    private final String productImageUrl;
+    private final int quantity;
+
+    public OrderItemEntity(Long productId, String productName, int productPrice, String productImageUrl, int quantity) {
+        this(null, productId, productName, productPrice, productImageUrl, quantity);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItemEntity that = (OrderItemEntity) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/test/java/cart/domain/OrderItemEntityTest.java
@@ -0,0 +1,28 @@
+package cart.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class OrderItemEntityTest {
+
+    @Test
+    void ID가_같으면_같은_객체이다() {
+        OrderItemEntity orderItemEntity = new OrderItemEntity(1L, 30L, "name", 1_000, "image", 10);
+        OrderItemEntity same = new OrderItemEntity(1L, 30L, "name", 1_000, "image", 10);
+        OrderItemEntity different = new OrderItemEntity(2L, 30L, "name", 1_000, "image", 10);
+
+        assertThat(orderItemEntity).isEqualTo(same);
+        assertThat(orderItemEntity).isNotEqualTo(different);
+    }
+
+    @Test
+    void ID가_같으면_같은_해시값을_가진다() {
+        OrderItemEntity orderItemEntity = new OrderItemEntity(1L, 30L, "name", 1_000, "image", 10);
+        OrderItemEntity same = new OrderItemEntity(1L, 30L, "name", 1_000, "image", 10);
+        OrderItemEntity different = new OrderItemEntity(2L, 30L, "name", 1_000, "image", 10);
+
+        assertThat(orderItemEntity.hashCode()).isEqualTo(same.hashCode());
+        assertThat(orderItemEntity.hashCode()).isNotEqualTo(different.hashCode());
+    }
+}
