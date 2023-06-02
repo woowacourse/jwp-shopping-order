cc89d07b64c90f6edd02f23259af4cb735c5ea77
Gyuseong Lee <lks971003@gmail.com>
2023-06-02 22:08:47 +0900

    refactor: 사용하지 않는 클래스 제거

--- src/main/java/cart/exception/ConditionTypeNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package cart.exception;
-
-public class ConditionTypeNotFoundException extends CartException {
-    public ConditionTypeNotFoundException() {
-        super("일치하는 할인 조건이 없습니다.");
-    }
-}
--- src/main/java/cart/exception/InvalidPriceException.java
+++
@@ -1,7 +0,0 @@
-package cart.exception;
-
-public class InvalidPriceException extends CartException {
-    public InvalidPriceException(final String message) {
-        super(message);
-    }
-}
