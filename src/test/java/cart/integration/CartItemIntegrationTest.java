0aa0aa2369d22af8d60d8f8f7c9e8d2721450191
jaehee329 <jjhs9803@gmail.com>
2023-06-04 00:19:15 +0900

    refactor: 기존의 통합 테스트 수정
    기존: Random Port SpringBootTest + RestAssured
    변경 후: SpringBootTest + AutoConfigureMockMvc + Transactional

--- src/test/java/cart/integration/CartItemIntegrationTest.java
+++ src/test/java/cart/integration/CartItemIntegrationTest.java
@@ -1,34 +1,37 @@
 package cart.integration;
 
-import cart.dao.MemberDao;
-import cart.domain.Member;
-import cart.application.dto.CartItemQuantityUpdateRequest;
-import cart.application.dto.CartItemRequest;
-import cart.application.dto.CartItemResponse;
-import cart.application.dto.ProductRequest;
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
+import static java.util.Base64.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-import static io.restassured.RestAssured.given;
-import static org.assertj.core.api.Assertions.assertThat;
+import cart.application.dto.CartItemQuantityUpdateRequest;
+import cart.application.dto.CartItemRequest;
+import cart.dao.MemberDao;
+import cart.dao.ProductDao;
+import cart.domain.Member;
+import cart.domain.Product;
 
+@Sql(scripts = "classpath:schema-truncate.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 public class CartItemIntegrationTest extends IntegrationTest {
 
     @Autowired
     private MemberDao memberDao;
+    @Autowired
+    private ProductDao productDao;
 
-    private Long productId;
+    private Long productId1;
     private Long productId2;
     private Member member;
     private Member member2;
@@ -36,9 +39,11 @@ public class CartItemIntegrationTest extends IntegrationTest {
     @BeforeEach
     void setUp() {
         super.setUp();
+        productId1 = productDao.createProduct(new Product("치킨", 10_000, "http://example.com/chicken.jpg"));
+        productId2 = productDao.createProduct(new Product("피자", 15_000, "http://example.com/pizza.jpg"));
 
-        productId = createProduct(new ProductRequest("치킨", 10_000, "http://example.com/chicken.jpg"));
-        productId2 = createProduct(new ProductRequest("피자", 15_000, "http://example.com/pizza.jpg"));
+        memberDao.addMember(new Member(1L, "a@a.com", "1234"));
+        memberDao.addMember(new Member(2L, "b@b.com", "1234"));
 
         member = memberDao.getMemberById(1L);
         member2 = memberDao.getMemberById(2L);
@@ -46,174 +51,150 @@ public class CartItemIntegrationTest extends IntegrationTest {
 
     @DisplayName("장바구니에 아이템을 추가한다.")
     @Test
-    void addCartItem() {
-        CartItemRequest cartItemRequest = new CartItemRequest(productId);
-        ExtractableResponse<Response> response = requestAddCartItem(member, cartItemRequest);
+    void addCartItem() throws Exception {
+        // given
+        CartItemRequest cartItemRequest = new CartItemRequest(productId1);
 
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        // when
+        ResultActions response = requestAddCartItem(member, cartItemRequest);
+
+        // then
+        response.andExpect(status().isCreated());
     }
 
     @DisplayName("잘못된 사용자 정보로 장바구니에 아이템을 추가 요청시 실패한다.")
     @Test
-    void addCartItemByIllegalMember() {
+    void addCartItemByIllegalMember() throws Exception {
+        // given
         Member illegalMember = new Member(member.getId(), member.getEmail(), member.getPassword() + "asdf");
-        CartItemRequest cartItemRequest = new CartItemRequest(productId);
-        ExtractableResponse<Response> response = requestAddCartItem(illegalMember, cartItemRequest);
+        CartItemRequest cartItemRequest = new CartItemRequest(productId1);
 
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
+        // when
+        ResultActions response = requestAddCartItem(illegalMember, cartItemRequest);
+
+        // then
+        response.andExpect(status().isUnauthorized());
     }
 
     @DisplayName("사용자가 담은 장바구니 아이템을 조회한다.")
     @Test
-    void getCartItems() {
-        Long cartItemId1 = requestAddCartItemAndGetId(member, productId);
+    void getCartItems() throws Exception {
+        // given
+        Long cartItemId1 = requestAddCartItemAndGetId(member, productId1);
         Long cartItemId2 = requestAddCartItemAndGetId(member, productId2);
 
-        ExtractableResponse<Response> response = requestGetCartItems(member);
+        // when
+        ResultActions response = requestGetCartItems(member);
 
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
-
-        List<Long> resultCartItemIds = response.jsonPath().getList(".", CartItemResponse.class).stream()
-                .map(CartItemResponse::getId)
-                .collect(Collectors.toList());
-        assertThat(resultCartItemIds).containsAll(Arrays.asList(cartItemId1, cartItemId2));
+        // then
+        response.andExpect(status().isOk())
+            .andExpect(jsonPath("$[0].id").value(cartItemId1))
+            .andExpect(jsonPath("$[1].id").value(cartItemId2));
     }
 
     @DisplayName("장바구니에 담긴 아이템의 수량을 변경한다.")
     @Test
-    void increaseCartItemQuantity() {
-        Long cartItemId = requestAddCartItemAndGetId(member, productId);
+    void increaseCartItemQuantity() throws Exception {
+        // given
+        Long cartItemId = requestAddCartItemAndGetId(member, productId1);
 
-        ExtractableResponse<Response> response = requestUpdateCartItemQuantity(member, cartItemId, 10);
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        // when
+        requestUpdateCartItemQuantity(member, cartItemId, 10);
 
-        ExtractableResponse<Response> cartItemsResponse = requestGetCartItems(member);
-
-        Optional<CartItemResponse> selectedCartItemResponse = cartItemsResponse.jsonPath()
-                .getList(".", CartItemResponse.class)
-                .stream()
-                .filter(cartItemResponse -> cartItemResponse.getId().equals(cartItemId))
-                .findFirst();
-
-        assertThat(selectedCartItemResponse.isPresent()).isTrue();
-        assertThat(selectedCartItemResponse.get().getQuantity()).isEqualTo(10);
+        // then
+        ResultActions cartItems = requestGetCartItems(member);
+        cartItems.andExpect(status().isOk())
+            .andExpect(jsonPath("$[0].id").value(cartItemId))
+            .andExpect(jsonPath("$[0].quantity").value(10));
     }
 
     @DisplayName("장바구니에 담긴 아이템의 수량을 0으로 변경하면, 장바구니에서 아이템이 삭제된다.")
     @Test
-    void decreaseCartItemQuantityToZero() {
-        Long cartItemId = requestAddCartItemAndGetId(member, productId);
+    void decreaseCartItemQuantityToZero() throws Exception {
+        // given
+        Long cartItemId1 = requestAddCartItemAndGetId(member, productId1);
+        Long cartItemId2 = requestAddCartItemAndGetId(member, productId2);
 
-        ExtractableResponse<Response> response = requestUpdateCartItemQuantity(member, cartItemId, 0);
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        // when
+        requestUpdateCartItemQuantity(member, cartItemId1, 0);
 
-        ExtractableResponse<Response> cartItemsResponse = requestGetCartItems(member);
-
-        Optional<CartItemResponse> selectedCartItemResponse = cartItemsResponse.jsonPath()
-                .getList(".", CartItemResponse.class)
-                .stream()
-                .filter(cartItemResponse -> cartItemResponse.getId().equals(cartItemId))
-                .findFirst();
-
-        assertThat(selectedCartItemResponse.isPresent()).isFalse();
+        // then
+        String response = requestGetCartItems(member).andExpect(status().isOk())
+            .andReturn().getResponse().getContentAsString();
+        assertThat(response).doesNotContain("\"id\":1,\"name\":\"치킨\"");
+        assertThat(response).contains("\"id\":2,\"name\":\"피자\"");
     }
 
     @DisplayName("다른 사용자가 담은 장바구니 아이템의 수량을 변경하려 하면 실패한다.")
     @Test
-    void updateOtherMembersCartItem() {
-        Long cartItemId = requestAddCartItemAndGetId(member, productId);
+    void updateOtherMembersCartItem() throws Exception {
+        // given
+        Long cartItemId1 = requestAddCartItemAndGetId(member, productId1);
 
-        ExtractableResponse<Response> response = requestUpdateCartItemQuantity(member2, cartItemId, 10);
+        // when
+        ResultActions response = requestUpdateCartItemQuantity(member2, cartItemId1, 10);
 
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.FORBIDDEN.value());
+        // then
+        response.andExpect(status().isForbidden());
     }
 
     @DisplayName("장바구니에 담긴 아이템을 삭제한다.")
     @Test
-    void removeCartItem() {
-        Long cartItemId = requestAddCartItemAndGetId(member, productId);
+    void removeCartItem() throws Exception {
+        Long cartItemId1 = requestAddCartItemAndGetId(member, productId1);
 
-        ExtractableResponse<Response> response = requestDeleteCartItem(cartItemId);
+        ResultActions response = requestDeleteCartItem(cartItemId1);
 
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
-
-        ExtractableResponse<Response> cartItemsResponse = requestGetCartItems(member);
-
-        Optional<CartItemResponse> selectedCartItemResponse = cartItemsResponse.jsonPath()
-                .getList(".", CartItemResponse.class)
-                .stream()
-                .filter(cartItemResponse -> cartItemResponse.getId().equals(cartItemId))
-                .findFirst();
-
-        assertThat(selectedCartItemResponse.isPresent()).isFalse();
+        response.andExpect(status().isNoContent());
+        int cartItemResponseLength = requestGetCartItems(member).andReturn().getResponse().getContentLength();
+        assertThat(cartItemResponseLength).isZero();
     }
 
-    private Long createProduct(ProductRequest productRequest) {
-        ExtractableResponse<Response> response = given()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(productRequest)
-                .when()
-                .post("/products")
-                .then()
-                .statusCode(HttpStatus.CREATED.value())
-                .extract();
-
-        return getIdFromCreatedResponse(response);
+    private long getIdFromCreatedResponse(ResultActions result) {
+        return Long.parseLong(result.andReturn().getResponse().getHeader("Location").split("/")[2]);
     }
 
-    private long getIdFromCreatedResponse(ExtractableResponse<Response> response) {
-        return Long.parseLong(response.header("Location").split("/")[2]);
+    private ResultActions requestAddCartItem(Member member, CartItemRequest cartItemRequest) throws
+        Exception {
+        String encodedAuthHeader = getEncodedAuthHeader(member);
+        String jsonRequest = new ObjectMapper().writeValueAsString(cartItemRequest);
+        return mockMvc.perform(MockMvcRequestBuilders.post("/cart-items")
+            .header("Authorization", encodedAuthHeader)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .content(jsonRequest));
     }
 
-    private ExtractableResponse<Response> requestAddCartItem(Member member, CartItemRequest cartItemRequest) {
-        return given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .auth().preemptive().basic(member.getEmail(), member.getPassword())
-                .body(cartItemRequest)
-                .when()
-                .post("/cart-items")
-                .then()
-                .log().all()
-                .extract();
+    private ResultActions requestGetCartItems(Member member) throws Exception {
+        String encodedAuthHeader = getEncodedAuthHeader(member);
+        return mockMvc.perform(get("/cart-items")
+            .header("Authorization", encodedAuthHeader)
+            .contentType(MediaType.APPLICATION_JSON_VALUE));
     }
 
-    private Long requestAddCartItemAndGetId(Member member, Long productId) {
-        ExtractableResponse<Response> response = requestAddCartItem(member, new CartItemRequest(productId));
-        return getIdFromCreatedResponse(response);
+    private String getEncodedAuthHeader(Member member) {
+        return "Basic " + new String(
+            getEncoder().encode(String.format("%s:%s", member.getEmail(), member.getPassword()).getBytes()));
     }
 
-    private ExtractableResponse<Response> requestGetCartItems(Member member) {
-        return given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .auth().preemptive().basic(member.getEmail(), member.getPassword())
-                .when()
-                .get("/cart-items")
-                .then()
-                .log().all()
-                .extract();
+    private Long requestAddCartItemAndGetId(Member member, Long productId) throws Exception {
+        ResultActions result = requestAddCartItem(member, new CartItemRequest(productId));
+        return getIdFromCreatedResponse(result);
     }
 
-    private ExtractableResponse<Response> requestUpdateCartItemQuantity(Member member, Long cartItemId, int quantity) {
+    private ResultActions requestUpdateCartItemQuantity(Member member, Long cartItemId, int quantity) throws Exception {
+        String encodedAuthHeader = getEncodedAuthHeader(member);
         CartItemQuantityUpdateRequest quantityUpdateRequest = new CartItemQuantityUpdateRequest(quantity);
-        return given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .auth().preemptive().basic(member.getEmail(), member.getPassword())
-                .when()
-                .body(quantityUpdateRequest)
-                .patch("/cart-items/{cartItemId}", cartItemId)
-                .then()
-                .log().all()
-                .extract();
+        String jsonRequest = new ObjectMapper().writeValueAsString(quantityUpdateRequest);
+        return mockMvc.perform(MockMvcRequestBuilders.patch("/cart-items/{cartItemId}", cartItemId)
+            .header("Authorization", encodedAuthHeader)
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .content(jsonRequest));
     }
 
-    private ExtractableResponse<Response> requestDeleteCartItem(Long cartItemId) {
-        return given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .auth().preemptive().basic(member.getEmail(), member.getPassword())
-                .when()
-                .delete("/cart-items/{cartItemId}", cartItemId)
-                .then()
-                .log().all()
-                .extract();
+    private ResultActions requestDeleteCartItem(Long cartItemId) throws Exception {
+        String encodedAuthHeader = getEncodedAuthHeader(member);
+        return mockMvc.perform(MockMvcRequestBuilders.delete("/cart-items/{cartItemId}", cartItemId)
+            .header("Authorization", encodedAuthHeader)
+            .contentType(MediaType.APPLICATION_JSON_VALUE));
     }
 }
