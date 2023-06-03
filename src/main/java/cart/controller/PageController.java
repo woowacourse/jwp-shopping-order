4bb52099f3d3c1668070fde23b114a63578fd28d
Combi153 <jcm04@naver.com>
2023-06-03 20:01:55 +0900

    refactor: PageController 로직 수정

--- src/main/java/cart/controller/PageController.java
+++ src/main/java/cart/controller/PageController.java
@@ -1,11 +1,16 @@
 package cart.controller;
 
+import cart.dto.PageRequest;
 import cart.repository.MemberRepository;
 import cart.service.ProductService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
+    private static final PageRequest BASIC_PAGE_REQUEST = new PageRequest(1, 10);
+
     private final ProductService productService;
     private final MemberRepository memberRepository;
 
@@ -14,15 +19,15 @@ public class PageController {
         this.memberRepository = memberRepository;
     }
 
-//    @GetMapping("/admin")
-//    public String admin(Model model) {
-//        model.addAttribute("products", productService.getAllProducts());
-//        return "admin";
-//    }
-//
-//    @GetMapping("/settings")
-//    public String members(Model model) {
-//        model.addAttribute("members", memberRepository.getAllMembers());
-//        return "settings";
-//    }
+    @GetMapping("/admin")
+    public String admin(Model model) {
+        model.addAttribute("products", productService.getAllProducts(BASIC_PAGE_REQUEST));
+        return "admin";
+    }
+
+    @GetMapping("/settings")
+    public String members(Model model) {
+        model.addAttribute("members", memberRepository.getAllMembers());
+        return "settings";
+    }
 }
