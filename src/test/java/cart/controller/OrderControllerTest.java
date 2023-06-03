26a8c0e7c37297113d8d75f0bff2713c1dde1b15
wugawuga <dnr3655@naver.com>
2023-06-03 11:53:37 +0900

    test: 쿠폰을 사용한 주문 테스트 추가

--- src/test/java/cart/controller/OrderControllerTest.java
+++ src/test/java/cart/controller/OrderControllerTest.java
@@ -14,15 +14,20 @@ import cart.domain.Member;
 import cart.domain.Order;
 import cart.domain.OrderItem;
 import cart.domain.Product;
+import cart.domain.coupon.Coupon;
 import cart.domain.coupon.MemberCoupon;
+import cart.domain.coupon.policy.DiscountPolicyType;
 import cart.dto.OrderSaveRequest;
+import cart.entity.MemberCouponEntity;
 import cart.repository.CartItemRepository;
+import cart.repository.CouponRepository;
 import cart.repository.MemberRepository;
 import cart.repository.OrderRepository;
 import cart.repository.ProductRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.Base64;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 import org.junit.jupiter.api.Test;
@@ -30,6 +35,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.transaction.annotation.Transactional;
@@ -58,6 +66,22 @@ class OrderControllerTest {
 
     @Autowired
     private OrderRepository orderRepository;
+
+    @Autowired
+    private CouponRepository couponRepository;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    private SimpleJdbcInsert jdbcInsert;
+
+    @BeforeEach
+    void init() {
+        jdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName("member_coupon")
+                .usingColumns("coupon_id", "member_id", "used")
+                .usingGeneratedKeyColumns("id");
+    }
 
     @Test
     void 주문을_완료한다() throws Exception {
@@ -92,6 +116,53 @@ class OrderControllerTest {
 
         Order findOrder = orderRepository.findByOrderIdAndMemberId(Long.valueOf(orderId), member.getId());
         assertThat(findOrder.getOrderItems()).hasSize(2);
+    }
+
+    @Test
+    void 쿠폰을_사용하여_주문을_완료한다() throws Exception {
+        // given
+        final Product product1 = productRepository.save(new Product("pizza1", "pizza1.jpg", 9000L));
+        final Product product2 = productRepository.save(new Product("pizza2", "pizza2.jpg", 10000L));
+        final Member member = memberRepository.save(new Member("pizza@pizza.com", "password"));
+
+        final Coupon savedCoupon = couponRepository.saveCoupon(
+                new Coupon("1만원 이상 1000원할인", DiscountPolicyType.findDiscountPolicy("price", 1000L), 10000L));
+
+        final MemberCouponEntity memberCouponEntity = new MemberCouponEntity(savedCoupon.getId(), member.getId(),
+                false);
+        final BeanPropertySqlParameterSource parameterSource = new BeanPropertySqlParameterSource(memberCouponEntity);
+        final long savedMemberCouponId = jdbcInsert.executeAndReturnKey(parameterSource).longValue();
+
+        final CartItem savedCartItem1 = cartItemRepository.save(new CartItem(member, product1));
+        final CartItem savedCartItem2 = cartItemRepository.save(new CartItem(member, product2));
+        final String header = "Basic " + new String(Base64.getEncoder().encode("pizza@pizza.com:password".getBytes()));
+
+        final OrderSaveRequest orderSaveRequest = new OrderSaveRequest(
+                List.of(savedCartItem1.getId(), savedCartItem2.getId()), savedMemberCouponId);
+        final String request = objectMapper.writeValueAsString(orderSaveRequest);
+
+        // when
+        MvcResult authorization = mockMvc.perform(post("/orders")
+                        .header("Authorization", header)
+                        .content(request)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andDo(print())
+                .andReturn();
+        String uri = authorization.getResponse().getHeader("Location");
+        String[] split = uri.split("/");
+        String orderId = split[split.length - 1];
+
+        // then
+        List<CartItem> findCartItems = cartItemRepository.findAllByMemberId(member.getId());
+        assertThat(findCartItems).isEmpty();
+
+        Order findOrder = orderRepository.findByOrderIdAndMemberId(Long.valueOf(orderId), member.getId());
+        assertThat(findOrder.getOrderItems()).hasSize(2);
+        assertThat(findOrder.getTotalPrice()).isEqualTo(19000);
+        assertThat(findOrder.getDeliveryFee()).isEqualTo(3000);
+        assertThat(findOrder.getDiscountPrice()).isEqualTo(1000);
+        assertThat(findOrder.getCalculateDiscountPrice().longValue()).isEqualTo(18000);
     }
 
     @Test
