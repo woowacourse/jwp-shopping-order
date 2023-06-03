850ce3de0dc0a660e84c794f7269651f73475971
echo <echo.backend@gmail.com>
2023-06-03 10:58:03 +0900

    chore: 전역 REST API 예외 처리 추가

--- src/main/java/cart/ui/ControllerExceptionHandler.java
+++ src/main/java/cart/ui/ControllerExceptionHandler.java
@@ -1,36 +1,33 @@
 package cart.ui;
 
 import cart.dto.ApiErrorResponse;
-import cart.exception.AuthenticationException;
-import cart.exception.CartItemException;
-import cart.exception.OrderException;
-import cart.exception.PaymentException;
-import cart.exception.ProductException;
+import cart.exception.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class ControllerExceptionHandler {
+public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(AuthenticationException.class)
-    public ResponseEntity<ApiErrorResponse> handleAuthenticationException(AuthenticationException e) {
+    public ResponseEntity<ApiErrorResponse> handleAuthenticationException(final AuthenticationException e) {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ApiErrorResponse.from(e));
     }
 
     @ExceptionHandler({CartItemException.NotFound.class, OrderException.NotFound.class,
             ProductException.NotFound.class, PaymentException.NotFound.class})
-    public ResponseEntity<ApiErrorResponse> handleNotFoundException(RuntimeException e) {
+    public ResponseEntity<ApiErrorResponse> handleNotFoundException(final RuntimeException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ApiErrorResponse.from(e));
     }
 
     @ExceptionHandler({CartItemException.IllegalMember.class, OrderException.IllegalMember.class})
-    public ResponseEntity<ApiErrorResponse> handleAuthorizationException(RuntimeException e) {
+    public ResponseEntity<ApiErrorResponse> handleAuthorizationException(final RuntimeException e) {
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ApiErrorResponse.from(e));
     }
 
     @ExceptionHandler({RuntimeException.class})
-    public ResponseEntity<ApiErrorResponse> handleRuntimeException(RuntimeException e) {
+    public ResponseEntity<ApiErrorResponse> handleRuntimeException(final RuntimeException e) {
         e.printStackTrace();
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ApiErrorResponse.from(e));
     }
