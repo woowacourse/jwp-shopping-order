03a0f252fbd246796a3a24d3d3c8a9c0d6d0273f
xxeol2 <jhj01177@naver.com>
2023-06-03 14:25:48 +0900

    refactor: 빈 장바구니 상품 검증

--- src/main/java/cart/domain/cart/CartItems.java
+++ src/main/java/cart/domain/cart/CartItems.java
@@ -1,6 +1,7 @@
 package cart.domain.cart;
 
 import cart.domain.Member;
+import cart.exception.StoreException;
 import java.util.List;
 
 public class CartItems {
@@ -8,7 +9,14 @@ public class CartItems {
     private final List<CartItem> cartItems;
 
     public CartItems(final List<CartItem> cartItems) {
+        validate(cartItems);
         this.cartItems = cartItems;
+    }
+
+    private void validate(final List<CartItem> cartItems) {
+        if (cartItems.isEmpty()) {
+            throw new StoreException("장바구니 상품이 없습니다.");
+        }
     }
 
     public void checkOwner(final Member member) {
--- src/test/java/cart/domain/coupon/MemberCouponTest.java
+++ src/test/java/cart/domain/coupon/MemberCouponTest.java
@@ -3,11 +3,14 @@ package cart.domain.coupon;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 
+import cart.domain.Member;
+import cart.domain.cart.CartItem;
 import cart.domain.cart.CartItems;
 import cart.fixture.CouponFixture.금액_10000원이상_1000원할인;
 import cart.fixture.MemberFixture.Member_test1;
+import cart.fixture.ProductFixture.피자_20000원;
 import java.time.LocalDateTime;
-import java.util.Collections;
+import java.util.List;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Nested;
@@ -24,7 +27,8 @@ class MemberCouponTest {
         @Test
         void 만료되었으면_거짓() {
             // given
-            CartItems cartItems = new CartItems(Collections.emptyList());
+            Member member = Member_test1.MEMBER;
+            CartItems cartItems = new CartItems(List.of(new CartItem(member, 피자_20000원.PRODUCT)));
             MemberCoupon memberCoupon = new MemberCoupon(1L, 금액_10000원이상_1000원할인.COUPON, Member_test1.MEMBER, false,
                     LocalDateTime.MIN,
                     LocalDateTime.now());
@@ -39,8 +43,9 @@ class MemberCouponTest {
         @Test
         void 이미_사용된_쿠폰이면_거짓() {
             // given
-            CartItems cartItems = new CartItems(Collections.emptyList());
-            MemberCoupon memberCoupon = new MemberCoupon(1L, 금액_10000원이상_1000원할인.COUPON, Member_test1.MEMBER, true,
+            Member member = Member_test1.MEMBER;
+            CartItems cartItems = new CartItems(List.of(new CartItem(member, 피자_20000원.PRODUCT)));
+            MemberCoupon memberCoupon = new MemberCoupon(1L, 금액_10000원이상_1000원할인.COUPON, member, true,
                     LocalDateTime.MAX,
                     LocalDateTime.now());
 
