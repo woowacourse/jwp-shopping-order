4e50355741dae76addee48c657984d8e6e99443a
HyunSeo Park (Hyena) <goatsfish@gmail.com>
2023-06-03 20:49:10 +0900

    chore: 금액, 포인트 자료형 변경 및 배송비 컬럼 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS product
 (
     id        BIGINT PRIMARY KEY AUTO_INCREMENT,
     name      VARCHAR(255) NOT NULL,
-    price     INT          NOT NULL,
+    price     double       NOT NULL,
     image_url VARCHAR(255) NOT NULL
 );
 
@@ -11,8 +11,8 @@ CREATE TABLE IF NOT EXISTS member
     id       BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email    VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    money    INT          NOT NULL,
-    point    INT          NOT NULL
+    money    double       NOT NULL,
+    point    double       NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS cart_item
@@ -20,7 +20,7 @@ CREATE TABLE IF NOT EXISTS cart_item
     id         BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     member_id  BIGINT NOT NULL,
     product_id BIGINT NOT NULL,
-    quantity   INT    NOT NULL
+    quantity   double NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS order_item
@@ -30,16 +30,17 @@ CREATE TABLE IF NOT EXISTS order_item
     product_id BIGINT       NOT NULL,
     member_id  BIGINT       NOT NULL,
     name       VARCHAR(255) NOT NULL,
-    price      INT          NOT NULL,
+    price      double       NOT NULL,
     image_url  VARCHAR(255) NOT NULL,
-    quantity   INT          NOT NULL
+    quantity   double       NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS orders
 (
-    id          BIGINT    NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    member_id   BIGINT    NOT NULL,
-    total_price INT       NOT NULL,
-    use_point   INT       NOT NULL,
-    created_at  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    id           BIGINT    NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    member_id    BIGINT    NOT NULL,
+    total_price  double    NOT NULL,
+    use_point    double    NOT NULL,
+    delivery_fee double    NOT NULL,
+    created_at   TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
