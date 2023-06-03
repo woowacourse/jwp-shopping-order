14ea8a77a82bc3773752f8efa0a6e765c2d33b38
iamjooon2 <iamjooon2@gmail.com>
2023-06-04 01:10:20 +0900

    feat: 관리자 페이지에서 사용자 등급 보이도록 변경

--- src/main/resources/static/js/settings.js
+++ src/main/resources/static/js/settings.js
@@ -1,6 +1,6 @@
 const selectMember = (member) => {
-    const { email, password } = member;
-    const string = `${email}:${password}`;
+    const { email, grade, password } = member;
+    const string = `${email}:${grade}:${password}`;
     localStorage.setItem('credentials', btoa(string));
     alert(`${email} 사용자로 설정 했습니다.`);
 }
