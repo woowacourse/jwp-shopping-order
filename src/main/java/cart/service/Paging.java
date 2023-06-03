4c6efa79e79ac5f1f2386d9ba9bb239773426a06
Combi153 <jcm04@naver.com>
2023-06-03 14:02:27 +0900

    feat: Paging 기능 추가

---
+++ src/main/java/cart/service/Paging.java
@@ -0,0 +1,30 @@
+package cart.service;
+
+import cart.dto.PageInfo;
+import cart.dto.PageRequest;
+
+public class Paging {
+
+    private static final int ORDER_COUNT = 1;
+
+    private final int page;
+    private final int size;
+
+    public Paging(final PageRequest pageRequest) {
+        this.page = pageRequest.getPage();
+        this.size = pageRequest.getSize();
+    }
+
+    public int getStart() {
+        return (page - ORDER_COUNT) * size;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public PageInfo getPageInfo(final int totalElement) {
+        final int totalPage = (int) Math.ceil(totalElement / (double) size);
+        return new PageInfo(page, size, totalElement, totalPage);
+    }
+}
---
+++ src/test/java/cart/service/PagingTest.java
@@ -0,0 +1,59 @@
+package cart.service;
+
+import cart.dto.PageInfo;
+import cart.dto.PageRequest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@SuppressWarnings("NonAsciiCharacters")
+class PagingTest {
+
+    private PageRequest pageRequest;
+
+    @BeforeEach
+    void setUp() {
+        pageRequest = new PageRequest(2, 10);
+    }
+
+    @Test
+    void 데이터_시작점을_얻는다() {
+        //given
+        final Paging paging = new Paging(pageRequest);
+
+        //when
+        final int start = paging.getStart();
+
+        //then
+        assertThat(start).isEqualTo(10);
+    }
+
+    @Test
+    void 데이터_사이즈를_얻는다() {
+        //given
+        final Paging paging = new Paging(pageRequest);
+
+        //when
+        final int size = paging.getSize();
+
+        //then
+        assertThat(size).isEqualTo(10);
+    }
+
+    @Test
+    void 페이지_정보를_얻는다() {
+        //given
+        final Paging paging = new Paging(pageRequest);
+
+        //when
+        final PageInfo pageInfo = paging.getPageInfo(21);
+
+        //then
+        assertThat(pageInfo).usingRecursiveComparison()
+                .isEqualTo(new PageInfo(2, 10, 21, 3));
+    }
+}
