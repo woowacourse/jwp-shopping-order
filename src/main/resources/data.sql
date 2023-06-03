f08e6b2eddb3e15efa49785031a0cdfbd73e5ca9
eunbii0213 <eunbii0213@naver.com>
2023-06-03 19:14:12 +0900

    fix: sql문 에러 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,8 +4,8 @@ INSERT INTO product (name, price, image_url, is_discounted, discount_rate) VALUE
 INSERT INTO product (name, price, image_url, is_discounted, discount_rate) VALUES ('샐러드', 20000, 'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80', 1, 50);
 INSERT INTO product (name, price, image_url, is_discounted, discount_rate) VALUES ('피자', 13000, 'https://images.unsplash.com/photo-1595854341625-f33ee10dbf94?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80', 1, 20);
 
-INSERT INTO member (email, password, member_rank, total_price) VALUES ('a@a.com', '1234', '일반', 0);
-INSERT INTO member (email, password, member_rank, total_price) VALUES ('b@b.com', '1234', 'gold', 0);
+INSERT INTO member (email, password, grade, total_price) VALUES ('a@a.com', '1234', '일반', 0);
+INSERT INTO member (email, password, grade, total_price) VALUES ('b@b.com', '1234', 'gold', 0);
 
 INSERT INTO cart_item (member_id, product_id, quantity) VALUES (1, 1, 2);
 INSERT INTO cart_item (member_id, product_id, quantity) VALUES (1, 2, 4);
