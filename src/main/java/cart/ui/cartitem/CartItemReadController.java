cf6a6e64cffa32dc8309056c4ee72d1846cdba74
ingpyo <juninpyo@naver.com>
2023-06-03 21:53:59 +0900

    feat: 금액을 계산해주는 로직 구현

--- src/main/java/cart/ui/cartitem/CartItemReadController.java
+++ src/main/java/cart/ui/cartitem/CartItemReadController.java
@@ -2,8 +2,12 @@ package cart.ui.cartitem;
 
 import cart.application.service.cartitem.CartItemReadService;
 import cart.application.service.cartitem.dto.CartResultDto;
-import cart.ui.MemberAuth;
+import cart.application.service.order.OrderMakeService;
+import cart.application.service.order.dto.CreateOrderDto;
+import cart.domain.member.Member;
+import cart.ui.cartitem.dto.CartPaymentResponse;
 import cart.ui.cartitem.dto.CartResponse;
+import cart.ui.order.dto.request.CreateOrderRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,20 +16,29 @@ import org.springframework.web.bind.annotation.*;
 public class CartItemReadController {
 
     private final CartItemReadService cartItemReadService;
+    private final OrderMakeService orderMakeService;
 
-    public CartItemReadController(final CartItemReadService cartItemReadService) {
+    public CartItemReadController(
+            final CartItemReadService cartItemReadService,
+            final OrderMakeService orderMakeService
+    ) {
         this.cartItemReadService = cartItemReadService;
+        this.orderMakeService = orderMakeService;
     }
 
     @GetMapping
-    public ResponseEntity<CartResponse> showCartItems(MemberAuth memberAuth) {
+    public ResponseEntity<CartResponse> showCartItems(final Member memberAuth) {
         final CartResultDto cartResult = cartItemReadService.findByMember(memberAuth);
         return ResponseEntity.ok(CartResponse.from(cartResult));
     }
 
     @PostMapping("/payment")
-    public ResponseEntity<CartPaymentResponse> calculatePrice(@RequestBody CartPaymentRequest cartPaymentRequest)  {
-        return null;
+    public ResponseEntity<CartPaymentResponse> calculatePrice(
+            final Member member,
+            @RequestBody CreateOrderRequest createOrderRequest
+    ) {
+        final int paymentPrice = orderMakeService.getPaymentPrice(member, CreateOrderDto.from(createOrderRequest));
+        return ResponseEntity.ok(new CartPaymentResponse(paymentPrice));
     }
 
 }
