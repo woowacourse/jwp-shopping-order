832d2bd591d2a43d8859542ed40f400b8ed89907
Combi153 <jcm04@naver.com>
2023-06-03 19:59:22 +0900

    feat: logger 추가

--- src/main/java/cart/controller/ControllerExceptionHandler.java
+++ src/main/java/cart/controller/ControllerExceptionHandler.java
@@ -3,6 +3,8 @@ package cart.controller;
 import cart.exception.AuthenticationException;
 import cart.exception.CartItemException;
 import cart.exception.PagingException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -18,28 +20,35 @@ import java.util.Map;
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
+    private final Logger logger = LoggerFactory.getLogger(ControllerExceptionHandler.class);
+
     @ExceptionHandler(AuthenticationException.class)
     public ResponseEntity<Void> handlerAuthenticationException(AuthenticationException e) {
+        logger.warn(e.getMessage());
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 
     @ExceptionHandler(CartItemException.IllegalMember.class)
     public ResponseEntity<Void> handleCartItemException(CartItemException.IllegalMember e) {
+        logger.warn(e.getMessage());
         return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
+        logger.warn(e.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 
     @ExceptionHandler(CartItemException.class)
     public ResponseEntity<String> handleCartItemException(CartItemException e) {
+        logger.warn(e.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
 
     @ExceptionHandler(PagingException.class)
     public ResponseEntity<String> handlePagingException(PagingException e) {
+        logger.warn(e.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 
@@ -52,12 +61,14 @@ public class ControllerExceptionHandler {
             String fieldName = ((FieldError) error).getField();
             String message = error.getDefaultMessage();
             errorMessageByFields.put(fieldName, message);
+            logger.warn(message);
         });
         return ResponseEntity.badRequest().body(errorMessageByFields);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleException(Exception e) {
+        logger.error(e.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("서버에서 에러가 발생했습니다.");
     }
 }
