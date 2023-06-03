6eed02ed4a89453eb550e56e8e928f24d9c50792
yoondgu <doyou2ing@naver.com>
2023-06-03 16:07:24 +0900

    build: DDL 프로덕션과 테스트 설정 분리
    - 실제 배포 시 이미 테이블이 있으면 다시 생성하지 않도록 함
    - 테스트 컨텍스트 생성 시 항상 테이블을 새로 만들도록 함

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,32 +1,24 @@
-DROP TABLE product IF EXISTS;
-DROP TABLE member IF EXISTS;
-DROP TABLE cart_item IF EXISTS;
-DROP TABLE orders IF EXISTS;
-DROP TABLE order_item IF EXISTS;
-DROP TABLE coupon_type IF EXISTS;
-DROP TABLE coupon IF EXISTS;
-
-CREATE TABLE product (
+CREATE TABLE IF NOT EXISTS product (
                          id BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
                          name VARCHAR(255) NOT NULL,
                          price INT NOT NULL,
                          image_url VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE member (
+CREATE TABLE IF NOT EXISTS member (
                         id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
                         email VARCHAR(255) NOT NULL UNIQUE,
                         password VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE cart_item (
+CREATE TABLE IF NOT EXISTS cart_item (
                            id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
                            member_id BIGINT NOT NULL,
                            product_id BIGINT NOT NULL,
                            quantity INT NOT NULL
 );
 
-CREATE TABLE orders (
+CREATE TABLE IF NOT EXISTS orders (
                         id BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
                         member_id BIGINT NOT NULL,
                         coupon_id BIGINT,
@@ -36,7 +28,7 @@ CREATE TABLE orders (
                         updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE order_item (
+CREATE TABLE IF NOT EXISTS order_item (
                             id BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
                             order_id BIGINT NOT NULL,
                             name VARCHAR(255) NOT NULL,
@@ -45,15 +37,14 @@ CREATE TABLE order_item (
                             quantity INT NOT NULL
 );
 
-
-CREATE TABLE coupon_type (
+CREATE TABLE IF NOT EXISTS coupon_type (
                              id BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
                              name VARCHAR(255) NOT NULL,
                              discount_type VARCHAR(10) NOT NULL,
                              discount_amount BIGINT NOT NULL
 );
 
-CREATE TABLE coupon (
+CREATE TABLE IF NOT EXISTS coupon (
                         id BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
                         member_id BIGINT NOT NULL,
                         coupon_type_id BIGINT NOT NULL,
