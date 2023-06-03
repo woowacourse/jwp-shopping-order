6709f7ebd9447e04259009f627d42821e4a2f21d
xxeol2 <jhj01177@naver.com>
2023-06-03 19:57:57 +0900

    feat: 장바구니 아이템 주문 controller 구현

--- src/main/java/cart/ui/OrderApiController.java
+++ src/main/java/cart/ui/OrderApiController.java
@@ -1,15 +1,19 @@
 package cart.ui;
 
+import cart.application.dto.order.CreateOrderByCartItemsRequest;
 import cart.application.dto.order.FindOrderCouponsResponse;
 import cart.application.dto.order.FindOrderDetailResponse;
 import cart.application.dto.order.FindOrdersResponse;
 import cart.application.service.MemberCouponService;
 import cart.application.service.OrderService;
 import cart.domain.Member;
+import java.net.URI;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,5 +45,12 @@ public class OrderApiController {
     @GetMapping
     public ResponseEntity<FindOrdersResponse> findAllOrders(Member member) {
         return ResponseEntity.ok(orderService.findOrders(member));
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> orderCartItems(Member member,
+            @RequestBody CreateOrderByCartItemsRequest request) {
+        long id = orderService.orderCartItems(member, request);
+        return ResponseEntity.created(URI.create("/orders/" + id)).build();
     }
 }
