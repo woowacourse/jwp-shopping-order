5a050a0ae03ec607d658d57806856802edb31422
pilyang <30036534+pilyang@users.noreply.github.com>
2023-06-03 16:47:04 +0900

    fix: 테스트시 데이터 스키마 생성 오류 수정

--- src/test/java/cart/domain/discountpolicy/DiscountPolicyProviderTest.java
+++ src/test/java/cart/domain/discountpolicy/DiscountPolicyProviderTest.java
@@ -3,11 +3,13 @@ package cart.domain.discountpolicy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@AutoConfigureTestDatabase
 class DiscountPolicyProviderTest {
 
     @Autowired
--- src/test/java/cart/integration/IntegrationTest.java
+++ src/test/java/cart/integration/IntegrationTest.java
@@ -2,12 +2,14 @@ package cart.integration;
 
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.annotation.DirtiesContext;
 
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureTestDatabase
 public class IntegrationTest {
     @LocalServerPort
     private int port;
