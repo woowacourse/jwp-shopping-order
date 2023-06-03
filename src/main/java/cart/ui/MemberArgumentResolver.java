838a9f223a4a73d775f9021248602ef0c6eec19f
kevstevie <jianna442@gmail.com>
2023-06-03 18:11:10 +0900

    fix: 인증 정보 없을 시 401코드 반환

--- src/main/java/cart/ui/MemberArgumentResolver.java
+++ src/main/java/cart/ui/MemberArgumentResolver.java
@@ -29,12 +29,12 @@ public class MemberArgumentResolver implements HandlerMethodArgumentResolver {
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
         String authorization = webRequest.getHeader(HttpHeaders.AUTHORIZATION);
         if (authorization == null) {
-            return null;
+            throw new AuthenticationException();
         }
 
         String[] authHeader = authorization.split(" ");
         if (!authHeader[0].equalsIgnoreCase("basic")) {
-            return null;
+            throw new AuthenticationException();
         }
 
         byte[] decodedBytes = Base64.decodeBase64(authHeader[1]);
