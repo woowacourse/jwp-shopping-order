b02fbb2e23f24f3c6e51a33033e15740ee1ac2c6
bjk1649 <qhwns1649@naver.com>
2023-06-03 17:43:45 +0900

    refactor: service에서 사용하는 repository를 인터페이스로 변경

--- src/main/java/cart/repository/CartItemRepository.java
+++ src/main/java/cart/repository/CartItemRepository.java
@@ -1,92 +1,20 @@
 package cart.repository;
 
-import cart.dao.CartItemDao;
 import cart.domain.CartItem;
 import cart.domain.Member;
-import cart.domain.Product;
-import cart.entity.CartItemEntity;
-import cart.exception.IllegalMemberException;
-import cart.exception.ResourceNotFoundException;
 import java.util.List;
-import java.util.stream.Collectors;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public class CartItemRepository {
+public interface CartItemRepository {
 
-    private final CartItemDao cartItemDao;
+    Long save(final CartItem cartItem);
 
-    public CartItemRepository(final CartItemDao cartItemDao) {
-        this.cartItemDao = cartItemDao;
-    }
+    CartItem findById(final Long id);
 
-    public CartItem findById(final Long id) {
-        return toDomain(cartItemDao.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("해당하는 상품이 없습니다.")));
-    }
+    List<CartItem> findMembersItemByCartIds(final Member member, final List<Long> ids);
 
-    public List<CartItem> findMembersItemByCartIds(final Member member, final List<Long> ids) {
-        final List<CartItem> cartItems = cartItemDao.findByMemberId(member.getId())
-                .stream()
-                .filter(cartItemEntity -> ids.contains(cartItemEntity.getId()))
-                .map(this::toDomain)
-                .collect(Collectors.toUnmodifiableList());
-        if (cartItems.size() != ids.size()) {
-            throw new IllegalMemberException("요청한 장바구니에 접근할 수 없는 유저입니다.");
-        }
-        return cartItems;
-    }
+    List<CartItem> findByMemberId(final Long memberId);
 
-    public List<CartItem> findByMemberId(final Long memberId) {
-        return cartItemDao.findByMemberId(memberId)
-                .stream()
-                .map(this::toDomain)
-                .collect(Collectors.toUnmodifiableList());
-    }
+    void update(final CartItem cartItem);
 
-    public Long save(final CartItem cartItem) {
-        return cartItemDao.save(toEntity(cartItem));
-    }
-
-    public void update(final CartItem cartItem) {
-        cartItemDao.update(toEntity(cartItem));
-    }
-
-    public void deleteById(final Long id) {
-        cartItemDao.deleteById(id);
-    }
-
-    private CartItem toDomain(final CartItemEntity cartItemEntity) {
-        return new CartItem(
-                cartItemEntity.getId(),
-                cartItemEntity.getQuantity(),
-                new Product(
-                        cartItemEntity.getProductId(),
-                        cartItemEntity.getProductName(),
-                        cartItemEntity.getProductPrice(),
-                        cartItemEntity.getProductImageUrl())
-                ,
-                new Member(
-                        cartItemEntity.getMemberId(),
-                        cartItemEntity.getMemberEmail(),
-                        cartItemEntity.getMemberPassword(),
-                        cartItemEntity.getMemberPoint()
-                )
-        );
-    }
-
-    private CartItemEntity toEntity(final CartItem cartItem) {
-        return new CartItemEntity(
-                cartItem.getId(),
-                cartItem.getMember().getId(),
-                cartItem.getMember().getEmail(),
-                cartItem.getMember().getPassword(),
-                cartItem.getMember().getPoint(),
-                cartItem.getProduct().getId(),
-                cartItem.getProduct().getName(),
-                cartItem.getProduct().getPrice(),
-                cartItem.getProduct().getImageUrl(),
-                cartItem.getQuantity()
-        );
-    }
+    void deleteById(final Long id);
 }
--- src/main/java/cart/repository/ProductRepository.java
+++ src/main/java/cart/repository/ProductRepository.java
@@ -1,51 +1,17 @@
 package cart.repository;
 
-import cart.dao.ProductDao;
 import cart.domain.Product;
-import cart.entity.ProductEntity;
-import cart.exception.ResourceNotFoundException;
 import java.util.List;
-import java.util.stream.Collectors;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public class ProductRepository {
+public interface ProductRepository {
 
-    private final ProductDao productDao;
+    Long save(final Product product);
 
-    public ProductRepository(final ProductDao productDao) {
-        this.productDao = productDao;
-    }
+    Product findById(final Long id);
 
-    public Product findById(final Long id) {
-        return toDomain(productDao.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("해당하는 상품이 없습니다.")));
-    }
+    List<Product> findAll();
 
-    public List<Product> findAll() {
-        return productDao.findAll().stream()
-                .map(this::toDomain)
-                .collect(Collectors.toUnmodifiableList());
-    }
+    void update(final Product product);
 
-    public Long save(final Product product) {
-        return productDao.save(toEntity(product));
-    }
-
-    public void update(final Product product) {
-        productDao.update(toEntity(product));
-    }
-
-    public void deleteById(final Long id) {
-        productDao.deleteById(id);
-    }
-
-    private Product toDomain(final ProductEntity productEntity) {
-        return new Product(productEntity.getId(), productEntity.getName(), productEntity.getPrice(),
-                productEntity.getImageUrl());
-    }
-
-    private ProductEntity toEntity(final Product product) {
-        return new ProductEntity(product.getId(), product.getName(), product.getPrice(), product.getImageUrl());
-    }
+    void deleteById(final Long id);
 }
