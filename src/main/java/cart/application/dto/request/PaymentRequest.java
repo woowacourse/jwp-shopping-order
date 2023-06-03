3138a898d8c5b3fb77a21da8b8d63521129b4712
Hyeonjae-K <kwonhyeonjae01@gmail.com>
2023-06-03 23:38:22 +0900

    refactor: PaymentRequest 수정

--- src/main/java/cart/application/dto/request/PaymentRequest.java
+++ src/main/java/cart/application/dto/request/PaymentRequest.java
@@ -7,18 +7,18 @@ import java.util.List;
 public class PaymentRequest {
 
     @NotEmpty(message = "장바구니가 비어있습니다.")
-    private final List<CartItemRequest> cartItemRequests;
+    private final List<Long> cartItemIds;
 
     @PositiveOrZero(message = "포인트는 0원 이상 사용 가능합니다.")
     private final Integer point;
 
-    public PaymentRequest(final List<CartItemRequest> cartItemRequests, final Integer point) {
-        this.cartItemRequests = cartItemRequests;
+    public PaymentRequest(final List<Long> cartItemIds, final Integer point) {
+        this.cartItemIds = cartItemIds;
         this.point = point;
     }
 
-    public List<CartItemRequest> getCartItemRequests() {
-        return cartItemRequests;
+    public List<Long> getCartItemIds() {
+        return cartItemIds;
     }
 
     public Integer getPoint() {
