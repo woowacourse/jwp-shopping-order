da010bc783483640ffe3da881e28e5ea93ff62fa
jaeyeon kim <jakind@naver.com>
2023-06-03 14:17:10 +0900

    feat : Coupon 할인 가격을 반환하는 Service Method 추가

--- src/main/java/cart/service/CouponService.java
+++ src/main/java/cart/service/CouponService.java
@@ -1,10 +1,13 @@
 package cart.service;
 
 import cart.controller.response.CouponResponseDto;
+import cart.domain.Coupon;
 import cart.domain.Member;
 import cart.domain.MemberCoupon;
+import cart.domain.util.DiscountCalculator;
 import cart.repository.CouponRepository;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 
@@ -32,6 +35,14 @@ public class CouponService {
         return memberCouponByMember.stream()
                 .map(CouponResponseDto::from)
                 .collect(Collectors.toList());
+    }
+
+    public Integer calculateDiscountPrice(final Member member, final Integer originPrice, final Long memberCouponId) {
+        Coupon coupon = couponRepository
+                .findCouponByMemberAndMemberCouponId(member, memberCouponId)
+                .orElseThrow(NoSuchElementException::new);
+
+        return DiscountCalculator.calculatePriceAfterDiscount(originPrice, coupon);
     }
 
 }
--- src/test/java/cart/service/CouponServiceTest.java
+++ src/test/java/cart/service/CouponServiceTest.java
@@ -1,6 +1,9 @@
 package cart.service;
 
+import static fixture.MemberCouponFixture.MEMBER_COUPON_3;
+import static fixture.MemberCouponFixture.MEMBER_COUPON_4;
 import static fixture.MemberFixture.MEMBER_1;
+import static fixture.MemberFixture.MEMBER_2;
 import static fixture.MemberFixture.MEMBER_3;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.tuple;
@@ -8,8 +11,12 @@ import static org.assertj.core.api.Assertions.tuple;
 import anotation.ServiceTest;
 import cart.controller.response.CouponResponseDto;
 import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @ServiceTest
@@ -37,6 +44,22 @@ class CouponServiceTest {
         assertThat(couponResponseDtos)
                 .extracting(CouponResponseDto::getMemberCouponId, CouponResponseDto::getName)
                 .containsExactly(tuple(1L, "정액 할인 쿠폰"), tuple(2L, "할인율 쿠폰"));
+    }
+
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("validateDiscountCalculator")
+    @DisplayName("쿠폰을 적용했을 때에 총 가격을 반환")
+    void calculateDiscountPrice(String testName, Integer originPrice, Long memberCouponId,Integer expectedValue) {
+        Integer money = couponService.calculateDiscountPrice(MEMBER_2, originPrice, memberCouponId);
+
+        assertThat(money).isEqualTo(expectedValue);
+    }
+
+    private static Stream<Arguments> validateDiscountCalculator() {
+        return Stream.of(
+                Arguments.of("정액 할인 쿠폰", 10000, MEMBER_COUPON_3.getId(), 5000),
+                Arguments.of("할인율 쿠폰", 10000, MEMBER_COUPON_4.getId(), 9000)
+        );
     }
 
 }
