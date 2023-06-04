2ed7c10cbb41357fe6a5fac73f92e0b1f5be1777
odo27 <odo27@naver.com>
2023-06-04 13:56:48 +0900

    feat: 날짜 출력 형식 변경

--- src/main/java/cart/dto/OrderResponse.java
+++ src/main/java/cart/dto/OrderResponse.java
@@ -1,5 +1,8 @@
 package cart.dto;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -26,12 +29,18 @@ public class OrderResponse {
     public static OrderResponse of(final Order order) {
         return new OrderResponse(
                 order.getId(),
-                order.getCreatedAt().toString(),
+                format(order.getCreatedAt()),
                 order.getCartItems().getCartItems().stream().map(OrderItemResponse::of).collect(Collectors.toList()),
                 order.getCartItems().getTotalPrice().getValue(),
                 order.getOrderPoint().getUsedPoint().getValue(),
                 order.getOrderPoint().getEarnedPoint().getValue()
         );
+    }
+
+    private static String format(final Timestamp timestamp) {
+        final LocalDateTime localDateTime = timestamp.toLocalDateTime();
+        final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+        return localDateTime.format(dateTimeFormatter);
     }
 
     public Long getOrderId() {
