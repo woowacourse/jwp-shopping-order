5fc9e96ebd9761459e7d70b994854406a3237a1e
somsom13 <somin1113@naver.com>
2023-06-04 10:46:01 +0900

    refactor: 로컬 docker 데이터베이스 초기화 파일 설정

--- docker/init.sql
+++ docker/init.sql
@@ -60,3 +60,24 @@ CREATE TABLE IF NOT EXISTS point_history
     FOREIGN KEY (member_id) REFERENCES member (id),
     FOREIGN KEY (order_id) REFERENCES `order` (id)
 );
+
+INSERT INTO product (name, price, image_url)
+VALUES ('치킨', 10000,
+        'https://images.unsplash.com/photo-1626082927389-6cd097cdc6ec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80'),
+       ('샐러드', 20000,
+        'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80'),
+       ('피자', 13000,
+        'https://images.unsplash.com/photo-1595854341625-f33ee10dbf94?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80');
+
+INSERT INTO member (email, password)
+VALUES ('a@a.com', '1234'),
+       ('b@b.com', '1234');
+
+INSERT INTO point (member_id, point)
+VALUES (1, 1000),
+       (2, 1000);
+
+INSERT INTO cart_item (member_id, product_id, quantity)
+VALUES (1, 1, 2),
+       (1, 2, 4),
+       (2, 3, 5);
