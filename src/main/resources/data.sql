467ea40237974dac5330d9eb9280dbd2a57a339f
mcodnjs <codnjs8989@gmail.com>
2023-06-04 16:22:06 +0900

    docs: API 명세 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,39 +8,5 @@ INSERT INTO product (name, price, image_url)
 VALUES ('피자', 13000,
         'https://images.unsplash.com/photo-1595854341625-f33ee10dbf94?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80');
 
-INSERT INTO member (name, password)
-VALUES ('a@a.com', '1234');
-INSERT INTO member (name, password)
-VALUES ('b@b.com', '1234');
-
-INSERT INTO cart_item (member_id, product_id, quantity)
-VALUES (1, 1, 2);
-INSERT INTO cart_item (member_id, product_id, quantity)
-VALUES (1, 2, 4);
-
-INSERT INTO cart_item (member_id, product_id, quantity)
-VALUES (2, 3, 5);
-
 INSERT INTO coupon (name, discount_rate, `period`, expired_at)
-VALUES ('신규 가입 할인 쿠폰', 10, 14, '2023-05-30 15:30:00');
-
-INSERT INTO member_coupon (member_id, coupon_id, issued_at, expired_at, is_used)
-VALUES (1, 1, NOW(), '2023-06-01 15:30:00', false);
-
-INSERT INTO `order` (member_id, total_price, discounted_total_price, delivery_price, ordered_at)
-VALUES (1, 40000, 36000, 3000, NOW());
-
-INSERT INTO order_product (order_id, product_id, ordered_product_price, quantity)
-VALUES (1, 1, 10000, 2);
-
-INSERT INTO order_product (order_id, product_id, ordered_product_price, quantity)
-VALUES (1, 2, 20000, 1);
-
-INSERT INTO order_coupon (order_id, coupon_id)
-VALUES (1, 1);
-
-INSERT INTO `order` (member_id, total_price, discounted_total_price, delivery_price, ordered_at)
-VALUES (1, 20000, 20000, 3000, NOW());
-
-INSERT INTO order_product (order_id, product_id, ordered_product_price, quantity)
-VALUES (2, 1, 10000, 2);
+VALUES ('신규 가입 할인 쿠폰', 10, 14, '9999-12-31');
