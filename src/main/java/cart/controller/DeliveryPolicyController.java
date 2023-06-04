8239a5375b35073761b0b466fa7cb24c3cb2136a
aiaiaiai1 <cvc100326@naver.com>
2023-06-05 03:54:52 +0900

    fix : 어노테이션 추가

--- src/main/java/cart/controller/DeliveryPolicyController.java
+++ src/main/java/cart/controller/DeliveryPolicyController.java
@@ -2,6 +2,7 @@ package cart.controller;
 
 import cart.dto.DeliveryPolicyResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/delivery-policy")
 public class DeliveryPolicyController {
 
+    @GetMapping
     public ResponseEntity<DeliveryPolicyResponse> getDeliveryPolicy() {
         DeliveryPolicyResponse deliveryPolicy = new DeliveryPolicyResponse(3000, 30000);
         return ResponseEntity.ok().body(deliveryPolicy);
