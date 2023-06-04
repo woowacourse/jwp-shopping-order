9a033731bffeaf3ceca68581cc4be99e421be776
eunbii0213 <eunbii0213@naver.com>
2023-06-04 18:52:22 +0900

    fix: 테스트 상품 중복 삽입 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,15 +1,15 @@
 
 INSERT INTO product (name, price, image_url, is_discounted, discount_rate)
 SELECT '치킨2', 10000, 'https://images.unsplash.com/photo-1626082927389-6cd097cdc6ec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80', 0, 0
-WHERE NOT EXISTS (SELECT 1 FROM product WHERE name = '치킨');
+WHERE NOT EXISTS (SELECT 1 FROM product WHERE name = '치킨2');
 
 INSERT INTO product (name, price, image_url, is_discounted, discount_rate)
 SELECT '샐러드2', 20000, 'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80', 1, 50
-WHERE NOT EXISTS (SELECT 1 FROM product WHERE name = '샐러드');
+WHERE NOT EXISTS (SELECT 1 FROM product WHERE name = '샐러드2');
 
 INSERT INTO product (name, price, image_url, is_discounted, discount_rate)
 SELECT '피자2', 13000, 'https://images.unsplash.com/photo-1595854341625-f33ee10dbf94?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80', 1, 20
-WHERE NOT EXISTS (SELECT 1 FROM product WHERE name = '피자');
+WHERE NOT EXISTS (SELECT 1 FROM product WHERE name = '피자2');
 
 INSERT INTO member (email, password, grade, total_purchase_amount)
 SELECT 'a@a.com', '1234', '일반', 0
