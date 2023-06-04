1b2879fd16b53a650db3ed96cbe5fa53f99f095a
parkmuhyeun <pjhg410@gmail.com>
2023-06-04 21:34:54 +0900

    feat: CORS 처리 추가

--- src/main/java/cart/WebMvcConfig.java
+++ src/main/java/cart/WebMvcConfig.java
@@ -4,6 +4,7 @@ import cart.dao.MemberDao;
 import cart.ui.MemberArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -19,5 +20,14 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new MemberArgumentResolver(memberDao));
+    }
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedMethods("*")
+            .allowedOrigins("https://dladncks1217.github.io/react-shopping-cart-prod/",
+                "https://step1--startling-rabanadas-09fe85.netlify.app/",
+                "http://localhost:3000", "http://127.0.0.1:3000");
     }
 }
