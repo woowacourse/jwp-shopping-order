dd55032dca4c0da38da3e66a95c3c93a37ed388b
mcodnjs <codnjs8989@gmail.com>
2023-06-04 16:49:34 +0900

    refactor: ResponseEntityExceptionHandler 추가

--- src/main/java/cart/exception/ErrorCode.java
+++ src/main/java/cart/exception/ErrorCode.java
@@ -11,6 +11,7 @@ public enum ErrorCode {
     INVALID_COUPON_ID("유효하지 않은 쿠폰 ID 입니다."),
     INVALID_ORDER_ID("유효하지 않은 주문번호 ID 입니다."),
     ALREADY_ISSUED_COUPON("이미 발급받은 쿠폰입니다."),
+    HTTP_REQUEST_EXCEPTION("HTTP 요청에 실패하였습니다."),
     INTERNAL_SERVER_ERROR("예상치 못한 서버 오류가 발생하였습니다.");
 
     private final String errorMessage;
--- src/main/java/cart/ui/GlobalExceptionHandler.java
+++ src/main/java/cart/ui/GlobalExceptionHandler.java
@@ -6,17 +6,62 @@ import cart.exception.BadRequestException;
 import cart.exception.ErrorResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static cart.exception.ErrorCode.HTTP_REQUEST_EXCEPTION;
 import static cart.exception.ErrorCode.INTERNAL_SERVER_ERROR;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @Override
+    protected ResponseEntity<Object> handleBindException(final BindException ex, final HttpHeaders headers, final HttpStatus status, final WebRequest request) {
+        List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
+        List<String> errorMessages = new ArrayList<>();
+        for (FieldError fieldError : fieldErrors) {
+            errorMessages.add(fieldError.getDefaultMessage());
+            logger.error(fieldError.getDefaultMessage());
+        }
+        return ResponseEntity.badRequest()
+                .body(errorMessages);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleServletRequestBindingException(final ServletRequestBindingException ex, final HttpHeaders headers, final HttpStatus status, final WebRequest request) {
+        logger.error(ex.getMessage());
+        return ResponseEntity.badRequest()
+                .body(new ErrorResponse(HTTP_REQUEST_EXCEPTION, ex.getMessage()));
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMissingServletRequestParameter(final MissingServletRequestParameterException ex, final HttpHeaders headers, final HttpStatus status, final WebRequest request) {
+        logger.error(ex.getMessage());
+        return ResponseEntity.badRequest()
+                .body(new ErrorResponse(HTTP_REQUEST_EXCEPTION, ex.getMessage()));
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(final HttpRequestMethodNotSupportedException ex, final HttpHeaders headers, final HttpStatus status, final WebRequest request) {
+        logger.error(ex.getMessage());
+        return ResponseEntity.badRequest()
+                .body(new ErrorResponse(HTTP_REQUEST_EXCEPTION, ex.getMessage()));
+    }
 
     @ExceptionHandler(AuthenticationException.class)
     public ResponseEntity<ErrorResponse> handlerAuthenticationException(AuthenticationException e) {
@@ -35,7 +80,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<ErrorResponse> handleException(BadRequestException e) {
         logger.error(e.getErrorCode().getErrorMessage());
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+        return ResponseEntity.badRequest()
                 .body(new ErrorResponse(e.getErrorCode(), e.getErrorCode().getErrorMessage()));
     }
 
@@ -43,7 +88,7 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleException(Exception e) {
         e.printStackTrace();
         logger.error(e.getMessage());
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+        return ResponseEntity.internalServerError()
                 .body(new ErrorResponse(INTERNAL_SERVER_ERROR, INTERNAL_SERVER_ERROR.getErrorMessage()));
     }
 }
