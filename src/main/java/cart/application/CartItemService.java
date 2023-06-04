2747c135b3424d2ead37214fbf2eb831d8bb7bfa
somsom13 <somin1113@naver.com>
2023-06-04 10:51:24 +0900

    chore: `@Transactional` 추가

--- src/main/java/cart/application/CartItemService.java
+++ src/main/java/cart/application/CartItemService.java
@@ -12,8 +12,10 @@ import cart.repository.ProductRepository;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class CartItemService {
 
     private final ProductRepository productRepository;
@@ -26,6 +28,7 @@ public class CartItemService {
         this.cartItemRepository = cartItemRepository;
     }
 
+    @Transactional(readOnly = true)
     public List<CartItemResponse> findByMember(Member member) {
         Cart cart = cartItemRepository.findByMember(member);
         return cart.getItems()
@@ -64,6 +67,7 @@ public class CartItemService {
         cartItemRepository.removeAllByIds(cartItemIds);
     }
 
+    @Transactional(readOnly = true)
     public Cart findAllByIds(List<Long> ids, Member member) {
         Cart cart = cartItemRepository.findAllByIds(ids);
         cart.validateOwner(member);
