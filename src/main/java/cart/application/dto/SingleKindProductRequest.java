c9019be9984cee7ad5db67bbd27b3c6eb28e59d3
jaehee329 <jjhs9803@gmail.com>
2023-06-04 19:37:11 +0900

    refactor: 명세의 오타 수정 반영

--- src/main/java/cart/application/dto/SingleKindProductRequest.java
+++ src/main/java/cart/application/dto/SingleKindProductRequest.java
@@ -2,15 +2,12 @@ package cart.application.dto;
 
 import javax.validation.constraints.Positive;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public class SingleKindProductRequest {
 
     @Positive(message = "유효하지 않은 상품 id 입니다")
     private Long productId;
 
     @Positive(message = "구매할 상품의 개수는 1개 이상이어야 합니다")
-    @JsonProperty("Quantity")
     private Integer quantity;
 
     private SingleKindProductRequest() {
