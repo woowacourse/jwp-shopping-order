60c648fbff04d62012fd2e701d6e31a1978b6ad5
ReO <yt930310@gmail.com>
2023-06-04 19:36:51 +0900

    refactor: 명세대로 수정

--- src/main/java/cart/dto/response/CouponsResponse.java
+++ src/main/java/cart/dto/response/CouponsResponse.java
@@ -3,13 +3,13 @@ package cart.dto.response;
 import java.util.List;
 
 public class CouponsResponse {
-    private final List<CouponResponse> couponResponses;
+    private final List<CouponResponse> coupons;
 
     public CouponsResponse(List<CouponResponse> couponResponses) {
-        this.couponResponses = couponResponses;
+        this.coupons = couponResponses;
     }
 
-    public List<CouponResponse> getCouponResponses() {
-        return couponResponses;
+    public List<CouponResponse> getCoupons() {
+        return coupons;
     }
 }
