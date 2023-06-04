da31bbc442dfcf2d1747e9346b2ba53cfa92c9bc
Rosie <dev.kayoung@gmail.com>
2023-06-04 15:22:50 +0900

    fix: 서버 내부 오류 메시지 숨김

--- src/main/java/cart/ui/ControllerExceptionHandler.java
+++ src/main/java/cart/ui/ControllerExceptionHandler.java
@@ -1,7 +1,14 @@
 package cart.ui;
 
+import cart.JwpCartApplication;
 import cart.dto.ApiErrorResponse;
-import cart.exception.*;
+import cart.exception.AuthenticationException;
+import cart.exception.CartItemException;
+import cart.exception.OrderException;
+import cart.exception.PaymentException;
+import cart.exception.ProductException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,26 +17,29 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 @ControllerAdvice
 public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
+    private final Logger logger = LoggerFactory.getLogger(JwpCartApplication.class);
+
     @ExceptionHandler(AuthenticationException.class)
-    public ResponseEntity<ApiErrorResponse> handleAuthenticationException(final AuthenticationException e) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ApiErrorResponse.from(e));
+    public ResponseEntity<ApiErrorResponse> handleAuthenticationException(
+            final AuthenticationException authenticationException) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ApiErrorResponse.from(authenticationException));
     }
 
     @ExceptionHandler({CartItemException.NotFound.class, OrderException.NotFound.class,
             ProductException.NotFound.class, PaymentException.NotFound.class})
-    public ResponseEntity<ApiErrorResponse> handleNotFoundException(final RuntimeException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ApiErrorResponse.from(e));
+    public ResponseEntity<ApiErrorResponse> handleNotFoundException(final RuntimeException runtimeException) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ApiErrorResponse.from(runtimeException));
     }
 
     @ExceptionHandler({CartItemException.IllegalMember.class, OrderException.IllegalMember.class})
-    public ResponseEntity<ApiErrorResponse> handleAuthorizationException(final RuntimeException e) {
-        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ApiErrorResponse.from(e));
+    public ResponseEntity<ApiErrorResponse> handleAuthorizationException(final RuntimeException runtimeException) {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ApiErrorResponse.from(runtimeException));
     }
 
     @ExceptionHandler({RuntimeException.class})
-    public ResponseEntity<ApiErrorResponse> handleRuntimeException(final RuntimeException e) {
-        e.printStackTrace();
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ApiErrorResponse.from(e));
+    public ResponseEntity<ApiErrorResponse> handleRuntimeException(final RuntimeException runtimeException) {
+        logger.error(runtimeException.getMessage(), runtimeException);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiErrorResponse("서버 내부 오류"));
     }
 
 }
