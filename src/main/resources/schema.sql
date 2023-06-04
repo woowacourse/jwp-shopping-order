0fa1fccccdae5d4e0f234b6bb12f5f52a19f1567
Woojin Lee <dhkep03@gmail.com>
2023-06-04 17:54:15 +0900

    feat: 데이터베이스 테이블 설계

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,13 +2,15 @@ CREATE TABLE product (
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     price INT NOT NULL,
-    image_url VARCHAR(255) NOT NULL
+    image_url VARCHAR(255) NOT NULL,
+    created_at     TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE member (
      id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
      email VARCHAR(255) NOT NULL UNIQUE,
-     password VARCHAR(255) NOT NULL
+     password VARCHAR(255) NOT NULL,
+     created_at     TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE cart_item (
@@ -16,6 +18,45 @@ CREATE TABLE cart_item (
     member_id BIGINT NOT NULL,
     product_id BIGINT NOT NULL,
     quantity INT NOT NULL,
+    created_at     TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (member_id) REFERENCES member(id),
     FOREIGN KEY (product_id) REFERENCES product(id)
 );
+
+CREATE TABLE orders (
+    id             BIGINT    NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    member_id      BIGINT    NOT NULL,
+    actual_price   INT       NOT NULL,
+    original_price INT       NOT NULL,
+    created_at     TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (member_id) REFERENCES member (id)
+);
+
+CREATE TABLE order_item (
+    id            BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    order_id      BIGINT       NOT NULL,
+    product_id    BIGINT       NOT NULL,
+    product_name  VARCHAR(255) NOT NULL,
+    product_image VARCHAR(255) NOT NULL,
+    product_price INT          NOT NULL,
+    quantity      INT          NOT NULL,
+    created_at     TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (order_id) REFERENCES orders (id)
+);
+
+CREATE TABLE coupon (
+    id            BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name          VARCHAR(255) NOT NULL,
+    type          VARCHAR(255) NOT NULL,
+    amount        INT          NOT NULL,
+    created_at     TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE member_coupon (
+    id            BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    coupon_id BIGINT  NOT NULL,
+    member_id BIGINT  NOT NULL,
+    FOREIGN KEY (member_id) REFERENCES member (id),
+    FOREIGN KEY (coupon_id) REFERENCES coupon (id)
+);
+
