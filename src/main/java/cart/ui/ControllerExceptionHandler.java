4a241638f548158ddddd7a28ebb37e6c4eed16e5
0chil <0@chll.it>
2023-06-05 15:38:29 +0900

    refactor(ExceptionHandler): 로깅 수준을 고려해 개선

--- src/main/java/cart/ui/ControllerExceptionHandler.java
+++ src/main/java/cart/ui/ControllerExceptionHandler.java
@@ -17,6 +17,7 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 
 import cart.dto.ErrorResponse;
 import cart.exception.ApplicationException;
+import cart.exception.AuthenticationException;
 
 @RestControllerAdvice
 public class ControllerExceptionHandler {
@@ -27,41 +28,50 @@ public class ControllerExceptionHandler {
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleUnhandled(Exception e) {
         logger.error(e.getMessage(), e.getCause());
-        e.printStackTrace();
+        if (logger.isDebugEnabled()) {
+            e.printStackTrace();
+        }
         return new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value());
     }
 
+    @ExceptionHandler(AuthenticationException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ResponseEntity<ErrorResponse> handlerAuthenticationException(AuthenticationException e) {
+        logger.info(e.getMessage(), e.getCause());
+        return ResponseEntity.status(e.status()).body(new ErrorResponse(e));
+    }
+
     @ExceptionHandler(ApplicationException.class)
-    public ResponseEntity<ErrorResponse> handlerAuthenticationException(ApplicationException e) {
-        logger.error(e.getMessage(), e.getCause());
+    public ResponseEntity<ErrorResponse> handlerApplicationException(ApplicationException e) {
+        logger.debug(e.getMessage(), e.getCause());
         return ResponseEntity.status(e.status()).body(new ErrorResponse(e));
     }
 
     @ExceptionHandler(NoHandlerFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleNoHandlerFound(NoHandlerFoundException e) {
-        logger.warn(e.getMessage(), e.getCause());
+            logger.debug(e.getMessage(), e.getCause());
         return new ErrorResponse(HttpStatus.NOT_FOUND.value(), "리소스를 찾지 못했습니다");
     }
 
     @ExceptionHandler(MissingServletRequestParameterException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleMissingRequestParameter(MissingServletRequestParameterException e) {
-        logger.warn(e.getMessage(), e.getCause());
+        logger.debug(e.getMessage(), e.getCause());
         return new ErrorResponse(HttpStatus.BAD_REQUEST.value(), "쿼리스트링 '" + e.getParameterName() + "'이 없거나 잘못됐습니다");
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleHttpMessageNotReadable(HttpMessageNotReadableException e) {
-        logger.warn(e.getMessage(), e.getCause());
+        logger.debug(e.getMessage(), e.getCause());
         return new ErrorResponse(HttpStatus.BAD_REQUEST.value(), "요청 페이로드를 읽지 못했습니다");
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
-        logger.warn(e.getMessage(), e.getCause());
+        logger.debug(e.getMessage(), e.getCause());
         String message = e.getFieldErrors().stream()
                 .map(DefaultMessageSourceResolvable::getDefaultMessage)
                 .collect(Collectors.joining(", "));
