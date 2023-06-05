20baf36ffc65251a080f80eae6099dfd472613a0
0chil <0@chll.it>
2023-06-05 17:21:11 +0900

    refactor(CouponController): 사용자의 쿠폰 발급 API 개선

--- src/main/java/cart/ui/CouponController.java
+++ src/main/java/cart/ui/CouponController.java
@@ -4,13 +4,14 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import cart.application.CouponService;
 import cart.domain.Member;
 import cart.domain.MemberCoupon;
+import cart.dto.MemberCouponIssueRequest;
 import cart.dto.MemberCouponResponse;
 
 @RestController
@@ -29,9 +30,9 @@ public class CouponController {
         return ResponseEntity.ok(MemberCouponResponse.of(memberCoupons));
     }
 
-    @PostMapping("/member/coupons/{couponId}")
-    public ResponseEntity<Void> issueCouponTo(Member member, @PathVariable Long couponId) {
-        couponService.issueCouponTo(member, couponId);
+    @PostMapping("/member/coupons")
+    public ResponseEntity<Void> issueCouponTo(Member member, @RequestBody MemberCouponIssueRequest issueRequest) {
+        couponService.issueCouponTo(member, issueRequest.getCouponId());
 
         return ResponseEntity.noContent().build();
     }
