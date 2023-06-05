47bb6a382f30ff653a5d59dd190448163068e109
Go-Jaecheol <gojaech@naver.com>
2023-06-05 12:58:10 +0900

    chore: 사용하지 않는 파일 삭제

--- src/main/resources/local-data.sql
+++ src/main/resources/local-data.sql
@@ -7,5 +7,7 @@ INSERT INTO member (email, password) VALUES ('b@b.com', '1234');
 
 INSERT INTO cart_item (member_id, product_id, quantity) VALUES (1, 1, 2);
 INSERT INTO cart_item (member_id, product_id, quantity) VALUES (1, 2, 4);
-
 INSERT INTO cart_item (member_id, product_id, quantity) VALUES (2, 3, 5);
+
+INSERT INTO coupon (name, min_amount, discount_amount) Values ('1000원 할인 쿠폰', 15000, 1000);
+INSERT INTO coupon (name, min_amount, discount_amount) Values ('사장님이 미쳤어요! 99999원 할인 쿠폰', 1000, 99999);
