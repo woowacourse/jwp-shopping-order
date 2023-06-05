97cde46ae86d910c6bfece61c727ab315f8b0ba3
hong-sile <gurwns9325@naver.com>
2023-06-05 20:58:18 +0900

    refactor: 메서드 분리로 중복로직 제거

--- src/main/java/cart/service/response/OrderResponseDto.java
+++ src/main/java/cart/service/response/OrderResponseDto.java
@@ -36,15 +36,24 @@ public class OrderResponseDto {
         final List<OrderProductResponseDto> orderProductResponses = order.getOrderProducts().stream()
                 .map(OrderProductResponseDto::from)
                 .collect(toUnmodifiableList());
-        final Builder responseBuilder = new Builder(
+
+        return order.getMemberCoupon()
+                .map(memberCoupon -> createOrderResponse(order, orderProductResponses,
+                        memberCoupon.getCoupon().getName()))
+                .orElseGet(() -> createOrderResponse(order, orderProductResponses, null));
+    }
+
+    private static OrderResponseDto createOrderResponse(final Order order,
+                                                        final List<OrderProductResponseDto> orderProductResponses,
+                                                        final String couponName) {
+        return new OrderResponseDto(
                 order.getId()
                 , orderProductResponses
                 , order.getOrderAt().toString()
                 , order.getOriginPrice().getValue()
+                , couponName
                 , order.getDiscountPrice().getValue()
                 , order.getTotalPrice().getValue());
-        order.getMemberCoupon().ifPresent(coupon -> responseBuilder.addCouponName(coupon.getCoupon().getName()));
-        return responseBuilder.build();
     }
 
     public Long getId() {
@@ -73,43 +82,5 @@ public class OrderResponseDto {
 
     public Integer getTotalPrice() {
         return totalPrice;
-    }
-
-    private static class Builder {
-        private final Long id;
-        private final List<OrderProductResponseDto> orderProducts;
-        private final String timestamp;
-        private final Integer originPrice;
-        private final Integer discountPrice;
-        private final Integer totalPrice;
-        private String couponName;
-
-        private Builder(final Long id, final List<OrderProductResponseDto> orderProducts, final String timestamp,
-                        final Integer originPrice,
-                        final Integer discountPrice, final Integer totalPrice) {
-            this.id = id;
-            this.orderProducts = orderProducts;
-            this.timestamp = timestamp;
-            this.originPrice = originPrice;
-            this.discountPrice = discountPrice;
-            this.totalPrice = totalPrice;
-        }
-
-        public static Builder from(final Long id, final List<OrderProductResponseDto> orderProducts,
-                                   final String timestamp,
-                                   final Integer originPrice,
-                                   final Integer discountPrice, final Integer totalPrice) {
-            return new Builder(id, orderProducts, timestamp, originPrice, discountPrice, totalPrice);
-        }
-
-        public Builder addCouponName(final String couponName) {
-            this.couponName = couponName;
-            return this;
-        }
-
-        public OrderResponseDto build() {
-            return new OrderResponseDto(id, orderProducts, timestamp, originPrice, couponName, discountPrice,
-                    totalPrice);
-        }
     }
 }
