d5beefa0ff58916a48f03a6c0bf5d04d10b12e37
JunDonghyuk <ehdgur4814@naver.com>
2023-06-05 13:12:31 +0900

    refactor: requestDto 제거 및 Jsoncreator 적용

--- src/main/java/cart/dto/cartitem/CartItemQuantityUpdateRequest.java
+++ src/main/java/cart/dto/cartitem/CartItemQuantityUpdateRequest.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 
 import javax.validation.constraints.PositiveOrZero;
 
-
 public class CartItemQuantityUpdateRequest {
 
     @PositiveOrZero(message = "장바구니 아이템의 수량은 0개 이상이어야합니다.")
