f27a1400ace95f7843c5bbbec2158106bf5ca3f8
kevstevie <jianna442@gmail.com>
2023-06-05 15:12:38 +0900

    fix: cors 수정

--- src/main/java/cart/application/config/WebMvcConfig.java
+++ src/main/java/cart/application/config/WebMvcConfig.java
@@ -27,8 +27,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                        "https://solo5star.github.io/react-shopping-cart-prod",
-                        "https://naveowo.github.io/react-shopping-cart-prod",
+                        "https://solo5star.github.io/",
+                        "https://naveowo.github.io/",
                         "http://localhost:3000"
                 )
                 .allowedMethods("*")
