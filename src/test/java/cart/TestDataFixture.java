592df125ee5bc2a3946b28c21e9ce3f6c754813d
hong-sile <gurwns9325@naver.com>
2023-06-05 14:03:24 +0900

    refactor: 사용하지 않는 테스트 데이터 픽스처 제거

--- src/test/java/cart/TestDataFixture.java
+++ src/test/java/cart/TestDataFixture.java
@@ -17,14 +17,11 @@ public class TestDataFixture {
             "https://images.unsplash.com/photo-1626082927389-6cd097cdc6ec?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80");
     public static final Product PRODUCT_2 = new Product("샐러드", new Price(20000),
             "https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80");
-    public static final Product PRODUCT_3 = new Product("피자", new Price(13000),
-            "https://images.unsplash.com/photo-1595854341625-f33ee10dbf94?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80");
 
     //Member
     public static final Member MEMBER_1 = new Member(1L, "a@a.com", "1234");
     public static final Member MEMBER_2 = new Member(2L, "b@b.com", "1234");
     public static final Member MEMBER_3 = new Member(3L, "sangun", "1234");
-    public static final Member MEMBER_4 = new Member(4L, "lopi", "1234");
 
     //Coupon
     public static final Coupon DISCOUNT_50_PERCENT
