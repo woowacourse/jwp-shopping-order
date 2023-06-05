b256eba5783f39deeff962e3b8e5170baef70b28
iamjooon2 <iamjooon2@gmail.com>
2023-06-05 15:38:57 +0900

    feat: 로깅 인터셉터 추가

---
+++ src/main/java/cart/config/LoggingInterceptorHandler.java
@@ -0,0 +1,26 @@
+package cart.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public final class LoggingInterceptorHandler implements HandlerInterceptor {
+    private static final Logger logger = LoggerFactory.getLogger(LoggingInterceptorHandler.class);
+
+    @Override
+    public boolean preHandle(
+            final HttpServletRequest request,
+            final HttpServletResponse response,
+            final Object handler
+    ) {
+        final String url = request.getRequestURL().toString();
+        logger.info("Http Request: {}", url);
+
+        return true;
+    }
+}
