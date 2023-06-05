d55c591b482066dbb171ac8b410336d9d4577abd
mcodnjs <codnjs8989@gmail.com>
2023-06-06 02:14:58 +0900

    refactor: 테스트 환경 설정 파일 수정

--- src/test/java/cart/integration/IntegrationTest.java
+++ src/test/java/cart/integration/IntegrationTest.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 
-@Sql("/test-schema.sql")
+@Sql("/init.sql")
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class IntegrationTest {
