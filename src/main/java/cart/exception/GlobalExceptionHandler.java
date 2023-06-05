44c9bc102ca6afe68f2dbbc04a702e6c0b4f26bd
kiarakim <kiarakim413@gmail.com>
2023-06-05 12:15:47 +0900

    refactor: 예외 로그 출력 코드 추가

--- src/main/java/cart/exception/GlobalExceptionHandler.java
+++ src/main/java/cart/exception/GlobalExceptionHandler.java
@@ -1,11 +1,5 @@
-package cart.ui;
+package cart.exception;
 
-import cart.exception.AuthenticationException;
-import cart.exception.CartItemException;
-import cart.exception.IllegalPointUsageException;
-import cart.exception.InvalidOrderCheckedException;
-import cart.exception.InvalidOrderProductException;
-import cart.exception.InvalidOrderQuantityException;
 import cart.exception.notfound.NotFoundException;
 import cart.exception.notfound.NotFoundResponse;
 
@@ -13,32 +7,36 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class ControllerExceptionHandler {
+public class GlobalExceptionHandler {
 	private final Logger logger = LoggerFactory.getLogger(getClass());
 
 	@ExceptionHandler(AuthenticationException.class)
-	public ResponseEntity<Void> handlerAuthenticationException(AuthenticationException e) {
+	public ResponseEntity<Void> handlerAuthenticationException(AuthenticationException exception) {
+		logger.error(exception.getMessage());
 		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
 	}
 
 	@ExceptionHandler(CartItemException.IllegalMember.class)
-	public ResponseEntity<Void> handleException(CartItemException.IllegalMember e) {
+	public ResponseEntity<Void> handleException(CartItemException.IllegalMember exception) {
+		logger.error(exception.getMessage());
 		return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
 	}
 
 	@ExceptionHandler(IllegalArgumentException.class)
 	public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException exception) {
+		logger.error(exception.getMessage());
 		return ResponseEntity.badRequest().body(exception.getMessage());
 	}
 
 	@ExceptionHandler(NotFoundException.class)
 	public ResponseEntity<NotFoundResponse> handleProductNotFoundException(NotFoundException exception) {
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new NotFoundResponse(exception.getId(), exception.getName()));
+		logger.error(exception.getMessage());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND)
+			.body(new NotFoundResponse(exception.getId(), exception.getName()));
 	}
 
 	@ExceptionHandler({
@@ -47,7 +45,6 @@ public class ControllerExceptionHandler {
 		InvalidOrderProductException.class,
 		IllegalPointUsageException.class
 	})
-
 	public ResponseEntity<String> handleCustomApiException(RuntimeException exception) {
 		logger.error(exception.getMessage());
 		return ResponseEntity.badRequest().body(exception.getClass().getSimpleName());
