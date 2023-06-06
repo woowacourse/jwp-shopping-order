cc8e9d5b801e8cb5adbb70b1d43ca78b3dc04d2f
jjongwa <troas96@naver.com>
2023-06-07 02:02:59 +0900

    refactor: 개행 추가

--- src/main/java/cart/domain/discountpolicy/PointPolicy.java
+++ src/main/java/cart/domain/discountpolicy/PointPolicy.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 public class PointPolicy {
 
     private static final double EARNED_RATE = 0.01;
+
     public int calculateEarnedPoint(int totalPrice) {
         return (int) (totalPrice * EARNED_RATE);
     }
