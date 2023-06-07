16ac14a811f9f3a231a12edd5b0f2c4df0e42a6e
kdkdhoho <hkim4410@naver.com>
2023-06-07 15:30:30 +0900

    refactor(PointService): 불필요한 로직 제거
    - ArgumentResolver로 생성된 member로 또 다시 member를 찾는 로직 제거

--- src/main/java/cart/service/PointService.java
+++ src/main/java/cart/service/PointService.java
@@ -3,21 +3,12 @@ package cart.service;
 import cart.domain.Member;
 import cart.domain.Point;
 import cart.dto.response.PointResponse;
-import cart.repository.MemberRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PointService {
 
-    private final MemberRepository memberRepository;
-
-    public PointService(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
-
     public PointResponse getUserPoint(final Member member) {
-        Member foundMember = memberRepository.findById(member.getId());
-
-        return new PointResponse(foundMember.getPointValue(), Point.MIN_USAGE_VALUE);
+        return new PointResponse(member.getPointValue(), Point.MIN_USAGE_VALUE);
     }
 }
