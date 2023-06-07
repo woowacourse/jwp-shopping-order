1481cc7e7627a63a99ab53327cd038e31263abf8
jiwon <ljwon77@daum.net>
2023-06-07 17:30:49 +0900

    fix: 예외 오류 수정

--- src/main/java/cart/exception/GlobalExceptionHandler.java
+++ src/main/java/cart/exception/GlobalExceptionHandler.java
@@ -15,10 +15,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+public class GlobalExceptionHandler {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
 
