6e422ea0eca04a0e974b4de3391b5683a530fdfa
pilyang <30036534+pilyang@users.noreply.github.com>
2023-06-07 18:29:56 +0900

    refactor: ProductDao 분리

---
+++ src/main/java/cart/persistance/dao/ProductDao.java
@@ -0,0 +1,73 @@
+package cart.persistance.dao;
+
+import cart.domain.product.Product;
+import cart.persistance.entity.ProductEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Component;
+
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.List;
+import java.util.Objects;
+
+@Component
+public class ProductDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public ProductDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<ProductEntity> getAllProducts() {
+        String sql = "SELECT * FROM product";
+        return jdbcTemplate.query(sql, (rs, rowNum) -> {
+            Long productId = rs.getLong("id");
+            String name = rs.getString("product_name");
+            int price = rs.getInt("price");
+            String imageUrl = rs.getString("image_url");
+            return new ProductEntity(productId, name, price, imageUrl);
+        });
+    }
+
+    public ProductEntity getProductById(Long productId) {
+        String sql = "SELECT * FROM product WHERE id = ?";
+        return jdbcTemplate.queryForObject(sql, new Object[]{productId}, (rs, rowNum) -> {
+            String name = rs.getString("product_name");
+            int price = rs.getInt("price");
+            String imageUrl = rs.getString("image_url");
+            return new ProductEntity(productId, name, price, imageUrl);
+        });
+    }
+
+    public Long createProduct(ProductEntity product) {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(
+                    "INSERT INTO product (product_name, price, image_url) VALUES (?, ?, ?)",
+                    Statement.RETURN_GENERATED_KEYS
+            );
+
+            ps.setString(1, product.getProductName());
+            ps.setInt(2, product.getPrice());
+            ps.setString(3, product.getImageUrl());
+
+            return ps;
+        }, keyHolder);
+
+        return Objects.requireNonNull(keyHolder.getKey()).longValue();
+    }
+
+    public void updateProduct(Long productId, ProductEntity product) {
+        String sql = "UPDATE product SET product_name = ?, price = ?, image_url = ? WHERE id = ?";
+        jdbcTemplate.update(sql, product.getProductName(), product.getPrice(), product.getImageUrl(), productId);
+    }
+
+    public void deleteProduct(Long productId) {
+        String sql = "DELETE FROM product WHERE id = ?";
+        jdbcTemplate.update(sql, productId);
+    }
+}
--- src/main/java/cart/repository/ProductRepository.java
+++ src/main/java/cart/repository/ProductRepository.java
@@ -1,7 +1,8 @@
 package cart.repository;
 
 import cart.domain.product.Product;
-import org.springframework.jdbc.core.JdbcTemplate;
+import cart.persistance.dao.ProductDao;
+import cart.persistance.entity.ProductEntity;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
@@ -10,63 +11,37 @@ import java.sql.PreparedStatement;
 import java.sql.Statement;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Repository
 public class ProductRepository {
 
-    private final JdbcTemplate jdbcTemplate;
+    private final ProductDao productDao;
 
-    public ProductRepository(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    public ProductRepository(final ProductDao productDao) {
+        this.productDao = productDao;
     }
 
     public List<Product> getAllProducts() {
-        String sql = "SELECT * FROM product";
-        return jdbcTemplate.query(sql, (rs, rowNum) -> {
-            Long productId = rs.getLong("id");
-            String name = rs.getString("product_name");
-            int price = rs.getInt("price");
-            String imageUrl = rs.getString("image_url");
-            return new Product(productId, name, price, imageUrl);
-        });
+        return productDao.getAllProducts().stream()
+                .map(ProductEntity::toDomain)
+                .collect(Collectors.toList());
     }
 
     public Product getProductById(Long productId) {
-        String sql = "SELECT * FROM product WHERE id = ?";
-        return jdbcTemplate.queryForObject(sql, new Object[]{productId}, (rs, rowNum) -> {
-            String name = rs.getString("product_name");
-            int price = rs.getInt("price");
-            String imageUrl = rs.getString("image_url");
-            return new Product(productId, name, price, imageUrl);
-        });
+        return productDao.getProductById(productId)
+                .toDomain();
     }
 
     public Long createProduct(Product product) {
-        KeyHolder keyHolder = new GeneratedKeyHolder();
-
-        jdbcTemplate.update(connection -> {
-            PreparedStatement ps = connection.prepareStatement(
-                    "INSERT INTO product (product_name, price, image_url) VALUES (?, ?, ?)",
-                    Statement.RETURN_GENERATED_KEYS
-            );
-
-            ps.setString(1, product.getName());
-            ps.setInt(2, product.getPrice());
-            ps.setString(3, product.getImageUrl());
-
-            return ps;
-        }, keyHolder);
-
-        return Objects.requireNonNull(keyHolder.getKey()).longValue();
+        return productDao.createProduct(ProductEntity.of(product));
     }
 
     public void updateProduct(Long productId, Product product) {
-        String sql = "UPDATE product SET product_name = ?, price = ?, image_url = ? WHERE id = ?";
-        jdbcTemplate.update(sql, product.getName(), product.getPrice(), product.getImageUrl(), productId);
+        productDao.updateProduct(productId, ProductEntity.of(product));
     }
 
     public void deleteProduct(Long productId) {
-        String sql = "DELETE FROM product WHERE id = ?";
-        jdbcTemplate.update(sql, productId);
+        productDao.deleteProduct(productId);
     }
 }
