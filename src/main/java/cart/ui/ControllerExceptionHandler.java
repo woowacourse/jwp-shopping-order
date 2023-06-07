d7cbc8e0581f70f4b6a865c505e8ebe14fd209df
Kong-hana01 <gksqlsl11@khu.ac.kr>
2023-06-08 01:39:52 +0900

    refactor: slf4j를 활용한 로그로 변경

--- src/main/java/cart/ui/ControllerExceptionHandler.java
+++ src/main/java/cart/ui/ControllerExceptionHandler.java
@@ -4,6 +4,8 @@ import cart.exception.AuthenticationException;
 import cart.exception.CartItemException;
 import cart.exception.OrderException;
 import cart.exception.OrderServerException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,35 +13,36 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.Arrays;
+
 @RestControllerAdvice
 public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
 
     private static final String INTERNAL_SERVER_ERROR_MESSAGE = "서버 내부에서 예외가 발생했습니다.";
+    private static final Logger logger = LoggerFactory.getLogger(ControllerExceptionHandler.class.getName());
 
     @ExceptionHandler(AuthenticationException.class)
     public ResponseEntity<String> handlerAuthenticationException(AuthenticationException exception) {
+        logger.info(exception.getMessage());
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(exception.getMessage());
     }
 
     @ExceptionHandler(CartItemException.IllegalMember.class)
     public ResponseEntity<String> handleCartItemException(CartItemException.IllegalMember exception) {
+        logger.info(exception.getMessage());
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(exception.getMessage());
     }
 
     @ExceptionHandler(OrderException.class)
     public ResponseEntity<String> handleOrderException(OrderException exception) {
+        logger.info(exception.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
     }
 
-    @ExceptionHandler(OrderServerException.class)
-    public ResponseEntity<String> handleOrderServerException(OrderServerException exception) {
-        System.out.println(exception.getMessage());
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(INTERNAL_SERVER_ERROR_MESSAGE);
-    }
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleException(Exception exception) {
-        System.out.println(exception.getMessage());
+    @ExceptionHandler({OrderServerException.class, Exception.class})
+    public ResponseEntity<String> handleOrderServerException(Exception exception) {
+        logger.error(exception.getMessage());
+        logger.error(Arrays.toString(exception.getStackTrace()));
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(INTERNAL_SERVER_ERROR_MESSAGE);
     }
 }
