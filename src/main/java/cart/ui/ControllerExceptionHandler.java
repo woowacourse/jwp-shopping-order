bbf4a692cc441e47b49d9e96d2c415c7f6f51cb4
yenawee <yaena0319@naver.com>
2023-06-07 17:37:53 +0900

    refactor : Exception 처리 에러 로그 레벨 수정

--- src/main/java/cart/ui/ControllerExceptionHandler.java
+++ src/main/java/cart/ui/ControllerExceptionHandler.java
@@ -1,9 +1,6 @@
 package cart.ui;
 
-import cart.exception.AuthenticationException;
-import cart.exception.CartItemException;
-import cart.exception.InvalidCardException;
-import cart.exception.OrderException;
+import cart.exception.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -17,31 +14,37 @@ public class ControllerExceptionHandler {
 
     @ExceptionHandler(AuthenticationException.class)
     public ResponseEntity<Void> handlerAuthenticationException(AuthenticationException e) {
-        LOGGER.info("An error occurred: {}", e.getMessage(), e);
+        LOGGER.warn("An error occurred: {}", e.getMessage(), e);
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
 
     @ExceptionHandler(CartItemException.IllegalMember.class)
     public ResponseEntity<Void> handleCartItemException(CartItemException.IllegalMember e) {
-        LOGGER.info("An error occurred: {}", e.getMessage(), e);
+        LOGGER.warn("An error occurred: {}", e.getMessage(), e);
         return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
     }
 
     @ExceptionHandler(InvalidCardException.class)
     public ResponseEntity<Void> handleInvalidCardException(InvalidCardException e) {
-        LOGGER.info("An error occurred: {}", e.getMessage(), e);
-        return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        LOGGER.warn("An error occurred: {}", e.getMessage(), e);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    @ExceptionHandler(InvalidPointException.class)
+    public ResponseEntity<Void> handleInvalidPointException(InvalidPointException e) {
+        LOGGER.warn("An error occurred: {}", e.getMessage(), e);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
 
     @ExceptionHandler(OrderException.class)
     public ResponseEntity<Void> handleInvalidOrderException(OrderException.IllegalMember e) {
-        LOGGER.info("An error occurred: {}", e.getMessage(), e);
+        LOGGER.warn("An error occurred: {}", e.getMessage(), e);
         return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleException(Exception e) {
-        LOGGER.info("An error occurred: {}", e.getMessage(), e);
-        return ResponseEntity.internalServerError().body(e.getMessage());
+        LOGGER.error("An error occurred: {}", e.getMessage(), e);
+        return ResponseEntity.internalServerError().body("서버에 오류가 발생했습니다. 관리자에게 문의해주세요");
     }
 }
