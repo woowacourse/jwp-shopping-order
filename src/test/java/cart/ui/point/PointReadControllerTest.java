a969e9ae9a3fb72338bf851632dc1ac5b8f06778
ingpyo <juninpyo@naver.com>
2023-06-08 15:36:24 +0900

    refactor: 사용하지 않는 주석 제거

--- src/test/java/cart/ui/point/PointReadControllerTest.java
+++ src/test/java/cart/ui/point/PointReadControllerTest.java
@@ -29,24 +29,4 @@ class PointReadControllerTest {
         RestAssured.port = port;
     }
 
-//    @Test
-//    @DisplayName("사용자별 누적포인트 조회")
-//    void findPointByMember() {
-//        String email = "leo@gmail.com";
-//        String password = "leo123";
-//        final PointHistory pointHistory = new PointHistory(1L, 1000, 0);
-//        pointRepository.createPointHistory(MemberFixture.디노_ID포함.getId(), pointHistory);
-//
-//        String base64Credentials = java.util.Base64.getEncoder().encodeToString((email + ":" + password).getBytes());
-//        ExtractableResponse<Response> response = RestAssured.given().log().all()
-//                .header("Authorization", "Basic " + base64Credentials)
-//                .when().get("/point")
-//                .then().log().all()
-//                .extract();
-//// TODO : 리스폰스 값 검증
-//        assertAll(
-//                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value())
-//        );
-//
-//    }
 }
