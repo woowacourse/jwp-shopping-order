97e0743af3f7dcf37df059b798dd1cc48b3a276d
hong-sile <gurwns9325@naver.com>
2023-06-08 22:40:14 +0900

    refactor: AutoConfigureTestDatabase로 데이터 중복이 일어나지 않도록 리팩터링

--- src/test/java/cart/service/OrderServiceTest.java
+++ src/test/java/cart/service/OrderServiceTest.java
@@ -18,10 +18,12 @@ import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@AutoConfigureTestDatabase
 @Transactional
 class OrderServiceTest {
 
