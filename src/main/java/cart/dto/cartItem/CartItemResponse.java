124a67bfcf6ffc229ea32d278c50f01bf16f667c
kwonyj1022 <kwonyj1022@naver.com>
2023-06-09 08:29:20 +0900

    test: CartItemService 테스트 작성

--- src/main/java/cart/dto/cartItem/CartItemResponse.java
+++ src/main/java/cart/dto/cartItem/CartItemResponse.java
@@ -3,6 +3,8 @@ package cart.dto.cartItem;
 import cart.domain.CartItem;
 import cart.dto.product.ProductResponse;
 
+import java.util.Objects;
+
 public class CartItemResponse {
     private Long id;
     private int quantity;
@@ -32,5 +34,31 @@ public class CartItemResponse {
 
     public ProductResponse getProduct() {
         return product;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        CartItemResponse that = (CartItemResponse) o;
+        return quantity == that.quantity && Objects.equals(id, that.id) && Objects.equals(product, that.product);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, quantity, product);
+    }
+
+    @Override
+    public String toString() {
+        return "CartItemResponse{" +
+                "id=" + id +
+                ", quantity=" + quantity +
+                ", product=" + product +
+                '}';
     }
 }
