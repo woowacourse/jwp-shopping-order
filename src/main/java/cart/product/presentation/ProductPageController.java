98e7aa26953c30199fa36b7c16662c0dc3528ddb
hanueleee <ezzeezz@naver.com>
2023-06-08 17:58:54 +0900

    refactor: pagecontroller를 각 도메인 패키지 내부로 분리

--- src/main/java/cart/product/presentation/ProductPageController.java
+++ src/main/java/cart/product/presentation/ProductPageController.java
@@ -1,30 +1,22 @@
-package cart.common;
+package cart.product.presentation;
 
-import cart.member.application.MemberService;
 import cart.product.application.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class PageController {
-    private final ProductService productService;
-    private final MemberService memberService;
+public class ProductPageController {
 
-    public PageController(ProductService productService, MemberService memberService) {
+    private final ProductService productService;
+
+    public ProductPageController(ProductService productService) {
         this.productService = productService;
-        this.memberService = memberService;
     }
 
     @GetMapping("/admin")
     public String admin(Model model) {
         model.addAttribute("products", productService.getAllProducts());
         return "admin";
-    }
-
-    @GetMapping("/settings")
-    public String members(Model model) {
-        model.addAttribute("members", memberService.getAllMembers());
-        return "settings";
     }
 }
