5927805c06800c171d2ab8dba8b23c2c4f6cf911
gitchan <eunkeeeee@gmail.com>
2023-06-08 14:12:16 +0900

    refactor(ProductRequest): 기본 생성자 private으로 변경

--- src/main/java/cart/dto/request/ProductRequest.java
+++ src/main/java/cart/dto/request/ProductRequest.java
@@ -15,7 +15,7 @@ public class ProductRequest {
     @NotNull(message = "상품의 이미지 URL을 입력해 주세요. 입력값 : ${validatedValue}")
     private String imageUrl;
 
-    public ProductRequest() {
+    private ProductRequest() {
     }
 
     public ProductRequest(String name, int price, String imageUrl) {
