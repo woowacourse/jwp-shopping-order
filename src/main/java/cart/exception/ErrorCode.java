2547a1f0205d985d603edfff38a61b710fdba692
ezzanzzan <cjsdmswjd821@naver.com>
2023-06-09 03:06:20 +0900

    refactor: 예외 핸들링 코드 리팩토링

--- src/main/java/cart/exception/ErrorCode.java
+++ src/main/java/cart/exception/ErrorCode.java
@@ -3,7 +3,8 @@ package cart.exception;
 public enum ErrorCode {
     FORBIDDEN_MEMBER("잘못된 사용자의 시도입니다."),
     UNAUTHORIZED_MEMBER("유효하지 않은 사용자의 시도입니다."),
-    INVALID_OVER_POINT("사용할 수 있는 포인트의 범위를 입력해주세요.");
+    INVALID_OVER_POINT("사용할 수 있는 포인트의 범위를 입력해주세요."),
+    INTERNAL_SERVER_ERROR("알 수 없는 오류입니다.");
 
     private final String errorMessage;
 
--- src/main/java/cart/ui/ControllerExceptionHandler.java
+++ src/main/java/cart/ui/ControllerExceptionHandler.java
@@ -1,5 +1,6 @@
 package cart.ui;
 
+import cart.exception.ErrorCode;
 import cart.exception.ErrorResponse;
 import cart.exception.ShoppingCartException;
 import org.slf4j.Logger;
@@ -13,17 +14,29 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class ControllerExceptionHandler {
     private static final Logger LOGGER = LoggerFactory.getLogger(ControllerExceptionHandler.class);
-    
+
     @ExceptionHandler(ShoppingCartException.class)
-    public ResponseEntity<ErrorResponse> handleException(ShoppingCartException e) {
+    public ResponseEntity<ErrorResponse> handleShoppingCartException(ShoppingCartException e) {
         LOGGER.info(e.getMessage());
-        return ResponseEntity.status(e.getHttpStatus()).body(new ErrorResponse(e.getMessage()));
+        return ResponseEntity
+                .status(e.getHttpStatus())
+                .body(new ErrorResponse(e.getMessage()));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         String errorMessage = e.getBindingResult().getAllErrors().get(0).getDefaultMessage();
         LOGGER.info(errorMessage);
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(errorMessage));
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse(errorMessage));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        LOGGER.info(e.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ErrorResponse(ErrorCode.INTERNAL_SERVER_ERROR.getErrorMessage()));
     }
 }
