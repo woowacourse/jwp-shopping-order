8e31e24180de9ceea5e20e173355d4c3f094ef79
splitube <voicesplit@daum.net>
2023-06-08 22:29:57 +0900

    feat: OrderTest 생성

--- src/main/java/cart/domain/order/Order.java
+++ src/main/java/cart/domain/order/Order.java
@@ -9,23 +9,26 @@ public class Order {
     private final Long id;
     private final Member member;
     private final OrderItems orderItems;
+    private final OrderPrice orderPrice;
     private final LocalDateTime orderTime;
 
-    private Order(final Long id, final Member member, final OrderItems orderItems, final LocalDateTime orderTime) {
+    private Order(final Long id, final Member member, final OrderItems orderItems, final OrderPrice orderPrice,
+        final LocalDateTime orderTime) {
         this.id = id;
         this.member = member;
         this.orderItems = orderItems;
+        this.orderPrice = orderPrice;
         this.orderTime = orderTime;
     }
 
     public static Order notPersisted(final Member member, final OrderItems orderItems,
-        final LocalDateTime orderTime) {
-        return new Order(null, member, orderItems, orderTime);
+        final OrderPrice orderPrice, final LocalDateTime orderTime) {
+        return new Order(null, member, orderItems, orderPrice, orderTime);
     }
 
     public static Order persisted(final Long id, final Member member, final OrderItems orderItems,
-        final LocalDateTime orderTime) {
-        return new Order(id, member, orderItems, orderTime);
+        final OrderPrice orderPrice, final LocalDateTime orderTime) {
+        return new Order(id, member, orderItems, orderPrice, orderTime);
     }
 
     public Long getId() {
@@ -46,6 +49,10 @@ public class Order {
 
     public Long getProductPrice() {
         return orderItems.getTotalPrice();
+    }
+
+    public OrderPrice getOrderPrice() {
+        return orderPrice;
     }
 
     public LocalDateTime getOrderTime() {
