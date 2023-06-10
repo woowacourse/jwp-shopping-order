eab83875b54dd7aa3005f387a54a2cfbc693f235
yenawee <yaena0319@naver.com>
2023-06-10 09:08:36 +0900

    feat : ProductController 유효성 검증 테스트

--- src/test/java/cart/ui/ProductControllerTest.java
+++ src/test/java/cart/ui/ProductControllerTest.java
@@ -1,7 +1,9 @@
 package cart.ui;
 
 import cart.dao.MemberDao;
+import cart.dto.ProductRequest;
 import cart.service.MemberService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +11,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ProductControllerTest.class)
@@ -24,10 +26,30 @@ class ProductControllerTest {
 
     @DisplayName("PathVariable 검증 - GET 요청에서 ID가 null 이면 예외 발생")
     @Test
-    public void validatePathVariable() throws Exception {
+    public void validatePathVariable_Get() throws Exception {
         String invalidId = null;
 
         mockMvc.perform(get("/products/{id}", invalidId))
+                .andExpect(status().isNotFound());
+    }
+
+    @DisplayName("PathVariable 검증 - PUT 요청에서 ID가 null 이면 예외 발생")
+    @Test
+    public void validatePathVariable_Put() throws Exception {
+        String invalidId = null;
+        ProductRequest productRequest = new ProductRequest("치킨", 10, "http://naver.com");
+
+        mockMvc.perform(put("/products/{id}", invalidId)
+                        .content(new ObjectMapper().writeValueAsString(productRequest)))
+                .andExpect(status().isNotFound());
+    }
+
+    @DisplayName("PathVariable 검증 - DELETE 요청에서 ID가 null 이면 예외 발생")
+    @Test
+    public void validatePathVariable_Delete() throws Exception {
+        String invalidId = null;
+
+        mockMvc.perform(delete("/products/{id}", invalidId))
                 .andExpect(status().isNotFound());
     }
 }
